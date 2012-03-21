da3eb9be66d4fdc8d4e6aa13be47a8a5dc7ebd00
Matthias Vallentin <vallentin@icir.org>
2012-03-20 22:14:02 -0700

    Add the profiler.

---
+++ src/util/profiler.cc
@@ -0,0 +1,106 @@
+#include "profiler.h"
+
+#include <iomanip>
+#include <sys/resource.h>   // getrusage
+
+namespace vast {
+namespace util {
+
+measurement::measurement()
+{
+    struct timeval begin;
+    ::gettimeofday(&begin, 0);
+    clock = static_cast<double>(begin.tv_sec) + 
+        static_cast<double>(begin.tv_usec) / 1000000.0;
+
+    struct rusage ru;        
+    ::getrusage(RUSAGE_SELF, &ru);        
+    struct timeval& u = ru.ru_utime;
+    struct timeval& s = ru.ru_stime;
+    
+    usr_time = static_cast<double>(u.tv_sec) + 
+        static_cast<double>(u.tv_usec) / 1000000.0;
+
+    sys_time = static_cast<double>(s.tv_sec) + 
+        static_cast<double>(s.tv_usec) / 1000000.0;
+}
+
+measurement& measurement::operator+=(measurement const& rhs)
+{
+    clock += rhs.clock;
+    usr_time += rhs.usr_time;
+    sys_time += rhs.sys_time;
+
+    return *this;
+}
+
+measurement& measurement::operator-=(measurement const& rhs)
+{
+    clock -= rhs.clock;
+    usr_time -= rhs.usr_time;
+    sys_time -= rhs.sys_time;
+
+    return *this;
+}
+
+std::ostream& operator<<(std::ostream& out, measurement const& m)
+{
+    out << std::fixed
+        << std::setw(18) << m.clock
+        << std::setw(14) << m.usr_time
+        << std::setw(14) << m.sys_time;
+
+    return out;
+}
+profiler::profiler(boost::asio::io_service& io_service)
+  : timer_(io_service)
+{
+}
+
+void profiler::init(fs::path const& filename, core::duration interval)
+{
+    interval_ = interval;
+
+    file_.open(filename);
+    file_.flags(std::ios::left);
+    file_
+        << std::setw(18) << "clock (c)"
+        << std::setw(14) << "user (c)"
+        << std::setw(14) << "sys (c)"
+        << std::setw(18) << "clock (d)"
+        << std::setw(14) << "user (d)"
+        << std::setw(14) << "sys (d)"
+        << std::endl;
+}
+
+void profiler::start()
+{
+    assert(file_.is_open());
+
+    timer_.expires_from_now(interval_);
+    measurement now;
+    timer_.async_wait(
+        [&](boost::system::error_code const& ec) { handle_timer(ec, now); });
+}    
+
+void profiler::stop()
+{
+    timer_.cancel();
+}
+
+void profiler::handle_timer(boost::system::error_code const& ec,
+                            measurement const& previous)
+{
+    if (ec == boost::asio::error::operation_aborted)
+        return;
+
+    measurement now;
+    file_ << now << now - previous << std::endl;
+
+    timer_.expires_at(timer_.expires_at() + interval_);
+    timer_.async_wait(
+        [&](boost::system::error_code const& ec) { handle_timer(ec, now); });
+}
+
+} // namespace util
+} // namespace vast
---
+++ src/util/profiler.h
@@ -0,0 +1,62 @@
+#ifndef VAST_UTIL_PROFILER_H
+#define VAST_UTIL_PROFILER_H
+
+#include <boost/asio/basic_waitable_timer.hpp>
+#include <boost/operators.hpp>
+#include "fs/fstream.h"
+#include "fs/path.h"
+#include "core/time.h"
+
+namespace vast {
+namespace util {
+
+/// A resoure measurement.
+struct measurement : boost::addable<measurement>
+                   , boost::subtractable<measurement>
+{
+    /// Measures the current system usage at construction time.
+    measurement();
+
+    measurement& operator+=(measurement const& rhs);
+    measurement& operator-=(measurement const& rhs);
+
+    double clock;           ///< Current wall clock time (@c gettimeofday).
+    double usr_time;        ///< Time spent in the process.
+    double sys_time;        ///< Time spent in the kernel.
+};
+
+std::ostream& operator<<(std::ostream& out, measurement const& s);
+
+/// A simple CPU profiler.
+class profiler
+{
+public:
+    /// Constructs the profiler.
+    /// @param io_service The Asio I/O service object.
+    profiler(boost::asio::io_service& io_service);
+
+    /// Initializes the profiler.
+    /// @param filename The log file where to write measurements to.
+    /// @param interval How often to take a measurment.
+    void init(fs::path const& filename,
+              core::duration interval = std::chrono::seconds(1));
+
+    /// Starts the profiler.
+    void start();
+
+    /// Stops the profiler.
+    void stop();
+
+private:
+    void handle_timer(boost::system::error_code const& ec,
+                      measurement const& previous);
+
+    core::duration interval_;
+    boost::asio::basic_waitable_timer<core::clock> timer_;
+    fs::ofstream file_;
+};
+
+} // namespace util
+} // namespace vast
+
+#endif
