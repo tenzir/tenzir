a54729e4c07c816692574462b6380aa75b69da13
Matthias Vallentin <vallentin@icir.org>
2012-03-20 22:28:31 -0700

    Add logger.

---
+++ src/util/logger.cc
@@ -0,0 +1,105 @@
+#include "util/logger.h"
+
+#include <boost/assert.hpp>
+#include <boost/log/attributes/clock.hpp>
+#include <boost/log/formatters/attr.hpp>
+#include <boost/log/formatters/date_time.hpp>
+#include <boost/log/formatters/format.hpp>
+#include <boost/log/formatters/message.hpp>
+#include <boost/log/utility/init/to_console.hpp>
+#include "fs/operations.h"
+
+namespace vast {
+namespace util {
+
+logger::logger(int cverb, int fverb, fs::path const& log_dir)
+  : core_(boost::log::keywords::channel = "core")
+  , broccoli_(boost::log::keywords::channel = "broccoli")
+  , comm_(boost::log::keywords::channel = "comm")
+  , event_(boost::log::keywords::channel = "event")
+  , fastbit_(boost::log::keywords::channel = "fastbit")
+  , meta_(boost::log::keywords::channel = "meta")
+  , query_(boost::log::keywords::channel = "query")
+  , store_(boost::log::keywords::channel = "store")
+{
+    namespace fmt = boost::log::formatters;
+
+    auto core = boost::log::core::get();
+
+    // Add a timestamps to both console and logfiles.
+    boost::log::attributes::local_clock timestamp;
+    core->add_global_attribute("TimeStamp", timestamp);
+
+    // Initialize the console.
+    auto console = boost::log::init_log_to_console();
+    console->set_filter(
+            boost::log::filters::attr<log::level>("Severity") <= cverb);
+    console->locked_backend()->auto_flush(true);
+    console->set_formatter(
+            fmt::format("%1% %|2$-10| %3%")
+                % fmt::date_time<boost::posix_time::ptime>("TimeStamp",
+                    "%Y-%m-%d %H:%M:%S.%f")
+                % fmt::attr<std::string>("Channel", "[%x]")
+                % fmt::message()
+            );
+
+    // Initialize the logfiles.
+    if (! fs::exists(log_dir))
+        fs::mkdir(log_dir);
+
+    // FIXME: Why does std::make_shared does not work here?
+    auto backend = boost::make_shared<file_backend>(
+        boost::log::keywords::file_name = log_dir / "vast.log",
+        boost::log::keywords::open_mode =
+        (std::ios_base::out | std::ios_base::app));
+
+    auto log_file = boost::make_shared<file_sink>(backend);
+
+    log_file->set_filter(
+        boost::log::filters::attr<log::level>("Severity") <= fverb);
+    log_file->set_formatter(
+        fmt::format("%1% %|2$-10| %3%")
+        % fmt::date_time<boost::posix_time::ptime>("TimeStamp",
+                                                   "%Y-%m-%d %H:%M:%S.%f")
+        % fmt::attr<std::string>("Channel", "[%x]")
+        % fmt::message()
+        );
+
+    core->add_sink(log_file);
+}
+
+logger::logger_t& logger::get(log::facility f)
+{
+    switch (f)
+    {
+        case log::core:
+            return core_;
+            break;
+        case log::broccoli:
+            return broccoli_;
+            break;
+        case log::comm:
+            return comm_;
+            break;
+        case log::event:
+            return event_;
+            break;
+        case log::fastbit:
+            return fastbit_;
+            break;
+        case log::meta:
+            return meta_;
+            break;
+        case log::query:
+            return query_;
+            break;
+        case log::store:
+            return store_;
+            break;
+        default:
+            BOOST_ASSERT(! "invalid logging facility");
+    };
+}
+
+} // namespace util
+} // namespace vast
---
+++ src/util/logger.h
@@ -0,0 +1,135 @@
+#ifndef VAST_UTIL_LOGGER_H
+#define VAST_UTIL_LOGGER_H
+
+#include <memory>
+#include <boost/log/sinks.hpp>
+#include <boost/log/sources/record_ostream.hpp>
+#include <boost/log/sources/severity_channel_logger.hpp>
+#include <boost/noncopyable.hpp>
+#include "fs/path.h"
+#include "config.h"
+
+namespace vast {
+namespace util {
+class logger;
+extern logger* LOGGER;
+}
+}
+
+// Standard logging macro.
+#define LOG(level, facility)                                                 \
+    BOOST_LOG_SEV(                                                           \
+            ::vast::util::LOGGER->get(vast::log::facility), vast::log::level)
+
+/// Conditional logging. Only execute the subsequent statement when the given
+/// severity level is active.
+#define LOG_BLOCK(level, facility)                                      \
+    if (::vast::util::LOGGER->get(log::facility)                        \
+        .open_record(boost::log::keywords::severity = vast::log::level))
+
+namespace vast {
+namespace log {
+
+/// Logging facility.
+enum facility
+{
+    core,
+    broccoli,
+    comm,
+    event,
+    fastbit,
+    meta,
+    query,
+    store
+};
+
+/// Logging severity level.
+/// \note Must be kept in sync with the corresponding ostream operator.
+enum level
+{
+    fatal,      ///< Unrecoverable errors.
+    error,      ///< General errors.
+    warn,       ///< Warnings.
+    info,       ///< Informational messages.
+    verbose,    ///< Verbose.
+    debug       ///< Debug information.
+};
+
+/// String description of the log levels.
+static char const* const str_lvl[] =
+{
+    "fatal",
+    "error",
+    "warning",
+    "info",
+    "verbose",
+    "debug"
+};
+
+// The formatting logic for the severity level.
+template <typename CharT, typename TraitsT>
+inline std::basic_ostream<CharT, TraitsT>& operator<<(
+std::basic_ostream<CharT, TraitsT>& out, level l)
+{
+    if (static_cast<std::size_t>(l) < (sizeof(str_lvl) / sizeof(*str_lvl)))
+        out << str_lvl[l];
+    else
+        out << static_cast<int>(l);
+
+    return out;
+}
+
+} // namespace log
+} // namespace vast
+
+namespace vast {
+namespace util {
+
+/// A flexible logger supporting multiple sinks.
+class logger : boost::noncopyable
+{
+    typedef boost::log::sinks::text_file_backend file_backend;
+#ifdef BOOST_LOG_NO_THREADS
+    typedef boost::log::sinks::unlocked_sink<file_backend> file_sink;
+#else
+    typedef boost::log::sinks::synchronous_sink<file_backend> file_sink;
+#endif
+
+public:
+    /// A logger type that supports both levels and facilities.
+#ifdef BOOST_LOG_NO_THREADS
+    typedef boost::log::sources::severity_channel_logger<
+        log::level, std::string> logger_t;
+#else
+    typedef boost::log::sources::severity_channel_logger_mt<
+        log::level, std::string> logger_t;
+#endif
+
+public:
+    /// Constructor. Create thread-safe logging sources for each facility and
+    /// initialize the sinks.
+    /// \param cverb The console verbosity.
+    /// \param fverb The logfile verbosity.
+    /// \param log_dir The logging directory.
+    logger(int cverb, int fverb, fs::path const& log_dir);
+
+    /// Get a logger object for a given facility.
+    /// \param fac The facility.
+    /// \return The logger object of type logger_t.
+    logger_t& get(log::facility fac);
+
+private:
+    logger_t core_;
+    logger_t broccoli_;
+    logger_t comm_;
+    logger_t event_;
+    logger_t fastbit_;
+    logger_t meta_;
+    logger_t query_;
+    logger_t store_;
+};
+
+} // namespace util
+} // namespace vast
+
+#endif
