2d4f25398632f29551ff924b2864190ba6cde636
Matthias Vallentin <vallentin@icir.org>
2012-03-20 22:11:13 -0700

    Use time setup from 0event.

---
+++ src/core/time.h
@@ -0,0 +1,16 @@
+#ifndef VAST_CORE_TIME_H
+#define VAST_CORE_TIME_H
+
+#include <ze/time.h>
+
+namespace vast {
+namespace core {
+
+using ze::clock;
+using ze::duration;
+using ze::time_point;
+
+} // namespace core
+} // namespace vast
+
+#endif
