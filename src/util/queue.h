6d52590cedee1c63b4ca5af5511bc0ff3162795a
Matthias Vallentin <vallentin@icir.org>
2012-03-20 22:22:07 -0700

    Brush up the queue.
    Specifically, improve move semantics on push_back.

--- src/util/queue.h
+++ src/util/queue.h
@@ -8,17 +8,18 @@
 namespace vast {
 namespace util {
 
-/// A thread-safe std::queue.
+/// A thread-safe @c std::queue.
 template <typename T>
-class queue
+class queue : std::queue<T>
 {
+    typedef std::queue<T> base;
+
 public:
-    typedef typename std::queue<T>::container_type container_type;
-    typedef typename std::queue<T>::value_type value_type;
-    typedef typename std::queue<T>::reference reference;
-    typedef typename std::queue<T>::const_reference const_reference;;
-        
-    /// Default Constructor.
+    using base::value_type;
+    using base::reference;
+    using base::const_reference;
+
+    /// Constructs an empty queue.
     queue()
     {
     }
@@ -27,21 +28,21 @@ public:
     queue& operator=(queue&) = delete;
 
     /// Pushes a new element to the end of the queue.
-    /// \param x The value to push in the queue.
-    /// \note The notification occurs \e after the mutex is unlocked, thus the
+    /// @param x The value to push in the queue.
+    /// @note The notification occurs @e after the mutex is unlocked, thus the
     /// waiting thread will be able to acquire the mutex without blocking.
-    void push(T const& x)
-    { 
+    void push(value_type x)
+    {
         std::unique_lock<std::mutex> lock(mutex_);
-        const bool empty = queue_.empty();
-        queue_.push(x); 
+        const bool empty = base::empty();
+        base::push(std::move(x));
 
         lock.unlock();
 
         if (empty)
             cond_.notify_one();
     }
-    
+
     /// Pushes a new element to the end of the queue. The element is
     /// constructed in-place, i.e., no copy or move operations are performed.
     /// The constructor of the element is called with exactly the same
@@ -50,8 +51,8 @@ public:
     void emplace(Args&&... args)
     {
         std::unique_lock<std::mutex> lock(mutex_);
-        bool const empty = queue_.empty();
-        queue_.emplace(std::forward<Args>(args)...); 
+        bool const empty = base::empty();
+        base::emplace(std::forward<Args>(args)...);
 
         lock.unlock();
 
@@ -59,60 +60,58 @@ public:
             cond_.notify_one();
     }
 
-    /// Get the top-most element or wait until an element is added. To avoid
+    /// Gets the top-most element or wait until an element is added. To avoid
     /// exception safety issues when returning data by-value, the queue uses a
     /// reference parameter to receive the result, transferring the ownership
-    /// out of the queue. If the copy constructor of type \c T throws, the
+    /// out of the queue. If the copy constructor of type @c T throws, the
     /// element is not removed from the queue.
-    /// \param x The reference parameter to receive the result. 
-    /// \note This function blocks when the queue is empty.
-    T pop()
+    /// @note This function blocks when the queue is empty.
+    value_type pop()
     {
         std::unique_lock<std::mutex> lock(mutex_);
-        while (queue_.empty())
+        while (base::empty())
             cond_.wait(lock);
-    
-        T x = queue_.front();
-        queue_.pop();
 
-        return std::move(x);
+        value_type x = base::front();
+        base::pop();
+
+        return x;
     }
 
-    /// Try to get the top-most queue element. 
-    /// \return \c true if the top-most element could be returned.
-    ///         \c false if the queue is empty.
-    /// \param x The reference parameter to receive the result. 
-    /// \note This is the \e non-blocking version of pop().
-    bool try_pop(T& x)
+    /// Tries to get the top-most queue element.
+    /// @param x The reference parameter to receive the result.
+    /// @return @c true if the top-most element could be returned.
+    ///         @c false if the queue is empty.
+    /// @note This is the @e non-blocking version of pop().
+    bool try_pop(value_type& x)
     {
         std::lock_guard<std::mutex> lock(mutex_);
-        if (queue_.empty())
+        if (base::empty())
             return false;
 
-        x = queue_.front();
-        queue_.pop();
+        x = base::front();
+        base::pop();
 
         return true;
     }
-    
-    /// Determine whether the queue is empty.
-    /// \return \c true if the queue is empty.
+
+    /// Determines whether the queue is empty.
+    /// @return @c true if the queue is empty.
     bool empty()
     {
         std::lock_guard<std::mutex> lock(mutex_);
-        return queue_.empty();
+        return base::empty();
     }
-    
-    /// Get the size of the queue.
-    /// \return The queue size.
+
+    /// Gets the size of the queue.
+    /// @return The queue size.
     std::size_t size()
     {
         std::lock_guard<std::mutex> lock(mutex_);
-        return queue_.size();
+        return base::size();
     }
 
 private:
-    std::queue<T> queue_;             ///< The underlying STL queue.
     std::mutex mutex_;                ///< Synchronizes queue access.
     std::condition_variable cond_;    ///< Avoids busy-waiting.
 };
