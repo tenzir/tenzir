8099b1b239e01a273bb719f43e56cef944be3450
Matthias Vallentin <vallentin@icir.org>
2012-03-20 20:12:29 -0700

    Add vast::comm forward declarations.

---
+++ src/comm/forward.h
@@ -0,0 +1,23 @@
+#ifndef VAST_COMM_FORWARD_H
+#define VAST_COMM_FORWARD_H
+
+#include <functional>
+#include <memory>
+#include <ze/forward.h>
+#include "core/forward.h"
+
+namespace vast {
+namespace comm {
+
+class connection;
+class io;
+
+typedef std::shared_ptr<connection> connection_ptr;
+typedef std::function<void(connection_ptr const&)> conn_handler;
+typedef std::function<void(std::shared_ptr<ze::event> const& event)> event_handler;
+
+
+} // namespace comm
+} // namespace vast
+
+#endif
