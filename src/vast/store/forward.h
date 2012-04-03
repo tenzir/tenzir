b80320e64bcfc49f00fcb3ea9decca50480c1790
Matthias Vallentin <vallentin@icir.org>
2012-04-03 00:01:46 -0700

    Add missing file.

---
+++ src/vast/store/forward.h
@@ -0,0 +1,13 @@
+#ifndef VAST_STORE_FORWARD_H
+#define VAST_STORE_FORWARD_H
+
+namespace vast {
+namespace store {
+
+class isegment;
+class osegment;
+
+} // namespace store
+} // namespace vast
+
+#endif
