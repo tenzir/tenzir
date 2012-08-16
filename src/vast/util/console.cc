8eed8219fe5b64e3b71f3c056757a63dca48506a
Matthias Vallentin <vallentin@icir.org>
2012-08-16 09:21:18 -0700

    Enhance util::console.
    In addition to buffer/unbuffer the console, one can now extract a single
    character from STDIN with the function util::console::get. This function uses
    select to poll the STDIN fd for events. It returns false either on error or
    when the process received a signal, or another condition interrupted select, or
    when a user-supplied timeout expires. This makes it more favorable than
    std::cin::get, which blocks forever if the user does not feed it with input.

--- src/vast/util/console.cc
+++ src/vast/util/console.cc
@@ -1,5 +1,6 @@
 #include <vast/util/console.h>
 
+#include <sys/select.h>
 #include <stdio.h>
 #include <termios.h>
 #include <cassert>
@@ -7,6 +8,7 @@
 
 namespace vast {
 namespace util {
+namespace console {
 
 static bool initialized = false;
 static struct termios buffered;
@@ -30,7 +32,7 @@ void unbuffer()
   if (! initialized)
     initialize();
 
-  if (tcsetattr(0, TCSANOW, &unbuffered) < 0)
+  if (tcsetattr(::fileno(stdin), TCSANOW, &unbuffered) < 0)
     throw exception("tcsetattr");
 }
 
@@ -38,9 +40,31 @@ void buffer()
 {
   assert(initialized);
 
-  if (tcsetattr(0, TCSANOW, &buffered) < 0)
+  if (tcsetattr(::fileno(stdin), TCSANOW, &buffered) < 0)
     throw exception("tcsetattr");
 }
 
+bool get(char& c, int timeout)
+{
+  fd_set rdset;
+  FD_ZERO(&rdset);
+  FD_SET(::fileno(stdin), &rdset);
+  struct timeval tv{0, timeout};
+  auto rc = ::select(::fileno(stdin) + 1, &rdset, nullptr, nullptr, &tv);
+  if (rc <= 0)
+    return false;
+
+  if (! FD_ISSET(fileno(stdin), &rdset))
+    return false;
+
+  auto i = ::fgetc(stdin);
+  if (::feof(stdin))
+    return false;
+
+  c = static_cast<char>(i);
+  return true;
+}
+
+} // namespace console
 } // namespace util
 } // namespace vast
--- src/vast/util/console.h
+++ src/vast/util/console.h
@@ -3,6 +3,7 @@
 
 namespace vast {
 namespace util {
+namespace console {
 
 /// Makes the console unbuffered.
 void unbuffer();
@@ -10,6 +11,22 @@ void unbuffer();
 /// Makes the console buffered again after a call to unbuffer.
 void buffer();
 
+/// Tries to extract a single a character from STDIN within a short time
+/// interval. The function blocks until either input arrives or the timeout
+/// expires.
+///
+/// @param c The character from STDIN.
+///
+/// @param timeout The maximum number of milliseconds to block.
+///
+/// @return `true` *iff* extracting a character from STDIN was successful
+/// within *timeout* milliseconds.
+///
+/// @post Iff the function returned `true`, *c* contains the next character
+/// from STDIN.
+bool get(char& c, int timeout = 100);
+
+} // namespace console
 } // namespace util
 } // namespace vast
 
