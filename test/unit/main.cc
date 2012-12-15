ef0f4ffff148db7de97751f6a7aad3a809cd2040
Matthias Vallentin <vallentin@icir.org>
2012-12-14 17:43:06 -0800

    Clean up after running tests.

--- test/unit/main.cc
+++ test/unit/main.cc
@@ -2,6 +2,7 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE "VAST Unit Test Suite"
 
+#include <ze.h>
 #include "test.h"
 #include "vast/configuration.h"
 #include "vast/logger.h"
@@ -13,14 +14,12 @@ int main(int argc, char* argv[])
   vast::init(config);
 
   boost::unit_test::unit_test_log.set_stream(vast::logger::get()->console());
-  char const* args[] = {"", "--log_level=test_suite"};
-  auto rc = boost::unit_test::unit_test_main(
-      &init_unit_test,
-      sizeof(args) / sizeof(char*),
-      const_cast<char**>(args));
+  auto rc = boost::unit_test::unit_test_main(&init_unit_test, argc, argv);
 
   if (rc)
     LOG(error, core) << "unit test suite exited with error code " << rc;
 
+  ze::rm(ze::logger::filename());
+  ze::shutdown();
   return rc;
 }
