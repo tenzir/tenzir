c33c0fae5d527420031615f133edad5091c50762
Matthias Vallentin <vallentin@icir.org>
2013-04-19 11:41:16 -0700

    Avoid overriding of serialization methods.

--- src/vast/detail/cppa_type_info.h
+++ src/vast/detail/cppa_type_info.h
@@ -12,14 +12,14 @@ template <typename T>
 class cppa_type_info : public cppa::util::abstract_uniform_type_info<T>
 {
 protected:
-  void serialize(void const* ptr, cppa::serializer* sink) const
+  void serialize(void const* ptr, cppa::serializer* sink) const final
   {
     auto x = reinterpret_cast<T const*>(ptr);
     cppa_serializer serializer(sink, this->name());
     serializer << *x;
   }
 
-  void deserialize(void* ptr, cppa::deserializer* source) const
+  void deserialize(void* ptr, cppa::deserializer* source) const final
   {
     this->assert_type_name(source);
     auto x = reinterpret_cast<T*>(ptr);
