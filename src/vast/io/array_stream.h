165bea587315e628408cf3a8ba1ced954d1d5925
Matthias Vallentin <vallentin@icir.org>
2013-06-28 12:22:11 +0200

    Add array stream construction functions for symmetry.

--- src/vast/io/array_stream.h
+++ src/vast/io/array_stream.h
@@ -10,7 +10,7 @@ namespace io {
 class array_input_stream : public input_stream
 {
   array_input_stream(array_input_stream const&) = delete;
-  array_input_stream& operator=(array_input_stream) = delete;
+  array_input_stream& operator=(array_input_stream const&) = delete;
 
 public:
   /// Constructs an array input stream.
@@ -18,6 +18,9 @@ public:
   /// @param size The size of *data*.
   /// @param block_size The size in bytes used to chop up the array buffer.
   array_input_stream(void const* data, size_t size, size_t block_size = 0);
+
+  array_input_stream(array_input_stream&&) = default;
+  array_input_stream& operator=(array_input_stream&&) = default;
 
   virtual bool next(void const** data, size_t* size) override;
   virtual void rewind(size_t bytes) override;
@@ -32,11 +35,19 @@ private:
   size_t position_ = 0;
 };
 
+template <typename Container>
+inline array_input_stream
+make_array_input_stream(Container const& container, size_t block_size = 0)
+{
+  return array_input_stream(container.data(), container.size(), block_size);
+}
+
+
 /// An output stream that writes into a raw array.
 class array_output_stream : public output_stream
 {
   array_output_stream(array_output_stream const&) = delete;
-  array_output_stream& operator=(array_output_stream) = delete;
+  array_output_stream& operator=(array_output_stream const&) = delete;
 
 public:
   /// Constructs an array output stream.
@@ -44,6 +55,9 @@ public:
   /// @param size The size of *data*.
   /// @param block_size The size in bytes used to chop up the array buffer.
   array_output_stream(void *data, size_t size, size_t block_size = 0);
+
+  array_output_stream(array_output_stream&&) = default;
+  array_output_stream& operator=(array_output_stream&&) = default;
 
   virtual bool next(void** data, size_t* size) override;
   virtual void rewind(size_t bytes) override;
@@ -56,6 +70,14 @@ private:
   size_t last_size_ = 0;
   size_t position_ = 0;
 };
+
+template <typename Container>
+inline array_output_stream
+make_array_output_stream(Container& container, size_t block_size = 0)
+{
+  return array_output_stream(container.data(), container.size(), block_size);
+}
+
 
 } // namespace io
 } // namespace vast
