49091b5f214aee2825f4c7fec8146cf5b6a10947
Matthias Vallentin <vallentin@icir.org>
2013-06-28 10:21:25 +0200

    Add operator-> as available in std::optional.

--- src/vast/option.h
+++ src/vast/option.h
@@ -5,9 +5,42 @@
 
 namespace vast {
 
-/// An optional value of `T`.
+/// An optional value of `T` with similar semantics as `std::optional`.
 template <typename T>
-using option = cppa::option<T>;
+class option : public cppa::option<T>
+{
+  typedef cppa::option<T> super;
+public:
+#ifdef VAST_HAVE_INHERTING_CONSTRUCTORS
+  using super::option;
+#else
+  option() = default;
+
+  option(T x)
+    : super(std::move(x))
+  {
+  }
+
+  template <typename T0, typename T1, typename... Ts>
+  option(T0&& x0, T1&& x1, Ts&&... args) 
+    : super(std::forward<T0>(x0),
+            std::forward<T1>(x1),
+            std::forward<Ts>(args)...)
+  {
+  }
+
+  option(const option&) = default;
+  option(option&&) = default;
+  option& operator=(option const&) = default;
+  option& operator=(option&&) = default;
+#endif
+
+  T* operator->() const
+  {
+    CPPA_REQUIRE(valid());
+    return &cppa::option<T>::get();
+  }
+};
 
 } // namespace vast
 
