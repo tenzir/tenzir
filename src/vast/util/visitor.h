c90222d1c6221d8dd83a939fe3a8f6ff626aef15
Matthias Vallentin <vallentin@icir.org>
2013-10-04 17:27:27 -0700

    Get rid of macros, use mixins for visitors.

--- src/vast/util/visitor.h
+++ src/vast/util/visitor.h
@@ -8,6 +8,7 @@ namespace util {
 // Visitors
 //
 
+/// The base class for mutable visitors.
 template <typename... Visitables>
 struct visitor;
 
@@ -25,6 +26,7 @@ struct visitor<First>
 };
 
 
+/// The base class for constant visitors.
 template <typename... Visitables>
 struct const_visitor;
 
@@ -41,40 +43,88 @@ struct const_visitor<First>
   virtual void visit(First const&) = 0;
 };
 
-//
-// Helper macros
-//
 
-#define VAST_ACCEPT_DECLARE(visitor)              \
-  virtual void accept(visitor& v);
+/// The mixin for base classes of a visitable hierarchy.
+/// @tparam Visitor The concrete visitor to support.
+template <typename... Visitors>
+struct visitable_with;
 
-#define VAST_ACCEPT_CONST_DECLARE(visitor)        \
-  virtual void accept(visitor& v) const;
-    
-#define VAST_ACCEPT_CONST_DEFINE(klass, visitor)  \
-  void klass::accept(visitor& v) const            \
-  {                                               \
-    v.visit(*this);                               \
+template <typename First, typename... Visitors>
+struct visitable_with<First, Visitors...> : visitable_with<Visitors...>
+{
+  using visitable_with<Visitors...>::accept;
+  virtual void accept(First& v) = 0;
+  virtual void accept(First& v) const = 0;
+};
+
+template <typename Last>
+struct visitable_with<Last>
+{
+  virtual void accept(Last& v) = 0;
+  virtual void accept(Last& v) const = 0;
+};
+
+
+/// The mixin for child classes of a visitable hierarchy.
+/// @tparam Base The common base class of this class.
+/// @tparam Derived This class.
+/// @tparam Visitor The concrete visitor to support.
+template <typename Base, typename Derived, typename... Visitors>
+struct visitable;
+
+template <typename Base, typename Derived, typename First, typename... Visitors>
+struct visitable<Base, Derived, First, Visitors...>
+  : visitable<Base, Derived, Visitors...>
+{
+  using visitable<Base, Derived, Visitors...>::accept;
+
+  virtual void accept(First& v)
+  {
+    v.visit(static_cast<Derived&>(*this));
   }
-    
-#define VAST_ACCEPT__DEFINE(klass, visitor)       \
-  void klass::accept(visitor& v)                  \
-  {                                               \
-    v.visit(*this);                               \
+  
+  virtual void accept(First& v) const
+  {
+    v.visit(static_cast<Derived const&>(*this));
   }
-    
-#define VAST_ACCEPT_CONST(visitor)        \
-  virtual void accept(visitor& v) const   \
-  {                                       \
-    v.visit(*this);                       \
+};
+
+template <typename Base, typename Derived, typename Last>
+struct visitable<Base, Derived, Last> : public Base
+{
+  virtual void accept(Last& v)
+  {
+    v.visit(static_cast<Derived&>(*this));
   }
-    
-#define VAST_ACCEPT(visitor)        \
-  virtual void accept(visitor& v)   \
-  {                                 \
-    v.visit(*this);                 \
+  
+  virtual void accept(Last& v) const
+  {
+    v.visit(static_cast<Derived const&>(*this));
   }
-    
+};
+
+
+/// A mixin for abstract child classes of a visitable hierarchy.
+/// @tparam Base The common base class of this class.
+/// @tparam Visitors The concrete visitors to support.
+template <typename Base, typename... Visitors>
+struct abstract_visitable;
+
+template <typename Base, typename First, typename... Visitors>
+struct abstract_visitable<Base, First, Visitors...>
+  : abstract_visitable<Base, Visitors...>
+{
+  using abstract_visitable<Base, Visitors...>::accept;
+  virtual void accept(First& v) = 0;
+  virtual void accept(First& v) const = 0;
+};
+
+template <typename Base, typename Last>
+struct abstract_visitable<Base, Last> : public Base
+{
+  virtual void accept(Last& v) = 0;
+  virtual void accept(Last& v) const = 0;
+};
 
 } // namespace util
 } // namespace vast
--- test/unit/visitor.cc
+++ test/unit/visitor.cc
@@ -1,41 +1,29 @@
 #include "test.h"
 #include "vast/util/visitor.h"
 
-struct base;
+using namespace vast;
+
 struct t1;
 struct t2;
+using concrete_visitor = util::const_visitor<t1, t2>;
 
-typedef vast::util::const_visitor<base, t1, t2> concrete_visitor;
-
-struct base
+struct t : util::visitable_with<concrete_visitor>
 {
-  VAST_ACCEPT(concrete_visitor)
-  VAST_ACCEPT_CONST(concrete_visitor)
+  virtual ~t() = default;
 };
 
-struct t1 : base
+struct t1 : util::visitable<t, t1, concrete_visitor>
 {
-  VAST_ACCEPT(concrete_visitor)
-  VAST_ACCEPT_CONST(concrete_visitor)
-
   int i = 42;
 };
 
-struct t2 : base
+struct t2 : util::visitable<t, t2, concrete_visitor>
 {
-  VAST_ACCEPT(concrete_visitor)
-  VAST_ACCEPT_CONST(concrete_visitor)
-
   double d = 4.2;
 };
 
-struct printer : concrete_visitor
+struct checker : concrete_visitor
 {
-  virtual void visit(base const&)
-  {
-    BOOST_CHECK(! "should not be invoked");
-  }
-
   virtual void visit(t1 const& x)
   {
     BOOST_CHECK_EQUAL(x.i, 42);
@@ -51,11 +39,9 @@ BOOST_AUTO_TEST_CASE(visitor)
 {
   t1 x;
   t2 y;
-
-  base* b1 = &x;
-  base* b2 = &y;
-
-  printer p;
-  b1->accept(p);
-  b2->accept(p);
+  t* b1 = &x;
+  t* b2 = &y;
+  checker v;
+  b1->accept(v);
+  b2->accept(v);
 }
