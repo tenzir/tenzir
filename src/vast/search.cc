30190db7e6bd9fc098a650ae1b93c2423b373a86
Matthias Vallentin <vallentin@icir.org>
2013-10-07 17:16:15 -0700

    Dissect query and keep state for each component.

--- src/vast/search.cc
+++ src/vast/search.cc
@@ -15,37 +15,107 @@ search::search(actor_ptr archive, actor_ptr index, actor_ptr schema_manager)
 {
 }
 
+namespace {
+
+class dissector : public expr::const_visitor
+{
+public:
+  dissector(std::map<expr::ast, search::state>& state_map, actor_ptr const& qry)
+    : state_(state_map),
+      query_{qry}
+  {
+  }
+
+  virtual void visit(expr::constant const&) { }
+  virtual void visit(expr::timestamp_extractor const&) { }
+  virtual void visit(expr::name_extractor const&) { }
+  virtual void visit(expr::id_extractor const&) { }
+  virtual void visit(expr::offset_extractor const&) { }
+  virtual void visit(expr::type_extractor const&) { }
+
+  virtual void visit(expr::conjunction const& conj)
+  {
+    expr::ast ast{std::unique_ptr<expr::node>(conj.clone())};
+    if (parent_ && state_.count(ast))
+      state_[ast].parents.insert(parent_);
+    if (! parent_)
+      state_[ast].queries.insert(query_);
+    parent_ = ast;
+    for (auto& clause : conj.operands)
+      clause->accept(*this);
+  }
+
+  virtual void visit(expr::disjunction const& disj)
+  {
+    expr::ast ast{std::unique_ptr<expr::node>(disj.clone())};
+    if (parent_ && state_.count(ast))
+      state_[ast].parents.insert(parent_);
+    if (! parent_)
+      state_[ast].queries.insert(query_);
+    parent_ = ast;
+    for (auto& term : disj.operands)
+      term->accept(*this);
+  }
+
+  virtual void visit(expr::relation const& rel)
+  {
+    assert(parent_);
+    expr::ast ast{std::unique_ptr<expr::node>(rel.clone())};
+    auto& s = state_[ast];
+    if (parent_ && state_.count(ast))
+      s.parents.insert(parent_);
+  }
+
+private:
+  expr::ast parent_;
+  std::map<expr::ast, search::state>& state_;
+  actor_ptr const& query_;
+};
+
+} // namespace <anonymous>
+
 void search::act()
 {
   become(
-      on(atom("DOWN"), arg_match) >> [=](uint32_t reason)
+      on(atom("DOWN"), arg_match) >> [=](uint32_t)
       {
         auto& client = last_sender();
-        VAST_LOG_ACTOR_INFO("got DOWN from client @" << client->id());
-        assert(queries_.count(client));
-        // TODO: don't shut down the queries, but rather put them in a detached
-        // state so that their results can be reused by related queries.
-        send_exit(queries_[client], reason);
-        queries_.erase(client);
+        VAST_LOG_ACTOR_INFO("removes queries from client @" << client->id());
+        assert(clients_.count(client));
+        auto er = clients_.equal_range(client);
+        for (auto i = er.first; i != er.second; ++i)
+        {
+          assert(queries_.count(i->second));
+          send_exit(queries_[i->second], exit::stop);
+        }
+        clients_.erase(client);
       },
       on(atom("query"), atom("create"), arg_match) >> [=](std::string const& q)
       {
         auto client = last_sender();
-        try
+        expr::ast ast{q};
+        if (! ast)
         {
-          auto qry = spawn<query>(archive_, index_, client, expr::ast{q});
-          monitor(client);
-          assert(queries_.count(client) == 0);
-          queries_.emplace(client, qry);
-          reply(qry);
-        }
-        catch (error::query const& e)
-        {
-          std::stringstream msg;
-          msg << "got invalid query expression: " << e.what();
-          VAST_LOG_ACTOR_ERROR(msg.str());
           reply(actor_ptr{});
+          return;
         }
+        auto qry = spawn<query>(archive_, index_, client, ast);
+        if (! queries_.count(ast))
+          queries_.emplace(ast, qry);
+        dissector d{state_, qry};
+        ast.accept(d);
+        auto found = false;
+        auto er = clients_.equal_range(client);
+        for (auto i = er.first; i != er.second; ++i)
+          if (i->second == ast)
+          {
+            found = true;
+            break;
+          }
+        if (! found)
+          clients_.emplace(client, std::move(ast));
+        monitor(client);
+        reply(qry);
       });
 }
 
--- src/vast/search.h
+++ src/vast/search.h
@@ -1,15 +1,24 @@
 #ifndef VAST_SEARCH_H
 #define VAST_SEARCH_H
 
-#include <set>
 #include <cppa/cppa.hpp>
 #include "vast/actor.h"
+#include "vast/bitstream.h"
+#include "vast/expression.h"
 
 namespace vast {
 
 class search : public actor<search>
 {
 public:
+  struct state
+  {
+    bitstream result;
+    bitstream coverage;
+    std::set<cppa::actor_ptr> queries;
+    std::set<expr::ast> parents;
+  };
+
   search(cppa::actor_ptr archive,
          cppa::actor_ptr index,
          cppa::actor_ptr schema_manager);
@@ -21,7 +30,9 @@ private:
   cppa::actor_ptr archive_;
   cppa::actor_ptr index_;
   cppa::actor_ptr schema_manager_;
-  std::map<cppa::actor_ptr, cppa::actor_ptr> queries_;
+  std::multimap<cppa::actor_ptr, expr::ast> clients_;
+  std::map<expr::ast, cppa::actor_ptr> queries_;
+  std::map<expr::ast, state> state_;
 };
 
 } // namespace vast
