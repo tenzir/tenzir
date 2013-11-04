3e8df9192426b28a6fdad02faeb98a5e51dc4e43
Matthias Vallentin <vallentin@icir.org>
2013-11-04 11:15:42 -0800

    Add names for ID space boundaries.

--- src/vast/aliases.h
+++ src/vast/aliases.h
@@ -2,11 +2,19 @@
 #define VAST_ALIASES_H
 
 #include <cstdint>
+#include <limits>
 
 namespace vast {
 
 /// Uniquely identifies a VAST event.
 using event_id = uint64_t;
+
+/// The smallest possible event ID.
+static constexpr event_id min_event_id = 1;
+
+/// The largest possible event ID.
+static constexpr event_id max_event_id =
+  std::numeric_limits<event_id>::max() - 1;
 
 /// Uniquely identifies a VAST type.
 using type_id = uint64_t;
