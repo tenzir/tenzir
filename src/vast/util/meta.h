c73f83009caf5cbbe2631c072fa289a4c3040888
Matthias Vallentin <vallentin@icir.org>
2014-08-05 21:35:11 -0700

    Hoist CAF's type list implementation.

--- src/vast/util/meta.h
+++ src/vast/util/meta.h
@@ -3,6 +3,7 @@
 
 #include <memory>
 #include <type_traits>
+#include <caf/detail/type_list.hpp>
 
 namespace vast {
 namespace util {
@@ -154,6 +155,48 @@ using is_smart_ptr =
 
 template <typename T>
 using is_ptr = bool_t<std::is_pointer<T>::value || is_smart_ptr<T>::value>;
+
+//
+// Type lists
+//
+
+using caf::detail::type_list;
+using caf::detail::empty_type_list;
+using caf::detail::is_type_list;
+using caf::detail::tl_head;
+using caf::detail::tl_tail;
+using caf::detail::tl_size;
+using caf::detail::tl_back;
+using caf::detail::tl_empty;
+using caf::detail::tl_slice;
+using caf::detail::tl_zip;
+using caf::detail::tl_unzip;
+using caf::detail::tl_index_of;
+using caf::detail::tl_reverse;
+using caf::detail::tl_find;
+using caf::detail::tl_find_if;
+using caf::detail::tl_forall;
+using caf::detail::tl_exists;
+using caf::detail::tl_count;
+using caf::detail::tl_count_not;
+using caf::detail::tl_concat;
+using caf::detail::tl_push_back;
+using caf::detail::tl_push_front;
+using caf::detail::tl_apply_all;
+using caf::detail::tl_map;
+using caf::detail::tl_map_conditional;
+using caf::detail::tl_pop_back;
+using caf::detail::tl_at;
+using caf::detail::tl_prepend;
+using caf::detail::tl_filter;
+using caf::detail::tl_filter_not;
+using caf::detail::tl_distinct;
+using caf::detail::tl_is_distinct;
+using caf::detail::tl_trim;
+using caf::detail::tl_apply;
+using caf::detail::tl_is_strict_subset;
+using caf::detail::tl_equal;
+using caf::get;
 
 } // namspace util
 } // namspace vast
