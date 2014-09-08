0be727fc5f373956385e008a624d0cf1aba85a83
Matthias Vallentin <vallentin@icir.org>
2014-09-08 14:44:28 -0700

    Require explicit libedit version.

--- cmake/FindEditline.cmake
+++ cmake/FindEditline.cmake
@@ -8,7 +8,7 @@
 # to be set before calling find_package:
 #
 #  EDITLINE_ROOT_DIR  Set this variable to the root installation of
-#                     editline if the module has problems finding 
+#                     editline if the module has problems finding
 #                     the proper installation path.
 #
 # Variables defined by this module:
@@ -16,10 +16,28 @@
 #  EDITLINE_FOUND              System has Editline libs/headers
 #  EDITLINE_LIBRARIES          The Editline libraries
 #  EDITLINE_INCLUDE_DIR        The location of Editline headers
+#  EDITLINE_VERSION            The full version of Editline
+#  EDITLINE_VERSION_MAJOR      The version major of Editline
+#  EDITLINE_VERSION_MINOR      The version minor of Editline
 
 find_path(EDITLINE_INCLUDE_DIR
   NAMES histedit.h
   HINTS ${EDITLINE_ROOT_DIR}/include)
+
+if (EDITLINE_INCLUDE_DIR)
+  file(STRINGS ${EDITLINE_INCLUDE_DIR}/histedit.h
+    editline_header REGEX "^#define.LIBEDIT_[A-Z]+.*$")
+
+  string(REGEX REPLACE ".*#define.LIBEDIT_MAJOR[ \t]+([0-9]+).*" "\\1"
+    EDITLINE_VERSION_MAJOR ${editline_header})
+  string(REGEX REPLACE ".*#define.LIBEDIT_MINOR[ \t]+([0-9]+).*" "\\1"
+    EDITLINE_VERSION_MINOR ${editline_header})
+
+  set(EDITLINE_VERSION_MAJOR ${EDITLINE_VERSION_MAJOR} CACHE STRING "" FORCE)
+  set(EDITLINE_VERSION_MINOR ${EDITLINE_VERSION_MINOR} CACHE STRING "" FORCE)
+  set(EDITLINE_VERSION ${EDITLINE_VERSION_MAJOR}.${EDITLINE_VERSION_MINOR}
+    CACHE STRING "" FORCE)
+endif ()
 
 find_library(EDITLINE_LIBRARIES
   NAMES edit
@@ -35,4 +53,8 @@ find_package_handle_standard_args(
 mark_as_advanced(
   EDITLINE_ROOT_DIR
   EDITLINE_LIBRARIES
-  EDITLINE_INCLUDE_DIR)
+  EDITLINE_INCLUDE_DIR
+  EDITLINE_VERSION
+  EDITLINE_VERSION_MAJOR
+  EDITLINE_VERSION_MINOR
+  )
