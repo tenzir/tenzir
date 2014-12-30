5f0e87a277112573dcc605b2ca257a97e9621af8
Matthias Vallentin <vallentin@icir.org>
2014-12-30 19:47:26 +0100

    Shorten CAF CMake scripts.

--- cmake/FindCAF.cmake
+++ cmake/FindCAF.cmake
@@ -1,23 +1,22 @@
-# Try to find libcaf headers and library.
+# Try to find CAF headers and library.
 #
 # Use this module as follows:
 #
-#     find_package(Libcaf)
+#     find_package(CAF)
 #
 # Variables used by this module (they can change the default behaviour and need
 # to be set before calling find_package):
 #
-#  LIBCAF_ROOT_DIR  Set this variable to the root installation of
-#                   libcaf if the module has problems finding 
-#                   the proper installation path.
+#  CAF_ROOT_DIR  Set this variable to the root installation of CAF if the
+#                module has problems finding the proper installation path.
 #
 # Variables defined by this module:
 #
-#  LIBCAF_FOUND              System has libcaf headers and library
-#  LIBCAF_LIBRARIES          Location of the libcaf library
-#  LIBCAF_INCLUDE_DIR        Location of the libcaf headers
+#  CAF_FOUND              System has CAF headers and library
+#  CAF_LIBRARIES          Location of the CAF library
+#  CAF_INCLUDE_DIR        Location of the CAF headers
 
-foreach (comp ${Libcaf_FIND_COMPONENTS})
+foreach (comp ${CAF_FIND_COMPONENTS})
   string(TOUPPER "${comp}" UPPERCOMP)
   if ("${comp}" STREQUAL "core")
     set(HDRNAME "caf/all.hpp")
@@ -26,12 +25,12 @@ foreach (comp ${Libcaf_FIND_COMPONENTS})
   endif ()
 
   set(HDRHINT "actor-framework/libcaf_${comp}")
-  unset(LIBCAF_INCLUDE_DIR)
-  find_path(LIBCAF_INCLUDE_DIR_${UPPERCOMP}
+  unset(CAF_INCLUDE_DIR)
+  find_path(CAF_INCLUDE_DIR_${UPPERCOMP}
             NAMES
               ${HDRNAME}
             HINTS
-              ${LIBCAF_ROOT_DIR}/include
+              ${CAF_ROOT_DIR}/include
               /usr/include
               /usr/local/include
               /opt/local/include
@@ -41,27 +40,27 @@ foreach (comp ${Libcaf_FIND_COMPONENTS})
               ../../${HDRHINT}
               ../../../${HDRHINT})
 
-  mark_as_advanced(LIBCAF_INCLUDE_DIR_${UPPERCOMP})
+  mark_as_advanced(CAF_INCLUDE_DIR_${UPPERCOMP})
 
-  if ("${LIBCAF_INCLUDE_DIR_${UPPERCOMP}}" STREQUAL "LIBCAF_INCLUDE_DIR_${UPPERCOMP}-NOTFOUND")
+  if ("${CAF_INCLUDE_DIR_${UPPERCOMP}}" STREQUAL "CAF_INCLUDE_DIR_${UPPERCOMP}-NOTFOUND")
     break ()
   else ()
     set(duplicate false)
-    foreach (p ${LIBCAF_INCLUDE_DIRS})
-      if (${p} STREQUAL ${LIBCAF_INCLUDE_DIR_${UPPERCOMP}})
+    foreach (p ${CAF_INCLUDE_DIRS})
+      if (${p} STREQUAL ${CAF_INCLUDE_DIR_${UPPERCOMP}})
         set(duplicate true)
       endif ()
     endforeach ()
     if (NOT duplicate)
-      set(LIBCAF_INCLUDE_DIRS ${LIBCAF_INCLUDE_DIRS} ${LIBCAF_INCLUDE_DIR_${UPPERCOMP}})
+      set(CAF_INCLUDE_DIRS ${CAF_INCLUDE_DIRS} ${CAF_INCLUDE_DIR_${UPPERCOMP}})
     endif()
   endif ()
 
-  find_library(LIBCAF_LIBRARY_${UPPERCOMP}
+  find_library(CAF_LIBRARY_${UPPERCOMP}
                NAMES
                  "caf_${comp}"
                HINTS
-                 ${LIBCAF_ROOT_DIR}/lib
+                 ${CAF_ROOT_DIR}/lib
                  /usr/lib
                  /usr/local/lib
                  /opt/local/lib
@@ -71,21 +70,21 @@ foreach (comp ${Libcaf_FIND_COMPONENTS})
                  ../../actor-framework/build/lib
                  ../../../actor-framework/build/lib)
 
-  mark_as_advanced(LIBCAF_LIBRARY_${UPPERCOMP})
+  mark_as_advanced(CAF_LIBRARY_${UPPERCOMP})
 
-  if ("${LIBCAF_LIBRARY_${UPPERCOMP}}" STREQUAL "LIBCAF_LIBRARY-NOTFOUND")
+  if ("${CAF_LIBRARY_${UPPERCOMP}}" STREQUAL "CAF_LIBRARY-NOTFOUND")
     break ()
   else ()
-    set(LIBCAF_LIBRARIES ${LIBCAF_LIBRARIES} ${LIBCAF_LIBRARY_${UPPERCOMP}})
+    set(CAF_LIBRARIES ${CAF_LIBRARIES} ${CAF_LIBRARY_${UPPERCOMP}})
   endif ()
 endforeach ()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(libcaf DEFAULT_MSG
-    LIBCAF_LIBRARIES
-    LIBCAF_INCLUDE_DIRS)
+find_package_handle_standard_args(CAF DEFAULT_MSG
+    CAF_LIBRARIES
+    CAF_INCLUDE_DIRS)
 
 mark_as_advanced(
-    LIBCAF_ROOT_DIR
-    LIBCAF_LIBRARIES
-    LIBCAF_INCLUDE_DIRS)
+    CAF_ROOT_DIR
+    CAF_LIBRARIES
+    CAF_INCLUDE_DIRS)
