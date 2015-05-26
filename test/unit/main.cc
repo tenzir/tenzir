c67ca7c230b3dc075c701d411fb30a663d035938
Matthias Vallentin <vallentin@icir.org>
2015-05-26 16:40:58 -0700

    Announce types before running test main function

--- test/unit/main.cc
+++ test/unit/main.cc
@@ -1,2 +1,10 @@
-#include <caf/test/unit_test.hpp>
+#define CAF_TEST_NO_MAIN
 #include <caf/test/unit_test_impl.hpp>
+
+#include "vast/announce.h"
+
+int main(int argc, char** argv)
+{
+  vast::announce_types();
+  return caf::test::main(argc, argv);
+}
