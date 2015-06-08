f977b83c46f66d272b67aa64de8a8aecd5a0b05a
Matthias Vallentin <vallentin@icir.org>
2015-06-08 13:13:44 +0200

    Simplify quoted string parser

--- src/vast/concept/parseable/string.h
+++ src/vast/concept/parseable/string.h
@@ -3,7 +3,9 @@
 
 #include "vast/concept/parseable/string/char.h"
 #include "vast/concept/parseable/string/char_class.h"
+#include "vast/concept/parseable/string/char_range.h"
 #include "vast/concept/parseable/string/c_string.h"
 #include "vast/concept/parseable/string/string.h"
+#include "vast/concept/parseable/string/quoted_string.h"
 
 #endif
