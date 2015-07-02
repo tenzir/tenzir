ce08bd510ea238a2780d308b7c9661bb48a63a96
Matthias Vallentin <vallentin@icir.org>
2015-07-02 23:35:21 +0200

    Enhance possible lambda for action parser
    There exist 4 types of labmdas we support, based on arity (either 0 or
    1) and result type (either void or non-void). The action parser
    implementation now selects the right function based on SFINAE.
    Previously only one lambda type was supported.

--- src/vast/concept/parseable/core/action.h
+++ src/vast/concept/parseable/core/action.h
@@ -1,6 +1,8 @@
 #ifndef VAST_CONCEPT_PARSEABLE_CORE_ACTION_H
 #define VAST_CONCEPT_PARSEABLE_CORE_ACTION_H
 
+#include <caf/detail/type_traits.hpp>
+
 #include "vast/concept/parseable/core/parser.h"
 
 namespace vast {
@@ -14,7 +16,15 @@ class action_parser : public parser<action_parser<Parser, Action>>
 {
 public:
   using inner_attribute = typename Parser::attribute;
-  using attribute = std::result_of_t<Action(inner_attribute)>;
+  using action_traits = caf::detail::get_callable_trait<Action>;
+  using action_result_type = typename action_traits::result_type;
+  static constexpr size_t action_arity = action_traits::num_args;
+  using attribute =
+    std::conditional_t<
+      std::is_same<action_result_type, void>{},
+      unused_type,
+      action_result_type
+    >;
 
   action_parser(Parser const& p, Action fun)
     : parser_{p},
@@ -22,14 +32,74 @@ public:
   {
   }
 
-  template <typename Iterator>
-  bool parse(Iterator& f, Iterator const& l, unused_type) const
+  // No argument, no return type.
+  template <typename Iterator, typename Attribute, typename A = Action>
+  auto parse(Iterator& f, Iterator const& l, Attribute&) const
+    -> std::enable_if_t<
+         caf::detail::get_callable_trait<A>::num_args == 0
+           && std::is_same<
+                typename caf::detail::get_callable_trait<A>::result_type,
+                void
+              >{},
+         bool
+       >
   {
-    return parser_.parse(f, l, unused);
+    inner_attribute x;
+    if (! parser_.parse(f, l, x))
+      return false;
+    action_();
+    return true;
   }
 
-  template <typename Iterator, typename Attribute>
-  bool parse(Iterator& f, Iterator const& l, Attribute& a) const
+  // One argument, no return type.
+  template <typename Iterator, typename Attribute, typename A = Action>
+  auto parse(Iterator& f, Iterator const& l, Attribute&) const
+    -> std::enable_if_t<
+         caf::detail::get_callable_trait<A>::num_args == 1
+           && std::is_same<
+                typename caf::detail::get_callable_trait<A>::result_type,
+                void
+              >{},
+         bool
+       >
+  {
+    inner_attribute x;
+    if (! parser_.parse(f, l, x))
+      return false;
+    action_(std::move(x));
+    return true;
+  }
+
+  // No argument, with return type.
+  template <typename Iterator, typename Attribute, typename A = Action>
+  auto parse(Iterator& f, Iterator const& l, Attribute& a) const
+    -> std::enable_if_t<
+         caf::detail::get_callable_trait<A>::num_args == 0
+           && ! std::is_same<
+                typename caf::detail::get_callable_trait<A>::result_type,
+                void
+              >{},
+         bool
+       >
+  {
+    inner_attribute x;
+    if (! parser_.parse(f, l, x))
+      return false;
+    a = action_();
+    return true;
+  }
+
+  // One argument, with return type.
+  template <typename Iterator, typename Attribute, typename A = Action>
+  auto parse(Iterator& f, Iterator const& l, Attribute& a) const
+    -> std::enable_if_t<
+         caf::detail::get_callable_trait<A>::num_args == 1
+           && ! std::is_same<
+                typename caf::detail::get_callable_trait<A>::result_type,
+                void
+              >{},
+         bool
+       >
   {
     inner_attribute x;
     if (! parser_.parse(f, l, x))
