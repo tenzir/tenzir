ac171513cc8696710958f7ea1620e35cee488662
Matthias Vallentin <vallentin@icir.org>
2015-07-20 10:59:21 -0700

    Increase low default ulimit -n of 256 on Mac OS
    The low default ulimit -n of 256 on Mac OS caused unit tests to fail.

--- test/unit/main.cc
+++ test/unit/main.cc
@@ -2,9 +2,30 @@
 #include <caf/test/unit_test_impl.hpp>
 
 #include "vast/announce.h"
+#include "vast/config.h"
+
+#ifdef VAST_MACOS
+#include <sys/resource.h> // setrlimit
+#endif
+
+namespace {
+
+bool adjust_resource_limits()
+{
+#ifdef VAST_MACOS
+  auto rl = ::rlimit{4096, 8192};
+  auto result = ::setrlimit(RLIMIT_NOFILE,  &rl);
+  return result == 0;
+#endif
+  return true;
+}
+
+} // namespace <anonymous>
 
 int main(int argc, char** argv)
 {
+  if (! adjust_resource_limits())
+    return 1;
   vast::announce_types();
   return caf::test::main(argc, argv);
 }
