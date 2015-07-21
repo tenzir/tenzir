451699430f387794a42cdb6bb03de5325e539dff
Matthias Vallentin <vallentin@icir.org>
2015-07-21 10:35:15 -0700

    Add synchronous wrapper for key-value store

--- src/vast/actor/key_value_store.h
+++ src/vast/actor/key_value_store.h
@@ -4,17 +4,80 @@
 #include <set>
 #include <string>
 
+#include <caf/scoped_actor.hpp>
+
+#include "vast/error.h"
 #include "vast/actor/actor.h"
 #include "vast/util/radix_tree.h"
 
 namespace vast {
 
 /// A replicated hierarchical key-value store.
-struct key_value_store : default_actor
+class key_value_store : public default_actor
 {
-  /// Spawns a backend.
+public:
+  /// Synchronous interface.
+  class wrapper
+  {
+  public:
+    // Constructs a key-value store.
+    wrapper(caf::actor& store);
+
+    /// Records a key-value pair.
+    /// @param key The key to store *value* under.
+    /// @param xs The values to associate with *key*.
+    /// @returns `true` on success.
+    template <typename... Ts>
+    bool put(std::string const& key, Ts&&... xs) const
+    {
+      auto result = true;
+      caf::scoped_actor self;
+      auto msg = make_message(put_atom::value, key, std::forward<Ts>(xs)...);
+      self->sync_send(store_, std::move(msg)).await(
+        [&](error const&) { result = false; },
+        [](ok_atom) { /* nop */ }
+      );
+      return result;
+    }
+
+    /// Deletes a key.
+    /// @param key The key to delete.
+    /// @returns The number of elements deleted. (0 or 1)
+    size_t erase(std::string const& key) const;
+
+    /// Deletes all key-value pairs having a specific value.
+    /// @param value The value of the key-value pairs to delete.
+    /// @returns The number of elements deleted.
+    //size_t erase(caf::message const& value) const;
+
+    /// Checks whether a key exists.
+    /// @param key The key to check.
+    /// @returns `true` if *key* exists in the store.
+    bool exists(std::string const& key) const;
+
+    /// Retrieves a value for a given key.
+    /// @param *key* The key to lookup.
+    /// @returns The value associated with *key* or an empty optional otherwise.
+    caf::message get(std::string const& key) const;
+
+    /// Enumerates all values for a given key prefix.
+    /// @param prefix The prefix to enumerate.
+    /// @returns All key-value pairs prefixed by *prefix*.
+    std::map<std::string, caf::message> list(std::string const& prefix) const;
+
+    /// Adds a peer to the store.
+    /// @param peer The actor to add as peer.
+    void add_peer(caf::actor const& peer) const;
+
+  private:
+    caf::actor& store_;
+  };
+
+  /// Spawns a key-value store.
+  /// @param seperator The namespace separator.
   key_value_store(std::string const& seperator = "/");
 
+private:
   void on_exit() override;
   caf::behavior make_behavior() override;
 
@@ -22,6 +85,7 @@ struct key_value_store : default_actor
   util::radix_tree<caf::message> data_;
   std::set<caf::actor> peers_;
 };
+
 
 } // namespace vast
 
