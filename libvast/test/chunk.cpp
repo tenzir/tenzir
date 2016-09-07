awesome-security/vast

Prepare for major overhaul
CAF's new actor system causes a major overhaul of how VAST works
internally. This means *a lot* of things have to change. This commit
merely prepares the scene for numerous refactoring commits.

The following changes ocurred:

  - VAST no longer keeps on its own serialization framework and instead
    fully embraces CAF's framework, which has come a long way and quite
    close in frunctionality. At this point, maintaining two nearly
    idententical frameworks no longer makes sense.

    Many types in VAST were serializable just because they modeled the
    State concept. These types now transparently work with CAF's new
    serialization framework because VAST's framework automatically
    generates the necessary free functions overloads (via expression
    SFINAE).

  - vast::block is gone. Since this was an implementation detail of
    vast::chunk anyway, its functionality now directly moved into
    vast::chunk. Chunks are now managed as shared_ptr<chunk> to make the
    shared ownership explicit.

  - vast::trial, vast::result, and vast::error are gone. vast::maybe
    supersedes both of them, and relying on CAF's error handling.

  - There's a new vast::system, which represent an actor system equipped
    with all VAST types.

  - All vast::io related functionality is gone. We no longer rely on a
    custom I/O framework, but instead use the existing one from the
    standard library. We have ported the custom compressed streams to
    std::streambuf-compliant stream buffers, which can now be used from
    any iostream.

  - Removal of polymorphic serialization hierachies. While enabling
    serialization of polymorphic objects held through a base pointer,
    the new serialization framework no longer relies on global state,
    making a transparent integration clunky and cumbersome. Therefore,
    we no longer support traditional object hierarchies during
    serialization.

  - Refactor byte swapping utilities. We now rely on comp

libvast/test/chunk.cpp
@@ -1,0 +1,96 @@
+#include "vast/chunk.hpp"
+#include "vast/event.hpp"
+#include "vast/load.hpp"
+#include "vast/save.hpp"
+#include "vast/concept/serializable/vast/chunk.hpp"
+
+#include "test.hpp"
+
+using namespace vast;
+
+namespace {
+
+struct fixture {
+  fixture() {
+    auto event_type = type::integer{};
+    event_type.name("i");
+    for (auto i = 0; i < 1e3; ++i) {
+      events.push_back(event::make(i, event_type));
+      events.back().id(1000 + i);
+    }
+  }
+
+  type event_type;
+  std::vector<event> events;
+};
+
+} // namespace <anonymous>
+
+FIXTURE_SCOPE(chunk_tests, fixture)
+
+TEST(chunk) {
+  MESSAGE("construct a chunk via chunk::writer");
+  chunk chk;
+  {
+    chunk::writer w{chk};
+    for (auto i = 0; i < 1e3; ++i)
+      REQUIRE(w.write(events[i]));
+  }
+  CHECK_EQUAL(chk.events(), 1e3);
+  MESSAGE("read from the chunk");
+  chunk::reader r{chk};
+  for (auto i = 0; i < 1e3; ++i) {
+    auto e = r.read();
+    REQUIRE(e);
+    CHECK_EQUAL(*e, event::make(i, event_type));
+  }
+  MESSAGE("construct a chunk from a sequence of events");
+  chunk chk2;
+  CHECK(chk2.compress(events));
+  CHECK(chk2 == chk);
+  MESSAGE("assign IDs to events in the chunk");
+  ewah_bitstream ids;
+  ids.append(42, false);
+  ids.append(1e3 - 1, true);
+  CHECK(!chk.ids(ids)); // 1 event ID missing.
+  ids.push_back(true);
+  CHECK(chk.ids(std::move(ids)));
+}
+
+TEST(chunk_event_extraction) {
+  chunk chk;
+  chk.compress(events);
+  chunk::reader r{chk};
+  maybe<event> e;
+  MESSAGE("try to read out-of-bounds");
+  CHECK(!r.read(10));
+  CHECK(!r.read(2024));
+  MESSAGE("get first event");
+  e = r.read(1000);
+  REQUIRE(e);
+  CHECK_EQUAL(*get<integer>(*e), 0);
+  REQUIRE_EQUAL(e->id(), 1000u);
+  MESSAGE("seek forward");
+  e = r.read(1720);
+  REQUIRE(e);
+  CHECK_EQUAL(*get<integer>(*e), 720);
+  REQUIRE_EQUAL(e->id(), 1720u);
+  MESSAGE("read last event");
+  e = r.read(1999);
+  REQUIRE(e);
+  CHECK_EQUAL(*get<integer>(*e), 999);
+  REQUIRE_EQUAL(e->id(), 1999u);
+  MESSAGE("attempt to read one-past-last event");
+  REQUIRE(!r.read(2000));
+}
+
+TEST(chunk_serialization) {
+  chunk chk, chk2;
+  chk.compress(events);
+  std::vector<char> buf;
+  save(buf, chk);
+  load(buf, chk2);
+  CHECK(chk == chk2);
+}
+
+FIXTURE_SCOPE_END()

libvast/vast/save.hpp
@@ -1,0 +1,74 @@
+#ifndef VAST_SAVE_HPP
+#define VAST_SAVE_HPP
+
+#include <fstream>
+#include <streambuf>
+#include <type_traits>
+
+#include <caf/stream_serializer.hpp>
+#include <caf/stream_deserializer.hpp>
+
+#include "vast/detail/process.hpp"
+#include "vast/detail/type_traits.hpp"
+
+#include "vast/compression.hpp"
+#include "vast/maybe.hpp"
+#include "vast/filesystem.hpp"
+#include "vast/streambuf.hpp"
+
+namespace vast {
+
+/// Serializes a sequence of objects into a streambuffer.
+/// @see load
+template <
+  compression Method = compression::null,
+  class Streambuf,
+  class T,
+  class... Ts
+>
+auto save(Streambuf& streambuf, T&& x, Ts&&... xs)
+-> std::enable_if_t<detail::is_streambuf<Streambuf>::value, maybe<void>> {
+  // A good optimizer clears that branch.
+  if (Method == compression::null) {
+    caf::stream_serializer<Streambuf&> s{streambuf};
+    detail::process(s, std::forward<T>(x), std::forward<Ts>(xs)...);
+  } else {
+    compressedbuf compressed{streambuf, Method};
+    caf::stream_serializer<compressedbuf&> s{compressed};
+    detail::process(s, std::forward<T>(x), std::forward<Ts>(xs)...);
+  }
+  return {};
+}
+
+/// Serializes a sequence of objects into a container of bytes.
+/// @see load
+template <
+  compression Method = compression::null,
+  class Container,
+  class T,
+  class... Ts
+>
+auto save(Container& container, T&& x, Ts&&... xs)
+-> std::enable_if_t<
+  detail::is_contiguous_byte_container<Container>::value,
+  maybe<void>
+> {
+  caf::containerbuf<Container> sink{container};
+  return save<Method>(sink, std::forward<T>(x), std::forward<Ts>(xs)...);
+}
+
+/// Serializes a sequence of objects into a file.
+/// @see load
+template <
+  compression Method = compression::null,
+  class T,
+  class... Ts
+>
+maybe<void> save(path const& p, T&& x, Ts&&... xs) {
+  std::ofstream fs{p.str()};
+  return save<Method>(*fs.rdbuf(), std::forward<T>(x), std::forward<Ts>(xs)...);
+}
+
+} // namespace vast
+
+#endif

tenzir/tenzir

    supersedes both of them, which relies on CAF's error handling.
    streambuffers compliant with the standard library API.

libvast/test/chunk.cpp
@@ -1,0 +1,100 @@
+#include "vast/chunk.hpp"
+#include "vast/event.hpp"
+#include "vast/load.hpp"
+#include "vast/save.hpp"
+#include "vast/concept/serializable/vast/chunk.hpp"
+#include "vast/concept/printable/vast/event.hpp"
+
+#include "test.hpp"
+
+using namespace vast;
+
+namespace {
+
+struct fixture {
+  fixture() {
+    auto event_type = type::integer{};
+    event_type.name("foo");
+    for (auto i = 0; i < 1e3; ++i) {
+      events.push_back(event::make(i, event_type));
+      events.back().id(1000 + i);
+    }
+  }
+
+  type event_type;
+  std::vector<event> events;
+};
+
+} // namespace <anonymous>
+
+FIXTURE_SCOPE(chunk_tests, fixture)
+
+TEST(chunk) {
+  MESSAGE("construct a chunk via chunk::writer");
+  chunk chk;
+  {
+    chunk::writer w{chk};
+    for (auto i = 0; i < 1e3; ++i)
+      REQUIRE(w.write(events[i]));
+  }
+  CHECK_EQUAL(chk.events(), 1e3);
+  MESSAGE("read from the chunk");
+  chunk::reader r{chk};
+  for (auto i = 0; i < 1e3; ++i) {
+    auto e = r.read();
+    REQUIRE(e);
+    auto expected = event::make(i, event_type);
+    expected.id(1000 + i);
+    CHECK_EQUAL(*e, expected);
+  }
+  MESSAGE("construct a chunk from a sequence of events");
+  chunk chk2;
+  CHECK(chk2 != chk);
+  CHECK(chk2.compress(events));
+  CHECK(chk2 == chk);
+  MESSAGE("assign IDs to events in the chunk");
+  ewah_bitstream ids;
+  ids.append(42, false);
+  ids.append(1e3 - 1, true);
+  CHECK(!chk.ids(ids)); // 1 event ID missing.
+  ids.push_back(true);
+  CHECK(chk.ids(std::move(ids)));
+}
+
+TEST(chunk_event_extraction) {
+  chunk chk;
+  chk.compress(events);
+  chunk::reader r{chk};
+  maybe<event> e;
+  MESSAGE("try to read out-of-bounds");
+  CHECK(!r.read(10));
+  CHECK(!r.read(2024));
+  MESSAGE("get first event");
+  e = r.read(1000);
+  REQUIRE(e);
+  CHECK_EQUAL(*get<integer>(*e), 0);
+  REQUIRE_EQUAL(e->id(), 1000u);
+  MESSAGE("seek forward");
+  e = r.read(1720);
+  REQUIRE(e);
+  CHECK_EQUAL(*get<integer>(*e), 720);
+  REQUIRE_EQUAL(e->id(), 1720u);
+  MESSAGE("read last event");
+  e = r.read(1999);
+  REQUIRE(e);
+  CHECK_EQUAL(*get<integer>(*e), 999);
+  REQUIRE_EQUAL(e->id(), 1999u);
+  MESSAGE("attempt to read one-past-last event");
+  REQUIRE(!r.read(2000));
+}
+
+TEST(chunk_serialization) {
+  chunk chk, chk2;
+  chk.compress(events);
+  std::vector<char> buf;
+  save(buf, chk);
+  load(buf, chk2);
+  CHECK(chk == chk2);
+}
+
+FIXTURE_SCOPE_END()

libvast/vast/save.hpp
@@ -1,0 +1,74 @@
+#ifndef VAST_SAVE_HPP
+#define VAST_SAVE_HPP
+
+#include <fstream>
+#include <streambuf>
+#include <type_traits>
+
+#include <caf/stream_serializer.hpp>
+#include <caf/stream_deserializer.hpp>
+
+#include "vast/detail/type_traits.hpp"
+#include "vast/detail/variadic_serialization.hpp"
+
+#include "vast/compression.hpp"
+#include "vast/maybe.hpp"
+#include "vast/filesystem.hpp"
+#include "vast/streambuf.hpp"
+
+namespace vast {
+
+/// Serializes a sequence of objects into a streambuffer.
+/// @see load
+template <
+  compression Method = compression::null,
+  class Streambuf,
+  class T,
+  class... Ts
+>
+auto save(Streambuf& streambuf, T&& x, Ts&&... xs)
+-> std::enable_if_t<detail::is_streambuf<Streambuf>::value, maybe<void>> {
+  // A good optimizer clears that branch.
+  if (Method == compression::null) {
+    caf::stream_serializer<Streambuf&> s{streambuf};
+    detail::write(s, std::forward<T>(x), std::forward<Ts>(xs)...);
+  } else {
+    compressedbuf compressed{streambuf, Method};
+    caf::stream_serializer<compressedbuf&> s{compressed};
+    detail::write(s, std::forward<T>(x), std::forward<Ts>(xs)...);
+  }
+  return {};
+}
+
+/// Serializes a sequence of objects into a container of bytes.
+/// @see load
+template <
+  compression Method = compression::null,
+  class Container,
+  class T,
+  class... Ts
+>
+auto save(Container& container, T&& x, Ts&&... xs)
+-> std::enable_if_t<
+  detail::is_contiguous_byte_container<Container>::value,
+  maybe<void>
+> {
+  caf::containerbuf<Container> sink{container};
+  return save<Method>(sink, std::forward<T>(x), std::forward<Ts>(xs)...);
+}
+
+/// Serializes a sequence of objects into a file.
+/// @see load
+template <
+  compression Method = compression::null,
+  class T,
+  class... Ts
+>
+maybe<void> save(path const& p, T&& x, Ts&&... xs) {
+  std::ofstream fs{p.str()};
+  return save<Method>(*fs.rdbuf(), std::forward<T>(x), std::forward<Ts>(xs)...);
+}
+
+} // namespace vast
+
+#endif