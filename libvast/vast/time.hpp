dd5eab8dad200b361473240e47fd092c22093d34
Matthias Vallentin <vallentin@icir.org>
2016-11-12 11:20:16 -0800

    Remove now obsolete chrono serialization overloads

--- libvast/vast/time.hpp
+++ libvast/vast/time.hpp
@@ -4,8 +4,6 @@
 #include <chrono>
 #include <cstdint>
 
-#include <caf/detail/scope_guard.hpp>
-
 namespace vast {
 
 class json;
@@ -27,30 +25,5 @@ bool convert(timestamp tp, double& d);
 bool convert(timestamp tp, json& j);
 
 } // namespace vast
-
-// Since ADL doesn't apply to type aliases, we must define these free functions
-// either in namespace caf or namespace std. Both are poor choices, but since
-// namespace caf has already a bunch of inspect overloads, we'll put 'em there.
-// We should revisit this after this ticket has been addressed:
-// https://github.com/actor-framework/actor-framework/issues/510.
-namespace caf {
-
-template <class Inspector>
-std::enable_if_t<Inspector::reads_state, typename Inspector::result_type>
-inspect(Inspector& f, vast::timestamp& t) {
-  return f(t.time_since_epoch());
-}
-
-template <class Inspector>
-std::enable_if_t<Inspector::writes_state, typename Inspector::result_type>
-inspect(Inspector& f, vast::timestamp& t) {
-  auto i = vast::interval{};
-  auto guard = caf::detail::make_scope_guard([&] {
-    t = vast::timestamp{i};
-  });
-  return f(i);
-}
-
-} // namespace caf
 
 #endif
