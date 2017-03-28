0a6ca554691f187ab5f78b6bf35debc97abe8f37
Matthias Vallentin <vallentin@icir.org>
2017-03-28 16:15:50 -0700

    Add put area to memory-mapped streambuffer

--- libvast/vast/detail/mmapbuf.hpp
+++ libvast/vast/detail/mmapbuf.hpp
@@ -8,8 +8,6 @@
 namespace vast {
 namespace detail {
 
-// TODO: add support for writing to the buffer via the put area.
-
 /// A memory-mapped streambuffer. The put and get areas corresponds to the
 /// mapped memory region.
 class mmapbuf : public std::streambuf {
@@ -20,27 +18,39 @@ public:
   ///             automatically.
   /// @param offset An offset where to begin mapping.
   /// @pre `offset < size`
-  explicit mmapbuf(const std::string& filename, size_t size = 0);
+  explicit mmapbuf(const std::string& filename, size_t size = 0,
+                   size_t offset = 0);
 
   /// Closes the opened file and unmaps the mapped memory region.
   ~mmapbuf();
+
+  /// Exposes the underlying memory region.
+  const char_type* data() const;
 
   /// Returns the size of the mapped memory region.
   size_t size() const;
 
 protected:
-  std::streamsize showmanyc() override;
+  // -- get area --------------------------------------------------------------
 
-  int_type underflow() override;
+  std::streamsize showmanyc() override;
 
   std::streamsize xsgetn(char_type* s, std::streamsize n) override;
 
+  // -- put area --------------------------------------------------------------
+
+  std::streamsize xsputn(const char_type* s, std::streamsize n) override;
+
+  // -- positioning -----------------------------------------------------------
+
   pos_type seekoff(off_type off,
                    std::ios_base::seekdir dir,
-                   std::ios_base::openmode which = std::ios_base::in) override;
+                   std::ios_base::openmode which =
+                     std::ios_base::in | std::ios_base::out) override;
 
   pos_type seekpos(pos_type pos,
-                   std::ios_base::openmode which = std::ios_base::in) override;
+                   std::ios_base::openmode which =
+                     std::ios_base::in | std::ios_base::out) override;
 
 private:
   size_t size_;
