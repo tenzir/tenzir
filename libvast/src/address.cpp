2b7cdfd6c62a3f2fb5d773343ac747f4a4c8ad65
Matthias Vallentin <vallentin@icir.org>
2017-10-28 21:25:20 +0200

    Add convenience factory for v4/6 addresses

--- libvast/src/address.cpp
+++ libvast/src/address.cpp
@@ -18,6 +18,14 @@ std::array<uint8_t, 12> const vast::address::v4_mapped_prefix = {
 
 namespace vast {
 
+address address::v4(const void* bytes, byte_order order) {
+  return address{bytes, ipv4, order};
+};
+
+address address::v6(const void* bytes, byte_order order) {
+  return address{bytes, ipv6, order};
+};
+
 address::address() {
   bytes_.fill(0);
 }
