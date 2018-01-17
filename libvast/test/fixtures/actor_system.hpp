5d515325aa94bfbbd67bb6665c836b549aab3e77
Dominik Charousset <dominik.charousset@tenzir.com>
2018-01-17 16:46:58 +0100

    Add new header to all C++ files

--- libvast/test/fixtures/actor_system.hpp
+++ libvast/test/fixtures/actor_system.hpp
@@ -1,3 +1,16 @@
+/******************************************************************************
+ *                    _   _____   __________                                  *
+ *                   | | / / _ | / __/_  __/     Visibility                   *
+ *                   | |/ / __ |_\ \  / /          Across                     *
+ *                   |___/_/ |_/___/ /_/       Space and Time                 *
+ *                                                                            *
+ * This file is part of VAST. It is subject to the license terms in the       *
+ * LICENSE file found in the top-level directory of this distribution and at  *
+ * http://vast.io/license. No part of VAST, including this file, may be       *
+ * copied, modified, propagated, or distributed except according to the terms *
+ * contained in the LICENSE file.                                             *
+ ******************************************************************************/
+
 #ifndef FIXTURES_ACTOR_SYSTEM_HPP
 #define FIXTURES_ACTOR_SYSTEM_HPP
 
