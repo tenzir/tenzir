13acfe3dd1737588c8a907e05fe850f51a6ce760
Dominik Charousset <dominik.charousset@tenzir.com>
2018-04-24 15:31:01 +0200

    Add new class for managing a set of INDEXER actors

---
+++ libvast/src/system/indexer_manager.cpp
@@ -0,0 +1,102 @@
+/******************************************************************************
+ *                    _   _____   __________                                  *
+ *                   | | / / _ | / __/_  __/     Visibility                   *
+ *                   | |/ / __ |_\ \  / /          Across                     *
+ *                   |___/_/ |_/___/ /_/       Space and Time                 *
+ *                                                                            *
+ * This file is part of VAST. It is subject to the license terms in the       *
+ * LICENSE file found in the top-level directory of this distribution and at  *
+ * http://vast.io/license. No part of VAST, including this file, may be       *
+ * copied, modified, propagated, or distributed except according to the terms *
+ * contained in the LICENSE file.                                             *
+ ******************************************************************************/
+
+#include "vast/system/indexer_manager.hpp"
+
+#include <caf/exit_reason.hpp>
+#include <caf/local_actor.hpp>
+#include <caf/send.hpp>
+
+#include "vast/load.hpp"
+#include "vast/save.hpp"
+#include "vast/system/indexer.hpp"
+
+namespace vast::system {
+
+indexer_manager::indexer_manager(path dir, indexer_factory f)
+  : dir_(dir),
+    make_event_indexer_(std::move(f)) {
+  // If the directory exists already, we must have some state from the past
+  // and are pre-loading all INDEXER types we are aware of.
+  if (exists(dir_)) {
+    if (auto result = load(dir_ / "meta", meta_data_)) {
+      for (auto& [str, t] : meta_data_.types)
+        indexers_.emplace(t, make_event_indexer(t));
+    } else {
+      VAST_ERROR("unable to read INDEXER types:", result.error());
+    }
+  }
+}
+
+indexer_manager::~indexer_manager() noexcept{
+  // Shutdown INDEXER actors.
+  for (auto& [t, a] : indexers_)
+    anon_send_exit(a, caf::exit_reason::user_shutdown);
+  // Save persistent state.
+  if (meta_data_.dirty) {
+    if (!exists(dir_))
+      mkdir(dir_);
+    save(dir_ / "meta", meta_data_);
+  }
+}
+
+std::pair<caf::actor, bool> indexer_manager::get_or_add(const type& key) {
+  auto i = indexers_.find(key);
+  if (i != indexers_.end())
+    return {i->second, false};
+  if (!meta_data_.dirty)
+    meta_data_.dirty = true;
+  auto digest = to_digest(key);
+  meta_data_.types.emplace(digest, key);
+  auto res = indexers_.emplace(key, make_event_indexer(key, digest));
+  VAST_ASSERT(res.second == true);
+  return {res.first->second, true};
+}
+
+std::vector<type> indexer_manager::types() const {
+  std::vector<type> result;
+  result.reserve(indexers_.size());
+  for (auto& [t, a] : indexers_)
+    result.emplace_back(t);
+  return result;
+}
+
+caf::actor indexer_manager::make_event_indexer(const type& key,
+                                               std::string digest) {
+  VAST_ASSERT(make_event_indexer_ != nullptr);
+  return make_event_indexer_(dir_ / digest, key);
+}
+
+caf::actor indexer_manager::make_event_indexer(const type& key) {
+  return make_event_indexer(key, to_digest(key));
+}
+
+std::string indexer_manager::to_digest(const type& x) {
+  return to_string(std::hash<type>{}(x));
+}
+
+indexer_manager_ptr make_indexer_manager(path dir,
+                                         indexer_manager::indexer_factory f) {
+  return caf::make_counted<indexer_manager>(std::move(dir), std::move(f));
+}
+
+indexer_manager_ptr make_indexer_manager(caf::local_actor* self, path dir) {
+  auto f = [self](path indexer_path, type indexer_type) {
+    VAST_DEBUG(self, "creates event-indexer for type", indexer_type);
+    return self->spawn<caf::lazy_init>(event_indexer, std::move(indexer_path),
+                                       std::move(indexer_type));
+  };
+  return make_indexer_manager(std::move(dir), f);
+}
+
+} // namespace vast::system
---
+++ libvast/vast/system/indexer_manager.hpp
@@ -0,0 +1,120 @@
+/******************************************************************************
+ *                    _   _____   __________                                  *
+ *                   | | / / _ | / __/_  __/     Visibility                   *
+ *                   | |/ / __ |_\ \  / /          Across                     *
+ *                   |___/_/ |_/___/ /_/       Space and Time                 *
+ *                                                                            *
+ * This file is part of VAST. It is subject to the license terms in the       *
+ * LICENSE file found in the top-level directory of this distribution and at  *
+ * http://vast.io/license. No part of VAST, including this file, may be       *
+ * copied, modified, propagated, or distributed except according to the terms *
+ * contained in the LICENSE file.                                             *
+ ******************************************************************************/
+
+#pragma once
+
+#include <caf/actor.hpp>
+#include <caf/fwd.hpp>
+#include <caf/ref_counted.hpp>
+
+#include "vast/detail/assert.hpp"
+#include "vast/expression.hpp"
+#include "vast/expression.hpp"
+#include "vast/expression_visitors.hpp"
+#include "vast/filesystem.hpp"
+#include "vast/logger.hpp"
+#include "vast/type.hpp"
+
+namespace vast::system {
+
+/// Manages a set of indexers.
+class indexer_manager : public caf::ref_counted {
+public:
+  /// Persistent meta state for manager instances.
+  struct meta_data {
+    /// Maps type digests (used as directory name) to types.
+    std::map<std::string, type> types;
+
+    /// Stores whether we modified `types` after loading it.
+    bool dirty = false;
+  };
+
+  using indexer_factory = std::function<caf::actor (path, type)>;
+
+  indexer_manager(path dir, indexer_factory f);
+
+  ~indexer_manager() noexcept override;
+
+  /// Applies all matching INDEXER actors for `expr` to `f` and returns the
+  /// number of type matches.
+  template <class F>
+  size_t for_each_match(const expression& expr, F f) const {
+    size_t num = 0;
+    for (auto& [t, a] : indexers_) {
+      VAST_ASSERT(a != nullptr);
+      auto resolved = visit(type_resolver{t}, expr);
+      if (resolved && visit(matcher{t}, *resolved)) {
+        VAST_DEBUG("found matching type for expression:", t);
+        f(a);
+        ++num;
+      }
+    }
+    return num;
+  }
+
+  /// Applies all matching INDEXER actors for `expr` to `f` and returns the
+  /// number of type matches.
+  template <class F>
+  void for_each(F f) const {
+    for (auto& [t, a] : indexers_) {
+      VAST_ASSERT(a != nullptr);
+      f(a);
+    }
+  }
+
+  /// Adds an INDEXER to the manager if no INDEXER is assigned to `key` yet.
+  /// @returns The INDEXER assigned to `key` and whether the INDEXER was
+  ///          newly added.
+  std::pair<caf::actor, bool> get_or_add(const type& key);
+
+  inline bool dirty() const noexcept {
+    return meta_data_.dirty;
+  }
+
+  /// Returns a list of all types known by the manager.
+  std::vector<type> types() const;
+
+private:
+  caf::actor make_event_indexer(const type& key, std::string digest);
+
+  caf::actor make_event_indexer(const type& key);
+
+  static std::string to_digest(const type& x);
+
+  std::unordered_map<type, caf::actor> indexers_;
+  meta_data meta_data_;
+  path dir_;
+  indexer_factory make_event_indexer_;
+};
+
+/// @relates indexer_manager::meta_data
+template <class Inspector>
+auto inspect(Inspector& f, indexer_manager::meta_data& x) {
+  return f(x.types);
+}
+
+using indexer_manager_ptr = caf::intrusive_ptr<indexer_manager>;
+
+/// Creates an indexer manager.
+indexer_manager_ptr make_indexer_manager(path dir,
+                                         indexer_manager::indexer_factory f);
+
+/// Creates an indexer manager that spawns `event_indexer` instances as
+/// children of `self`.
+/// @param `self` The parent actor.
+/// @warning `self` must outlive the returned indexer manager and no other
+///           actor (or thread) may acquire non-const access to the returned
+///           indexer manager.
+indexer_manager_ptr make_indexer_manager(caf::local_actor* self, path dir);
+
+} // namespace vast::system
