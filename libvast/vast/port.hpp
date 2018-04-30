d95254237a495393b2f2baf6c4c18696903b56b5
Matthias Vallentin <vallentin@icir.org>
2018-04-30 15:35:27 +0200

    Create a well-defined order over port types

--- libvast/vast/port.hpp
+++ libvast/vast/port.hpp
@@ -30,7 +30,12 @@ public:
   using number_type = uint16_t;
 
   /// The transport layer type.
-  enum port_type : uint8_t { unknown, tcp, udp, icmp };
+  enum port_type : uint8_t {
+    unknown = 0,
+    tcp,
+    udp,
+    icmp
+  };
 
   /// Constructs the empty port, i.e., @c 0/unknown.
   port() = default;
