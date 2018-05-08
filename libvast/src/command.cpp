6d5f60afab6c7e24195e0d7edfed7b4e5393be05
Sebastian Wölke <Sebastian.Woelke@posteo.de>
2018-05-08 11:59:00 +0200

    Clean-up command
    Remove commented out legacy code
    Improve const correctness

--- libvast/src/command.cpp
+++ libvast/src/command.cpp
@@ -31,59 +31,6 @@ command::~command() {
   // nop
 }
 
-//int command::run(caf::actor_system& sys, XXoption_mapXX& options,
-                 //argument_iterator begin, argument_iterator end) {
-  //VAST_TRACE(VAST_ARG(options), VAST_ARG("args", begin, end));
-  //// Split the arguments.
-  //auto args = caf::message_builder{begin, end}.move_to_message();
-  //auto [local_args, subcmd, subcmd_args] = separate_args(args);
-  //begin += local_args.size();
-  //// Parse arguments for this command.
-  //auto res = local_args.extract_opts(opts_);
-  //if (res.opts.count("help") != 0) {
-    //std::cout << res.helptext << std::endl;
-    //if (!nested_.empty()) {
-      //std::cout << "\nSubcommands:\n";
-      //for (auto& kvp : nested_)
-        //std::cout << "  " << kvp.first << "\n";
-    //}
-    //std::cout << std::endl;
-    //return EXIT_SUCCESS;
-  //}
-  //// Populate the map with our key/value pairs for all options.
-  //for (auto& kvp : kvps_)
-    //options.emplace(kvp());
-  //// Check whether the options allow for further processing.
-  //switch (proceed(sys, options, begin, end)) {
-    //default:
-        //// nop
-        //break;
-    //case stop_successful:
-      //return EXIT_SUCCESS;
-    //case stop_with_error:
-      //return EXIT_FAILURE;
-  //}
-  //// Invoke run_impl if no subcommand was defined.
-  //if (subcmd.empty()) {
-    //VAST_ASSERT(subcmd_args.empty());
-    //return run_impl(sys, options, begin, end);
-  //}
-  //// Consume CLI arguments if we have arguments but don't have subcommands.
-  //if (nested_.empty()) {
-    //return run_impl(sys, options, begin, end);
-  //}
-  //// Dispatch to subcommand.
-  //auto i = nested_.find(subcmd);
-  //if (i == nested_.end()) {
-    //std::cerr << "no such command: " << full_name() << " " << subcmd
-      //<< std::endl
-      //<< std::endl;
-    //usage();
-    //return EXIT_FAILURE;
-  //}
-  //return i->second->run(sys, options, begin + 1, end);
-//}
-
 int command::run(caf::actor_system& sys, option_map& options,
                      argument_iterator begin, argument_iterator end) {
   VAST_TRACE(VAST_ARG(std::string(name_)), VAST_ARG("args", begin, end),
@@ -140,19 +87,13 @@ int command::run(caf::actor_system& sys, option_map& options,
   return i->second->run(sys, options, position + 1, end);
 }
 
-//int command::run(caf::actor_system& sys, argument_iterator begin,
-                 //argument_iterator end) {
-  //XXoption_mapXX options;
-  //return run(sys, options, begin, end);
-//}
-
 int command::run(caf::actor_system& sys, argument_iterator begin,
                  argument_iterator end) {
   option_map options;
   return run(sys, options, begin, end);
 }
 
-std::string command::usage() {
+std::string command::usage() const {
   std::stringstream result;
   result << opts_.usage() << "\n";
   result << "\nSubcommands:\n";
@@ -161,7 +102,7 @@ std::string command::usage() {
   return result.str();
 }
 
-std::string command::full_name() {
+std::string command::full_name() const {
   std::string result;
   if (is_root())
     return result;
@@ -172,22 +113,9 @@ std::string command::full_name() {
   return result;
 }
 
-std::string command::name() {
-  return std::string{name_};
-}
-
 bool command::is_root() const noexcept {
   return parent_ == nullptr;
 }
-
-//command::proceed_result command::proceed(caf::actor_system&,
-                                         //XXoption_mapXX& options,
-                                         //argument_iterator begin,
-                                         //argument_iterator end) {
-  //VAST_UNUSED(options, begin, end);
-  //VAST_TRACE(VAST_ARG(options), VAST_ARG("args", begin, end));
-  //return proceed_ok;
-//}
 
 command::proceed_result command::proceed(caf::actor_system&,
                                          option_map& options,
@@ -199,14 +127,6 @@ command::proceed_result command::proceed(caf::actor_system&,
   return proceed_ok;
 }
 
-//int command::run_impl(caf::actor_system&, XXoption_mapXX& options,
-                      //argument_iterator begin, argument_iterator end) {
-  //VAST_UNUSED(options, begin, end);
-  //VAST_TRACE(VAST_ARG(options), VAST_ARG("args", begin, end));
-  //usage();
-  //return EXIT_FAILURE;
-//}
-
 int command::run_impl(caf::actor_system&, option_map& options,
                       argument_iterator begin, argument_iterator end) {
   VAST_UNUSED(options, begin, end);
@@ -216,31 +136,9 @@ int command::run_impl(caf::actor_system&, option_map& options,
   return EXIT_FAILURE;
 }
 
-//std::tuple<caf::message, std::string, caf::message>
-//command::separate_args(const caf::message& args) {
-  //auto arg = [&](size_t i) -> const std::string& {
-    //VAST_ASSERT(args.match_element<std::string>(i));
-    //return args.get_as<std::string>(i);
-  //};
-  //size_t pos = 0;
-  //while (pos < args.size()) {
-    //if (arg(pos).compare(0, 2, "--") == 0) {
-      //// Simply skip over long options.
-      //++pos;
-    //} else if (arg(pos).compare(0, 1, "-") == 0) {
-      //// We assume short options always have an argument.
-      //// TODO: we could look into the argument instead of just assuming it
-      ////       always take an argument.
-      //pos += 2;
-    //} else {
-      //// Found the end of the options list.
-      //return std::make_tuple(args.take(pos), arg(pos), args.drop(pos+ 1));
-    //}
-  //}
-  //return std::make_tuple(args, "", caf::none);
-//}
-
-expected<void> command::add_opt(std::string_view name, std::string_view description, data default_value) {
+expected<void> command::add_opt(std::string_view name,
+                                std::string_view description,
+                                data default_value) {
   return opts_.add(name, description, std::move(default_value));
 }
 
--- libvast/vast/command.hpp
+++ libvast/vast/command.hpp
@@ -41,10 +41,6 @@ class command {
 public:
   // -- member types -----------------------------------------------------------
 
-  // TODO #### remove me
-  /// Maps names of config parameters to their value.
-  //using XXoption_mapXX = std::map<std::string, caf::config_value>;
-
   /// Iterates over CLI arguments.
   using argument_iterator = std::vector<std::string>::const_iterator;
 
@@ -63,18 +59,6 @@ public:
 
   virtual ~command();
 
-  // TODO #### remove me
-  /// Runs the command and blocks until execution completes.
-  /// @returns An exit code suitable for returning from main.
-  //int run(caf::actor_system& sys, argument_iterator begin,
-          //argument_iterator end);
-
-  // TODO #### remove me
-  /// Runs the command and blocks until execution completes.
-  /// @returns An exit code suitable for returning from main.
-  //int run(caf::actor_system& sys, XXoption_mapXX& options,
-          //argument_iterator begin, argument_iterator end);
-
   /// Runs the command and blocks until execution completes.
   /// @returns An exit code suitable for returning from main.
   int run(caf::actor_system& sys, argument_iterator begin,
@@ -87,13 +71,10 @@ public:
 
 
   /// Creates a summary of all option declarations and available commands.
-  std::string usage();
+  std::string usage() const;
 
   /// Returns the full name for this command.
-  std::string full_name();
-
-  /// Returns the name for this command.
-  std::string name();
+  std::string full_name() const;
 
   /// Queries whether this command has no parent.
   bool is_root() const noexcept;
@@ -103,11 +84,10 @@ public:
     return is_root() ? *this : parent_->root();
   }
 
-  inline std::string_view name() const noexcept {
+  std::string_view name() const noexcept {
     return name_;
   }
 
-  // TODO update doxygen comments
   /// Defines a sub-command.
   /// @param name The name of the command.
   /// @param xs The parameters required to construct the command.
@@ -122,50 +102,7 @@ public:
     return result;
   }
 
-  // TODO #### remove me
-  //template <class T>
-  //caf::optional<T> get(const XXoption_mapXX& xs, const std::string& name) {
-    //// Map T to the clostest type in config_value.
-    //using cfg_type =
-      //typename std::conditional<
-        //std::is_integral_v<T> && !std::is_same_v<bool, T>,
-        //int64_t,
-        //typename std::conditional<
-          //std::is_floating_point_v<T>,
-          //double,
-          //T
-          //>::type
-        //>::type;
-    //auto i = xs.find(name);
-    //if (i == xs.end())
-      //return caf::none;
-    //auto result = caf::get_if<cfg_type>(&i->second);
-    //if (!result)
-      //return caf::none;
-    //return static_cast<T>(*result);
-  //}
-
-  // TODO #### remove me
-  //template <class T>
-  //T get_or(const XXoption_mapXX& xs, const std::string& name, T fallback) {
-    //auto result = get<T>(xs, name);
-    //if (!result)
-      //return fallback;
-    //return *result;
-  //}
-
 protected:
-  // TODO #### remove me
-  /// Checks whether a command is ready to proceed, i.e., whether the
-  /// configuration allows for calling `run_impl` or `run` on a nested command.
-  //virtual proceed_result proceed(caf::actor_system& sys, XXoption_mapXX& options,
-                                 //argument_iterator begin,
-                                 //argument_iterator end);
-
-  // TODO #### remove me
-  //virtual int run_impl(caf::actor_system& sys, XXoption_mapXX& options,
-                       //argument_iterator begin, argument_iterator end);
-
   /// Checks whether a command is ready to proceed, i.e., whether the
   /// configuration allows for calling `run_impl` or `run` on a nested command.
   virtual proceed_result proceed(caf::actor_system& sys, option_map& options,
@@ -175,56 +112,15 @@ protected:
   virtual int run_impl(caf::actor_system& sys, option_map& options,
                        argument_iterator begin, argument_iterator end);
 
-  // TODO #### remove me
-  //template <class T>
-  //void add_opt(std::string name, std::string descr, T& ref) {
-    //opts_.emplace_back(name, std::move(descr), ref);
-    //// Extract the long name from the full name (format: "long,l").
-    //auto pos = name.find_first_of(',');
-    //if (pos < name.size())
-      //name.resize(pos);
-    //kvps_.emplace_back([name = std::move(name), &ref] {
-      //// Map T to the clostest type in config_value.
-      //using cfg_type =
-        //typename std::conditional<
-          //std::is_integral_v<T> && !std::is_same_v<bool, T>,
-          //int64_t,
-          //typename std::conditional<
-            //std::is_floating_point_v<T>,
-            //double,
-            //T
-            //>::type
-          //>::type;
-      //cfg_type copy = ref;
-      //return std::make_pair(name, caf::config_value{std::move(copy)});
-    //});
-  //}
-
   expected<void> add_opt(std::string_view name,
                              std::string_view description, data default_value);
 
 private:
-  // TODO #### remove me
-  /// Separates arguments into the arguments for the current command, the name
-  /// of the subcommand, and the arguments for the subcommand.
-  //std::tuple<caf::message, std::string, caf::message>
-  //separate_args(const caf::message& args);
-
   std::map<std::string_view, std::unique_ptr<command>> nested_;
   command* parent_;
 
-  // TODO: string_view does not have the ownership of the string.
-  // Check whether this is ok here.
   /// The user-provided name.
   std::string_view name_;
-
-  // TODO #### remove me
-  /// List of all accepted options.
-  //std::vector<caf::message::cli_arg> opts_;
-
-  // TODO #### remove me
-  /// List of function objects that return CLI options as name/value pairs.
-  //std::vector<std::function<std::pair<std::string, caf::config_value>()>> kvps_;
 
   /// List of all accepted options.
   option_declaration_set opts_;
