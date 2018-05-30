571cd7e33553e99cc96b236499b769fe2d82230a
Dominik Charousset <dominik.charousset@tenzir.com>
2018-05-30 10:40:30 +0200

    Test add, lookup and flush of column indexes

--- libvast/test/system/column_index.cpp
+++ libvast/test/system/column_index.cpp
@@ -17,8 +17,12 @@
 #include "fixtures/events.hpp"
 #include "fixtures/filesystem.hpp"
 
+#include "vast/concept/parseable/to.hpp"
+#include "vast/concept/parseable/vast/expression.hpp"
 #include "vast/system/column_index.hpp"
+#include "vast/type.hpp"
 
+using namespace vast;
 using namespace vast::system;
 
 namespace {
@@ -28,78 +32,45 @@ struct fixture : fixtures::events, fixtures::filesystem {
     directory /= "column-index";
   }
 
-  void add_col(caf::expected<column_index_ptr> x) {
+  template <class T>
+  T unbox(expected<T> x) {
     REQUIRE(x);
-    cols.emplace_back(std::move(*x));
+    return std::move(*x);
   }
-
-  std::vector<column_index_ptr> cols;
 };
 
 } // namespace <anonymous>
 
 FIXTURE_SCOPE(column_index_tests, fixture)
 
-TEST(todo) {
-  MESSAGE("generate column layout for bro conn logs");
-  const auto conn_log_type = bro_conn_log[0].type();
-  /*
-  auto ecols = column_index::make_default_indexes(directory, bro_conn_log);
-  REQUIRE(ecols);
-  auto cols = std::move(*ecols);
-  MESSAGE("ingesting events");
-  for (auto& entry : bro_conn_log)
-    add_to_index(cols, entry);
-  // Event indexers operate with predicates, whereas partitions take entire
-  // expressions.
-  MESSAGE("querying data using predicate 'id.resp_p == 995/?'");
-  auto pred = to<predicate>("id.resp_p == 995/?");
-  REQUIRE(pred);
-  bitmap result;
-  self->request(i, infinite, *pred).receive(
-    [&](bitmap& bm) {
-      result = std::move(bm);
-    },
-    error_handler()
-  );
-  CHECK_EQUAL(rank(result), 53u);
-  auto check_uid = [](const event& e, const std::string& uid) {
-    auto& v = get<vector>(e.data());
-    return v[1] == uid;
-  };
-  for (auto i : select(result))
-    if (i == 819)
-      CHECK(check_uid(bro_conn_log[819], "KKSlmtmkkxf")); // first
-    else if (i == 3594)
-      CHECK(check_uid(bro_conn_log[3594], "GDzpFiROJQi")); // intermediate
-    else if (i == 6338)
-      CHECK(check_uid(bro_conn_log[6338], "zwCckCCgXDb")); // last
-  MESSAGE("shutting down indexer");
-  self->send(i, system::shutdown_atom::value);
-  self->wait_for(i);
-  CHECK(exists(directory));
-  CHECK(exists(directory / "data" / "id" / "orig_h"));
-  CHECK(exists(directory / "meta" / "time"));
-  MESSAGE("respawning indexer from file system");
-  i = self->spawn(system::event_indexer, directory, conn_log_type);
-  // Same as above: submit the query and verify the result.
-  self->request(i, infinite, *pred).receive(
-    [&](bitmap& bm) {
-      CHECK_EQUAL(rank(bm), 53u);
-    },
-    error_handler()
-  );
-  // Test another query that involves a map-reduce computation of value
-  // indexers.
-  pred = to<predicate>(":addr == 65.55.184.16");
-  REQUIRE(pred);
-  self->request(i, infinite, *pred).receive(
-    [&](const bitmap& bm) {
-      CHECK_EQUAL(rank(bm), 2u);
-    },
-    error_handler()
-  );
-  */
+TEST(flat type) {
+  MESSAGE("ingest integer values");
+  auto col = unbox(make_flat_data_index(directory, integer_type{}));
+  std::vector<int> xs{1, 2, 3, 1, 2, 3, 1, 2, 3};
+  for (size_t i = 0; i < xs.size(); ++i) {
+    event x{xs[i]};
+    x.id(i);
+    col->add(std::move(x));
+  }
+  MESSAGE("generate test queries");
+  auto is1 = unbox(to<predicate>(":int == +1"));
+  auto is2 = unbox(to<predicate>(":int == +2"));
+  auto is3 = unbox(to<predicate>(":int == +3"));
+  auto is4 = unbox(to<predicate>(":int == +4"));
+  MESSAGE("verify column index");
+  CHECK_EQUAL(unbox(col->lookup(is1)), make_ids({0, 3, 6}, xs.size()));
+  CHECK_EQUAL(unbox(col->lookup(is2)), make_ids({1, 4, 7}, xs.size()));
+  CHECK_EQUAL(unbox(col->lookup(is3)), make_ids({2, 5, 8}, xs.size()));
+  CHECK_EQUAL(unbox(col->lookup(is4)), make_ids({}, xs.size()));
+  MESSAGE("persist and reload from disk");
+  col->flush_to_disk();
+  col.reset();
+  col = unbox(make_flat_data_index(directory, integer_type{}));
+  MESSAGE("verify column index again");
+  CHECK_EQUAL(unbox(col->lookup(is1)), make_ids({0, 3, 6}, xs.size()));
+  CHECK_EQUAL(unbox(col->lookup(is2)), make_ids({1, 4, 7}, xs.size()));
+  CHECK_EQUAL(unbox(col->lookup(is3)), make_ids({2, 5, 8}, xs.size()));
+  CHECK_EQUAL(unbox(col->lookup(is4)), make_ids({}, xs.size()));
 }
 
 FIXTURE_SCOPE_END()
