6e249c9713a6201fdce54396abd9745202da93a5
Dominik Charousset <dominik.charousset@tenzir.com>
2018-06-02 18:50:49 +0200

    Add factory for creating events with ID

--- libvast/vast/event.hpp
+++ libvast/vast/event.hpp
@@ -38,6 +38,20 @@ public:
     return value::make(std::forward<T>(x), std::move(t));
   }
 
+  /// Type-safe factory function to construct an event from data and type with
+  /// an ID.
+  /// @tparam A type convertible to a value.
+  /// @param x An instance of type `T`.
+  /// @param t The type of the value.
+  /// @param i The ID of the event.
+  /// @returns A valid event if *t* can successfully check *x*.
+  template <class T>
+  static event make(T&& x, vast::type t, vast::id i) {
+    event result = value::make(std::forward<T>(x), std::move(t));
+    result.id(i);
+    return result;
+  }
+
   /// Type-safe factory function to construct an event from an unchecked value.
   /// @param v The value to check and convert into an event.
   /// @returns A valid event according *v* if `v.type().check(v.data())`.
