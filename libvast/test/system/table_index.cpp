800e227f37e329d0cd1313133060d92591633e2e
Dominik Charousset <dominik.charousset@tenzir.com>
2018-06-02 12:16:41 +0200

    Streamline and improve unit test

--- libvast/test/system/table_index.cpp
+++ libvast/test/system/table_index.cpp
@@ -32,15 +32,32 @@ using namespace vast::system;
 namespace {
 
 struct fixture : fixtures::events, fixtures::filesystem {
-  fixture() {
-    directory /= "column-layout";
-  }
-
   template <class T>
   T unbox(expected<T> x) {
-    REQUIRE(x);
+    if (!x)
+      FAIL("error: " << x.error());
     return std::move(*x);
   }
+
+  ids query(std::string_view what) {
+    return unbox(tbl->lookup(unbox(to<expression>(what))));
+  }
+
+  void reset(table_index&& new_tbl) {
+    tbl = std::make_unique<table_index>(std::move(new_tbl));
+  }
+
+  void reset(expected<table_index>&& new_tbl) {
+    if (!new_tbl)
+      FAIL("error: " << new_tbl.error());
+    reset(std::move(*new_tbl));
+  }
+
+  void add(event x) {
+    tbl->add(std::move(x));
+  }
+
+  std::unique_ptr<table_index> tbl;
 };
 
 } // namespace <anonymous>
@@ -48,40 +65,40 @@ struct fixture : fixtures::events, fixtures::filesystem {
 FIXTURE_SCOPE(table_index_tests, fixture)
 
 TEST(flat type) {
-  // Some test data.
+  MESSAGE("generate table layout for flat integer type");
+  reset(make_table_index(directory, integer_type{}));
+  MESSAGE("ingest test data (integers)");
   std::vector<int> xs{1, 2, 3, 1, 2, 3, 1, 2, 3};
-  MESSAGE("generate test queries");
-  auto is1 = unbox(to<predicate>(":int == +1"));
-  auto is2 = unbox(to<predicate>(":int == +2"));
-  auto is3 = unbox(to<predicate>(":int == +3"));
-  auto is4 = unbox(to<predicate>(":int == +4"));
-  { // lifetime of the first table index
-    auto tbl = unbox(make_table_index(directory, integer_type{}));
-    MESSAGE("ingest integer values");
-    for (size_t i = 0; i < xs.size(); ++i) {
-      event x{xs[i]};
-      x.id(i);
-      tbl.add(std::move(x));
-    }
-    MESSAGE("verify table index");
-    CHECK_EQUAL(unbox(tbl.lookup(is1)), make_ids({0, 3, 6}, xs.size()));
-    CHECK_EQUAL(unbox(tbl.lookup(is2)), make_ids({1, 4, 7}, xs.size()));
-    CHECK_EQUAL(unbox(tbl.lookup(is3)), make_ids({2, 5, 8}, xs.size()));
-    CHECK_EQUAL(unbox(tbl.lookup(is4)), make_ids({}, xs.size()));
-    MESSAGE("(automatically) persist table index to disk");
+  for (size_t i = 0; i < xs.size(); ++i) {
+    event x{xs[i]};
+    x.id(i);
+    add(std::move(x));
   }
-  { // lifetime scope of the second table index
-    MESSAGE("restore table index from disk");
-    auto tbl = unbox(make_table_index(directory, integer_type{}));
-    MESSAGE("verify table index again");
-    CHECK_EQUAL(unbox(tbl.lookup(is1)), make_ids({0, 3, 6}, xs.size()));
-    CHECK_EQUAL(unbox(tbl.lookup(is2)), make_ids({1, 4, 7}, xs.size()));
-    CHECK_EQUAL(unbox(tbl.lookup(is3)), make_ids({2, 5, 8}, xs.size()));
-    CHECK_EQUAL(unbox(tbl.lookup(is4)), make_ids({}, xs.size()));
-  }
+  auto res = [&](auto... args) {
+    return make_ids({args...}, xs.size());
+  };
+  MESSAGE("verify table index");
+  CHECK_EQUAL(query(":int == +1"), res(0, 3, 6));
+  CHECK_EQUAL(query(":int == +2"), res(1, 4, 7));
+  CHECK_EQUAL(query(":int == +3"), res(2, 5, 8));
+  CHECK_EQUAL(query(":int == +4"), res());
+  CHECK_EQUAL(query(":int != +1"), res(1, 2, 4, 5, 7, 8));
+  CHECK_EQUAL(query("!(:int == +1)"), res(1, 2, 4, 5, 7, 8));
+  CHECK_EQUAL(query(":int > +1 && :int < +3"), res(1, 4, 7));
+  MESSAGE("(automatically) persist table index and restore from disk");
+  reset(make_table_index(directory, integer_type{}));
+  MESSAGE("verify table index again");
+  CHECK_EQUAL(query(":int == +1"), res(0, 3, 6));
+  CHECK_EQUAL(query(":int == +2"), res(1, 4, 7));
+  CHECK_EQUAL(query(":int == +3"), res(2, 5, 8));
+  CHECK_EQUAL(query(":int == +4"), res());
+  CHECK_EQUAL(query(":int != +1"), res(1, 2, 4, 5, 7, 8));
+  CHECK_EQUAL(query("!(:int == +1)"), res(1, 2, 4, 5, 7, 8));
+  CHECK_EQUAL(query(":int > +1 && :int < +3"), res(1, 4, 7));
 }
 
 TEST(record type) {
+  MESSAGE("generate table layout for record type");
   auto tbl_type = record_type{
     {"x", record_type{
       {"a", integer_type{}},
@@ -91,88 +108,68 @@ TEST(record type) {
       {"a", string_type{}}
     }}
   };
+  reset(make_table_index(directory, tbl_type));
+  MESSAGE("ingest test data (records)");
   auto mk_row = [&](int x, bool y, std::string z) {
     return value::make(vector{vector{x, y}, vector{std::move(z)}}, tbl_type);
   };
   // Some test data.
-  std::vector<value> xs{mk_row(1, true, "abc"),    mk_row(10, false, "def"),
-                        mk_row(5, true, "hello"),  mk_row(1, true, "d e f"),
-                        mk_row(15, true, "world"), mk_row(5, true, "bar"),
-                        mk_row(10, true, "a b c"), mk_row(10, true, "baz"),
-                        mk_row(5, true, "foo"),    mk_row(1, true, "test")};
-  MESSAGE(caf::deep_to_string(xs));
-  MESSAGE("generate test queries");
-  auto x_a_is1 = unbox(to<predicate>("x.a == +1"));
-  { // lifetime of the first table index
-    auto tbl = unbox(make_table_index(directory, tbl_type));
-    CHECK_EQUAL(tbl.num_data_columns(), 3u);
-    MESSAGE("ingest table values");
-    for (size_t i = 0; i < xs.size(); ++i) {
-      event x{xs[i]};
-      x.id(i);
-      MESSAGE("ingest" <<  caf::deep_to_string(x));
-      tbl.add(std::move(x));
-    }
-    MESSAGE("verify table index");
-    CHECK_EQUAL(unbox(tbl.lookup(x_a_is1)), make_ids({0, 3, 9}, xs.size()));
-    MESSAGE("(automatically) persist table index to disk");
+  std::vector<value> xs{mk_row(1, true, "abc"),     mk_row(10, false, "def"),
+                        mk_row(5, true, "hello"),   mk_row(1, true, "d e f"),
+                        mk_row(15, true, "world"),  mk_row(5, true, "bar"),
+                        mk_row(10, false, "a b c"), mk_row(10, false, "baz"),
+                        mk_row(5, false, "foo"),    mk_row(1, true, "test")};
+  for (size_t i = 0; i < xs.size(); ++i) {
+    event x{xs[i]};
+    x.id(i);
+    add(std::move(x));
   }
-  { // lifetime scope of the second table index
-    MESSAGE("restore table index from disk");
-    auto tbl = unbox(make_table_index(directory, tbl_type));
-    MESSAGE("verify table index again");
-    CHECK_EQUAL(unbox(tbl.lookup(x_a_is1)), make_ids({0, 3, 9}, xs.size()));
-  }
+  auto res = [&](auto... args) {
+    return make_ids({args...}, xs.size());
+  };
+  MESSAGE("verify table index");
+  CHECK_EQUAL(query("x.a == +1"), res(0, 3, 9));
+  CHECK_EQUAL(query("x.a > +1"), res(1, 2, 4, 5, 6, 7, 8));
+  CHECK_EQUAL(query("x.a > +1 && x.b == T"), res(2, 4, 5));
+  MESSAGE("(automatically) persist table index and restore from disk");
+  reset(make_table_index(directory, tbl_type));
+  MESSAGE("verify table index again");
+  CHECK_EQUAL(query("x.a == +1"), res(0, 3, 9));
+  CHECK_EQUAL(query("x.a > +1"), res(1, 2, 4, 5, 6, 7, 8));
+  CHECK_EQUAL(query("x.a > +1 && x.b == T"), res(2, 4, 5));
 }
 
 TEST(bro conn logs) {
-  auto pred = unbox(to<predicate>("id.resp_p == 995/?"));
-  const auto tbl_type = bro_conn_log[0].type();
-  { // lifetime of the first table index
-    MESSAGE("generate column layout for bro conn logs");
-    auto tbl = unbox(make_table_index(directory, tbl_type));
-    CHECK_EQUAL(tbl.num_meta_columns(), 2u);
-    MESSAGE("ingesting events");
-    for (auto& entry : bro_conn_log) {
-      auto err = tbl.add(entry);
-      if (err) {
-        FAIL("error during ingestion: " << caf::to_string(err));
-      }
-    }
-    MESSAGE("verify table index");
-    auto result = unbox(tbl.lookup(pred));
-    CHECK_EQUAL(rank(result), 53u);
-    auto check_uid = [](const event& e, const std::string& uid) {
-      auto& v = get<vector>(e.data());
-      return v[1] == uid;
-    };
-    for (auto i : select(result))
-      if (i == 819)
-        CHECK(check_uid(bro_conn_log[819], "KKSlmtmkkxf")); // first
-      else if (i == 3594)
-        CHECK(check_uid(bro_conn_log[3594], "GDzpFiROJQi")); // intermediate
-      else if (i == 6338)
-        CHECK(check_uid(bro_conn_log[6338], "zwCckCCgXDb")); // last
-    MESSAGE("(automatically) persist table index to disk");
+  MESSAGE("generate table layout for bro conn logs");
+  auto tbl_type = bro_conn_log[0].type();
+  reset(make_table_index(directory, tbl_type));
+  CHECK_EQUAL(tbl->num_meta_columns(), 2u);
+  MESSAGE("ingest test data (bro conn log)");
+  for (auto& entry : bro_conn_log) {
+    add(entry);
   }
-  { // lifetime scope of the second table index
-    MESSAGE("restore table index from disk");
-    auto tbl = unbox(make_table_index(directory, tbl_type));
-    MESSAGE("verify table index again");
-    auto result = unbox(tbl.lookup(pred));
-    CHECK_EQUAL(rank(result), 53u);
-    auto check_uid = [](const event& e, const std::string& uid) {
-      auto& v = get<vector>(e.data());
-      return v[1] == uid;
-    };
-    for (auto i : select(result))
-      if (i == 819)
-        CHECK(check_uid(bro_conn_log[819], "KKSlmtmkkxf")); // first
-      else if (i == 3594)
-        CHECK(check_uid(bro_conn_log[3594], "GDzpFiROJQi")); // intermediate
-      else if (i == 6338)
-        CHECK(check_uid(bro_conn_log[6338], "zwCckCCgXDb")); // last
-  }
+  MESSAGE("verify table index");
+  CHECK_EQUAL(rank(query("id.resp_p == 995/?")), 53u);
+  CHECK_EQUAL(rank(query("id.resp_p == 5355/?")), 49u);
+  CHECK_EQUAL(rank(query("id.resp_p == 995/? || id.resp_p == 5355/?")), 102u);
+  CHECK_EQUAL(rank(query("&time > 1970-01-01")), bro_conn_log.size());
+  CHECK_EQUAL(rank(query("proto == \"udp\"")), 5306u);
+  CHECK_EQUAL(rank(query("proto == \"tcp\"")), 3135u);
+  CHECK_EQUAL(rank(query("uid == \"nkCxlvNN8pi\"")), 1u);
+  CHECK_EQUAL(rank(query("orig_bytes < 400")), 5332u);
+  CHECK_EQUAL(rank(query("orig_bytes < 400 && proto == \"udp\"")), 4357u);
+  MESSAGE("(automatically) persist table index and restore from disk");
+  reset(make_table_index(directory, tbl_type));
+  MESSAGE("verify table index again");
+  CHECK_EQUAL(rank(query("id.resp_p == 995/?")), 53u);
+  CHECK_EQUAL(rank(query("id.resp_p == 5355/?")), 49u);
+  CHECK_EQUAL(rank(query("id.resp_p == 995/? || id.resp_p == 5355/?")), 102u);
+  CHECK_EQUAL(rank(query("&time > 1970-01-01")), bro_conn_log.size());
+  CHECK_EQUAL(rank(query("proto == \"udp\"")), 5306u);
+  CHECK_EQUAL(rank(query("proto == \"tcp\"")), 3135u);
+  CHECK_EQUAL(rank(query("uid == \"nkCxlvNN8pi\"")), 1u);
+  CHECK_EQUAL(rank(query("orig_bytes < 400")), 5332u);
+  CHECK_EQUAL(rank(query("orig_bytes < 400 && proto == \"udp\"")), 4357u);
 }
 
 FIXTURE_SCOPE_END()
