6d902b6a0895baac2609eba4e2cb3fcb699742d4
Dominik Charousset <dominik@charousset.de>
2018-07-06 19:35:43 +0200

    Add documentation to default settings

--- libvast/vast/defaults.hpp
+++ libvast/vast/defaults.hpp
@@ -21,25 +21,55 @@ namespace vast::defaults {
 
 namespace command {
 
+/// Path to persistent state.
 extern const char* directory;
+
+/// Locator for connecting to a remote node in "host:port" notation.
 extern const char* endpoint;
+
+/// Server ID for the consensus module.
 extern const char* id;
+
+/// Path for reading input events or `-` for reading from STDIN.
 extern const char* read_path;
+
+/// Path to alternate schema.
 extern const char* schema_path;
+
+/// Path for writing query results or `-` for writing to STDOUT.
 extern const char* write_path;
+
+/// Inverse factor by which to delay packets. For example, if 5, then for two
+/// packets spaced *t* seconds apart, the source will sleep for *t/5* seconds.
 extern int64_t pseudo_realtime_factor;
+
+/// Number of bytes to keep per event.
 extern size_t cutoff;
+
+/// Flow table expiration interval.
 extern size_t flow_expiry;
+
+/// Flush to disk after that many packets.
 extern size_t flush_interval;
+
+/// Maximum number of results.
 extern size_t max_events;
+
+/// Maximum flow lifetime before eviction.
 extern size_t max_flow_age;
+
+
+/// Number of concurrent flows to track.
 extern size_t max_flows;
+
+/// The unique ID of this node.
 extern std::string node_id;
 
 } // namespace command
 
 namespace system {
 
+/// Maximum size for sources that generate table slices.
 extern size_t table_slice_size;
 
 } // namespace system
