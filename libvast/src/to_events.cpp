6b6b4976299f65c2d4d6ff29fc573cf1c80d68dd
Matthias Vallentin <vallentin@icir.org>
2018-08-29 11:30:22 +0200

    Fix seek inefficiency in to_events

--- libvast/src/to_events.cpp
+++ libvast/src/to_events.cpp
@@ -57,23 +57,19 @@ std::vector<event> to_events(const table_slice& slice,
 
 void to_events(std::vector<event>& storage, const table_slice& slice,
                const ids& row_ids) {
-  auto offset = slice.offset();
-  auto end_id = offset + slice.rows();
-  auto i = select(row_ids);
-  for (; !i.done() && i.get() < offset; i.next()) {
-    // Seek first ID in slice.
-    // TODO: this is O(n), but there's currently no simple way to "jump" to a
-    //       particular position with select()
-  }
-  for (; !i.done() && i.get() < end_id; i.next()) {
-    auto event_id = i.get();
-    // TODO: inefficient, because we wrap single values into vectors
-    auto timestamps = subset(slice, event_id - offset, 1, 0, 1);
-    auto values = subset(slice, event_id - offset, 1, 1);
+  auto begin = slice.offset();
+  auto end = begin + slice.rows();
+  auto rng = select(row_ids);
+  VAST_ASSERT(rng);
+  for (rng.next_at(begin); rng && rng.get() < end; rng.next()) {
+    auto id = rng.get();
+    // FIXME: inefficient, because we wrap single values into vectors
+    auto timestamps = subset(slice, id - begin, 1, 0, 1);
+    auto values = subset(slice, id - begin, 1, 1);
     VAST_ASSERT(timestamps.size() == 1);
     VAST_ASSERT(values.size() == 1);
-    storage.emplace_back(to_event(values[0], timestamps[0],
-                                  slice.layout().name(), event_id));
+    auto& name = slice.layout().name();
+    storage.emplace_back(to_event(values[0], timestamps[0], name, id));
   }
 }
 
