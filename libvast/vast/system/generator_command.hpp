8973fced1293a5a7de3843f3b1047bc1f0e35686
Tobias Mayer <tobim@fastmail.fm>
2018-09-27 15:48:19 +0200

    Add generator command and hook up the test source

---
+++ libvast/vast/system/generator_command.hpp
@@ -0,0 +1,92 @@
+/******************************************************************************
+ *                    _   _____   __________                                  *
+ *                   | | / / _ | / __/_  __/     Visibility                   *
+ *                   | |/ / __ |_\ \  / /          Across                     *
+ *                   |___/_/ |_/___/ /_/       Space and Time                 *
+ *                                                                            *
+ * This file is part of VAST. It is subject to the license terms in the       *
+ * LICENSE file found in the top-level directory of this distribution and at  *
+ * http://vast.io/license. No part of VAST, including this file, may be       *
+ * copied, modified, propagated, or distributed except according to the terms *
+ * contained in the LICENSE file.                                             *
+ ******************************************************************************/
+
+#pragma once
+
+#include <memory>
+#include <string>
+#include <string_view>
+
+#include <caf/scoped_actor.hpp>
+#include <caf/typed_actor.hpp>
+#include <caf/typed_event_based_actor.hpp>
+
+#include "vast/concept/parseable/to.hpp"
+#include "vast/concept/parseable/vast/expression.hpp"
+#include "vast/concept/parseable/vast/schema.hpp"
+#include "vast/defaults.hpp"
+#include "vast/detail/make_io_stream.hpp"
+#include "vast/expression.hpp"
+#include "vast/logger.hpp"
+#include "vast/system/reader_command_base.hpp"
+#include "vast/system/reader_command_base.hpp"
+#include "vast/system/signal_monitor.hpp"
+#include "vast/system/source.hpp"
+#include "vast/system/tracker.hpp"
+
+namespace vast::system {
+
+/// Default implementation for import sub-commands. Compatible with Bro and MRT
+/// formats.
+/// @relates application
+template <class Generator>
+class generator_command : public reader_command_base {
+public:
+  generator_command(command* parent, std::string_view name)
+      : reader_command_base(parent, name) {
+    add_opt<std::string>("schema,s", "path to alternate schema");
+    add_opt<size_t>("seed", "the random seed");
+    add_opt<size_t>("num,N", "events to generate");
+  }
+
+protected:
+  expected<caf::actor> make_source(caf::scoped_actor& self,
+                                   const caf::config_value_map& options,
+                                   argument_iterator begin,
+                                   argument_iterator end) override {
+    VAST_TRACE(VAST_ARG("args", begin, end));
+    auto seed = get_or(options, "seed", 0u);
+    auto num = get_or(options, "num", 100u);
+    Generator generator{seed, num};
+    auto src = self->spawn(default_source<Generator>, std::move(generator));
+    // Supply an alternate schema, if requested.
+    auto schema_file = caf::get_if<std::string>(&options, "schema");
+    if (schema_file && !schema_file->empty()) {
+      auto str = load_contents(*schema_file);
+      if (!str)
+        return str.error();
+      auto sch = to<schema>(*str);
+      if (!sch)
+        return sch.error();
+      // Send anonymously, since we can't process the reply here.
+      anon_send(src, put_atom::value, std::move(*sch));
+    }
+    // Attempt to parse the remainder as an expression.
+    if (begin != end) {
+      auto str = std::accumulate(
+        std::next(begin), end, *begin,
+        [](std::string a, const std::string& b) { return a += ' ' + b; });
+      auto expr = to<expression>(str);
+      if (!expr)
+        return expr.error();
+      expr = normalize_and_validate(*expr);
+      if (!expr)
+        return expr.error();
+      anon_send(src, std::move(*expr));
+    }
+    return src;
+  }
+};
+
+} // namespace vast::system
+
