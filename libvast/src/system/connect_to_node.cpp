c90cdd747fcb85fe6dc6c3a64ee5ee1facd3a7de
Dominik Charousset <dominik@charousset.de>
2018-11-09 22:06:56 +0100

    Add missing include

--- libvast/src/system/connect_to_node.cpp
+++ libvast/src/system/connect_to_node.cpp
@@ -26,6 +26,7 @@
 
 #include "vast/concept/parseable/vast/endpoint.hpp"
 #include "vast/defaults.hpp"
+#include "vast/error.hpp"
 #include "vast/filesystem.hpp"
 #include "vast/logger.hpp"
 #include "vast/system/node.hpp"
