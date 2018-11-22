ae22d86411b17877983ee16151b4701eb9919b4b
Dominik Charousset <dominik.charousset@tenzir.com>
2018-11-22 17:20:03 +0100

    Factor out streaming-related status code

---
+++ libvast/vast/detail/fill_status_map.hpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+ *                    _   _____   __________                                  *
+ *                   | | / / _ | / __/_  __/     Visibility                   *
+ *                   | |/ / __ |_\ \  / /          Across                     *
+ *                   |___/_/ |_/___/ /_/       Space and Time                 *
+ *                                                                            *
+ * This file is part of VAST. It is subject to the license terms in the       *
+ * LICENSE file found in the top-level directory of this distribution and at  *
+ * http://vast.io/license. No part of VAST, including this file, may be       *
+ * copied, modified, propagated, or distributed except according to the terms *
+ * contained in the LICENSE file.                                             *
+ ******************************************************************************/
+
+#pragma once
+
+#include <caf/fwd.hpp>
+
+namespace vast::detail {
+
+/// Fills `xs` with state from the stream manager `mgr`.
+void fill_status_map(caf::dictionary<caf::config_value>& xs,
+                     const caf::stream_manager& mgr);
+
+} // namespace vast::detail
