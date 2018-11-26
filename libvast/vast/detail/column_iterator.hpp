abd30b727498a4bb3eacb2fffd614de3807ec343
Dominik Charousset <dominik@charousset.de>
2018-11-26 15:32:12 +0100

    Add custom column_iterator type

---
+++ libvast/vast/detail/column_iterator.hpp
@@ -0,0 +1,134 @@
+/******************************************************************************
+ *                    _   _____   __________                                  *
+ *                   | | / / _ | / __/_  __/     Visibility                   *
+ *                   | |/ / __ |_\ \  / /          Across                     *
+ *                   |___/_/ |_/___/ /_/       Space and Time                 *
+ *                                                                            *
+ * This file is part of VAST. It is subject to the license terms in the       *
+ * LICENSE file found in the top-level directory of this distribution and at  *
+ * http://vast.io/license. No part of VAST, including this file, may be       *
+ * copied, modified, propagated, or distributed except according to the terms *
+ * contained in the LICENSE file.                                             *
+ ******************************************************************************/
+
+#pragma once
+
+#include <cstddef>
+#include <iterator>
+
+#include "vast/detail/narrow.hpp"
+
+namespace vast::detail {
+
+/// Iterates a 1-dimensional row-major array as if it were a column in a
+/// 2-dimensional matrix.
+template <class T>
+class column_iterator : public std::iterator<std::random_access_iterator_tag, T,
+                                             ptrdiff_t, T*, T&> {
+public:
+  // -- member types ---------------------------------------------------------
+
+  using super = std::iterator<std::random_access_iterator_tag,
+                              T, ptrdiff_t, T*, T&>;
+
+  using difference_type = typename super::difference_type;
+
+  using value_type = typename super::value_type;
+
+  using pointer = typename super::pointer;
+
+  using reference = typename super::reference;
+
+  // -- constructors, destructors, and assignment operators ------------------
+
+  column_iterator(pointer ptr, difference_type rows) : ptr_(ptr), rows_(rows) {
+    //nop
+  }
+
+  column_iterator(pointer ptr, size_t rows)
+    : column_iterator(ptr, narrow_cast<difference_type>(rows)) {
+    //nop
+  }
+
+  column_iterator(const column_iterator&) = default;
+
+  column_iterator& operator=(const column_iterator&) = default;
+
+  // -- operators ------------------------------------------------------------
+
+  bool operator==(const column_iterator& other) const noexcept {
+    return ptr_ == other.ptr_;
+  }
+
+  bool operator!=(const column_iterator& other) const noexcept {
+    return ptr_ != other.ptr_;
+  }
+
+  column_iterator& operator++() {
+    ptr_ += rows_;
+    return *this;
+  }
+
+  column_iterator& operator+=(difference_type n) {
+    ptr_ += rows_ * n;
+    return *this;
+  }
+
+  column_iterator operator+(difference_type n) const {
+    auto result{*this};
+    result += n;
+    return result;
+  }
+
+  column_iterator operator++(int) {
+    auto result = *this;
+    ptr_ += rows_;
+    return result;
+  }
+
+  column_iterator& operator--() {
+    ptr_ -= rows_;
+    return *this;
+  }
+
+  column_iterator& operator-=(difference_type n) {
+    ptr_ -= rows_ * n;
+    return *this;
+  }
+
+  column_iterator operator-(difference_type n) const {
+    auto result{*this};
+    result -= n;
+    return result;
+  }
+
+  column_iterator operator--(int) {
+    auto result = *this;
+    ptr_ -= rows_;
+    return result;
+  }
+
+  difference_type operator-(const column_iterator& other) {
+    return (ptr_ - other.ptr_) / rows_;
+  }
+
+  reference operator[](difference_type pos) {
+    return ptr_[pos * rows_];
+  }
+
+  reference operator*() {
+    return *ptr_;
+  }
+
+  pointer operator->() {
+    return ptr_;
+  }
+
+private:
+  // -- member variables -----------------------------------------------------
+
+  pointer ptr_;
+  difference_type rows_;
+};
+
+} // namespace vast::detail
