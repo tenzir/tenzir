005faf2221ba6591162d8943dda6ff75276a99f4
Dominik Charousset <dominik.charousset@tenzir.com>
2018-12-18 10:30:46 +0100

    Re-implement INDEXER unit test

--- libvast/test/system/indexer.cpp
+++ libvast/test/system/indexer.cpp
@@ -36,45 +36,34 @@
 using namespace caf;
 using namespace vast;
 
-/*
-
 namespace {
 
 struct fixture : fixtures::deterministic_actor_system_and_events {
-  void init(const record_type& layout) {
-    auto& fields = layout.fields;
-    indexers.reserve(fields.size());
-    for (size_t column = 0; column < fields.size(); ++column)
-      if (!has_skip_attribute(fields[column].type))
-        indexers.emplace_back(system::spawn_indexer(self.ptr(), directory,
-                                                    fields[column].type,
-                                                    column));
+  void init(type col_type) {
+    indexer = system::spawn_indexer(self.ptr(), directory, col_type, 0);
     run();
   }
 
   void ingest(std::vector<table_slice_ptr> slices) {
     VAST_ASSERT(slices.size() > 0);
     auto& layout = slices[0]->layout();
-    init(layout);
+    VAST_ASSERT(layout.fields.size() == 1);
+    init(layout.fields[0].type);
     VAST_ASSERT(std::all_of(slices.begin(), slices.end(), [&](auto& slice) {
       return slice->layout() == layout;
     }));
-    vast::detail::spawn_container_source(sys, std::move(slices), indexers);
+    vast::detail::spawn_container_source(sys, std::move(slices), indexer);
     run();
   }
 
   ids query(std::string_view what) {
     // Spin up an EVALUATOR for collecting the results.
-    auto expr = unbox(to<expression>(what));
-    auto eval = self->spawn(system::evaluator, indexers);
-    self->send(eval, std::move(expr), self);
+    auto pred = unbox(to<predicate>(what));
+    self->send(indexer, curried(pred));
     run();
     // Fetch results from mailbox.
     ids result;
-    bool done = false;
-    while (!done)
-      self->receive([&](const ids &hits) { result |= hits; },
-                    [&](system::done_atom) { done = true; });
+    self->receive([&](const ids& hits) { result |= hits; });
     if (result.size() < num_ids)
       result.append_bits(false, num_ids - result.size());
     return result;
@@ -95,7 +84,7 @@ struct fixture : fixtures::deterministic_actor_system_and_events {
   size_t num_ids = 0;
 
   /// Our actors-under-test.
-  std::vector<actor> indexers;
+  actor indexer;
 };
 
 } // namespace <anonymous>
@@ -117,50 +106,15 @@ TEST(integer rows) {
     CHECK_EQUAL(query(":int == +3"), res(2u, 5u, 8u));
     CHECK_EQUAL(query(":int == +4"), res());
     CHECK_EQUAL(query(":int != +1"), res(1u, 2u, 4u, 5u, 7u, 8u));
-    CHECK_EQUAL(query("!(:int == +1)"), res(1u, 2u, 4u, 5u, 7u, 8u));
-    CHECK_EQUAL(query(":int > +1 && :int < +3"), res(1u, 4u, 7u));
   };
   verify();
   MESSAGE("kill INDEXER");
-  for (auto& indexer : indexers)
-    anon_send_exit(indexer, exit_reason::kill);
+  anon_send_exit(indexer, exit_reason::kill);
   run();
   MESSAGE("reload INDEXER from disk");
-  init(layout);
-  MESSAGE("verify table index again");
-  verify();
-}
-
-TEST(bro conn logs) {
-  MESSAGE("ingest bro conn log");
-  ingest(bro_conn_log_slices);
-  MESSAGE("verify table index");
-  num_ids = bro_conn_log.size();
-  auto verify = [&] {
-    CHECK_EQUAL(rank(query("id.resp_p == 53/?")), 3u);
-    CHECK_EQUAL(rank(query("id.resp_p == 137/?")), 5u);
-    CHECK_EQUAL(rank(query("id.resp_p == 53/? || id.resp_p == 137/?")), 8u);
-    CHECK_EQUAL(rank(query("&time > 1970-01-01")), bro_conn_log.size());
-    CHECK_EQUAL(rank(query("proto == \"udp\"")), 20u);
-    CHECK_EQUAL(rank(query("proto == \"tcp\"")), 0u);
-    CHECK_EQUAL(rank(query("uid == \"nkCxlvNN8pi\"")), 1u);
-    CHECK_EQUAL(rank(query("orig_bytes < 400")), 17u);
-    CHECK_EQUAL(rank(query("orig_bytes < 400 && proto == \"udp\"")), 17u);
-    CHECK_EQUAL(rank(query(":addr == fe80::219:e3ff:fee7:5d23")), 1u);
-    CHECK_EQUAL(query(":addr == 192.168.1.104"), res(5u, 6u, 9u, 11u));
-  };
-  verify();
-  MESSAGE("kill INDEXER");
-  for (auto& indexer: indexers)
-    anon_send_exit(indexer, exit_reason::kill);
-  indexers.clear();
-  run();
-  MESSAGE("reload INDEXER from disk");
-  init(bro_conn_log_layout());
+  init(layout.fields[0].type);
   MESSAGE("verify table index again");
   verify();
 }
 
 FIXTURE_SCOPE_END()
-
-*/
