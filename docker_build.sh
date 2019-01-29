279bdf6d50bd0f55d231770874a4ef648db01a9d
Tobias Mayer <tobim@fastmail.fm>
2019-01-29 16:24:39 +0100

    Save the images from docker_build.sh

--- docker_build.sh
+++ docker_build.sh
@@ -8,3 +8,6 @@ docker tag vast-io/vast:latest vast-io/vast:${VERSION}
 # Tag the builder stage
 BUILDER=$(docker images --filter "label=builder=true" --format '{{.CreatedAt}}\t{{.ID}}' | sort -nr | head -n 1 | cut -f2)
 docker tag ${BUILDER} vast-io/vast:builder
+# Export the images to gzipped archives
+docker save vast-io/vast:latest | gzip > vast-${VERSION}-docker.tar.gz
+docker save vast-io/vast:builder | gzip > vast-builder-${VERSION}-docker.tar.gz
