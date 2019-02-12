01a759f8fc1fe9cb8af1f55f7c2418b03fff2ed4
Tobias Mayer <tobim@fastmail.fm>
2019-02-12 14:25:35 +0100

    Refactor port parser

--- libvast/vast/concept/parseable/vast/port.hpp
+++ libvast/vast/concept/parseable/vast/port.hpp
@@ -34,13 +34,10 @@ struct port_type_parser : parser<port_type_parser> {
   bool parse(Iterator& f, const Iterator& l, port::port_type& x) const {
     using namespace parsers;
     using namespace parser_literals;
-    static auto p
-      = ( "?"_p ->* [] { return port::unknown; }
-         | "tcp"_p ->* [] { return port::tcp; }
-         | "udp"_p ->* [] { return port::udp; }
-         | "icmp"_p ->* [] { return port::icmp; }
-         )
-      ;
+    auto p = (
+      "?"_p->*[] { return port::unknown; } | "tcp"_p->*[] { return port::tcp; }
+      | "udp"_p->*[] { return port::udp; }
+      | "icmp"_p->*[] { return port::icmp; });
     return p(f, l, x);
   }
 };
@@ -52,32 +49,28 @@ struct parser_registry<port::port_type> {
 
 namespace parsers {
 
-static auto const port_type = port_type_parser{};
+auto const port_type = port_type_parser{};
 
 } // namespace parsers
 
 struct port_parser : parser<port_parser> {
   using attribute = port;
 
-  template <class Iterator>
-  bool parse(Iterator& f, const Iterator& l, unused_type) const {
+  template <class Iterator, class Attribute>
+  bool parse(Iterator& f, const Iterator& l, Attribute& x) const {
     using namespace parsers;
     using namespace parser_literals;
     auto p = u16 >> '/' >> parsers::port_type;
-    return p(f, l, unused);
-  }
-
-  template <class Iterator>
-  bool parse(Iterator& f, const Iterator& l, port& x) const {
-    using namespace parsers;
-    using namespace parser_literals;
-    static auto p =  u16 >> '/' >> parsers::port_type;
-    port::number_type n;
-    port::port_type t;
-    if (!p(f, l, n, t))
-      return false;
-    x = {n, t};
-    return true;
+    if constexpr (std::is_same_v<Attribute, unused_type>) {
+      return p(f, l, unused);
+    } else {
+      port::number_type n;
+      port::port_type t;
+      if (!p(f, l, n, t))
+        return false;
+      x = {n, t};
+      return true;
+    }
   }
 };
 
@@ -88,9 +81,8 @@ struct parser_registry<port> {
 
 namespace parsers {
 
-static auto const port = port_parser{};
+auto const port = port_parser{};
 
 } // namespace parsers
 
 } // namespace vast
-
