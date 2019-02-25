64866694778b7c1c824f28b7cef1a9666885c40c
Matthias Vallentin <vallentin@icir.org>
2019-02-25 16:27:07 +0100

    Polish script
    The actual commands are still not POSIX compliant, though.

--- scripts/git-setup.sh
+++ scripts/git-setup.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-set -e
-
 usage() {
   echo "usage: $(basename $0) [options] [<format-show|format|pre-commit>]"
   echo
@@ -11,34 +9,34 @@ usage() {
 }
 
 install_format_show() {
-  git config --local --get "alias.format-show" >/dev/null
-  if [ "$force" = TRUE ] || [ $? -ne 0 ]; then
-    echo 'Adding alias for `git format-show`'
-    git config --local "alias.format-show" '! R=0; while read line; do R=1; echo "$line"; done < <(git diff -U0 --no-color $(git merge-base origin/master HEAD) | $(git rev-parse --show-toplevel)/scripts/clang-format-diff.py -p1); exit $R'
+  git config --local --get alias.format-show >/dev/null
+  if [ $? -ne 0 ] || [ "$force" = TRUE ]; then
+    echo 'adding alias for `git format-show`'
+    git config --local alias.format-show '! R=0; while read line; do R=1; echo "$line"; done < <(git diff -U0 --no-color $(git merge-base origin/master HEAD) | $(git rev-parse --show-toplevel)/scripts/clang-format-diff.py -p1); exit $R'
   else
-    echo 'Alias for `git format-show` already exists, skipping.'
+    echo 'alias for `git format-show` already exists, skipping'
   fi
 }
 
 install_format() {
-  git config --local --get "alias.format" >/dev/null
-  if [ "$force" = TRUE ] || [ $? -ne 0 ]; then
-    echo 'Adding alias for `git format`'
-    git config --local "alias.format" '! git diff -U0 --no-color $(git merge-base origin/master HEAD) | $(git rev-parse --show-toplevel)/scripts/clang-format-diff.py -i -p1'
+  git config --local --get alias.format >/dev/null
+  if [ $? -ne 0 ] || [ "$force" = TRUE ]; then
+    echo 'adding alias for `git format`'
+    git config --local alias.format '! git diff -U0 --no-color $(git merge-base origin/master HEAD) | $(git rev-parse --show-toplevel)/scripts/clang-format-diff.py -i -p1'
   else
-    echo 'Alias for `git format` already exists, skipping.'
+    echo 'alias for `git format` already exists, skipping'
   fi
 }
 
 install_pre_commit() {
   GIT_DIR=$(git rev-parse --git-dir)
   PRE_COMMIT=${GIT_DIR}/hooks/pre-commit
-  if [ "$force" = TRUE ] || [ ! -f ${PRE_COMMIT} ]; then
-    echo 'Adding pre-commit hook for format checking'
-    printf "#!/usr/bin/env bash\ngit format-show" > ${PRE_COMMIT}
+  if [ ! -f ${PRE_COMMIT} ] || [ "$force" = TRUE ]; then
+    echo 'adding pre-commit hook for format checking'
+    printf "#!/bin/sh\ngit format-show" > ${PRE_COMMIT}
     chmod +x ${PRE_COMMIT}
   else
-    echo 'Git pre-commit hook already exists, skipping.'
+    echo 'git pre-commit hook already exists, skipping'
   fi
 }
 
@@ -54,16 +52,15 @@ while getopts "fh?" opt; do
     ;;
   esac
 done
+shift $(expr $OPTIND - 1)
 
-if [ "$#" -lt "$OPTIND" ]; then
+if [ -z "$@" ]; then
   # Add all if no arguments.
   install_format_show
   install_format
   install_pre_commit
 else
-  shift $(expr $OPTIND - 1)
-  part=$1
-  case "$part" in
+  case "$1" in
     format-show)
       install_format_show
       ;;
