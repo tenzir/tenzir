13e9c4b7c654c261dc069a7158a54fe265afd10c
Tobias Mayer <tobim@fastmail.fm>
2019-03-11 11:34:33 +0100

    Make git format and pre-commit usable cross repo

--- scripts/git-setup.sh
+++ scripts/git-setup.sh
@@ -18,6 +18,7 @@ usage() {
   echo
   echo 'available options:'
   echo "    -f              force installation (overwrite existing)"
+  echo "    -p <path>       path to clang-format-diff.py"
   echo "    -h|-?           display this help"
 }
 
@@ -25,7 +26,7 @@ install_format_show_branch() {
   git config --local --get alias.format-show-branch >/dev/null
   if [ $? -ne 0 ] || [ "$force" = TRUE ]; then
     echo 'Adding alias for `git format-show-branch`'
-    git config --local alias.format-show-branch '! git diff -U0 --no-color --cached $(git merge-base origin/master HEAD) | $(git rev-parse --show-toplevel)/scripts/clang-format-diff.py -p1'
+    git config --local alias.format-show-branch "!f() { git diff -U0 --no-color \$@ \$(git merge-base origin/master HEAD) | ${format_call} -p1; }; f"
   else
     echo 'alias for `git format-show-branch` already exists, skipping'
   fi
@@ -35,7 +36,7 @@ install_format_show() {
   git config --local --get alias.format-show >/dev/null
   if [ $? -ne 0 ] || [ "$force" = TRUE ]; then
     echo 'Adding alias for `git format-show`'
-    git config --local alias.format-show '! git diff -U0 --no-color --cached | $(git rev-parse --show-toplevel)/scripts/clang-format-diff.py -p1'
+    git config --local alias.format-show "!f() { git diff -U0 --no-color \$@ | ${format_call} -p1; }; f"
   else
     echo 'alias for `git format-show` already exists, skipping'
   fi
@@ -45,7 +46,7 @@ install_format_branch() {
   git config --local --get alias.format-branch >/dev/null
   if [ $? -ne 0 ] || [ "$force" = TRUE ]; then
     echo 'adding alias for `git format-branch`'
-    git config --local alias.format-branch '! git diff -U0 --no-color --cached $(git merge-base origin/master HEAD) | $(git rev-parse --show-toplevel)/scripts/clang-format-diff.py -i -p1'
+    git config --local alias.format-branch "!f() { git diff -U0 --no-color \$@ \$(git merge-base origin/master HEAD) | ${format_call} -i -p1; }; f"
   else
     echo 'alias for `git format-branch` already exists, skipping'
   fi
@@ -55,7 +56,7 @@ install_format() {
   git config --local --get alias.format >/dev/null
   if [ $? -ne 0 ] || [ "$force" = TRUE ]; then
     echo 'adding alias for `git format`'
-    git config --local alias.format '! git diff -U0 --no-color --cached | $(git rev-parse --show-toplevel)/scripts/clang-format-diff.py -i -p1'
+    git config --local alias.format "!f() { git diff -U0 --no-color \$@ | ${format_call} -i -p1; }; f"
   else
     echo 'alias for `git format` already exists, skipping'
   fi
@@ -66,18 +67,28 @@ install_pre_commit() {
   PRE_COMMIT=${GIT_DIR}/hooks/pre-commit
   if [ ! -f ${PRE_COMMIT} ] || [ "$force" = TRUE ]; then
     echo 'adding pre-commit hook for format checking'
-    printf "#!/bin/sh\ngit format-show" > ${PRE_COMMIT}
+    printf "#!/bin/sh\ngit format-show --cached" > ${PRE_COMMIT}
     chmod +x ${PRE_COMMIT}
   else
     echo 'git pre-commit hook already exists, skipping'
   fi
 }
 
+dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+current_git_tl=$(git rev-parse --show-toplevel)
+script_git_tl=$(git -C ${dir} rev-parse --show-toplevel)
 force=FALSE
-while getopts "fh?" opt; do
+fixed_path=FALSE
+clang_format_diff=${dir}/clang-format-diff.py
+format_call='$(git rev-parse --show-toplevel)/scripts/clang-format-diff.py'
+while getopts "fp:h?" opt; do
   case "$opt" in
     f)
       force=TRUE
+      ;;
+    p)
+      fixed_path=TRUE
+      clang_format_diff="$OPTARG"
       ;;
     h|\?)
       usage
@@ -86,6 +97,14 @@ while getopts "fh?" opt; do
   esac
 done
 shift $(expr $OPTIND - 1)
+
+if [ ! "${script_git_tl}" = "${current_git_tl}" ]; then
+    fixed_path=TRUE
+fi
+if [ "${fixed_path}" = TRUE ]; then
+    echo "using fixed path to clang_format_diff: ${clang_format_diff}"
+    format_call="${clang_format_diff}"
+fi
 
 if [ -z "$@" ]; then
   # Add all if no arguments.
