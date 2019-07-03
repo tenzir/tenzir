52ddd8ef7093dfc88e6984c373a5045142b261ad
Tobias Mayer <tobim@fastmail.fm>
2019-07-03 10:04:13 +0200

    Add csv::reader scaffold

--- libvast/src/format/csv.cpp
+++ libvast/src/format/csv.cpp
@@ -13,10 +13,27 @@
 
 #include "vast/format/csv.hpp"
 
+
 #include <ostream>
+
+#include "vast/concept/parseable/core.hpp"
+#include "vast/concept/parseable/numeric.hpp"
+#include "vast/concept/parseable/stream.hpp"
+#include "vast/concept/parseable/string.hpp"
+#include "vast/concept/parseable/to.hpp"
+#include "vast/concept/parseable/vast/address.hpp"
+#include "vast/concept/parseable/vast/offset.hpp"
+#include "vast/concept/parseable/vast/schema.hpp"
+#include "vast/concept/parseable/vast/si.hpp"
+#include "vast/concept/parseable/vast/time.hpp"
+
+#include "vast/concept/parseable/core/operators.hpp"
+#include "vast/concept/parseable/core/rule.hpp"
+#include "vast/concept/parseable/string.hpp"
 
 #include "vast/concept/printable/vast/view.hpp"
 #include "vast/error.hpp"
+#include "vast/schema.hpp"
 #include "vast/table_slice.hpp"
 
 namespace vast::format::csv {
@@ -127,6 +144,88 @@ caf::error writer::write(const table_slice& x) {
 
 const char* writer::name() const {
   return "csv-writer";
+}
+
+reader::reader(caf::atom_value table_slice_type,
+               std::unique_ptr<std::istream> in)
+  : super(table_slice_type) {
+  if (in != nullptr)
+    reset(std::move(in));
+}
+
+void reader::reset(std::unique_ptr<std::istream> in) {
+  VAST_ASSERT(in != nullptr);
+  input_ = std::move(in);
+  lines_ = std::make_unique<detail::line_range>(*input_);
+}
+
+caf::error reader::schema(vast::schema s) {
+  schema_ = std::move(s);
+  return caf::none;
+}
+
+vast::schema reader::schema() const {
+  return schema_;
+}
+
+const char* reader::name() const {
+  return "csv-reader";
+}
+
+caf::optional<record_type> make_layout(const std::vector<std::string>& names,
+                                       const schema& s) {
+  // TODO: implement
+  return record_type{};
+}
+
+template <class Iterator>
+caf::optional<rule<Iterator>>
+make_ordered_parser(const record_type& layout,
+                    table_slice_builder_ptr builder) {
+  // TODO: implement
+  return {};
+}
+
+caf::error reader::read_header(std::string_view line) {
+  auto p = schema_parser::id % ',';
+  std::vector<std::string> columns;
+  if (!p(line, columns))
+    return make_error(ec::parse_error, "unable to parse csv header");
+  auto layout = make_layout(columns, schema_);
+  if (!layout)
+    return make_error(ec::parse_error, "unable to parse csv header");
+  if (!reset_builder(*layout))
+    return make_error(ec::parse_error,
+                      "unable to create a bulider for parsed layout at",
+                      lines_->line_number());
+  parser_ = make_ordered_parser<iterator_type>(*layout, builder_);
+  if (!parser_)
+    return make_error(ec::parse_error, "unable generate a parser");
+  return caf::none;
+}
+
+caf::error reader::read_impl(size_t max_events, size_t max_slice_size,
+                             consumer& cons) {
+  VAST_ASSERT(max_events > 0);
+  VAST_ASSERT(max_slice_size > 0);
+  if (!parser_)
+    if (auto err = read_header(lines_->get()))
+      return err;
+  auto& p = *parser_;
+  size_t produced = 0;
+  for (; produced < max_events; lines_->next()) {
+    // EOF check.
+    if (lines_->done())
+      return finish(cons, make_error(ec::end_of_input, "input exhausted"));
+    auto& line = lines_->get();
+    // if (!p(line))
+    //  return make_error(ec::type_clash, "unable to parse csv line");
+    produced++;
+    if (builder_->rows() == max_slice_size)
+      if (auto err = finish(cons))
+        return err;
+  }
+  return finish(cons);
 }
 
 } // namespace vast::format::csv
