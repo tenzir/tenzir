4ff6345e4cd0105df850b16ea1ea250b8a08fcd8
Tobias Mayer <tobim@fastmail.fm>
2019-07-07 03:10:32 +0200

    Add proxy header for parseable/vast/*.hpp

---
+++ libvast/vast/concept/parseable/vast.hpp
@@ -0,0 +1,33 @@
+/******************************************************************************
+ *                    _   _____   __________                                  *
+ *                   | | / / _ | / __/_  __/     Visibility                   *
+ *                   | |/ / __ |_\ \  / /          Across                     *
+ *                   |___/_/ |_/___/ /_/       Space and Time                 *
+ *                                                                            *
+ * This file is part of VAST. It is subject to the license terms in the       *
+ * LICENSE file found in the top-level directory of this distribution and at  *
+ * http://vast.io/license. No part of VAST, including this file, may be       *
+ * copied, modified, propagated, or distributed except according to the terms *
+ * contained in the LICENSE file.                                             *
+ ******************************************************************************/
+
+#pragma once
+
+#include "vast/concept/parseable/vast/address.hpp"
+#include "vast/concept/parseable/vast/base.hpp"
+#include "vast/concept/parseable/vast/data.hpp"
+#include "vast/concept/parseable/vast/endpoint.hpp"
+#include "vast/concept/parseable/vast/expression.hpp"
+#include "vast/concept/parseable/vast/http.hpp"
+#include "vast/concept/parseable/vast/identifier.hpp"
+#include "vast/concept/parseable/vast/json.hpp"
+#include "vast/concept/parseable/vast/offset.hpp"
+#include "vast/concept/parseable/vast/pattern.hpp"
+#include "vast/concept/parseable/vast/port.hpp"
+#include "vast/concept/parseable/vast/schema.hpp"
+#include "vast/concept/parseable/vast/si.hpp"
+#include "vast/concept/parseable/vast/subnet.hpp"
+#include "vast/concept/parseable/vast/time.hpp"
+#include "vast/concept/parseable/vast/type.hpp"
+#include "vast/concept/parseable/vast/uri.hpp"
+#include "vast/concept/parseable/vast/uuid.hpp"
