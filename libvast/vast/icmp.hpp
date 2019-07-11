f326718d74c575ab2cb736cb552c3a9d4da30fa5
Matthias Vallentin <vallentin@icir.org>
2019-07-11 11:35:59 +0200

    Move ICMP utitlity functions into implementation

--- libvast/vast/icmp.hpp
+++ libvast/vast/icmp.hpp
@@ -15,7 +15,7 @@
 
 #include <cstdint>
 
-#include <caf/optional.hpp>
+#include <caf/fwd.hpp>
 
 namespace vast {
 
@@ -55,66 +55,12 @@ enum class icmp6_type : uint8_t {
 /// @param x The ICMP type.
 /// @returns For a request type, the response type - and vice versa.
 /// @relates icmp_type
-inline caf::optional<icmp_type> dual(icmp_type x) {
-  switch (x) {
-    default:
-      return caf::none;
-    case icmp_type::echo:
-      return icmp_type::echo_reply;
-    case icmp_type::echo_reply:
-      return icmp_type::echo;
-    case icmp_type::tstamp:
-      return icmp_type::tstamp_reply;
-    case icmp_type::tstamp_reply:
-      return icmp_type::tstamp;
-    case icmp_type::info:
-      return icmp_type::info_reply;
-    case icmp_type::info_reply:
-      return icmp_type::info;
-    case icmp_type::rtr_solicit:
-      return icmp_type::rtr_advert;
-    case icmp_type::rtr_advert:
-      return icmp_type::rtr_solicit;
-    case icmp_type::mask:
-      return icmp_type::mask_reply;
-    case icmp_type::mask_reply:
-      return icmp_type::mask;
-  }
-}
+caf::optional<icmp_type> dual(icmp_type x);
 
 /// Computes the dual to a given ICMP6 type.
 /// @param x The ICMP6 type.
 /// @returns For a request type, the response type - and vice versa.
 /// @relates icmp6_type
-inline caf::optional<icmp6_type> dual(icmp6_type x) {
-  switch (x) {
-    default:
-      return caf::none;
-    case icmp6_type::echo_request:
-      return icmp6_type::echo_reply;
-    case icmp6_type::echo_reply:
-      return icmp6_type::echo_request;
-    case icmp6_type::mld_listener_query:
-      return icmp6_type::mld_listener_report;
-    case icmp6_type::mld_listener_report:
-      return icmp6_type::mld_listener_query;
-    case icmp6_type::nd_router_solicit:
-      return icmp6_type::nd_router_advert;
-    case icmp6_type::nd_router_advert:
-      return icmp6_type::nd_router_solicit;
-    case icmp6_type::nd_neighbor_solicit:
-      return icmp6_type::nd_neighbor_advert;
-    case icmp6_type::nd_neighbor_advert:
-      return icmp6_type::nd_neighbor_solicit;
-    case icmp6_type::wru_request:
-      return icmp6_type::wru_reply;
-    case icmp6_type::wru_reply:
-      return icmp6_type::wru_request;
-    case icmp6_type::haad_request:
-      return icmp6_type::haad_reply;
-    case icmp6_type::haad_reply:
-      return icmp6_type::haad_request;
-  }
-}
+caf::optional<icmp6_type> dual(icmp6_type x);
 
 } // namespace vast
