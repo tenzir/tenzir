ba0a7fa1bdff67f6263ec83202351fbb872ca0f4
Dominik Lohmann <mail@dominiklohmann.de>
2019-10-04 12:37:30 +0200

    Clean command.cpp
    Remove the unused, commented-out code and move anonymous namespace to
    the top.

--- libvast/src/command.cpp
+++ libvast/src/command.cpp
@@ -35,6 +35,103 @@
 
 namespace vast {
 
+namespace {
+
+using std::accumulate;
+
+// Returns the field size for printing all names in `xs`.
+auto field_size(const command::children_list& xs) {
+  return accumulate(xs.begin(), xs.end(), size_t{0}, [](size_t x, auto& y) {
+    return std::max(x, y->name.size());
+  });
+}
+
+// Returns the field size for printing all names in `xs`.
+auto field_size(const caf::config_option_set& xs) {
+  return accumulate(xs.begin(), xs.end(), size_t{0}, [](size_t x, auto& y) {
+    // We print parameters in the form "[-h | -? | --help] <type>" (but we omit
+    // the type for boolean). So, "[]" adds 2 characters, each short name adds
+    // 5 characters with "-X | ", the long name gets the 2 character prefix
+    // "--", and finally we add an extra space plus the type name.
+    auto tname = y.type_name();
+    auto tname_size = tname == "bool" ? 0u : tname.size() + 3;
+    return std::max(x, 4 + (y.short_names().size() * 5) + y.long_name().size()
+                         + tname_size);
+  });
+}
+
+void parameters_helptext(const command& cmd, std::ostream& out) {
+  out << "parameters:\n";
+  auto fs = field_size(cmd.options);
+  for (auto& opt : cmd.options) {
+    out << "  ";
+    std::string lst = "[";
+    for (auto ch : opt.short_names()) {
+      lst += '-';
+      lst += ch;
+      lst += " | ";
+    }
+    lst += "--";
+    lst.insert(lst.end(), opt.long_name().begin(), opt.long_name().end());
+    lst += ']';
+    auto tname = opt.type_name();
+    if (tname != "bool") {
+      lst += " <";
+      lst.insert(lst.end(), tname.begin(), tname.end());
+      lst += '>';
+    }
+    out.width(fs);
+    out << lst << "  " << opt.description() << '\n';
+  }
+}
+
+// Prints the helptext for a command without children.
+void flat_helptext(const command& cmd, std::ostream& out) {
+  // A trivial command without parameters prints its name and description.
+  if (cmd.options.empty()) {
+    out << "usage: " << cmd.full_name() << "\n\n" << cmd.description << "\n\n";
+    return;
+  }
+  // A command with parameters prints 1) its name, 2) a description, and 3) a
+  // list of available parameters.
+  out << "usage: " << cmd.full_name() << " [<parameters>]\n\n"
+      << cmd.description << "\n\n";
+  parameters_helptext(cmd, out);
+}
+
+void subcommand_helptext(const command& cmd, std::ostream& out) {
+  out << "subcommands:\n";
+  auto fs = field_size(cmd.children);
+  for (auto& child : cmd.children) {
+    if (child->visible) {
+      out << "  ";
+      out.width(fs);
+      out << child->name << "  " << child->description << '\n';
+    }
+  }
+}
+
+// Prints the helptext for a command without children.
+void nested_helptext(const command& cmd, std::ostream& out) {
+  // A trivial command without parameters prints name, description and
+  // children.
+  if (cmd.options.empty()) {
+    out << "usage: " << cmd.full_name() << " <command>"
+        << "\n\n"
+        << cmd.description << "\n\n";
+    subcommand_helptext(cmd, out);
+    return;
+  }
+  out << "usage: " << cmd.full_name() << " [<parameters>] <command>"
+      << "\n\n"
+      << cmd.description << "\n\n";
+  parameters_helptext(cmd, out);
+  out << '\n';
+  subcommand_helptext(cmd, out);
+}
+
+} // namespace
+
 std::string command::full_name() const {
   std::string result{name};
   for (auto ptr = parent; ptr != nullptr && ptr->parent != nullptr;
@@ -274,103 +371,6 @@ const command* resolve(const command& cmd, std::string_view name) {
   return resolve(cmd, words.begin(), words.end());
 }
 
-namespace {
-
-using std::accumulate;
-
-// Returns the field size for printing all names in `xs`.
-auto field_size(const command::children_list& xs) {
-  return accumulate(xs.begin(), xs.end(), size_t{0}, [](size_t x, auto& y) {
-    return std::max(x, y->name.size());
-  });
-}
-
-// Returns the field size for printing all names in `xs`.
-auto field_size(const caf::config_option_set& xs) {
-  return accumulate(xs.begin(), xs.end(), size_t{0}, [](size_t x, auto& y) {
-    // We print parameters in the form "[-h | -? | --help] <type>" (but we omit
-    // the type for boolean). So, "[]" adds 2 characters, each short name adds
-    // 5 characters with "-X | ", the long name gets the 2 character prefix
-    // "--", and finally we add an extra space plus the type name.
-    auto tname = y.type_name();
-    auto tname_size = tname == "bool" ? 0u : tname.size() + 3;
-    return std::max(x, 4 + (y.short_names().size() * 5) + y.long_name().size()
-                         + tname_size);
-  });
-}
-
-void parameters_helptext(const command& cmd, std::ostream& out) {
-  out << "parameters:\n";
-  auto fs = field_size(cmd.options);
-  for (auto& opt : cmd.options) {
-    out << "  ";
-    std::string lst = "[";
-    for (auto ch : opt.short_names()) {
-      lst += '-';
-      lst += ch;
-      lst += " | ";
-    }
-    lst += "--";
-    lst.insert(lst.end(), opt.long_name().begin(), opt.long_name().end());
-    lst += ']';
-    auto tname = opt.type_name();
-    if (tname != "bool") {
-      lst += " <";
-      lst.insert(lst.end(), tname.begin(), tname.end());
-      lst += '>';
-    }
-    out.width(fs);
-    out << lst << "  " << opt.description() << '\n';
-  }
-}
-
-// Prints the helptext for a command without children.
-void flat_helptext(const command& cmd, std::ostream& out) {
-  // A trivial command without parameters prints its name and description.
-  if (cmd.options.empty()) {
-    out << "usage: " << cmd.full_name() << "\n\n" << cmd.description << "\n\n";
-    return;
-  }
-  // A command with parameters prints 1) its name, 2) a description, and 3) a
-  // list of available parameters.
-  out << "usage: " << cmd.full_name() << " [<parameters>]\n\n"
-      << cmd.description << "\n\n";
-  parameters_helptext(cmd, out);
-}
-
-void subcommand_helptext(const command& cmd, std::ostream& out) {
-  out << "subcommands:\n";
-  auto fs = field_size(cmd.children);
-  for (auto& child : cmd.children) {
-    if (child->visible) {
-      out << "  ";
-      out.width(fs);
-      out << child->name << "  " << child->description << '\n';
-    }
-  }
-}
-
-// Prints the helptext for a command without children.
-void nested_helptext(const command& cmd, std::ostream& out) {
-  // A trivial command without parameters prints name, description and
-  // children.
-  if (cmd.options.empty()) {
-    out << "usage: " << cmd.full_name() << " <command>"
-        << "\n\n"
-        << cmd.description << "\n\n";
-    subcommand_helptext(cmd, out);
-    return;
-  }
-  out << "usage: " << cmd.full_name() << " [<parameters>] <command>"
-      << "\n\n"
-      << cmd.description << "\n\n";
-  parameters_helptext(cmd, out);
-  out << '\n';
-  subcommand_helptext(cmd, out);
-}
-
-} // namespace <anonymous>
-
 void helptext(const command& cmd, std::ostream& out) {
   // Make sure fields are filled left-to-right.
   out << std::left;
@@ -398,86 +398,5 @@ std::string documentationtext(const command& cmd) {
   documentationtext(cmd, oss);
   return oss.str();
 }
-/*
-
-std::string command::usage() const {
-  std::stringstream result;
-  result << opts_.help_text();
-  if (!children_.empty()) {
-    result << "\nsubcommands:\n";
-    for (auto& child : children_)
-      result << "  " << child->name() << "\n";
-  }
-  return result.str();
-}
-
-std::string command::full_name() const {
-  std::string result;
-  if (is_root())
-    return result;
-  result = parent_->full_name();
-  if (!result.empty())
-    result += ' ';
-  result += name_;
-  return result;
-}
-
-caf::error command::proceed(caf::actor_system&,
-                            const caf::settings& options,
-                            argument_iterator begin, argument_iterator end) {
-  VAST_UNUSED(options, begin, end);
-  VAST_TRACE(VAST_ARG(std::string{name_}), VAST_ARG("args", begin, end),
-             VAST_ARG(options));
-  return caf::none;
-}
-
-caf::message command::run_impl(caf::actor_system&,
-                               const caf::settings& options,
-                               argument_iterator begin, argument_iterator end) {
-  VAST_UNUSED(options, begin, end);
-  VAST_TRACE(VAST_ARG(std::string{name_}), VAST_ARG("args", begin, end),
-             VAST_ARG(options));
-  usage();
-  return caf::none;
-}
-
-caf::message command::wrap_error(caf::error err) {
-  return caf::make_message(std::move(err));
-}
-
-caf::error command::parse_error(caf::pec code, argument_iterator error_position,
-                                argument_iterator begin,
-                                argument_iterator end) const {
-  std::stringstream result;
-  result << "Failed to parse command" << "\n";
-  result << "  Command: " << name() << " " << detail::join(begin, end, " ")
-         << "\n";
-  result << "  Description: " << to_string(code) << "\n";
-  if (error_position != end)
-    result << "  Position: " << *error_position << "";
-  else
-    result << "  Position: at the end";
-  return make_error(ec::parse_error, result.str());
-}
-
-caf::error command::unknown_subcommand_error(argument_iterator error_position,
-                                             argument_iterator end) const {
-  VAST_ASSERT(error_position != end);
-  auto& str = *error_position;
-  std::string helptext;
-  if (!str.empty() && str.front() == '-') {
-    helptext = "invalid option: ";
-    helptext += str;
-  } else {
-    helptext = "unknown command: ";
-    if (!is_root()) {
-      helptext += full_name();
-      helptext += ' ';
-    }
-    helptext += str;
-  }
-  return make_error(ec::syntax_error, std::move(helptext));
-}
-*/
 
 } // namespace vast
