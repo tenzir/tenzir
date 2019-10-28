5739cf1a344b93535e8dc8f285b85a0ad6075011
Dominik Lohmann <mail@dominiklohmann.de>
2019-10-28 15:23:48 +0100

    Add null sink to exporter

---
+++ doc/cli/vast-export-null.md
@@ -0,0 +1,2 @@
+The null export format does not render its results, and is used for debugging
+and benchmarking only.
---
+++ libvast/vast/format/null.hpp
@@ -0,0 +1,34 @@
+/******************************************************************************
+ *                    _   _____   __________                                  *
+ *                   | | / / _ | / __/_  __/     Visibility                   *
+ *                   | |/ / __ |_\ \  / /          Across                     *
+ *                   |___/_/ |_/___/ /_/       Space and Time                 *
+ *                                                                            *
+ * This file is part of VAST. It is subject to the license terms in the       *
+ * LICENSE file found in the top-level directory of this distribution and at  *
+ * http://vast.io/license. No part of VAST, including this file, may be       *
+ * copied, modified, propagated, or distributed except according to the terms *
+ * contained in the LICENSE file.                                             *
+ ******************************************************************************/
+
+#pragma once
+
+#include "vast/defaults.hpp"
+#include "vast/format/ostream_writer.hpp"
+
+namespace vast::format::null {
+
+class writer : public format::ostream_writer {
+public:
+  using defaults = vast::defaults::export_::ascii;
+
+  using super = format::ostream_writer;
+
+  using super::super;
+
+  caf::error write(const table_slice&) override;
+
+  const char* name() const override;
+};
+
+} // namespace vast::format::null
