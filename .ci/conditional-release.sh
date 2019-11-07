0720575949722a5bcd7293736afed8442ebc15b4
Felix Ortmann <felix.ortmann@tenzir.com>
2019-11-07 15:57:23 +0100

    Remove date in package name, increase readability

--- .ci/conditional-release.sh
+++ .ci/conditional-release.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
-annotation=$(git tag -l --format='%(contents)' "$CIRRUS_TAG")
-last_commit_message=$(git log -1 --pretty=%B)
+set -eox pipefail
+
+annotation="$(git tag -l --format='%(contents)' "$CIRRUS_TAG")"
+last_commit_message="$(git log -1 --pretty=%B)"
 
 if [[ "$annotation" == "$last_commit_message" ]]; then
         echo "Not an annotaged-tag build. No need to release."
-        exit 0
+        exit
 fi
 
-# problem with the date command: release at midnight may lead to errors
+version="$(git describe)"
+
 files_to_upload=(
-    "$(date +%Y-%m-%d)-VAST-$(git describe)-Linux-Release.tar.gz"
-    "$(date +%Y-%m-%d)-VAST-$(git describe)-Darwin-Release.tar.gz"
-    "$(date +%Y-%m-%d)-VAST-$(git describe)-FreeBSD-Release.tar.gz"
+    "VAST-$version-Linux-Release.tar.gz"
+    "VAST-$version-Darwin-Release.tar.gz"
+    "VAST-$version-FreeBSD-Release.tar.gz"
 )
 
 attachments=""
 retrieve_artifacts_mule() {
     mkdir release_artifacts
     for artifact in "${files_to_upload[@]}"; do
-        artifact_path="/home/mule/test-artifacts/$artifact"
-        if ! ssh -i download_key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null mule@tenzir.dfn-cert.de stat "$artifact_path"> /dev/null 2>&1; then
+        artifact_path="/home/mule/artifacts/$artifact"
+        if ! ssh -i download_key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
+                mule@tenzir.dfn-cert.de stat "$artifact_path"> /dev/null 2>&1; then
             echo File not found: "$artifact_path"
             exit 1
         fi
@@ -34,13 +38,15 @@ retrieve_artifacts_mule() {
 }
 
 create_changelog() {
-    echo -e "$CIRRUS_TAG\n" > github_release.md
-    echo -e "\n" >> github_release.md
-    echo -e "$annotation\n" >> github_release.md
-    echo -e "\n" >> github_release.md
-    echo "[CHANGELOG.md](https://github.com/tenzir/vast/$CIRRUS_TAG/CHANGELOG.md)" >> github_release.md
+    cat << EOF > github_release.md
+$CIRRUS_TAG
+
+$annotation
+
+For a detailed list of changes, please refer to our [changelog](https://github.com/tenzir/vast/$CIRRUS_TAG/CHANGELOG.md).
+EOF
 }
 
 retrieve_artifacts_mule
 create_changelog
-eval hub release create "$attachments" -F github_release.md "$CIRRUS_TAG"
+hub release create $attachments -F github_release.md "$CIRRUS_TAG"
