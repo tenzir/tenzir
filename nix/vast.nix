6fc98fe9e80b5add75cc22668299f93e3b758755
Tobias Mayer <tobim@fastmail.fm>
2020-01-14 11:40:21 +0100

    Extract VAST expression as function

---
+++ nix/vast.nix
@@ -0,0 +1,74 @@
+{ stdenv, lib, nix-gitignore, runCommand
+, cmake, pkgconfig, git, pandoc
+, caf, libpcap, arrow-cpp
+, python3Packages, jq, tcpdump
+, static ? stdenv.hostPlatform.isMusl
+}:
+let
+  isCross = stdenv.buildPlatform != stdenv.hostPlatform;
+
+  python = python3Packages.python.withPackages( ps: with ps; [
+    coloredlogs
+    jsondiff
+    pyarrow
+    pyyaml
+    schema
+  ]);
+
+  src = nix-gitignore.gitignoreSource [ "/nix" ] ../.;
+
+  nix-gitDescribe = src:
+    runCommand "gitDescribe.out" {} ''
+      cd ${src}
+      echo -n "$(${git}/bin/git describe --tags --long --dirty)" > $out
+    '';
+
+  version = stdenv.lib.fileContents (nix-gitDescribe src);
+in
+
+stdenv.mkDerivation rec {
+  inherit src version;
+  pname = "vast";
+
+  preConfigure = ''
+    substituteInPlace cmake/FindPCAP.cmake \
+      --replace /bin/sh "${stdenv.shell}" \
+      --replace nm "''${NM}"
+  '';
+
+  nativeBuildInputs = [ cmake pkgconfig pandoc ];
+  buildInputs = [ arrow-cpp caf libpcap ];
+
+  cmakeFlags = [
+    "-DCAF_ROOT_DIR=${caf}"
+    "-DVAST_RELOCATABLE_INSTALL=OFF"
+    "-DVAST_VERSION_TAG=${version}"
+    # gen-table-slices runs at build time
+    "-DCMAKE_SKIP_BUILD_RPATH=OFF"
+  ] ++ lib.optional static "-DVAST_STATIC_EXECUTABLE:BOOL=ON";
+
+  hardeningDisable = lib.optional static "pic";
+
+  doCheck = false;
+  checkTarget = "test";
+
+  dontStrip = isCross;
+  postFixup = lib.optionalString isCross ''
+    ${stdenv.cc.targetPrefix}strip -s $out/bin/vast
+    ${stdenv.cc.targetPrefix}strip -s $out/bin/zeek-to-vast
+  '';
+
+  doInstallCheck = true;
+  installCheckInputs = [ python jq tcpdump ];
+  installCheckPhase = ''
+    python ../integration/integration.py --app ${placeholder "out"}/bin/vast
+  '';
+
+  meta = with lib; {
+    description = "Visibility Across Space and Time";
+    homepage = http://vast.io/;
+    license = licenses.bsd3;
+    platforms = platforms.unix;
+    maintainers = with maintainers; [ tobim ];
+  };
+}
