c2b6977dddbc5a63f79baf349f0c6cf1cb75938c
Benno Evers <benno.martin.evers@gmail.com>
2020-04-15 11:21:41 +0200

    Change default listen address

--- libvast/src/system/start_command.cpp
+++ libvast/src/system/start_command.cpp
@@ -65,7 +65,7 @@ caf::message start_command_impl(start_command_extra_steps extra_steps,
     return caf::make_message(std::move(node_opt.error()));
   auto& node = node_opt->get();
   // Publish our node.
-  auto host = node_endpoint.host.empty() ? nullptr : node_endpoint.host.c_str();
+  auto host = node_endpoint.host.empty() ? "localhost" : node_endpoint.host.c_str();
   auto publish = [&]() -> caf::expected<uint16_t> {
     if (use_encryption)
 #ifdef VAST_USE_OPENSSL
@@ -80,8 +80,8 @@ caf::message start_command_impl(start_command_extra_steps extra_steps,
   auto bound_port = publish();
   if (!bound_port)
     return caf::make_message(std::move(bound_port.error()));
-  VAST_INFO_ANON("VAST node is listening on", (host ? host : "")
-                 << ':' << *bound_port);
+  auto listen_addr = std::string{host} + ':' + std::to_string(*bound_port);
+  VAST_INFO_ANON("VAST node is listening on", listen_addr);
   // Run user-defined extra code.
   if (extra_steps != nullptr)
     if (auto err = extra_steps(self, invocation.options, node))
