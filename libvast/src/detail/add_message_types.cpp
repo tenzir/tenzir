1d14c7e74ee7897e0edc8579213c1671523f3462
Dominik Lohmann <mail@dominiklohmann.de>
2020-04-15 13:34:10 +0200

    Register system::report as a valid message type

--- libvast/src/detail/add_message_types.cpp
+++ libvast/src/detail/add_message_types.cpp
@@ -64,6 +64,7 @@ void add_message_types(caf::actor_system_config& cfg) {
   cfg.add_message_type<system::component_map_entry>(
     "vast::system::component_map_entry");
   cfg.add_message_type<system::registry>("vast::system::registry");
+  cfg.add_message_type<system::report>("vast::system::report");
   cfg.add_message_type<system::performance_report>("vast::system::performance_"
                                                    "report");
   cfg.add_message_type<system::query_status>("vast::system::query_status");
