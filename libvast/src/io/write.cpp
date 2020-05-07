380d0988e32b7f56254f6f0022022fd170a0ed92
Matthias Vallentin <matthias@tenzir.com>
2020-05-07 12:53:17 +0200

    Add one-shot read/write utilities

---
+++ libvast/src/io/write.cpp
@@ -0,0 +1,33 @@
+/******************************************************************************
+ *                    _   _____   __________                                  *
+ *                   | | / / _ | / __/_  __/     Visibility                   *
+ *                   | |/ / __ |_\ \  / /          Across                     *
+ *                   |___/_/ |_/___/ /_/       Space and Time                 *
+ *                                                                            *
+ * This file is part of VAST. It is subject to the license terms in the       *
+ * LICENSE file found in the top-level directory of this distribution and at  *
+ * http://vast.io/license. No part of VAST, including this file, may be       *
+ * copied, modified, propagated, or distributed except according to the terms *
+ * contained in the LICENSE file.                                             *
+ ******************************************************************************/
+
+#include "vast/io/write.hpp"
+
+#include "vast/error.hpp"
+#include "vast/filesystem.hpp"
+
+namespace vast::io {
+
+caf::error write(const path& filename, span<const byte> xs) {
+  file f{filename};
+  if (!f.open(file::write_only))
+    return make_error(ec::filesystem_error, "failed open file");
+  size_t bytes_written;
+  if (!f.write(xs.data(), xs.size(), &bytes_written))
+    return make_error(ec::filesystem_error, "failed to write chunk");
+  if (bytes_written != xs.size())
+    return make_error(ec::filesystem_error, "incomplete write");
+  return caf::none;
+}
+
+} // namespace vast::io
---
+++ libvast/vast/io/write.hpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+ *                    _   _____   __________                                  *
+ *                   | | / / _ | / __/_  __/     Visibility                   *
+ *                   | |/ / __ |_\ \  / /          Across                     *
+ *                   |___/_/ |_/___/ /_/       Space and Time                 *
+ *                                                                            *
+ * This file is part of VAST. It is subject to the license terms in the       *
+ * LICENSE file found in the top-level directory of this distribution and at  *
+ * http://vast.io/license. No part of VAST, including this file, may be       *
+ * copied, modified, propagated, or distributed except according to the terms *
+ * contained in the LICENSE file.                                             *
+ ******************************************************************************/
+
+#include "vast/byte.hpp"
+#include "vast/fwd.hpp"
+#include "vast/span.hpp"
+
+#include "caf/fwd.hpp"
+
+namespace vast::io {
+
+/// Performs a one-shot write of an immutable buffer into a file.
+/// @param filename The file to write to.
+/// @param xs The buffer to read from.
+/// @returns An error if the operation failed.
+caf::error write(const path& filename, span<const byte> xs);
+
+} // namespace vast::io
