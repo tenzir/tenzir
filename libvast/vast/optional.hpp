f4a50d74c05cb9e31564d45d700dceadf19e0024
Benno Evers <benno.martin.evers@gmail.com>
2020-05-18 18:42:03 +0200

    Add conversion between optional types

--- libvast/vast/optional.hpp
+++ libvast/vast/optional.hpp
@@ -15,10 +15,20 @@
 
 #include <caf/optional.hpp>
 
+#include <optional>
+
 namespace vast {
 
 /// A drop-in replacement for C++17's std::optional.
 using caf::optional;
+
+template <typename T>
+std::optional<T> to_std(caf::optional<T>&& opt) {
+  std::optional<T> result;
+  if (opt)
+    result = std::move(*opt);
+  return result;
+}
 
 } // namespace vast
 
