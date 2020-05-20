1d3aafa70279fb13981469a596f10571fffc4e86
Felix Ortmann <flx.ortmann@gmail.com>
2020-05-20 14:43:51 +0200

    Bump library version for next release

--- pyvast/setup.py
+++ pyvast/setup.py
@@ -33,5 +33,5 @@ setup(
     python_requires=">=3.7",
     setup_requires=["setuptools", "wheel"],
     url="https://github.com/tenzir/vast",
-    version="2020.04.24",
+    version="2020.05.29",
 )
