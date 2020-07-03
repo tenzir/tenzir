e3232b125583b80e57ccec0c1ec1c953b95fc0e8
Tobias Mayer <tobim@fastmail.fm>
2020-07-03 16:03:49 +0200

    Extend shell.nix to allow toolchain selection

--- shell.nix
+++ shell.nix
@@ -1,7 +1,20 @@
-{ pkgs ? import <nixpkgs> {} }:
-((import ./default.nix { inherit pkgs; }).override{
-  versionOverride = "dev";
-}).overrideAttrs (old: {
-  src = null;
-  hardeningDisable = (old.hardeningDisable or []) ++ [ "fortify" ];
-})
+{ pkgs ? import <nixpkgs> { }, useClang ? false }:
+let
+  inherit (pkgs) lib;
+  llvmPkgs = pkgs.llvmPackages_10;
+  stdenv = if useClang then llvmPkgs.libcxxStdenv else pkgs.stdenv;
+  mkShell = pkgs.mkShell.override { inherit stdenv; };
+  vast =
+    ((import ./default.nix { inherit pkgs; }).override{
+      versionOverride = "dev";
+    }).overrideAttrs (old: {
+      src = null;
+    });
+in
+mkShell {
+  name = "vast-dev-" + (if useClang then "clang" else "gcc");
+  hardeningDisable = [ "fortify" ];
+  inputsFrom = [ vast ];
+  buildInputs = [ llvmPkgs.lldClang.bintools ];
+  NIX_CFLAGS_LINK = "-fuse-ld=lld";
+}
