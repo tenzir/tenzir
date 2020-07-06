929beb8a75eef2f6f04fe0234ec3f0c24a73ecc1
Tobias Mayer <tobim@fastmail.fm>
2020-07-06 07:03:46 +0200

    Optionally use the rev from origin

--- nix/static-binary.sh
+++ nix/static-binary.sh
@@ -1,19 +1,32 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -i bash -p git nix coreutils
+#!nix-shell -i bash -p git nix coreutils nix-prefetch-github
 
 dir="$(dirname "$(readlink -f "$0")")"
+toplevel="$(git -C ${dir} rev-parse --show-toplevel)"
 desc="$(git -C ${dir} describe)"
+vast_rev="$(git -C "${toplevel}" rev-parse HEAD)"
 
-target="${1:-vast}"
+target="${STATIC_BINARY_TARGET:-vast}"
 
-read -r -d '' exp <<EOF
-with import ${dir} {};
-pkgsStatic."${target}".override {
-  versionOverride = "${desc}";
-}
+if [ "$1" == "--use-head" ]; then
+  source_json="$(nix-prefetch-github --rev=${vast_rev} tenzir vast)"
+  read -r -d '' exp <<EOF
+  with import ${dir} {};
+  pkgsStatic."${target}".override {
+    vast-source = fetchFromGitHub (builtins.fromJSON ''${source_json}'');
+    versionOverride = "${desc}";
+  }
 EOF
+else
+  read -r -d '' exp <<EOF
+  with import ${dir} {};
+  pkgsStatic."${target}".override {
+    versionOverride = "${desc}";
+  }
+EOF
+fi
 
-nix-build -E "${exp}"
+result=$(nix-build --no-out-link -E "${exp}")
 
 mkdir -p build
 tar -C result \
