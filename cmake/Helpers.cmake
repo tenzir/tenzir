9d10d42f81e3732d619b22ee4e70cf4ece83fa7b
Dominik Lohmann <mail@dominiklohmann.de>
2020-07-21 10:23:34 +0200

    Link targets for multi-config generators
    This allows the use of `./build/bin/vast` to run the latest compiled
    build configuration for multi-config generators.

--- cmake/Helpers.cmake
+++ cmake/Helpers.cmake
@@ -52,3 +52,22 @@ macro (install_symlink _filepath _sympath)
                     \"${_rel}\" \"\${_dst}\")
   ")
 endmacro (install_symlink)
+
+# For multi-config generators, the actual binary output path is not
+# `${EXECUTABLE_OUTPUT_PATH}`, but rather `${EXECUTABLE_OUTPUT_PATH}/$<CONFIG>`.
+# This macro creates a symlink to the actual target file in
+# `${EXECUTABLE_OUTPUT_PATH}`, such that the latest compiled build configuration
+# can be run using `./${EXECUTABLE_OUTPUT_PATH}/${_target}`.
+macro (multi_config_target_symlink _target)
+  get_property(_is_multi_config GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
+  if (_is_multi_config)
+    add_custom_command(
+      TARGET "${_target}"
+      POST_BUILD
+      COMMAND "${CMAKE_COMMAND}" -E create_symlink "$<TARGET_FILE:${_target}>"
+              "${EXECUTABLE_OUTPUT_PATH}/${_target}"
+      COMMENT
+        "Linking ${EXECUTABLE_OUTPUT_PATH}/${_target} -> $<TARGET_FILE:${_target}>"
+    )
+  endif ()
+endmacro (multi_config_target_symlink)
