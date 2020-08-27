c05af2ac14d36f602907e686fd112a55b08a91ca
Tobias Mayer <tobim@fastmail.fm>
2020-08-27 13:51:13 +0200

    Update nixpkgs to dc791270

--- nix/pinned.nix
+++ nix/pinned.nix
@@ -1,9 +1,9 @@
 # See https://nixos.wiki/wiki/FAQ/Pinning_Nixpkgs for more information on pinning
 builtins.fetchTarball {
   # Descriptive name to make the store path easier to identify
-  name = "nixpkgs-unstable-2020-06-07";
-  # Commit hash for nixpkgs-unstable as of date
-  url = https://github.com/NixOS/nixpkgs-channels/archive/029a5de08390bb03c3f44230b064fd1850c6658a.tar.gz;
+  name = "nixpkgs-2020-08-26";
+  # Commit hash for nixpkgs as of date
+  url = https://github.com/NixOS/nixpkgs/archive/dc7912706e959646d82a97477f5ec637fbef4c27.tar.gz;
   # Hash obtained using `nix-prefetch-url --unpack <url>`
-  sha256 = "03fjkzhrs2avcvdabgm7a65rnyjaqbqdnv4q86qyjkkwg64g5m8x";
+  sha256 = "1l31c7vg7c9k2n4ysjddgqgf13gfvf0agfpxq9ln8rxss22cddhw";
 }
