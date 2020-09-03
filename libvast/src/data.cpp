eef6bd67bd59726cbe058761997ca7b773f9b118
Matthias Vallentin <matthias@tenzir.com>
2020-09-03 18:03:42 +0200

    Remove extraneous semicolon

--- libvast/src/data.cpp
+++ libvast/src/data.cpp
@@ -161,7 +161,7 @@ caf::optional<record> make_record(const record_type& rt, std::vector<data> xs) {
   auto begin = xs.begin();
   auto end = xs.end();
   return make_record(rt, begin, end);
-};
+}
 
 record flatten(const record& r) {
   record result;
