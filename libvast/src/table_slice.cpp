5ffd80f0138c8871c105c3e33b179f7e82309a2a
Dominik Lohmann <mail@dominiklohmann.de>
2020-11-04 10:50:52 +0100

    Move test utils for table_slice to libvast_test

--- libvast/src/table_slice.cpp
+++ libvast/src/table_slice.cpp
@@ -19,12 +19,10 @@
 #include "vast/defaults.hpp"
 #include "vast/detail/assert.hpp"
 #include "vast/detail/byte_swap.hpp"
-#include "vast/detail/append.hpp"
 #include "vast/detail/overload.hpp"
 #include "vast/error.hpp"
 #include "vast/expression.hpp"
 #include "vast/factory.hpp"
-#include "vast/format/test.hpp"
 #include "vast/ids.hpp"
 #include "vast/logger.hpp"
 #include "vast/table_slice_builder.hpp"
@@ -236,33 +234,6 @@ caf::error unpack(const fbs::table_slice::v0& x, table_slice_ptr& y) {
   return source(y);
 }
 
-caf::expected<std::vector<table_slice_ptr>>
-make_random_table_slices(size_t num_slices, size_t slice_size,
-                         record_type layout, id offset, size_t seed) {
-  schema sc;
-  sc.add(layout);
-  // We have no access to the actor system, so we can only pick the default
-  // table slice type here. This ignores any user-defined overrides. However,
-  // this function is only meant for testing anyways.
-  caf::settings opts;
-  caf::put(opts, "vast.import.test.seed", seed);
-  caf::put(opts, "vast.import.max-events", std::numeric_limits<size_t>::max());
-  format::test::reader src{defaults::import::table_slice_type, std::move(opts),
-                           nullptr};
-  src.schema(std::move(sc));
-  std::vector<table_slice_ptr> result;
-  auto add_slice = [&](table_slice_ptr ptr) {
-    ptr.unshared().offset(offset);
-    offset += ptr->rows();
-    result.emplace_back(std::move(ptr));
-  };
-  result.reserve(num_slices);
-  if (auto err = src.read(num_slices * slice_size, slice_size, add_slice)
-                   .first)
-    return err;
-  return result;
-}
-
 void select(std::vector<table_slice_ptr>& result, const table_slice_ptr& xs,
             const ids& selection) {
   VAST_ASSERT(xs != nullptr);
@@ -362,33 +333,6 @@ uint64_t rows(const std::vector<table_slice_ptr>& slices) {
   auto result = uint64_t{0};
   for (auto& slice : slices)
     result += slice->rows();
-  return result;
-}
-
-std::vector<std::vector<data>>
-to_data(const table_slice& slice, size_t first_row, size_t num_rows) {
-  VAST_ASSERT(first_row < slice.rows());
-  VAST_ASSERT(num_rows <= slice.rows() - first_row);
-  if (num_rows == 0)
-    num_rows = slice.rows() - first_row;
-  std::vector<std::vector<data>> result;
-  result.reserve(num_rows);
-  for (size_t i = 0; i < num_rows; ++i) {
-    std::vector<data> xs;
-    xs.reserve(slice.columns());
-    for (size_t j = 0; j < slice.columns(); ++j)
-      xs.emplace_back(materialize(slice.at(first_row + i, j)));
-    result.push_back(std::move(xs));
-  }
-  return result;
-}
-
-std::vector<std::vector<data>>
-to_data(const std::vector<table_slice_ptr>& slices) {
-  std::vector<std::vector<data>> result;
-  result.reserve(rows(slices));
-  for (auto& slice : slices)
-    detail::append(result, to_data(*slice));
   return result;
 }
 
