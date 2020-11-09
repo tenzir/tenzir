6c01e10416fd6d12a7349537943e124e180f39ab
Dominik Lohmann <mail@dominiklohmann.de>
2020-11-09 14:59:52 +0100

    Add note on as_<encoding>_table_slice functions

--- libvast/vast/table_slice.hpp
+++ libvast/vast/table_slice.hpp
@@ -168,6 +168,9 @@ public:
 
 #if VAST_HAVE_ARROW
 
+  // FIXME: Introduce `as_arrow_table_slice` and `as_msgpack_table_slice`
+  // functions after removing `legacy_table_slice_ptr`, and remove this
+  // function.
   /// Converts a table slice to an Apache Arrow Record Batch.
   /// @returns The pointer to the Record Batch on success, or `nullptr`.
   /// @param x The table slice to convert.
