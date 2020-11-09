feabdc8c866d3451815211050e4862df0e45b8ae
Dominik Lohmann <mail@dominiklohmann.de>
2020-11-09 22:47:08 +0100

    Cleanup table_slice_builder
    This prepares the `table_slice_builder` for the upcoming changes by
    doing some much-needed housecleaning.

--- libvast/src/table_slice_builder.cpp
+++ libvast/src/table_slice_builder.cpp
@@ -13,43 +13,56 @@
 
 #include "vast/table_slice_builder.hpp"
 
-#include <algorithm>
-
 #include "vast/data.hpp"
 #include "vast/detail/overload.hpp"
+#include "vast/table_slice.hpp"
+
+#include <caf/make_counted.hpp>
+
+#include <algorithm>
 
 namespace vast {
 
-table_slice_builder::table_slice_builder(record_type layout)
-  : layout_(std::move(layout)) { // nop
-}
+// -- constructors, destructors, and assignment operators ----------------------
 
-table_slice_builder::~table_slice_builder() {
+table_slice_builder::table_slice_builder(record_type layout) noexcept
+  : layout_(std::move(layout)) {
   // nop
 }
+
+table_slice_builder::~table_slice_builder() noexcept {
+  // nop
+}
+
+// -- properties ---------------------------------------------------------------
 
 bool table_slice_builder::recursive_add(const data& x, const type& t) {
-  return caf::visit(
-    detail::overload{
-      [&](const list& xs, const record_type& rt) {
-        for (size_t i = 0; i < xs.size(); ++i) {
-          if (!recursive_add(xs[i], rt.fields[i].type))
-            return false;
-        }
-        return true;
-      },
-      [&](const auto&, const auto&) { return add(make_view(x)); },
+  auto f = detail::overload{
+    [&](const list& xs, const record_type& rt) {
+      for (size_t i = 0; i < xs.size(); ++i) {
+        if (!recursive_add(xs[i], rt.fields[i].type))
+          return false;
+      }
+      return true;
     },
-    x, t);
-}
-
-void table_slice_builder::reserve(size_t) {
-  // nop
+    [&](const auto&, const auto&) { return add(make_view(x)); },
+  };
+  return caf::visit(f, x, t);
 }
 
 size_t table_slice_builder::columns() const noexcept {
   return layout_.fields.size();
 }
+
+const record_type& table_slice_builder::layout() const noexcept {
+  return layout_;
+}
+
+void table_slice_builder::reserve([[maybe_unused]] size_t num_rows) {
+  // nop
+}
+
+// -- intrusive_ptr facade -----------------------------------------------------
 
 void intrusive_ptr_add_ref(const table_slice_builder* ptr) {
   intrusive_ptr_add_ref(static_cast<const caf::ref_counted*>(ptr));
