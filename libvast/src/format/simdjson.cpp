f390ff63f108de0e3969dece5665ab907045dfd4
ngrodzitski <utromvecherom@gmail.com>
2020-12-22 23:50:06 +0300

    Add a copy of json reader routine for future transformation to simdjson reader

---
+++ libvast/src/format/simdjson.cpp
@@ -0,0 +1,231 @@
+/******************************************************************************
+ *                    _   _____   __________                                  *
+ *                   | | / / _ | / __/_  __/     Visibility                   *
+ *                   | |/ / __ |_\ \  / /          Across                     *
+ *                   |___/_/ |_/___/ /_/       Space and Time                 *
+ *                                                                            *
+ * This file is part of VAST. It is subject to the license terms in the       *
+ * LICENSE file found in the top-level directory of this distribution and at  *
+ * http://vast.io/license. No part of VAST, including this file, may be       *
+ * copied, modified, propagated, or distributed except according to the terms *
+ * contained in the LICENSE file.                                             *
+ ******************************************************************************/
+
+#include "vast/concept/parseable/vast/json.hpp"
+
+#include "vast/concept/parseable/vast/address.hpp"
+#include "vast/concept/parseable/vast/subnet.hpp"
+#include "vast/concept/parseable/vast/time.hpp"
+#include "vast/concept/printable/to_string.hpp"
+#include "vast/concept/printable/vast/data.hpp"
+#include "vast/concept/printable/vast/json.hpp"
+#include "vast/data.hpp"
+#include "vast/format/json.hpp"
+#include "vast/logger.hpp"
+#include "vast/policy/include_field_names.hpp"
+#include "vast/table_slice.hpp"
+#include "vast/table_slice_builder.hpp"
+#include "vast/type.hpp"
+#include "vast/view.hpp"
+
+#include <caf/detail/pretty_type_name.hpp>
+#include <caf/expected.hpp>
+#include <caf/none.hpp>
+
+namespace vast::format::json {
+namespace {
+
+struct convert {
+  template <class T>
+  using expected = caf::expected<T>;
+  using json = vast::json;
+
+  caf::expected<data> operator()(json::boolean b, const bool_type&) const {
+    return b;
+  }
+
+  caf::expected<data> operator()(json::number n, const integer_type&) const {
+    return detail::narrow_cast<integer>(n);
+  }
+
+  caf::expected<data> operator()(json::number n, const count_type&) const {
+    return detail::narrow_cast<count>(n);
+  }
+
+  caf::expected<data> operator()(json::number n, const real_type&) const {
+    return detail::narrow_cast<real>(n);
+  }
+
+  caf::expected<data> operator()(json::number s, const time_type&) const {
+    auto secs = std::chrono::duration<json::number>(s);
+    auto since_epoch = std::chrono::duration_cast<duration>(secs);
+    return time{since_epoch};
+  }
+
+  caf::expected<data> operator()(json::number s, const duration_type&) const {
+    auto secs = std::chrono::duration<json::number>(s);
+    return std::chrono::duration_cast<duration>(secs);
+  }
+
+  caf::expected<data> operator()(json::string s, const string_type&) const {
+    return s;
+  }
+
+  template <class T,
+            typename std::enable_if_t<has_parser_v<type_to_data<T>>, int> = 0>
+  caf::expected<data> operator()(const json::string& s, const T&) const {
+    using value_type = type_to_data<T>;
+    value_type x;
+    if (!make_parser<value_type>{}(s, x))
+      return make_error(ec::parse_error, "unable to parse",
+                        caf::detail::pretty_type_name(typeid(value_type)), ":",
+                        s);
+    return x;
+  }
+
+  caf::expected<data>
+  operator()(const json::string& s, const enumeration_type& e) const {
+    auto i = std::find(e.fields.begin(), e.fields.end(), s);
+    if (i == e.fields.end())
+      return make_error(ec::parse_error, "invalid:", s);
+    return detail::narrow_cast<enumeration>(std::distance(e.fields.begin(), i));
+  }
+
+  caf::expected<data>
+  operator()(const json::array& a, const list_type& v) const {
+    list xs;
+    xs.reserve(a.size());
+    for (auto& x : a) {
+      if (auto elem = caf::visit(*this, x, v.value_type))
+        xs.push_back(*std::move(elem));
+      else
+        return elem;
+    }
+    return xs;
+  }
+
+  caf::expected<data>
+  operator()(const json::object& o, const map_type& m) const {
+    map xs;
+    xs.reserve(o.size());
+    for (auto& [k, v] : o) {
+      // TODO: Properly unwrap the key type instead of wrapping is in json.
+      auto key = caf::visit(*this, json{k}, m.key_type);
+      if (!key)
+        return key.error();
+      auto val = caf::visit(*this, v, m.value_type);
+      if (!val)
+        return val.error();
+      xs[*key] = *val;
+    }
+    return xs;
+  }
+
+  caf::expected<data>
+  operator()(const json::string& str, const bool_type&) const {
+    if (bool x; parsers::json_boolean(str, x))
+      return x;
+    return make_error(ec::convert_error, "cannot convert from", str, "to bool");
+  }
+
+  caf::expected<data>
+  operator()(const json::string& str, const real_type&) const {
+    if (real x; parsers::json_number(str, x))
+      return x;
+    return make_error(ec::convert_error, "cannot convert from", str, "to real");
+  }
+
+  caf::expected<data>
+  operator()(const json::string& str, const integer_type&) const {
+    if (integer x; parsers::json_int(str, x))
+      return x;
+    if (real x; parsers::json_number(str, x)) {
+      VAST_WARNING_ANON("json-reader narrowed", str, "to type int");
+      return detail::narrow_cast<integer>(x);
+    }
+    return make_error(ec::convert_error, "cannot convert from", str, "to int");
+  }
+
+  caf::expected<data>
+  operator()(const json::string& str, const count_type&) const {
+    if (count x; parsers::json_count(str, x))
+      return x;
+    if (real x; parsers::json_number(str, x)) {
+      VAST_WARNING_ANON("json-reader narrowed", str, "to type count");
+      return detail::narrow_cast<count>(x);
+    }
+    return make_error(ec::convert_error, "cannot convert from", str,
+                      "to count");
+  }
+
+  template <class T, class U>
+  caf::expected<data> operator()(T, U) const {
+    if constexpr (std::is_same_v<std::decay_t<T>, caf::none_t>) {
+      // Iff there is no specific conversion available, but the LHS is JSON
+      // `null`, we always want to return VAST `nil`.
+      return caf::none;
+    } else {
+      VAST_ERROR_ANON("json-reader cannot convert from",
+                      caf::detail::pretty_type_name(typeid(T)), "to",
+                      caf::detail::pretty_type_name(typeid(U)));
+      return make_error(ec::syntax_error, "invalid json type");
+    }
+  }
+};
+
+const vast::json* lookup(std::string_view field, const vast::json::object& xs) {
+  VAST_ASSERT(!field.empty());
+  auto lookup_flat = [&]() {
+    auto r = xs.find(field);
+    return r == xs.end() ? nullptr : &r->second;
+  };
+  auto i = field.find('.');
+  if (i == std::string_view::npos)
+    return lookup_flat();
+  // We have to deal with a nested field name in a potentially nested JSON
+  // object.
+  auto r = xs.find(field.substr(0, i));
+  if (r == xs.end())
+    // Attempt to access JSON field with flattened name.
+    return lookup_flat();
+  auto obj = caf::get_if<vast::json::object>(&r->second);
+  if (obj == nullptr)
+    return nullptr;
+  field.remove_prefix(i + 1);
+  return lookup(field, *obj);
+}
+
+} // namespace
+
+caf::error writer::write(const table_slice& x) {
+  json_printer<policy::oneline> printer;
+  return print<policy::include_field_names>(printer, x, "{", ", ", "}");
+}
+
+const char* writer::name() const {
+  return "json-writer";
+}
+
+caf::error add(table_slice_builder& builder, const vast::json::object& xs,
+               const record_type& layout) {
+  for (auto& field : layout.fields) {
+    auto i = lookup(field.name, xs);
+    // Non-existing fields are treated as empty (unset).
+    if (!i) {
+      if (!builder.add(make_data_view(caf::none)))
+        return make_error(ec::unspecified, "failed to add caf::none to table "
+                                           "slice builder");
+      continue;
+    }
+    auto x = caf::visit(convert{}, *i, field.type);
+    if (!x)
+      return make_error(ec::convert_error, x.error().context(),
+                        "could not convert", field.name, ":", to_string(*i));
+    if (!builder.add(make_data_view(*x)))
+      return make_error(ec::type_clash, "unexpected type", field.name, ":",
+                        to_string(*i));
+  }
+  return caf::none;
+}
+
+} // namespace vast::format::json
---
+++ libvast/vast/format/simdjson.hpp
@@ -0,0 +1,269 @@
+/******************************************************************************
+ *                    _   _____   __________                                  *
+ *                   | | / / _ | / __/_  __/     Visibility                   *
+ *                   | |/ / __ |_\ \  / /          Across                     *
+ *                   |___/_/ |_/___/ /_/       Space and Time                 *
+ *                                                                            *
+ * This file is part of VAST. It is subject to the license terms in the       *
+ * LICENSE file found in the top-level directory of this distribution and at  *
+ * http://vast.io/license. No part of VAST, including this file, may be       *
+ * copied, modified, propagated, or distributed except according to the terms *
+ * contained in the LICENSE file.                                             *
+ ******************************************************************************/
+
+#pragma once
+
+#include "vast/concept/hashable/hash_append.hpp"
+#include "vast/concept/hashable/xxhash.hpp"
+#include "vast/concept/parseable/vast/json.hpp"
+#include "vast/defaults.hpp"
+#include "vast/detail/flat_map.hpp"
+#include "vast/detail/line_range.hpp"
+#include "vast/detail/string.hpp"
+#include "vast/error.hpp"
+#include "vast/format/multi_layout_reader.hpp"
+#include "vast/format/ostream_writer.hpp"
+#include "vast/fwd.hpp"
+#include "vast/json.hpp"
+#include "vast/logger.hpp"
+#include "vast/schema.hpp"
+
+#include <caf/expected.hpp>
+#include <caf/fwd.hpp>
+#include <caf/settings.hpp>
+
+#include <chrono>
+
+namespace vast::format::json {
+
+class writer : public ostream_writer {
+public:
+  using defaults = vast::defaults::export_::json;
+
+  using super = ostream_writer;
+
+  using super::super;
+
+  caf::error write(const table_slice& x) override;
+
+  const char* name() const override;
+};
+
+/// Adds a JSON object to a table slice builder according to a given layout.
+/// @param builder The builder to add the JSON object to.
+/// @param xs The JSON object to add to *builder.
+/// @param layout The record type describing *xs*.
+/// @returns An error iff the operation failed.
+caf::error add(table_slice_builder& builder, const vast::json::object& xs,
+               const record_type& layout);
+
+/// @relates reader
+struct default_selector {
+  caf::optional<record_type> operator()(const vast::json::object& obj) const {
+    if (type_cache.empty())
+      return caf::none;
+    // Iff there is only one type in the type cache, allow the JSON reader to
+    // use it despite not being an exact match.
+    if (type_cache.size() == 1)
+      return type_cache.begin()->second;
+    std::vector<std::string> cache_entry;
+    auto build_cache_entry = [&cache_entry](auto& prefix, const vast::json&) {
+      cache_entry.emplace_back(detail::join(prefix.begin(), prefix.end(), "."));
+    };
+    each_field(vast::json{obj}, build_cache_entry);
+    std::sort(cache_entry.begin(), cache_entry.end());
+    if (auto search_result = type_cache.find(cache_entry);
+        search_result != type_cache.end())
+      return search_result->second;
+    return caf::none;
+  }
+
+  caf::error schema(vast::schema sch) {
+    if (sch.empty())
+      return make_error(ec::invalid_configuration, "no schema provided or type "
+                                                   "too restricted");
+    for (auto& entry : sch) {
+      if (!caf::holds_alternative<record_type>(entry))
+        continue;
+      auto layout = flatten(caf::get<record_type>(entry));
+      std::vector<std::string> cache_entry;
+      for (auto& [k, v] : layout.fields)
+        cache_entry.emplace_back(k);
+      std::sort(cache_entry.begin(), cache_entry.end());
+      type_cache.insert({std::move(cache_entry), std::move(layout)});
+    }
+    return caf::none;
+  }
+
+  vast::schema schema() const {
+    vast::schema result;
+    for (const auto& [k, v] : type_cache)
+      result.add(v);
+    return result;
+  }
+
+  static const char* name() {
+    return "json-reader";
+  }
+
+  detail::flat_map<std::vector<std::string>, record_type> type_cache = {};
+};
+
+/// A reader for JSON data. It operates with a *selector* to determine the
+/// mapping of JSON object to the appropriate record type in the schema.
+template <class Selector = default_selector>
+class reader final : public multi_layout_reader {
+public:
+  using super = multi_layout_reader;
+
+  /// Constructs a JSON reader.
+  /// @param table_slice_type The ID for table slice type to build.
+  /// @param options Additional options.
+  /// @param in The stream of JSON objects.
+  reader(caf::atom_value table_slice_type, const caf::settings& options,
+         std::unique_ptr<std::istream> in = nullptr);
+
+  void reset(std::unique_ptr<std::istream> in);
+
+  caf::error schema(vast::schema sch) override;
+
+  vast::schema schema() const override;
+
+  const char* name() const override;
+
+  vast::system::report status() const override;
+
+protected:
+  caf::error read_impl(size_t max_events, size_t max_slice_size,
+                       consumer& f) override;
+
+private:
+  using iterator_type = std::string_view::const_iterator;
+
+  Selector selector_;
+  std::unique_ptr<std::istream> input_;
+  std::unique_ptr<detail::line_range> lines_;
+  caf::optional<size_t> proto_field_;
+  std::vector<size_t> port_fields_;
+  mutable size_t num_invalid_lines_ = 0;
+  mutable size_t num_unknown_layouts_ = 0;
+  mutable size_t num_lines_ = 0;
+};
+
+// -- implementation ----------------------------------------------------------
+
+template <class Selector>
+reader<Selector>::reader(caf::atom_value table_slice_type,
+                         const caf::settings& options,
+                         std::unique_ptr<std::istream> in)
+  : super(table_slice_type, options) {
+  if (in != nullptr)
+    reset(std::move(in));
+}
+
+template <class Selector>
+void reader<Selector>::reset(std::unique_ptr<std::istream> in) {
+  VAST_ASSERT(in != nullptr);
+  input_ = std::move(in);
+  lines_ = std::make_unique<detail::line_range>(*input_);
+}
+
+template <class Selector>
+caf::error reader<Selector>::schema(vast::schema s) {
+  return selector_.schema(std::move(s));
+}
+
+template <class Selector>
+vast::schema reader<Selector>::schema() const {
+  return selector_.schema();
+}
+
+template <class Selector>
+const char* reader<Selector>::name() const {
+  return Selector::name();
+}
+
+template <class Selector>
+vast::system::report reader<Selector>::status() const {
+  using namespace std::string_literals;
+  uint64_t invalid_line = num_invalid_lines_;
+  uint64_t unknown_layout = num_unknown_layouts_;
+  if (num_invalid_lines_ > 0)
+    VAST_WARNING(this, "failed to parse", num_invalid_lines_, "of", num_lines_,
+                 "recent lines");
+  if (num_unknown_layouts_ > 0)
+    VAST_WARNING(this, "failed to find a matching type for",
+                 num_unknown_layouts_, "of", num_lines_, "recent lines");
+  num_invalid_lines_ = 0;
+  num_unknown_layouts_ = 0;
+  num_lines_ = 0;
+  return {
+    {name() + ".invalid-line"s, invalid_line},
+    {name() + ".unknown-layout"s, unknown_layout},
+  };
+}
+
+template <class Selector>
+caf::error reader<Selector>::read_impl(size_t max_events, size_t max_slice_size,
+                                       consumer& cons) {
+  VAST_TRACE("json-reader", VAST_ARG(max_events), VAST_ARG(max_slice_size));
+  VAST_ASSERT(max_events > 0);
+  VAST_ASSERT(max_slice_size > 0);
+  size_t produced = 0;
+  table_slice_builder_ptr bptr = nullptr;
+  while (produced < max_events) {
+    if (lines_->done())
+      return finish(cons, make_error(ec::end_of_input, "input exhausted"));
+    if (batch_events_ > 0 && batch_timeout_ > reader_clock::duration::zero()
+        && last_batch_sent_ + batch_timeout_ < reader_clock::now()) {
+      VAST_DEBUG(this, "reached batch timeout");
+      return finish(cons, ec::timeout);
+    }
+    bool timed_out = lines_->next_timeout(read_timeout_);
+    if (timed_out) {
+      VAST_DEBUG(this, "stalled at line", lines_->line_number());
+      return ec::stalled;
+    }
+    auto& line = lines_->get();
+    ++num_lines_;
+    if (line.empty()) {
+      // Ignore empty lines.
+      VAST_DEBUG(this, "ignores empty line at", lines_->line_number());
+      continue;
+    }
+    vast::json j;
+    if (!parsers::json(line, j)) {
+      if (num_invalid_lines_ == 0)
+        VAST_WARNING(this, "failed to parse line", lines_->line_number(), ":",
+                     line);
+      ++num_invalid_lines_;
+      continue;
+    }
+    auto xs = caf::get_if<vast::json::object>(&j);
+    if (!xs)
+      return make_error(ec::type_clash, "not a json object");
+    auto&& layout = selector_(*xs);
+    if (!layout) {
+      if (num_unknown_layouts_ == 0)
+        VAST_WARNING(this, "failed to find a matching type at line",
+                     lines_->line_number(), ":", line);
+      ++num_unknown_layouts_;
+      continue;
+    }
+    bptr = builder(*layout);
+    if (bptr == nullptr)
+      return make_error(ec::parse_error, "unable to get a builder");
+    if (auto err = add(*bptr, *xs, *layout)) {
+      err.context() += caf::make_message("line", lines_->line_number());
+      return finish(cons, err);
+    }
+    produced++;
+    batch_events_++;
+    if (bptr->rows() == max_slice_size)
+      if (auto err = finish(cons, bptr))
+        return err;
+  }
+  return finish(cons);
+}
+
+} // namespace vast::format::json
