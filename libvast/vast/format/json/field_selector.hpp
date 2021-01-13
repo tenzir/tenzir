935daac71735cf3749d9be266417a883c5b502ac
Dominik Lohmann <mail@dominiklohmann.de>
2021-01-13 18:39:32 +0100

    Align unknown type warnings
    This changes the log message to function exactly like it does for the
    regular JSON reader.

--- libvast/vast/format/json/field_selector.hpp
+++ libvast/vast/format/json/field_selector.hpp
@@ -63,10 +63,13 @@ struct field_selector {
                    "field with a non-string value");
       return caf::none;
     }
-    auto it = types.find(std::string{event_type.value()});
+    auto field = std::string{event_type.value()};
+    auto it = types.find(field);
     if (it == types.end()) {
-      VAST_VERBOSE(this, "does not have a layout for", Specification::field,
-                   std::string{event_type.value()});
+      // Keep a list of failed keys to avoid spamming the user with warnings.
+      if (unknown_types.insert(field).second)
+        VAST_WARNING(this, "does not have a layout for", Specification::field,
+                     field);
       return caf::none;
     }
     return it->second;
