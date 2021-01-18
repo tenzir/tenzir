c436480961c8c4c36ecba88b965906476b9004f3
Benno Evers <benno.evers@tenzir.com>
2021-01-18 17:28:08 +0100

    Add debug tool to replicate index startup

---
+++ tools/meta-index-dump/meta_index_dump.cpp
@@ -0,0 +1,150 @@
+/******************************************************************************
+ *                    _   _____   __________                                  *
+ *                   | | / / _ | / __/_  __/     Visibility                   *
+ *                   | |/ / __ |_\ \  / /          Across                     *
+ *                   |___/_/ |_/___/ /_/       Space and Time                 *
+ *                                                                            *
+ * This file is part of VAST. It is subject to the license terms in the       *
+ * LICENSE file found in the top-level directory of this distribution and at  *
+ * http://vast.io/license. No part of VAST, including this file, may be       *
+ * copied, modified, propagated, or distributed except according to the terms *
+ * contained in the LICENSE file.                                             *
+ ******************************************************************************/
+
+#include "vast/chunk.hpp"
+#include "vast/concept/parseable/core.hpp"
+#include "vast/concept/parseable/numeric.hpp"
+#include "vast/concept/parseable/string.hpp"
+#include "vast/concept/parseable/vast/si.hpp"
+#include "vast/concept/printable/to_string.hpp"
+#include "vast/concept/printable/vast/type.hpp"
+#include "vast/concept/printable/vast/uuid.hpp"
+#include "vast/detail/line_range.hpp"
+#include "vast/directory.hpp"
+#include "vast/fbs/index.hpp"
+#include "vast/fbs/partition.hpp"
+#include "vast/fbs/segment.hpp"
+#include "vast/fbs/utils.hpp"
+#include "vast/ids.hpp"
+#include "vast/io/read.hpp"
+#include "vast/meta_index.hpp"
+#include "vast/path.hpp"
+#include "vast/qualified_record_field.hpp"
+#include "vast/system/partition.hpp"
+#include "vast/table_slice.hpp"
+#include "vast/type.hpp"
+#include "vast/uuid.hpp"
+
+#include <caf/binary_deserializer.hpp>
+
+#include <flatbuffers/flatbuffers.h>
+
+#include <cstddef>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace vast;
+
+static caf::settings get_status_proc() {
+  using namespace parser_literals;
+  auto is = std::ifstream{"/proc/self/status"};
+  auto lines = detail::line_range{is};
+  caf::settings result;
+  auto skip = ignore(+parsers::any);
+  auto ws = ignore(+parsers::space);
+  auto kvp = [&](const char* k, const std::string_view human_friendly, auto v) {
+    using T = decltype(v);
+    return (k >> ws >> si_parser<T>{} >> skip)->*[=, &result](T x) {
+      result[human_friendly] = x;
+    };
+  };
+  auto rss = kvp("VmRSS:", "current-memory-usage", size_t{});
+  auto size = kvp("VmHWM:", "peak-memory-usage", size_t{});
+  auto swap = kvp("VmSwap:", "swap-space-usage", size_t{});
+  auto p = rss | size | swap | skip;
+  while (true) {
+    lines.next();
+    if (lines.done())
+      break;
+    auto line = lines.get();
+    if (!p(line))
+      std::cerr << "failed to parse /proc/self/status:" << line << std::endl;
+  }
+  return result;
+}
+
+int main(int argc, char** argv) {
+  if (argc < 2) {
+    std::cerr << "Usage: " << argv[0] << " <path/to/vast.db>\n";
+    return 1;
+  }
+
+  auto dbdir = vast::path{argv[1]};
+  if (!exists(dbdir)) {
+    std::cerr << "directory not found\n";
+    return 1;
+  }
+  auto dir = dbdir / "index";
+  auto fname = dir / "index.bin";
+  if (!exists(fname)) {
+    std::cerr << "file not found: " << fname.str() << std::endl;
+    return 1;
+  }
+  std::cout << "loading state from" << fname.str() << std::endl;
+  auto buffer = io::read(fname);
+  if (!buffer) {
+    std::cerr << "failed to read index file:" << render(buffer.error())
+              << std::endl;
+    return 1;
+  }
+  // TODO: Create a `index_ondisk_state` struct and move this part of the
+  // code into an `unpack()` function.
+  auto index = fbs::GetIndex(buffer->data());
+  if (index->index_type() != fbs::index::Index::v0) {
+    std::cerr << "invalid index version\n";
+    return 1;
+  }
+  meta_index meta_idx;
+  auto index_v0 = index->index_as_v0();
+  auto partition_uuids = index_v0->partitions();
+  VAST_ASSERT(partition_uuids);
+  for (auto uuid_fb : *partition_uuids) {
+    VAST_ASSERT(uuid_fb);
+    vast::uuid partition_uuid;
+    unpack(*uuid_fb, partition_uuid);
+    auto partition_path = dir / to_string(partition_uuid);
+    if (exists(partition_path)) {
+      // persisted_partitions.insert(partition_uuid);
+      // Use blocking operations here since this is part of the startup.
+      auto chunk = chunk::mmap(partition_path);
+      if (!chunk) {
+        std::cerr << "could not mmap partition at" << partition_path.str()
+                  << std::endl;
+        continue;
+      }
+      auto partition = fbs::GetPartition(chunk->data());
+      if (partition->partition_type() != fbs::partition::Partition::v0) {
+        std::cerr << "found unsupported version for partition"
+                  << to_string(partition_uuid) << std::endl;
+        continue;
+      }
+      auto partition_v0 = partition->partition_as_v0();
+      VAST_ASSERT(partition_v0);
+      partition_synopsis ps;
+      system::unpack(*partition_v0, ps);
+      std::cout << "merging partition synopsis from"
+                << to_string(partition_uuid) << std::endl;
+      meta_idx.merge(partition_uuid, std::move(ps));
+    } else {
+      std::cerr << "found partition" << to_string(partition_uuid)
+                << "in the index state but not on disk; this may have been "
+                   "caused by an unclean shutdown\n";
+    }
+  }
+
+  auto memory_stats = get_status_proc();
+  std::cout << "Memory stats:\n" << to_string(memory_stats) << std::endl;
+}
