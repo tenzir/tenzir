b816d2314fbf31d485cec20b2da2662470afec02
Dominik Lohmann <mail@dominiklohmann.de>
2021-02-02 14:24:45 +0100

    Make trace logging work without GNU extensions

--- libvast/vast/logger.hpp
+++ libvast/vast/logger.hpp
@@ -57,8 +57,12 @@
 
 #if VAST_LOG_LEVEL >= VAST_LOG_LEVEL_TRACE
 
-#  define VAST_TRACE(format, ...)                                              \
-    VAST_DEBUG("ENTER {} " format, __func__, ##__VA_ARGS__);                   \
+#  define VAST_TRACE(...)                                                      \
+    [func_name_ = __func__](auto&& format, auto&&... args) {                   \
+      VAST_DEBUG(::fmt::format("ENTER {} {}", func_name_,                      \
+                               std::forward<decltype(format)>(format)),        \
+                 std::forward<decltype(args)>(args)...);                       \
+    }(__VA_ARGS__);                                                            \
     auto CAF_UNIFYN(vast_log_trace_guard_) = ::caf::detail::make_scope_guard(  \
       [=, func_name_ = __func__] { VAST_DEBUG("EXIT {}", func_name_); })
 
