4530d4678977994e25e2426707523af914ba6211
Matthias Vallentin <matthias@tenzir.com>
2021-02-14 20:25:42 +0100

    Complete full detection parsing logic
    The "1/all of X" syntax now works. We also apply the "re" modifier and
    translate an exact string into a pattern.

--- libvast/src/detail/sigma.cpp
+++ libvast/src/detail/sigma.cpp
@@ -21,6 +21,7 @@
 #include "vast/expression_visitors.hpp"
 
 #include <map>
+#include <regex>
 #include <string>
 #include <tuple>
 #include <vector>
@@ -35,14 +36,100 @@ namespace {
 
 using expression_map = std::map<std::string, expression>;
 
-/// Parses the 'detection' attribute from a Sigma rule. See the Sigma wiki for
+/// A symbol-table-like parser for Sigma search identifers. In addition to the
+/// exact match as in a symbol table, this parser also performs the additional
+/// syntax "1/all of X" where X can be "them", a search identifier, or a
+/// wildcard pattern. This parsers is effective a predicate operand in the
+/// "condition" field of the "detection" attribute.
+struct search_id_symbol_table : parser<search_id_symbol_table> {
+  using attribute = expression;
+
+  enum class quantifier { all, any };
+
+  explicit search_id_symbol_table(const expression_map& exprs) {
+    id.symbols.reserve(exprs.size());
+    for (auto& [key, value] : exprs)
+      id.symbols.emplace(key, value);
+  }
+
+  template <class Connective>
+  static expression join(std::vector<expression> xs) {
+    Connective result;
+    result.reserve(xs.size());
+    std::move(xs.begin(), xs.end(), std::back_insert_iterator(result));
+    return expression{std::move(result)};
+  }
+
+  // Forces a conjunction or disjunction on a given expression.
+  template <quantifier Quantifier>
+  static expression force(expression x) {
+    if constexpr (Quantifier == quantifier::all) {
+      if (auto xs = caf::get_if<disjunction>(&x)) {
+        conjunction result;
+        result.reserve(xs->size());
+        std::move(xs->begin(), xs->end(), std::back_insert_iterator(result));
+        return expression{std::move(result)};
+      }
+    } else {
+      if (auto xs = caf::get_if<conjunction>(&x)) {
+        disjunction result;
+        result.reserve(xs->size());
+        std::move(xs->begin(), xs->end(), std::back_insert_iterator(result));
+        return expression{std::move(result)};
+      }
+    }
+    return x;
+  }
+
+  template <class Iterator, class Attribute>
+  bool parse(Iterator& f, const Iterator& l, Attribute& result) const {
+    using namespace parser_literals;
+    auto select_all = [this] {
+      std::vector<expression> result;
+      result.reserve(id.symbols.size());
+      for (auto& x : id.symbols)
+        result.push_back(x.second);
+      return result;
+    };
+    // Performs *-wildcard search on all search identifiers.
+    auto search = [this](std::string str) {
+      str = std::regex_replace(str, std::regex("\\*"), ".*");
+      auto rx = std::regex{str};
+      disjunction result;
+      for (auto& [sym, expr] : id.symbols)
+        if (std::regex_search(sym.begin(), sym.end(), rx))
+          result.push_back(expr);
+      return expression{std::move(result)};
+    };
+    // clang-format off
+    auto ws = ignore(*parsers::space);
+    auto them
+      = "them"_p ->* select_all
+      ;
+    auto pattern
+      = (+parsers::any - parsers::space) ->* search
+      ;
+    auto expr
+      = "all of"_p >> ws >> them ->* join<conjunction>
+      | "1 of"_p >> ws >> them ->* join<disjunction>
+      | "all of"_p >> ws >> id ->* force<quantifier::all>
+      | "any of"_p >> ws >> id ->* force<quantifier::any>
+      | id
+      | pattern
+      ;
+    // clang-format on
+    return expr(f, l, result);
+  }
+
+  symbol_table<expression> id;
+};
+
+/// Parses the "detection" attribute from a Sigma rule. See the Sigma wiki for
 /// details: https://github.com/Neo23x0/sigma/wiki/Specification#detection
 struct detection_parser : parser<detection_parser> {
   using attribute = expression;
 
-  explicit detection_parser(const expression_map& exprs) {
-    for (auto& [key, value] : exprs)
-      search_id.symbols.emplace(key, value);
+  explicit detection_parser(const expression_map& exprs) : search_id{exprs} {
   }
 
   static expression to_expr(
@@ -101,7 +188,7 @@ struct detection_parser : parser<detection_parser> {
     return p(f, l, result);
   }
 
-  symbol_table<expression> search_id;
+  search_id_symbol_table search_id;
 };
 
 caf::expected<expression> parse_search_id(const data& x) {
@@ -111,18 +198,51 @@ caf::expected<expression> parse_search_id(const data& x) {
       auto keys = split(key, "|");
       auto extractor = field_extractor{std::string{keys[0]}};
       auto op = relational_operator::equal;
-      // Parse modifiers. TODO: do this exhaustively.
       auto all = false;
+      auto re = false;
+      // Value factory that takes into account whether the `re` modifier is
+      // present.
+      auto make = [&](const data& value) {
+        // The following invariants apply according to the Sigma spec:
+        // - All values are treated as case-insensitive strings
+        // - You can use wildcard characters '*' and '?' in strings
+        // - Wildcards can be escaped with \, e.g. \*. If some wildcard after a
+        //   backslash should be searched, the backslash has to be escaped: \\*.
+        // - Regular expressions are case-sensitive by default
+        // - You don't have to escape characters except the string quotation
+        //   marks '
+        if (re)
+          if (auto str = caf::get_if<std::string>(&value))
+            return data{pattern{*str}};
+        // TODO: figure out a solution for case-insensitve search
+        // TODO: apply the wildcard behavior above.
+        return value;
+      };
+      // Parse modifiers.
       for (auto i = keys.begin() + 1; i != keys.end(); ++i) {
-        if (*i == "all")
+        if (*i == "all") {
           all = true;
-        if (*i == "contains")
+        } else if (*i == "contains") {
           op = relational_operator::ni;
-        if (*i == "endswith" || *i == "startswith")
+        } else if (*i == "endswith" || *i == "startswith") {
           // Once we have regex support we should transform a lot of these
           // modifier in pattern qualifiers, e.g., `endswith` for a value X
           // should become /X$/.
           op = relational_operator::ni;
+        } else if (*i == "base64") {
+          // TODO
+        } else if (*i == "base64offset") {
+          // TODO
+        } else if (*i == "utf16le" || *i == "wide") {
+          // TODO
+        } else if (*i == "utf16be") {
+          // TODO
+        } else if (*i == "utf16") {
+          // TODO
+        } else if (*i == "re") {
+          re = true;
+          op = relational_operator::match;
+        }
       }
       // Parse RHS.
       if (caf::holds_alternative<record>(rhs))
@@ -134,13 +254,13 @@ caf::expected<expression> parse_search_id(const data& x) {
             return caf::make_error(ec::type_clash, "nested lists disallowed");
           if (caf::holds_alternative<record>(value))
             return caf::make_error(ec::type_clash, "nested records disallowed");
-          connective.emplace_back(predicate{extractor, op, value});
+          connective.emplace_back(predicate{extractor, op, make(value)});
         }
         auto expr = all ? expression{conjunction(std::move(connective))}
                         : expression{disjunction(std::move(connective))};
         result.emplace_back(std::move(expr));
       } else {
-        result.emplace_back(predicate{std::move(extractor), op, rhs});
+        result.emplace_back(predicate{std::move(extractor), op, make(rhs)});
       }
     }
     return result;
