42e225075a5cb3812297b44b5921028f0629fac6
Matthias Vallentin <matthias@tenzir.com>
2021-03-19 16:28:26 +0100

    Perform minor touch-ups

--- libvast/src/system/component_registry.cpp
+++ libvast/src/system/component_registry.cpp
@@ -31,21 +31,19 @@ bool component_registry::add(caf::actor comp, std::string type,
 
 caf::expected<component_registry::component>
 component_registry::remove(const std::string& label) {
-  auto result = component{};
   auto i = components_.find(label);
   if (i == components_.end())
     return caf::no_error;
-  result = std::move(i->second);
+  auto result = std::move(i->second);
   components_.erase(i);
   return result;
 }
 
 caf::expected<component_registry::component>
 component_registry::remove(const caf::actor& comp) {
-  auto result = component{};
   for (auto i = components_.begin(); i != components_.end(); ++i) {
     if (i->second.actor == comp) {
-      result = std::move(i->second);
+      auto result = std::move(i->second);
       components_.erase(i);
       return result;
     }
