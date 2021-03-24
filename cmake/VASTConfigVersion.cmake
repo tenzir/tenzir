f77b4ede0a98a056295732591c1298f4827ef628
Dominik Lohmann <mail@dominiklohmann.de>
2021-03-24 12:30:41 +0100

    Use non-deprecated FILE GLOB_RECURSE (CMP0009)

--- cmake/VASTConfigVersion.cmake
+++ cmake/VASTConfigVersion.cmake
@@ -45,6 +45,13 @@ endif ()
 string(REGEX REPLACE "^(.*)[-0-|-]\\\$" "\\1" VAST_VERSION_TAG
                      "${VAST_VERSION_TAG}")
 
+cmake_policy(PUSH)
+if (POLICY CMP0009)
+  # Do not follow symlinks in FILE GLOB_RECURSE by default.
+  # https://cmake.org/cmake/help/latest/policy/CMP0009.html
+  cmake_policy(SET CMP0009 NEW)
+endif ()
+
 file(
   GLOB_RECURSE
   hash_files
@@ -53,6 +60,8 @@ file(
   "${CMAKE_CURRENT_LIST_DIR}/../libvast/*.cpp"
   "${CMAKE_CURRENT_LIST_DIR}/../cmake/*"
   "${CMAKE_CURRENT_LIST_DIR}/../CMakeLists.txt")
+
+cmake_policy(POP)
 
 list(FILTER hash_files EXCLUDE REGEX
      "^${CMAKE_CURRENT_SOURCE_DIR}/libvast/aux/")
