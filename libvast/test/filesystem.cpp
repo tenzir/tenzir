18ae04de47796d8b66087adc257f669a7af76451
Joe Loser <joeloser93@gmail.com>
2021-04-01 15:00:49 -0400

    Remove vast::path
    Problem:
    - There are no longer any uses of `vast::path`. They have all been
      replaced with their `std::filesystem` equivalents.
    
    Solution:
    - Remove `vast::path` and associated tests.

--- libvast/test/filesystem.cpp
+++ libvast/test/filesystem.cpp
@@ -8,7 +8,6 @@
 
 #include "vast/detail/system.hpp"
 #include "vast/file.hpp"
-#include "vast/path.hpp"
 #include "vast/si_literals.hpp"
 
 #define SUITE filesystem
@@ -23,115 +22,6 @@
 #endif
 
 using namespace vast;
-
-TEST(path_operations) {
-  path p(".");
-  CHECK(p.basename() == ".");
-  CHECK(p.parent() == "");
-
-  p = "..";
-  CHECK(p.basename() == "..");
-  CHECK(p.parent() == "");
-
-  p = "/";
-  CHECK(p.basename() == "/");
-  CHECK(p.parent() == "");
-
-  p = "foo";
-  CHECK(p.basename() == "foo");
-  CHECK(p.parent() == "");
-
-  p = "/foo";
-  CHECK(p.basename() == "foo");
-  CHECK(p.parent() == "/");
-
-  p = "foo/";
-  CHECK(p.basename() == ".");
-  CHECK(p.parent() == "foo");
-
-  p = "/foo/";
-  CHECK(p.basename() == ".");
-  CHECK(p.parent() == "/foo");
-
-  p = "foo/bar";
-  CHECK(p.basename() == "bar");
-  CHECK(p.parent() == "foo");
-
-  p = "/foo/bar";
-  CHECK(p.basename() == "bar");
-  CHECK(p.parent() == "/foo");
-
-  p = "/.";
-  CHECK(p.basename() == ".");
-  CHECK(p.parent() == "/");
-
-  p = "./";
-  CHECK(p.basename() == ".");
-  CHECK(p.parent() == ".");
-
-  p = "/..";
-  CHECK(p.basename() == "..");
-  CHECK(p.parent() == "/");
-
-  p = "../";
-  CHECK(p.basename() == ".");
-  CHECK(p.parent() == "..");
-
-  p = "foo/.";
-  CHECK(p.basename() == ".");
-  CHECK(p.parent() == "foo");
-
-  p = "foo/..";
-  CHECK(p.basename() == "..");
-  CHECK(p.parent() == "foo");
-
-  p = "foo/./";
-  CHECK(p.basename() == ".");
-  CHECK(p.parent() == "foo/.");
-
-  p = "foo/../";
-  CHECK(p.basename() == ".");
-  CHECK(p.parent() == "foo/..");
-
-  p = "foo/./bar";
-  CHECK(p.basename() == "bar");
-  CHECK(p.parent() == "foo/.");
-
-  p = "/usr/local/bin/foo";
-  CHECK(p.parent() == "/usr/local/bin");
-  CHECK(p.basename() == "foo");
-  CHECK(path("/usr/local/bin/foo.bin").basename(true) == "foo");
-
-  CHECK(p.root() == "/");
-  CHECK(path("usr/local").root() == "");
-
-  CHECK(p.complete() == p);
-  CHECK(path("foo/").complete() == path::current() / "foo/");
-
-  auto pieces = split(p);
-  REQUIRE(pieces.size() == 5);
-  CHECK(pieces[0] == "/");
-  CHECK(pieces[1] == "usr");
-  CHECK(pieces[2] == "local");
-  CHECK(pieces[3] == "bin");
-  CHECK(pieces[4] == "foo");
-}
-
-TEST(file_and_directory_manipulation) {
-  path base = "vast-unit-test-file-system-test";
-  path p("/tmp");
-  p /= base / std::to_string(detail::process_id());
-  CHECK(!p.is_regular_file());
-  CHECK(!exists(p));
-  CHECK(!mkdir(p));
-  CHECK(exists(p));
-  CHECK(p.is_directory());
-  CHECK(std::filesystem::remove_all(std::filesystem::path{p.str()}));
-  CHECK(!p.is_directory());
-  CHECK(p.parent().is_directory());
-  CHECK(std::filesystem::remove_all(std::filesystem::path{p.parent().str()}));
-  CHECK(!p.parent().is_directory());
-}
 
 FIXTURE_SCOPE(chunk_tests, fixtures::filesystem)
 
