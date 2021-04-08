269b7b21201659bf6cfa88f2310d2871f261f840
ngrodzitski <utromvecherom@gmail.com>
2021-04-08 10:54:41 +0300

    fixup: clang-format

--- libvast/test/schema.cpp
+++ libvast/test/schema.cpp
@@ -9,7 +9,6 @@
 #define SUITE schema
 
 #include "vast/schema.hpp"
-#include "vast/data.hpp"
 
 #include "vast/test/test.hpp"
 
@@ -19,6 +18,7 @@
 #include "vast/concept/printable/to_string.hpp"
 #include "vast/concept/printable/vast/error.hpp"
 #include "vast/concept/printable/vast/schema.hpp"
+#include "vast/data.hpp"
 #include "vast/detail/deserialize.hpp"
 #include "vast/detail/serialize.hpp"
 
