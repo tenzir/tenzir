6f20fb61e781e939fe2e30d3789a53c4acb89158
Dominik Lohmann <mail@dominiklohmann.de>
2021-04-28 14:45:30 +0200

    Always add a stub source to plugins
    In case a plugin specifies only header files in SOURCES we did not
    correctly create a stub source file. Now we always generate one instead
    of relying on any heuristics.

--- cmake/VASTRegisterPlugin.cmake
+++ cmake/VASTRegisterPlugin.cmake
@@ -126,19 +126,17 @@ function (VASTRegisterPlugin)
     endif ()
   endif ()
 
-  if (NOT PLUGIN_SOURCES)
-    # Create a stub source file with an identfier if no sources except for the
-    # entrypoint exist.
-    string(MAKE_C_IDENTIFIER "${PLUGIN_TARGET}" plugin_identifier)
-    file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/stub.cpp"
-         "void vast_plugin_${plugin_identifier}_stub() {}")
-    set(PLUGIN_SOURCES "${CMAKE_CURRENT_BINARY_DIR}/stub.cpp")
-  else ()
-    # Deduplicate the entrypoint so plugin authors can grep for sources while
-    # still specifying the entrypoint manually.
-    if ("${PLUGIN_ENTRYPOINT}" IN_LIST PLUGIN_SOURCES)
-      list(REMOVE_ITEM PLUGIN_SPURCES "${PLUGIN_ENTRYPOINT}")
-    endif ()
+  # Create a stub source file with an identfier in case SOURCES contains no
+  # source files (except for the entrypoint).
+  string(MAKE_C_IDENTIFIER "${PLUGIN_TARGET}" plugin_identifier)
+  file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/stub.cpp"
+       "void vast_plugin_${plugin_identifier}_stub() {}")
+  set(PLUGIN_SOURCES "${CMAKE_CURRENT_BINARY_DIR}/stub.cpp")
+
+  # Deduplicate the entrypoint so plugin authors can grep for sources while
+  # still specifying the entrypoint manually.
+  if ("${PLUGIN_ENTRYPOINT}" IN_LIST PLUGIN_SOURCES)
+    list(REMOVE_ITEM PLUGIN_SPURCES "${PLUGIN_ENTRYPOINT}")
   endif ()
 
   # Create a static library target for our plugin _without_ the entrypoint.
