6d8a7e6147cb2ee07af74607e50e9903788ea62e
Benno Evers <benno.evers@tenzir.com>
2021-04-29 16:07:38 +0200

    Add overload with message for VAST_ASSERT

--- libvast/vast/detail/assert.hpp
+++ libvast/vast/detail/assert.hpp
@@ -10,20 +10,36 @@
 
 #include "vast/config.hpp"
 #include "vast/detail/backtrace.hpp"
+#include "vast/detail/pp.hpp"
 
 #if VAST_ENABLE_ASSERTIONS
 #  include <cstdio>
 #  include <cstdlib>
-#  define VAST_ASSERT(expr)                                                    \
+
+#  define VAST_ASSERT_2(expr, msg)                                             \
     do {                                                                       \
       if (static_cast<bool>(expr) == false) {                                  \
         /* NOLINTNEXTLINE */                                                   \
         ::fprintf(stderr, "%s:%u: assertion failed '%s'\n", __FILE__,          \
-                  __LINE__, #expr);                                            \
+                  __LINE__, msg);                                              \
         ::vast::detail::backtrace();                                           \
         ::abort();                                                             \
       }                                                                        \
     } while (false)
+
+#  define VAST_ASSERT_1(expr) VAST_ASSERT_2(expr, #  expr)
+
 #else
-#  define VAST_ASSERT(expr) static_cast<void>(expr)
+
+#  define VAST_ASSERT_2(expr, msg)                                             \
+    do {                                                                       \
+      static_cast<void>(expr);                                                 \
+      static_cast<void>(msg);                                                  \
+    } while (false)
+
+#  define VAST_ASSERT_1(expr) static_cast<void>(expr)
+
 #endif
+
+#define VAST_ASSERT(...)                                                       \
+  VAST_PP_OVERLOAD(VAST_ASSERT_, __VA_ARGS__)(__VA_ARGS__)
