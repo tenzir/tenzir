f0eed18a8b5eb96196d14d81f1656edde42787e7
Tobias Mayer <tobim@fastmail.fm>
2021-04-29 08:56:17 +0200

    Add missing include guard

--- libvast/vast/system/node_control.hpp
+++ libvast/vast/system/node_control.hpp
@@ -6,6 +6,8 @@
 // SPDX-FileCopyrightText: (c) 2019 The VAST Contributors
 // SPDX-License-Identifier: BSD-3-Clause
 
+#pragma once
+
 #include "vast/atoms.hpp"
 #include "vast/command.hpp"
 #include "vast/defaults.hpp"
