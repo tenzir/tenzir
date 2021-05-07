36089abea7bfb69c2697d0c2ce52640244d20f2b
Benno Evers <benno.evers@tenzir.com>
2021-05-07 16:48:03 +0200

    Add missing copyright banners

--- libvast/src/transform_steps/replace.cpp
+++ libvast/src/transform_steps/replace.cpp
@@ -1,3 +1,11 @@
+//    _   _____   __________
+//   | | / / _ | / __/_  __/     Visibility
+//   | |/ / __ |_\ \  / /          Across
+//   |___/_/ |_/___/ /_/       Space and Time
+//
+// SPDX-FileCopyrightText: (c) 2021 The VAST Contributors
+// SPDX-License-Identifier: BSD-3-Clause
+
 #include "vast/transform_steps/replace.hpp"
 
 #include "vast/arrow_table_slice_builder.hpp"
