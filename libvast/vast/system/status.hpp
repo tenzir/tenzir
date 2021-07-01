c9c3562433502a569394c400940483d428e8bfee
Tobias Mayer <tobim@fastmail.fm>
2021-07-01 22:47:57 +0200

    Add a utility to simplify status collection
    Many of our status message handlers have lot's of boilerplate to
    forward the requests to children and insert the collected subsection
    into a unified response.
    
    The tooling introduced here aims to reduce code duplication and abstract
    the response lifetime management out of the status handlers.

--- libvast/vast/system/status.hpp
+++ libvast/vast/system/status.hpp
@@ -10,9 +10,122 @@
 
 #include "vast/fwd.hpp"
 
+#include "vast/defaults.hpp"
+#include "vast/logger.hpp"
+#include "vast/system/actors.hpp"
+
+#include <caf/duration.hpp>
+#include <caf/settings.hpp>
+#include <caf/typed_response_promise.hpp>
+
 namespace vast::system {
 
 /// The verbosity level of a status request.
 enum class status_verbosity { info, detailed, debug };
+
+template <class Ptr, class Result>
+struct status_request_state_base {
+  // The actor handling the original request.
+  Ptr self;
+  // Promise to the original request.
+  caf::typed_response_promise<Result> promise;
+  // Maps nodes to a map associating components with status information.
+  caf::settings content;
+};
+
+struct no_extra {
+  static void
+  deliver(caf::typed_response_promise<caf::settings>&& rp, caf::settings&& s) {
+    rp.deliver(std::move(s));
+  }
+};
+
+template <class Ptr, class Result, class Extra>
+struct status_request_state : status_request_state_base<Ptr, Result>, Extra {};
+
+template <class Extra, class Result = caf::settings, class Ptr>
+auto make_status_request_state(Ptr self) {
+  using state_type = status_request_state<Ptr, Result, Extra>;
+  // We need a custom deleter to deliver the promise, so we can't use
+  // make_shared here.
+  auto rs = std::shared_ptr<state_type>{
+    new state_type{{self, self->template make_response_promise<Result>(), {}},
+                   Extra{}},
+    [](state_type* x) {
+      x->Extra::deliver(std::move(x->promise), std::move(x->content));
+      delete x; // NOLINT
+    }};
+  return rs;
+}
+
+template <class Ptr>
+std::shared_ptr<status_request_state<Ptr, caf::settings, no_extra>>
+make_status_request_state(Ptr self) {
+  return make_status_request_state<no_extra>(self);
+}
+
+/// Requests a status response from another actor.
+/// @tparam F The callback for a successful response.
+/// @tparam Fe The callback for a failed request.
+/// @tparam Ptr The type of the `self` pointer parameter.
+/// @tparam Result The type that the promise delivers.
+/// @tparam Extra User supplied extra fields for accumulation.
+/// @tparam Resp The handle type for the responder actor.
+/// @param rs A shared-pointer to the request state.
+/// @param timeout The timeout for the request.
+/// @param verbosity The requested verbosity level.
+/// @param responder The actor to retrieve additional status from.
+/// @param f The callback for a successful response.
+/// @param fe The callback for a failed request.
+template <class F, class Fe, class Ptr, class Result, class Extra, class Resp>
+// TODO: Restrict this to Fs that are callable with caf::settings to make it
+// possible to use `std::string_view` for the other overload.
+void collect_status(
+  const std::shared_ptr<status_request_state<Ptr, Result, Extra>>& rs,
+  std::chrono::milliseconds timeout, status_verbosity verbosity, Resp responder,
+  F&& f, Fe&& fe) {
+  // The overload for 'request(...)' taking a 'std::chrono::duration' does not
+  // respect the specified message priority, so we convert to 'caf::duration' by
+  // hand.
+  rs->self
+    ->template request<caf::message_priority::high>(
+      responder, caf::duration{timeout}, atom::status_v, verbosity)
+    .then(
+      [rs, f = std::forward<F>(f)](caf::settings& response) {
+        f(response);
+      },
+      [rs, fe = std::forward<Fe>(fe)](caf::error& err) {
+        fe(err);
+      });
+}
+
+/// Requests a status response from another actor. Convenience overload for
+/// cases without extra state.
+/// @tparam Ptr The type of the `self` pointer parameter.
+/// @tparam Result The type that the promise delivers.
+/// @tparam Extra User supplied extra fields for accumulation.
+/// @tparam Resp The handle type for the responder actor.
+/// @param rs A shared-pointer to the request state.
+/// @param timeout The timeout for the request.
+/// @param verbosity The requested verbosity level.
+/// @param responder The actor to retrieve additional status from.
+/// @param s The settings object to insert the response into.
+/// @param key The key at which the response shall be inserted.
+template <class Ptr, class Result, class Extra, class Resp>
+void collect_status(
+  const std::shared_ptr<status_request_state<Ptr, Result, Extra>>& rs,
+  std::chrono::milliseconds timeout, status_verbosity verbosity, Resp responder,
+  caf::settings& s, const char* key) {
+  collect_status(
+    rs, timeout, verbosity, responder,
+    [key, &s](caf::settings& response) {
+      put(s, std::string_view{key}, std::move(response));
+    },
+    [self = rs->self, key, &s](caf::error& err) {
+      VAST_WARN("{} failed to retrieve status for the key {}: {}", self, key,
+                fmt::to_string(err));
+      put(s, std::string_view{key}, fmt::to_string(err));
+    });
+}
 
 } // namespace vast::system
