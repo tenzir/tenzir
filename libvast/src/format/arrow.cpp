6e1fef0ea2146209111d42a637780fbdf494da6c
Tobias Mayer <tobim@fastmail.fm>
2021-07-29 16:17:20 +0200

    Fix compilation wih Arrow 5.0

--- libvast/src/format/arrow.cpp
+++ libvast/src/format/arrow.cpp
@@ -6,24 +6,29 @@
 // SPDX-FileCopyrightText: (c) 2019 The VAST Contributors
 // SPDX-License-Identifier: BSD-3-Clause
 
+#include "vast/format/arrow.hpp"
+
+#include "vast/arrow_table_slice.hpp"
+#include "vast/arrow_table_slice_builder.hpp"
 #include "vast/config.hpp"
+#include "vast/detail/assert.hpp"
+#include "vast/detail/byte_swap.hpp"
+#include "vast/detail/fdoutbuf.hpp"
+#include "vast/detail/string.hpp"
+#include "vast/error.hpp"
+#include "vast/table_slice_builder.hpp"
+#include "vast/type.hpp"
 
-#  include "vast/arrow_table_slice.hpp"
-#  include "vast/arrow_table_slice_builder.hpp"
-#  include "vast/detail/assert.hpp"
-#  include "vast/detail/byte_swap.hpp"
-#  include "vast/detail/fdoutbuf.hpp"
-#  include "vast/detail/string.hpp"
-#  include "vast/error.hpp"
-#  include "vast/format/arrow.hpp"
-#  include "vast/table_slice_builder.hpp"
-#  include "vast/type.hpp"
+#include <arrow/util/config.h>
+#include <caf/none.hpp>
 
-#  include <arrow/util/config.h>
+#if ARROW_VERSION_MAJOR < 5
 #  include <arrow/util/io_util.h>
-#  include <caf/none.hpp>
+#else
+#  include <arrow/io/stdio.h>
+#endif
 
-#  include <stdexcept>
+#include <stdexcept>
 
 namespace vast::format::arrow {
 
@@ -73,11 +78,11 @@ bool writer::layout(const record_type& layout) {
   current_layout_ = layout;
   auto schema = make_arrow_schema(layout);
   current_builder_ = arrow_table_slice_builder::make(layout);
-#  if ARROW_VERSION_MAJOR >= 2
+#if ARROW_VERSION_MAJOR >= 2
   auto writer_result = ::arrow::ipc::MakeStreamWriter(out_.get(), schema);
-#  else
+#else
   auto writer_result = ::arrow::ipc::NewStreamWriter(out_.get(), schema);
-#  endif
+#endif
   if (writer_result.ok()) {
     current_batch_writer_ = std::move(*writer_result);
     return true;
