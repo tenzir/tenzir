4812c374c288b40a5c55b58bde2ea3a8d0e31c61
Dominik Lohmann <mail@dominiklohmann.de>
2021-09-23 11:22:29 +0200

    Prepare repository for VAST 2021.09.30-rc1

--- cmake/VASTVersionFallback.cmake
+++ cmake/VASTVersionFallback.cmake
@@ -1,3 +1,3 @@
 # IMPORTANT: When updating this file, also run the target `update-changelog` and
 # push the updated CHANGELOG file.
-set(VAST_VERSION_FALLBACK "2021.08.26")
+set(VAST_VERSION_FALLBACK "2021.09.30-rc1")
