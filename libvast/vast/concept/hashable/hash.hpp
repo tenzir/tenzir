2e6f444ebb676e5487bdf03c989bf68fc548b727
Matthias Vallentin <matthias@vallentin.net>
2021-10-11 13:06:25 +0200

    Don't go through hash_append for one-shot hashing

--- libvast/vast/concept/hashable/hash.hpp
+++ libvast/vast/concept/hashable/hash.hpp
@@ -16,35 +16,6 @@
 #include <utility>
 
 namespace vast {
-namespace detail {
-
-/// Wraps a one-shot hash in the interface of an incremental hash algorithm.
-template <one_shot_hash HashAlgorithm>
-class one_shot_wrapper {
-public:
-  static constexpr detail::endian endian = HashAlgorithm::endian;
-
-  using result_type = typename HashAlgorithm::result_type;
-
-  template <class... Args>
-  explicit one_shot_wrapper(Args&&... args) : h_{std::forward<Args>(args)...} {
-  }
-
-  void operator()(const void* data, size_t size) noexcept {
-    // Guaranteed to be called exactly once, via contiguously_hashable.
-    result_ = h_.make(data, size);
-  }
-
-  explicit operator result_type() noexcept {
-    return result_;
-  }
-
-private:
-  HashAlgorithm h_;
-  result_type result_;
-};
-
-} // namespace detail
 
 /// Generic function to compute a hash over a byte sequence.
 /// @param x The value to hash.
@@ -62,9 +33,8 @@ template <incremental_hash HashAlgorithm = default_hash, class T, class... Args>
 template <one_shot_hash HashAlgorithm = default_hash, class T, class... Args>
   requires(contiguously_hashable<T, HashAlgorithm>)
 [[nodiscard]] auto hash(T&& x, Args&&... args) noexcept {
-  detail::one_shot_wrapper<HashAlgorithm> h{std::forward<Args>(args)...};
-  hash_append(h, x);
-  return static_cast<typename HashAlgorithm::result_type>(h);
+  HashAlgorithm h{std::forward<Args>(args)...};
+  return h.make(std::addressof(x), sizeof(x));
 }
 
 } // namespace vast
