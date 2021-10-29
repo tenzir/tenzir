3cdaa18eea0f25d7e6ba7b46af4d0275bb7e86a3
Matthias Vallentin <matthias@vallentin.net>
2021-10-29 09:46:37 +0200

    Only consider 64-bit architectures for now

--- libvast/vast/detail/worm.hpp
+++ libvast/vast/detail/worm.hpp
@@ -18,45 +18,25 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <utility>
 
 namespace vast::detail {
 
-// TODO: maybe move to vast/config.hpp or alike?
-#ifdef __SIZEOF_INT128__
-constexpr bool have_128bit = true;
-#else
-constexpr bool have_128bit = false;
-#endif
-
-/// Wide odd regenerative multiplication (Worm). A generic version of fastrange
-/// modulo reduction.
-inline void wide_mul(size_t a, uint64_t h, size_t& upper, uint64_t& lower) {
-  if constexpr (have_128bit) { // 64 bit
-    __uint128_t wide = static_cast<__uint128_t>(a) * h;
-    upper = static_cast<uint64_t>(wide >> 64);
-    lower = static_cast<uint64_t>(wide);
-  } else { // 32 bit
-    uint64_t semiwide = (a & 0xffffffff) * (h & 0xffffffff);
-    uint32_t lower_of_lower = static_cast<uint32_t>(semiwide);
-    uint32_t upper_of_lower = static_cast<uint32_t>(semiwide >> 32);
-    semiwide = (a & 0xffffffff) * (h >> 32);
-    semiwide += upper_of_lower;
-    upper = static_cast<size_t>(semiwide >> 32);
-    lower = (semiwide << 32) | lower_of_lower;
-  }
+inline std::pair<uint64_t, uint64_t> wide_mul(uint64_t a, uint64_t h) {
+  __uint128_t wide = static_cast<__uint128_t>(a) * h;
+  return {static_cast<uint64_t>(wide >> 64), static_cast<uint64_t>(wide)};
 }
 
+/// Lemire's fast modulo reduction.
 inline uint64_t fastrange64(uint64_t a, uint64_t h) {
-  size_t result;
-  uint64_t discard;
-  wide_mul(a, h, result, discard);
-  return result;
+  return wide_mul(a, h).first;
 }
 
-inline uint64_t worm64(size_t a, uint64_t& h) {
-  size_t result;
-  wide_mul(a, h, result, h);
-  return result;
+/// Dillinger's wide odd regenerative multiplication.
+inline uint64_t worm64(uint64_t a, uint64_t& h) {
+  auto result = wide_mul(a, h);
+  h = result.second;
+  return result.first;
 }
 
 } // namespace vast::detail
