d5103fc4fc31eaf8a2b3484118d0eef82b51b5c4
Benno Evers <benno.evers@tenzir.com>
2021-11-18 12:45:35 +0100

    Prepare repository for VAST 2021.11.18

--- pyvast/setup.py
+++ pyvast/setup.py
@@ -38,5 +38,5 @@ setup(
     python_requires=">=3.7",
     setup_requires=["setuptools", "wheel"],
     url="https://github.com/tenzir/vast",
-    version="2021.09.30",
+    version="2021.11.18",
 )
