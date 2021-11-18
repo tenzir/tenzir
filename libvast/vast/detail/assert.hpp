1ef90aa64780ef17581066ef8083678c5a523d0c
tobim <tobim@fastmail.fm>
2021-11-18 10:14:49 +0100

    Silence clang-tidy on disabled assertions
    Co-authored-by: Dominik Lohmann <mail@dominiklohmann.de>

--- libvast/vast/detail/assert.hpp
+++ libvast/vast/detail/assert.hpp
@@ -33,11 +33,11 @@
 
 #  define VAST_ASSERT_2(expr, msg)                                             \
     do {                                                                       \
-      static_cast<void>(sizeof(expr));                                         \
-      static_cast<void>(sizeof(msg));                                          \
+      static_cast<void>(sizeof(decltype(expr)));                               \
+      static_cast<void>(sizeof(decltype(msg)));                                \
     } while (false)
 
-#  define VAST_ASSERT_1(expr) static_cast<void>(sizeof(expr))
+#  define VAST_ASSERT_1(expr) static_cast<void>(sizeof(decltype(expr)))
 
 #endif
 
