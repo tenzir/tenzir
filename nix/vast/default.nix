9a0d153adb0d5837020f99425473faf0653a3d13
Tobias Mayer <tobim@fastmail.fm>
2021-11-21 11:36:19 +0100

    Remove brittle version extraction from the nix build
    The old logic relied on using `git describe` which doesn't work when
    the `.git` directory is not copied to the store. This happens for
    example with `fetchFromGithub` or when using flakes.

--- nix/vast/default.nix
+++ nix/vast/default.nix
@@ -2,7 +2,6 @@
 , lib
 , vast-source
 , nix-gitignore
-, nix-gitDescribe
 , cmake
 , cmake-format
 , pkgconfig
@@ -44,8 +43,7 @@ let
 
   src = vast-source;
 
-  version = if (versionOverride != null) then versionOverride else lib.fileContents (nix-gitDescribe src);
-
+  version = if (versionOverride != null) then versionOverride else "v1.0.0";
 in
 
 stdenv.mkDerivation rec {
