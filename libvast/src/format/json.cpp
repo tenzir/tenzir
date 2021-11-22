e081d9cfd85b7ccbbe4d2d8dc083849bb0c8172d
Dominik Lohmann <mail@dominiklohmann.de>
2021-11-22 14:59:24 +0100

    Make JSON field selectors configurable
    This adds a new `--selector=<field>[:<prefix>]` option to the JSON
    import. The *field* contains the *event name*, which when prefixed with
    the optional *prefix* is used for selecting the event type.
    
    This previously existed as a hard-coded option for the Zeek Stremaing
    JSON and Suricata Eve JSON readers, which still exist unchanged.
    
    The following commands are now equivalent:
    
    ```sh
    # Import Suricata Eve JSON
    vast import suricata
    vast import json --selector=event_type:suricata
    
    # Import Zeek Streaming JSON
    vast import zeek-json
    vast import json --selector=_path:zeek
    ```

--- libvast/src/format/json.cpp
+++ libvast/src/format/json.cpp
@@ -20,6 +20,8 @@
 #include "vast/concept/printable/vast/data.hpp"
 #include "vast/concept/printable/vast/json.hpp"
 #include "vast/data.hpp"
+#include "vast/format/json/default_selector.hpp"
+#include "vast/format/json/field_selector.hpp"
 #include "vast/logger.hpp"
 #include "vast/policy/include_field_names.hpp"
 #include "vast/table_slice.hpp"
@@ -32,6 +34,8 @@
 #include <caf/none.hpp>
 
 namespace vast::format::json {
+
+// -- utility -----------------------------------------------------------------
 
 namespace {
 
@@ -1105,6 +1109,8 @@ add(const ::simdjson::dom::object& object, table_slice_builder& builder) {
   return self(self, object, layout, "");
 }
 
+// -- writer ------------------------------------------------------------------
+
 writer::writer(ostream_ptr out, const caf::settings& options)
   : super{std::move(out)} {
   flatten_ = get_or(options, "vast.export.json.flatten", false);
@@ -1127,6 +1133,142 @@ caf::error writer::write(const table_slice& x) {
 
 const char* writer::name() const {
   return "json-writer";
+}
+
+// -- reader ------------------------------------------------------------------
+
+reader::reader(const caf::settings& options, std::unique_ptr<std::istream> in)
+  : super(options) {
+  if (in != nullptr)
+    reset(std::move(in));
+  if (const auto selector_opt
+      = caf::get_if<std::string>(&options, "vast.import.json.selector")) {
+    auto split = detail::split(*selector_opt, ":");
+    if (split.size() > 2) {
+      VAST_ERROR("{} failed to parse selector '{}': must contain at most one "
+                 "':'; ignoring option");
+      selector_ = std::make_unique<default_selector>();
+    } else {
+      VAST_ASSERT(!split.empty());
+      auto field_name = std::string{split[0]};
+      auto type_prefix = split.size() == 2 ? std::string{split[1]} : "";
+      if (!type_prefix.empty())
+        reader_name_ = fmt::format("{}-reader", type_prefix);
+      selector_ = std::make_unique<field_selector>(std::move(field_name),
+                                                   std::move(type_prefix));
+    }
+  } else {
+    selector_ = std::make_unique<default_selector>();
+  }
+}
+
+void reader::reset(std::unique_ptr<std::istream> in) {
+  VAST_ASSERT(in != nullptr);
+  input_ = std::move(in);
+  lines_ = std::make_unique<detail::line_range>(*input_);
+}
+
+caf::error reader::schema(vast::schema s) {
+  return selector_->schema(s);
+}
+
+vast::schema reader::schema() const {
+  return selector_->schema();
+}
+
+const char* reader::name() const {
+  return reader_name_.c_str();
+}
+
+vast::system::report reader::status() const {
+  using namespace std::string_literals;
+  uint64_t invalid_line = num_invalid_lines_;
+  uint64_t unknown_layout = num_unknown_layouts_;
+  if (num_invalid_lines_ > 0)
+    VAST_WARN("{} failed to parse {} of {} recent lines",
+              detail::pretty_type_name(this), num_invalid_lines_, num_lines_);
+  if (num_unknown_layouts_ > 0)
+    VAST_WARN("{} failed to find a matching type for {} of {} recent lines",
+              detail::pretty_type_name(this), num_unknown_layouts_, num_lines_);
+  num_invalid_lines_ = 0;
+  num_unknown_layouts_ = 0;
+  num_lines_ = 0;
+  return {
+    {name() + ".invalid-line"s, invalid_line},
+    {name() + ".unknown-layout"s, unknown_layout},
+  };
+}
+
+caf::error
+reader::read_impl(size_t max_events, size_t max_slice_size, consumer& cons) {
+  VAST_TRACE_SCOPE("{} {}", VAST_ARG(max_events), VAST_ARG(max_slice_size));
+  VAST_ASSERT(max_events > 0);
+  VAST_ASSERT(max_slice_size > 0);
+  size_t produced = 0;
+  table_slice_builder_ptr bptr = nullptr;
+  while (produced < max_events) {
+    if (lines_->done())
+      return finish(cons, caf::make_error(ec::end_of_input, "input exhausted"));
+    if (batch_events_ > 0 && batch_timeout_ > reader_clock::duration::zero()
+        && last_batch_sent_ + batch_timeout_ < reader_clock::now()) {
+      VAST_DEBUG("{} reached batch timeout", detail::pretty_type_name(this));
+      return finish(cons, ec::timeout);
+    }
+    bool timed_out = lines_->next_timeout(read_timeout_);
+    if (timed_out) {
+      VAST_DEBUG("{} stalled at line {}", detail::pretty_type_name(this),
+                 lines_->line_number());
+      return ec::stalled;
+    }
+    auto& line = lines_->get();
+    ++num_lines_;
+    if (line.empty()) {
+      // Ignore empty lines.
+      VAST_DEBUG("{} ignores empty line at {}", detail::pretty_type_name(this),
+                 lines_->line_number());
+      continue;
+    }
+    auto parse_result = json_parser_.parse(line);
+    if (parse_result.error() != ::simdjson::error_code::SUCCESS) {
+      if (num_invalid_lines_ == 0)
+        VAST_WARN("{} failed to parse line {}: {}",
+                  detail::pretty_type_name(this), lines_->line_number(), line);
+      ++num_invalid_lines_;
+      continue;
+    }
+    auto get_object_result = parse_result.get_object();
+    if (get_object_result.error() != ::simdjson::error_code::SUCCESS) {
+      if (num_invalid_lines_ == 0)
+        VAST_WARN("{} failed to parse line as JSON object {}: {}",
+                  detail::pretty_type_name(this), lines_->line_number(), line);
+      ++num_invalid_lines_;
+      continue;
+    }
+    auto&& layout = (*selector_)(get_object_result.value());
+    if (!layout) {
+      if (num_unknown_layouts_ == 0)
+        VAST_WARN("{} failed to find a matching type at line {}: {}",
+                  detail::pretty_type_name(this), lines_->line_number(), line);
+      ++num_unknown_layouts_;
+      continue;
+    }
+    bptr = builder(*layout);
+    if (bptr == nullptr)
+      return caf::make_error(ec::parse_error, "unable to get a builder");
+    if (auto err = add(get_object_result.value(), *bptr))
+      return finish(cons, //
+                    caf::make_error(ec::logic_error,
+                                    fmt::format("failed to add line {} of "
+                                                "layout {} to builder: {}",
+                                                lines_->line_number(), *layout,
+                                                err)));
+    produced++;
+    batch_events_++;
+    if (bptr->rows() == max_slice_size)
+      if (auto err = finish(cons, bptr))
+        return err;
+  }
+  return finish(cons);
 }
 
 } // namespace vast::format::json
