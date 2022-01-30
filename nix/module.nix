3398b317bc53429c69da199b79687f9baf675097
GTrunSec <gtrunsec@hardenedlinux.org>
2022-01-30 12:46:58 -0800

    nix: add yaml and settings both support in module

--- nix/module.nix
+++ nix/module.nix
@@ -4,7 +4,16 @@ let
   inherit (lib) mkIf mkOption mkEnableOption;
   cfg = config.services.vast;
   format = pkgs.formats.yaml { };
-  configFile = format.generate "vast.yaml" cfg.settings;
+  #The settings and extraConfigFile of yaml will be merged in the final configFile
+  configFile =
+    let
+      #needs convert yaml to json to be able to use importJson
+      toJsonFile = pkgs.runCommand "extraConfigFile.json" { preferLocalBuild = true; } ''
+        ${pkgs.remarshal}/bin/yaml2json  -i ${cfg.extraConfigFile} -o $out
+      '';
+    in
+    format.generate "vast.yaml" (if cfg.extraConfigFile == null then cfg.settings else (lib.recursiveUpdate cfg.settings (lib.importJSON toJsonFile)));
+
   port = lib.toInt (lib.last (lib.splitString ":" cfg.settings.vast.endpoint));
 in
 {
@@ -23,6 +32,15 @@ in
       type = lib.types.bool;
       default = false;
       description = "Open the listening port of the VAST endpoint.";
+    };
+
+    extraConfigFile = mkOption {
+      type = lib.types.nullOr lib.types.path;
+      default = null;
+      example = lib.literalExpression ''
+        extraConfigFile = ./vast.yaml;
+      '';
+      description = "Load the yaml config file";
     };
 
     settings = mkOption {
