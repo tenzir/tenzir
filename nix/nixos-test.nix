b190e873abe3e34f5e6f4ad6c1b154297a4db8eb
GTrunSec <gtrunsec@hardenedlinux.org>
2022-02-07 13:35:12 -0800

    Add client nixosModule for a cluster mode
    nix build .\#hydraJobs.vast-vm-cluster-systemd.x86_64-linux --print-build-logs --no-link

--- nix/nixos-test.nix
+++ nix/nixos-test.nix
@@ -1,4 +1,12 @@
-{ makeTest, pkgs, self }:
+{ makeTest, pkgs, self, inputs, pinned }:
+let
+  vastStatic = pinned.pkgsStatic.vast.override {
+    withPlugins = [
+      ../plugins/broker
+      ../plugins/pcap
+    ];
+  };
+in
 {
   vast-vm-systemd = makeTest
     {
@@ -42,4 +50,71 @@
       inherit pkgs;
       inherit (pkgs) system;
     };
+
+  vast-vm-cluster-systemd = makeTest
+    {
+      name = "vast-cluster-vm-systemd";
+
+      nodes = {
+        machine = { lib, ... }: {
+          imports = [ self.nixosModules.vast ];
+
+          virtualisation = {
+            memorySize = 4046;
+            cores = 2;
+          };
+
+          networking = {
+            interfaces.eth0.ipv4.addresses = lib.mkForce [{ address = "192.168.1.5"; prefixLength = 24; }];
+          };
+          services.vast = {
+            enable = true;
+            # FIXME: plugins fails on Non-Static build Vast
+            package = vastStatic;
+            openFirewall = true;
+            settings = {
+              vast = {
+                endpoint = "192.168.1.5:42000";
+              };
+            };
+          };
+        };
+
+        client = { lib, pkgs, ... }: {
+          virtualisation = {
+            memorySize = 4046;
+            cores = 2;
+          };
+
+          imports = [ self.nixosModules.vast-client ];
+
+          environment.systemPackages = with pkgs;[ nmap ];
+
+          networking = {
+            interfaces.eth0.ipv4.addresses = lib.mkForce [{ address = "192.168.1.10"; prefixLength = 24; }];
+          };
+
+          services.vast-client = {
+            enable = true;
+            settings.vast.endpoint = "192.168.1.5:42000";
+            package = vastStatic;
+            integrations.pcap.enable = true;
+            integrations.pcap.interface = "eth0";
+          };
+        };
+      };
+      testScript = { nodes, ... }: ''
+        start_all()
+        machine.wait_for_unit("network-online.target")
+        machine.wait_for_unit("vast.service")
+        client.wait_for_unit("network-online.target")
+        print(machine.succeed("ls -il /var/lib/vast"))
+        print(client.succeed("systemctl status vast-pcap.service"))
+      '';
+    }
+    {
+      inherit pkgs;
+      inherit (pkgs) system;
+    };
+
 }
