59e978929e3da83b9f32925b45e20e9f05bf5fe7
Gyozo Gaspar <gyozo.gaspar@tenzir.com>
2022-02-09 11:50:49 +0100

    Create unit tests for suricata data

---
+++ libvast/vast/test/test_data.hpp
@@ -0,0 +1,16 @@
+//    _   _____   __________
+//   | | / / _ | / __/_  __/     Visibility
+//   | |/ / __ |_\ \  / /          Across
+//   |___/_/ |_/___/ /_/       Space and Time
+//
+// SPDX-FileCopyrightText: (c) 2022 Tenzir GmbH
+
+#pragma once
+
+#include <string>
+
+namespace vast::test {
+
+extern std::string EVE_JSON;
+
+} // namespace vast::test
