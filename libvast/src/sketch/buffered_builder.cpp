77f9ca9a4921f76334a3badda348707612f9d55d
Matthias Vallentin <matthias@vallentin.net>
2022-02-15 06:35:49 +0100

    Rewrite hash visitation with native visitor

--- libvast/src/sketch/buffered_builder.cpp
+++ libvast/src/sketch/buffered_builder.cpp
@@ -8,6 +8,8 @@
 
 #include "vast/sketch/buffered_builder.hpp"
 
+#include "vast/arrow_extension_types.hpp"
+#include "vast/detail/type_traits.hpp"
 #include "vast/error.hpp"
 #include "vast/hash/hash.hpp"
 #include "vast/sketch/sketch.hpp"
@@ -15,7 +17,6 @@
 
 #include <arrow/array.h>
 #include <arrow/record_batch.h>
-#include <arrow/visitor.h>
 #include <caf/error.hpp>
 #include <caf/expected.hpp>
 #include <fmt/format.h>
@@ -27,197 +28,160 @@ namespace vast::sketch {
 
 namespace {
 
-/// Hashes all non-null array elements into a container.
-template <class Container>
-struct hash_visitor : public arrow::ArrayVisitor {
+/// Applies a function over all non-null hash values. Since a null value
+/// effectively adds an extra value to every domain, the function return value
+/// is the number of null values that have been skipped.
+///
+/// We may consider rewriting this visitor as an Arrow Compute function at some
+/// point.
+template <class Function>
+struct array_hasher {
 public:
-  hash_visitor(Container& container) : digests_{container} {
+  explicit array_hasher(Function& f) : f_{f} {
   }
 
-  arrow::Status Visit(const arrow::NullArray& array) final {
-    return visit(array);
+  caf::expected<size_t> operator()(const arrow::NullArray&) {
+    return caf::make_error(ec::unimplemented, "null type not supported");
   }
 
-  arrow::Status Visit(const arrow::BooleanArray& array) final {
-    return visit(array);
+  caf::expected<size_t> operator()(const arrow::BooleanArray& xs) {
+    if (xs.false_count() > 0)
+      f_(0);
+    if (xs.true_count() > 0)
+      f_(1);
+    return xs.null_count();
   }
 
-  arrow::Status Visit(const arrow::Int8Array& array) final {
-    return visit(array);
-  }
-
-  arrow::Status Visit(const arrow::Int16Array& array) final {
-    return visit(array);
-  }
-
-  arrow::Status Visit(const arrow::Int32Array& array) final {
-    return visit(array);
-  }
-
-  arrow::Status Visit(const arrow::Int64Array& array) final {
-    return visit(array);
-  }
-
-  arrow::Status Visit(const arrow::UInt8Array& array) final {
-    return visit(array);
-  }
-
-  arrow::Status Visit(const arrow::UInt16Array& array) final {
-    return visit(array);
-  }
-
-  arrow::Status Visit(const arrow::UInt32Array& array) final {
-    return visit(array);
-  }
-
-  arrow::Status Visit(const arrow::UInt64Array& array) final {
-    return visit(array);
-  }
-
-  arrow::Status Visit(const arrow::HalfFloatArray& array) final {
-    return visit(array);
-  }
-
-  arrow::Status Visit(const arrow::FloatArray& array) final {
-    return visit(array);
-  }
-
-  arrow::Status Visit(const arrow::DoubleArray& array) final {
-    return visit(array);
-  }
-
-  arrow::Status Visit(const arrow::StringArray& array) final {
-    return visit(array);
-  }
-
-  arrow::Status Visit(const arrow::BinaryArray& array) final {
-    return visit(array);
-  }
-
-  arrow::Status Visit(const arrow::LargeStringArray& array) final {
-    return visit(array);
-  }
-
-  arrow::Status Visit(const arrow::LargeBinaryArray& array) final {
-    return visit(array);
-  }
-
-  arrow::Status Visit(const arrow::FixedSizeBinaryArray& array) final {
-    return visit(array);
-  }
-
-  // arrow::Status Visit(const arrow::Date32Array& array) final;
-  // arrow::Status Visit(const arrow::Date64Array& array) final;
-  // arrow::Status Visit(const arrow::Time32Array& array) final;
-  // arrow::Status Visit(const arrow::Time64Array& array) final;
-
-  arrow::Status Visit(const arrow::TimestampArray& array) final {
-    return visit(array);
-  }
-
-  // arrow::Status Visit(const arrow::DayTimeIntervalArray& array) final;
-  // arrow::Status Visit(const arrow::MonthDayNanoIntervalArray& array) final;
-  // arrow::Status Visit(const arrow::MonthIntervalArray& array) final;
-  // arrow::Status Visit(const arrow::DurationArray& array) final;
-  // arrow::Status Visit(const arrow::Decimal128Array& array) final;
-  // arrow::Status Visit(const arrow::Decimal256Array& array) final;
-
-  arrow::Status Visit(const arrow::ListArray& array) final {
-    return visit(array);
-  }
-
-  arrow::Status Visit(const arrow::LargeListArray& array) final {
-    return visit(array);
-  }
-
-  // arrow::Status Visit(const arrow::MapArray& array) final;
-  // arrow::Status Visit(const arrow::FixedSizeListArray& array) final;
-
-  arrow::Status Visit(const arrow::StructArray& array) final {
-    return visit(array);
-  }
-
-  // arrow::Status Visit(const arrow::SparseUnionArray& array) final;
-  // arrow::Status Visit(const arrow::DenseUnionArray& array) final;
-  // arrow::Status Visit(const arrow::DictionaryArray& array) final;
-  // arrow::Status Visit(const arrow::ExtensionArray& array) final;
-
-private:
-  template <class T>
-  arrow::Status visit(T& array) {
-    using type_class = typename T::TypeClass;
-    if (array.null_count() > 0)
-      // TODO: treat NULL as a first-class value in all array types.
-      ;
-    if constexpr (std::is_same_v<type_class, arrow::NullType>) {
-      return arrow::Status::TypeError("null type not supported");
-    } else if constexpr (std::is_same_v<type_class, arrow::BooleanType>) {
-      // Map false to 0 and true to 1.
-      if (array.false_count() > 0)
-        digests_.insert(0);
-      if (array.true_count() > 0)
-        digests_.insert(1);
-    } else {
-      for (auto i = 0; i < array.length(); ++i) {
-        if (!array.IsNull(i)) {
-          auto digest = uint64_t{0};
-          if constexpr (detail::is_any_v<type_class, arrow::Int8Type,
-                                         arrow::Int16Type, arrow::Int32Type,
-                                         arrow::Int64Type>) {
-            digest = hash(int64_t{array.Value(i)});
-          } else if constexpr (detail::is_any_v<
-                                 type_class, arrow::UInt8Type, arrow::UInt16Type,
-                                 arrow::UInt32Type, arrow::UInt64Type>) {
-            digest = hash(uint64_t{array.Value(i)});
-          } else if constexpr (detail::is_any_v<type_class, arrow::HalfFloatType,
-                                                arrow::FloatType,
-                                                arrow::DoubleType>) {
-            digest = hash(static_cast<double>(array.Value(i)));
-          } else if constexpr (std::is_same_v<type_class, arrow::TimestampType>) {
-            const auto& ts
-              = static_cast<const arrow::TimestampType&>(*array.type());
-            auto seed = static_cast<default_hash::seed_type>(ts.unit());
-            digest = seeded_hash<default_hash>{seed}(array.Value(i));
-          } else if constexpr (std::is_same_v<type_class,
-                                              arrow::FixedSizeBinaryType>) {
-            // We cover IP addresses and subnets using this type currently.
-            // (These should be extension types in the future.)
-            auto bytes = as_bytes(array.GetView(i));
-            if (bytes.size() == 16) {
-              // An IP address is uniquely represented and therefore hashable as
-              // a single block of 16 bytes. A fixed-size span gets also hashed
-              // as single block, which we use here.
-              auto addr = address::v6(bytes.template first<16>());
-              digest = hash(addr);
-            } else if (bytes.size() == 17) {
-              auto addr = address::v6(bytes.template first<16>());
-              auto length = static_cast<uint8_t>(bytes[16]);
-              digest = hash(subnet{addr, length});
-            } else {
-              return arrow::Status::TypeError("unsupported type", array.type());
-            }
-          } else if constexpr (detail::is_any_v<type_class, arrow::ListType,
-                                                arrow::LargeListType>) {
-            // We are considering a flattened version of the array and recurse
-            // into the individual lists.
-            auto status = array.values()->Accept(this);
-            if (!status.ok())
-              return status;
-          } else if constexpr (std::is_same_v<type_class, arrow::StructType>) {
-            // TODO: implement
-            return arrow::Status::TypeError("nested records not yet supported");
-          } else {
-            // Handle an arrow::util::string_view.
-            digest = hash(as_bytes(array.GetView(i)));
-          }
-          digests_.insert(digest);
-        }
+  // Overload that handles all stateless hash computations that only dependend
+  // on the array value.
+  template <class Array>
+    requires detail::is_any_v<
+      Array, arrow::Int8Array, arrow::Int16Array, arrow::Int32Array,
+      arrow::Int64Array, arrow::UInt8Array, arrow::UInt16Array,
+      arrow::UInt32Array, arrow::UInt64Array, arrow::HalfFloatArray,
+      arrow::FloatArray, arrow::DoubleArray, arrow::StringArray>
+      caf::expected<size_t>
+  operator()(const Array& xs) {
+    for (auto i = 0; i < xs.length(); ++i) {
+      if (!xs.IsNull(i)) {
+        if constexpr (detail::is_any_v<Array, arrow::Int8Array,
+                                       arrow::Int16Array, arrow::Int32Array,
+                                       arrow::Int64Array>)
+          f_(hash(static_cast<int64_t>(xs.Value(i))));
+        else if constexpr (detail::is_any_v<
+                             Array, arrow::UInt8Array, arrow::UInt16Array,
+                             arrow::UInt32Array, arrow::UInt64Array>)
+          f_(hash(static_cast<uint64_t>(xs.Value(i))));
+        else if constexpr (detail::is_any_v<Array, arrow::HalfFloatArray,
+                                            arrow::FloatArray,
+                                            arrow::DoubleArray>)
+          f_(hash(static_cast<double>(xs.Value(i))));
+        else if constexpr (std::is_same_v<Array, arrow::StringArray>)
+          f_(hash(as_bytes(xs.GetView(i))));
+        else if constexpr (std::is_same_v<Array, arrow::StringArray>)
+          f_(hash(as_bytes(xs.GetView(i))));
+        else
+          static_assert(detail::always_false_v<Array>, "missing array type");
       }
     }
-    return arrow::Status::OK();
+    return xs.null_count();
   }
 
-  Container& digests_;
+  // Overload that handles types that have a pair form (x,y) where x in X and
+  // y in Y. We can use a seeded hash to cover the cross product of X and Y but
+  // treating x as seed.
+  template <class Array>
+    requires detail::is_any_v<Array, arrow::DurationArray, arrow::TimestampArray>
+      caf::expected<size_t>
+  operator()(const Array& xs) {
+    auto unit = arrow::TimeUnit::type{};
+    if constexpr (std::is_same_v<Array, arrow::DurationArray>)
+      unit = static_cast<const arrow::DurationType&>(*xs.type()).unit();
+    else if constexpr (std::is_same_v<Array, arrow::TimestampArray>)
+      unit = static_cast<const arrow::TimestampType&>(*xs.type()).unit();
+    else
+      static_assert(detail::always_false_v<Array>, "missing array type");
+    auto seed = static_cast<default_hash::seed_type>(unit);
+    for (auto i = 0; i < xs.length(); ++i)
+      if (!xs.IsNull(i))
+        f_(seeded_hash<default_hash>{seed}(xs.Value(i)));
+    return xs.null_count();
+  }
+
+  caf::expected<size_t> operator()(const pattern_array& xs) {
+    const auto& ys = static_cast<const arrow::StringArray&>(*xs.storage());
+    return (*this)(ys);
+  }
+
+  caf::expected<size_t> operator()(const address_array& xs) {
+    const auto& ys
+      = static_cast<const arrow::FixedSizeBinaryArray&>(*xs.storage());
+    for (auto i = 0; i < xs.length(); ++i) {
+      if (!xs.IsNull(i)) {
+        auto bytes = as_bytes(ys.GetView(i));
+        VAST_ASSERT(bytes.size() == 16);
+        // Hash a fixed-size span because we'd otherwise hash the size as well.
+        auto span = bytes.first<16>();
+        f_(hash(address{span}));
+      }
+    }
+    return xs.null_count();
+  }
+
+  caf::expected<size_t> operator()(const subnet_array& xs) {
+    // We treat the subnet length as seed to compute a one-pass hash.
+    const auto& structs = static_cast<const arrow::StructArray&>(*xs.storage());
+    const auto& lengths
+      = static_cast<const arrow::UInt8Array&>(*structs.field(0));
+    const auto& addrs = static_cast<const address_array&>(*structs.field(1));
+    const auto& ys
+      = static_cast<const arrow::FixedSizeBinaryArray&>(*addrs.storage());
+    VAST_ASSERT(lengths.length() == ys.length());
+    VAST_ASSERT(lengths.null_count() == ys.null_count());
+    for (auto i = 0; i < ys.length(); ++i) {
+      if (!ys.IsNull(i)) {
+        auto seed = static_cast<default_hash::seed_type>(lengths.Value(i));
+        auto bytes = as_bytes(ys.GetView(i));
+        VAST_ASSERT(bytes.size() == 16);
+        auto span = bytes.first<16>();
+        auto digest = seeded_hash<default_hash>{seed}(span);
+        f_(digest);
+      }
+    }
+    return xs.null_count();
+  }
+
+  caf::expected<size_t> operator()(const enum_array& xs) {
+    // Only hash the unique (string) values that we have encountered.
+    const auto& ys = static_cast<const arrow::DictionaryArray&>(*xs.storage());
+    return caf::visit(*this, *ys.dictionary());
+  }
+
+  caf::expected<size_t> operator()(const arrow::ListArray& xs) {
+    // Lists are transparent for hashing.
+    return caf::visit(*this, *xs.values());
+  }
+
+  caf::expected<size_t> operator()(const arrow::MapArray& xs) {
+    const auto& base = static_cast<const arrow::ListArray&>(xs);
+    const auto& kvps = static_cast<const arrow::StructArray&>(*base.values());
+    // Treat keys and values independently.
+    auto result = size_t{0};
+    for (const auto& field : kvps.fields())
+      if (auto x = caf::visit(*this, *field))
+        result += *x;
+      else
+        return x.error();
+    return result;
+  }
+
+  caf::expected<size_t> operator()(const arrow::StructArray&) {
+    // In case there will be structs as first-class values at some point, we
+    // will need to hash the cross product row-wise.
+    return caf::make_error(ec::logic_error, "invalid flat index field access");
+  }
+
+  Function& f_;
 };
 
 } // namespace
@@ -227,12 +191,19 @@ caf::error buffered_builder::add(table_slice x, offset off) {
   const auto& layout = caf::get<record_type>(x.layout());
   auto idx = layout.flat_index(off);
   auto array = record_batch->column(idx);
-  hash_visitor visitor{digests_};
-  if (auto status = array->Accept(&visitor); !status.ok())
-    caf::make_error(ec::unspecified, //
-                    fmt::format("failed to hash table slice column {}: {}", idx,
-                                status.ToString()));
-  return {};
+  auto add = [this](uint64_t digest) {
+    digests_.insert(digest);
+  };
+  auto nulls = caf::visit(array_hasher{add}, *array);
+  if (!nulls)
+    return caf::make_error(ec::unspecified, //
+                           fmt::format("failed to hash table slice column {}: "
+                                       "{}",
+                                       idx, nulls.error()));
+  // Treat null value as additional value in every domain.
+  if (*nulls > 0)
+    ; // FIXME: do something meaningful
+  return caf::none;
 }
 
 caf::expected<sketch> buffered_builder::finish() {
