488f74166cc33e527971153343eaaef26966f97a
Remi Dettai <rdettai@gmail.com>
2022-03-03 17:58:27 +0100

    [lint] use black as python linter to comply with ci

--- cloud/aws/lambda-handler.py
+++ cloud/aws/lambda-handler.py
@@ -6,7 +6,7 @@ logging.basicConfig(level=logging.INFO)
 
 
 def handler(event, context):
-    '''An AWS Lambda handler that runs the provided command with bash and returns the standard output'''
+    """An AWS Lambda handler that runs the provided command with bash and returns the standard output"""
     try:
         logging.info("event: ", event)
         src_cmd = event["cmd"]
@@ -14,12 +14,12 @@ def handler(event, context):
 
         # execute the command as bash and return the std outputs
         parsed_cmd = ["/bin/bash", "-c", src_cmd]
-        os.environ['VAST_ENDPOINT'] = host
-        process = subprocess.Popen(parsed_cmd,
-                                   stdout=subprocess.PIPE,
-                                   stderr=subprocess.PIPE)
+        os.environ["VAST_ENDPOINT"] = host
+        process = subprocess.Popen(
+            parsed_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
         stdout, stderr = process.communicate()
-        res = stdout if stdout != b'' else stderr
+        res = stdout if stdout != b"" else stderr
 
         return {"result": res.decode("utf-8"), "parsed_cmd": parsed_cmd}
 
