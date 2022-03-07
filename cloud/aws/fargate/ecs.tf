dd6a21df890e1b0a0ba81f5042b4b3d685368603
Remi Dettai <rdettai@gmail.com>
2022-03-07 09:19:43 +0100

    Move efs related resources to submodule

--- cloud/aws/fargate/ecs.tf
+++ cloud/aws/fargate/ecs.tf
@@ -23,52 +23,13 @@ resource "aws_security_group" "ecs_tasks" {
   }
 }
 
-resource "aws_security_group" "efs" {
-  name        = "${module.env.module_name}_${var.name}_efs_${module.env.stage}"
-  description = "allow inbound nfs access"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    protocol        = "tcp"
-    from_port       = 2049
-    to_port         = 2049
-    security_groups = [aws_security_group.ecs_tasks.id]
-  }
-
-}
-
-resource "aws_efs_file_system" "efs_volume" {
-  count = var.storage_type == "EFS" ? 1 : 0
-  tags = {
-    Name = "${module.env.module_name}-${var.name}-${module.env.stage}"
-  }
-}
-
-resource "aws_efs_mount_target" "efs_target" {
-  count           = var.storage_type == "EFS" ? 1 : 0
-  file_system_id  = aws_efs_file_system.efs_volume[0].id
-  subnet_id       = var.subnet_id
-  security_groups = [aws_security_group.efs.id]
-}
-
-
-resource "aws_efs_access_point" "access_point_for_fargate" {
-  count          = var.storage_type == "EFS" ? 1 : 0
-  file_system_id = aws_efs_file_system.efs_volume[0].id
-
-  root_directory {
-    path = "/storage"
-    creation_info {
-      owner_gid   = 1000
-      owner_uid   = 1000
-      permissions = "755"
-    }
-  }
-
-  posix_user {
-    gid = 1000
-    uid = 1000
-  }
+module "efs" {
+  source                    = "./efs"
+  count                     = var.storage_type == "EFS" ? 1 : 0
+  name                      = var.name
+  vpc_id                    = var.vpc_id
+  subnet_id                 = var.subnet_id
+  ingress_security_group_id = aws_security_group.ecs_tasks.id
 }
 
 resource "aws_ecs_task_definition" "fargate_task_def" {
@@ -77,8 +38,8 @@ resource "aws_ecs_task_definition" "fargate_task_def" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = var.task_cpu
   memory                   = var.task_memory
-  task_role_arn            = aws_iam_role.ecs_task_role.arn  // necessary to access other aws services
-  execution_role_arn       = var.ecs_task_execution_role_arn // necessary to log and access ecr
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
+  execution_role_arn       = var.ecs_task_execution_role_arn
   container_definitions    = jsonencode(local.container_definition)
   depends_on               = [aws_cloudwatch_log_group.fargate_logging] // make sure the first task does not fail because log group is not available yet
 
@@ -87,11 +48,11 @@ resource "aws_ecs_task_definition" "fargate_task_def" {
     dynamic "efs_volume_configuration" {
       for_each = var.storage_type == "EFS" ? [1] : []
       content {
-        file_system_id     = aws_efs_file_system.efs_volume[0].id
+        file_system_id     = module.efs[0].file_system_id
         root_directory     = "/storage"
         transit_encryption = "ENABLED"
         authorization_config {
-          access_point_id = aws_efs_access_point.access_point_for_fargate[0].id
+          access_point_id = module.efs[0].access_point_id
           iam             = "ENABLED"
         }
       }
