79002854c47bc2a87f25807309bf482a35d13d6a
Gyozo Gaspar <gyozo.gaspar@tenzir.com>
2022-03-15 14:25:21 +0100

    Remove old json suricata reader test

--- libvast/test/format/json.cpp
+++ libvast/test/format/json.cpp
@@ -12,19 +12,13 @@
 
 #define SUITE format
 
-#include "vast/fwd.hpp"
-
 #include "vast/concept/parseable/to.hpp"
 #include "vast/concept/parseable/vast/json.hpp"
-#include "vast/concept/parseable/vast/schema.hpp"
 #include "vast/concept/parseable/vast/time.hpp"
-#include "vast/format/reader_factory.hpp"
 #include "vast/table_slice_builder_factory.hpp"
 #include "vast/test/fixtures/actor_system.hpp"
 #include "vast/test/fixtures/events.hpp"
-#include "vast/test/schemas.hpp"
 #include "vast/test/test.hpp"
-#include "vast/test/test_data.hpp"
 
 using namespace vast;
 using namespace std::string_literals;
@@ -116,97 +110,6 @@ TEST(json to data) {
   reference[count{1}] = data{"FOO"};
   reference[count{1024}] = data{"BAR!"};
   CHECK_EQUAL(materialize(slice.at(0, 17)), data{reference});
-}
-
-TEST(json reader - suricata) {
-  using factory = vast::factory<format::reader>;
-  factory::initialize();
-  auto reader = factory::get("suricata")(caf::settings{});
-  auto input = std::make_unique<std::istringstream>(vast::test::EVE_JSON);
-  (*reader)->reset(std::move(input));
-  std::vector<table_slice> slices;
-  auto add_slice = [&](table_slice slice) {
-    slices.emplace_back(std::move(slice));
-  };
-  auto suricata_schema
-    = unbox(to<schema>(vast::test::BASE_SCHEMA + vast::test::SURICATA_SCHEMA));
-  (*reader)->schema(suricata_schema);
-  auto [err, num] = (*reader)->read(10, 10, add_slice);
-  CHECK_EQUAL(err, ec::end_of_input);
-  REQUIRE_EQUAL(num, 7u);
-  vast::table_slice::size_type NETFLOW_SLICE{0};
-  vast::table_slice::size_type STATS_SLICE{1};
-  vast::table_slice::size_type HTTP_SLICE{2};
-  vast::table_slice::size_type FLOW_SLICE{3};
-  vast::table_slice::size_type FILEINFO_SLICE{4};
-  vast::table_slice::size_type DNS_SLICE{5};
-  vast::table_slice::size_type ALERT_SLICE{6};
-  vast::table_slice::size_type index = 0;
-  for (const auto& slice : slices) {
-    if (slice.layout().name() == "suricata.netflow") {
-      NETFLOW_SLICE = index++;
-      continue;
-    }
-    if (slice.layout().name() == "suricata.stats") {
-      STATS_SLICE = index++;
-      continue;
-    }
-    if (slice.layout().name() == "suricata.http") {
-      HTTP_SLICE = index++;
-      continue;
-    }
-    if (slice.layout().name() == "suricata.flow") {
-      FLOW_SLICE = index++;
-      continue;
-    }
-    if (slice.layout().name() == "suricata.fileinfo") {
-      FILEINFO_SLICE = index++;
-      continue;
-    }
-    if (slice.layout().name() == "suricata.dns") {
-      DNS_SLICE = index++;
-      continue;
-    }
-    if (slice.layout().name() == "suricata.alert") {
-      ALERT_SLICE = index++;
-      continue;
-    }
-    VAST_ERROR("'json reader - suricata' test cannot handle layout: {}",
-               slice.layout().name());
-    REQUIRE_EQUAL(1, 0);
-  }
-  REQUIRE_EQUAL(slices.size(), 7u);
-  // The following code assumes that the order of Slices is stable after parsing
-  // and does not change randomly.
-  REQUIRE_EQUAL(slices[NETFLOW_SLICE].columns(), 18u);
-  REQUIRE_EQUAL(slices[NETFLOW_SLICE].rows(), 1u);
-  REQUIRE_EQUAL(slices[STATS_SLICE].columns(), 1u);
-  REQUIRE_EQUAL(slices[STATS_SLICE].rows(), 1u);
-  REQUIRE_EQUAL(slices[HTTP_SLICE].columns(), 24u);
-  REQUIRE_EQUAL(slices[HTTP_SLICE].rows(), 1u);
-  REQUIRE_EQUAL(slices[FLOW_SLICE].columns(), 23u);
-  REQUIRE_EQUAL(slices[FLOW_SLICE].rows(), 1u);
-  REQUIRE_EQUAL(slices[FILEINFO_SLICE].columns(), 35u);
-  REQUIRE_EQUAL(slices[FILEINFO_SLICE].rows(), 1u);
-  REQUIRE_EQUAL(slices[DNS_SLICE].columns(), 37u);
-  REQUIRE_EQUAL(slices[DNS_SLICE].rows(), 1u);
-  REQUIRE_EQUAL(slices[ALERT_SLICE].columns(), 39u);
-  REQUIRE_EQUAL(slices[ALERT_SLICE].rows(), 1u);
-  const vast::table_slice::size_type APP_PROTO_COLUMN{17};
-  CHECK_EQUAL(slices[NETFLOW_SLICE].at(0, APP_PROTO_COLUMN), data{"failed"});
-  const vast::table_slice::size_type TIMESTAMP_COLUMN{0};
-  auto ts = to<vast::time>("2019-05-02T13:56:53.843674");
-  CHECK_EQUAL(slices[STATS_SLICE].at(0, TIMESTAMP_COLUMN), data{*ts});
-  const vast::table_slice::size_type TX_ID_COLUMN{23};
-  CHECK_EQUAL(slices[HTTP_SLICE].at(0, TX_ID_COLUMN), data{vast::count{0}});
-  const vast::table_slice::size_type REASON_COLUMN{20};
-  CHECK_EQUAL(slices[FLOW_SLICE].at(0, REASON_COLUMN), data{"timeout"});
-  const vast::table_slice::size_type APP_PROTO_COLUMN2{34};
-  CHECK_EQUAL(slices[FILEINFO_SLICE].at(0, APP_PROTO_COLUMN2), data{"http"});
-  const vast::table_slice::size_type RRTYPE_COLUMN{22};
-  CHECK_EQUAL(slices[DNS_SLICE].at(0, RRTYPE_COLUMN), data{"A"});
-  const vast::table_slice::size_type LINKTYPE_COLUMN{38};
-  CHECK_EQUAL(slices[ALERT_SLICE].at(0, LINKTYPE_COLUMN), data{vast::count{1}});
 }
 
 TEST(json hex number parser) {
