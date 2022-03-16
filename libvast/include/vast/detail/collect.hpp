602a09e211406410ec17148b66a7cc360880a3ea
Benno Evers <benno.evers@tenzir.com>
2022-03-16 13:40:02 +0100

    Implement review feedback

--- libvast/include/vast/detail/collect.hpp
+++ libvast/include/vast/detail/collect.hpp
@@ -3,7 +3,7 @@
 //   | |/ / __ |_\ \  / /          Across
 //   |___/_/ |_/___/ /_/       Space and Time
 //
-// SPDX-FileCopyrightText: (c) 2021 The VAST Contributors
+// SPDX-FileCopyrightText: (c) 2022 The VAST Contributors
 // SPDX-License-Identifier: BSD-3-Clause
 
 #include <vast/detail/generator.hpp>
@@ -11,17 +11,32 @@
 namespace vast::detail {
 
 /// A utility function to collect all results produced by a
-/// `vast::detail::generator<T>` into a suitable standard container.
+/// `vast::detail::generator<T>` into a suitable container.
+/// Example:
+///     auto g = vast::detail::generator<string_view>{};
+///     auto v = vast::detail::collect<std::vector<std::string>>(g);
+template <class Container, class T>
+  requires requires(Container c, T& t) {
+    c.reserve(size_t{});
+    c.emplace(c.end(), t);
+  }
+Container collect(vast::detail::generator<T> g, size_t size_hint = 0) {
+  Container result = {};
+  if (size_hint)
+    result.reserve(size_hint);
+  for (auto& x : g)
+    result.emplace(result.end(), x);
+  return result;
+}
+
+/// A utility function to collect all results produced by a
+/// `vast::detail::generator<T>` into a `std::vector<T>`.
 /// Example:
 ///     auto g = vast::detail::generator<int>{};
-///     auto v = vast::detail::collect<std::vector>(g);
-template <template <class, class> class Container, class Generated>
-Container<Generated, std::allocator<Generated>>
-collect(vast::detail::generator<Generated> g) {
-  Container<Generated, std::allocator<Generated>> result;
-  for (auto& x : g)
-    result.push_back(x);
-  return result;
+///     auto v = vast::detail::collect(g);
+template <class T>
+std::vector<T> collect(vast::detail::generator<T> g, size_t size_hint = 0) {
+  return collect<std::vector<T>>(std::move(g), size_hint);
 }
 
 } // namespace vast::detail
