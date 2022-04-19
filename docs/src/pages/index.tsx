d42ba9e3eb2825ff1333620c76b1c45518e7c790
Dominik Lohmann <mail@dominiklohmann.de>
2022-04-19 10:13:04 +0200

    Fix link to Get Started page

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -24,7 +24,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/start">
+            to="/docs/get-started">
             Get Started
           </Link>
         </div>
