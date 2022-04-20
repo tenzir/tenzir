ad779f53a497ba3de285f0d172c14fb05c564159
Dominik Lohmann <mail@dominiklohmann.de>
2022-04-20 11:26:42 +0200

    Modernize `identity` transform step

--- libvast/src/transform_steps/identity.cpp
+++ libvast/src/transform_steps/identity.cpp
@@ -6,28 +6,39 @@
 // SPDX-FileCopyrightText: (c) 2021 The VAST Contributors
 // SPDX-License-Identifier: BSD-3-Clause
 
-#include "vast/transform_steps/identity.hpp"
-
 #include "vast/error.hpp"
 #include "vast/logger.hpp"
 #include "vast/plugin.hpp"
 #include "vast/table_slice_builder_factory.hpp"
+#include "vast/transform.hpp"
 
 #include <arrow/type.h>
 
 namespace vast {
 
-caf::error
-identity_step::add(type layout, std::shared_ptr<arrow::RecordBatch> batch) {
-  VAST_TRACE("identity step adds batch");
-  transformed_.emplace_back(layout, std::move(batch));
-  return caf::none;
-}
+namespace {
 
-caf::expected<std::vector<transform_batch>> identity_step::finish() {
-  VAST_DEBUG("identity step finished transformation");
-  return std::exchange(transformed_, {});
-}
+// Does nothing with the input.
+class identity_step : public transform_step {
+public:
+  identity_step() noexcept = default;
+
+  caf::error
+  add(type layout, std::shared_ptr<arrow::RecordBatch> batch) override {
+    VAST_TRACE("identity step adds batch");
+    transformed_.emplace_back(layout, std::move(batch));
+    return caf::none;
+  }
+
+  caf::expected<std::vector<transform_batch>> finish() override {
+    VAST_DEBUG("identity step finished transformation");
+    return std::exchange(transformed_, {});
+  }
+
+private:
+  /// The slices being transformed.
+  std::vector<transform_batch> transformed_ = {};
+};
 
 class identity_step_plugin final : public virtual transform_plugin {
 public:
@@ -46,6 +57,8 @@ public:
     return std::make_unique<identity_step>();
   }
 };
+
+} // namespace
 
 } // namespace vast
 
