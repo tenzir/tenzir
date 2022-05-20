1dce47ad258fc433d14c267ab185f617f6af31bb
Thomas Peiselt <pi@kulturguerilla.org>
2022-05-20 07:51:40 +0200

    Move plugin declaration to header file

--- plugins/parquet-store/parquet_store.cpp
+++ plugins/parquet-store/parquet_store.cpp
@@ -504,64 +504,41 @@ system::store_builder_actor::behavior_type store_builder(
   };
 }
 
-/// The plugin entrypoint for the aggregate transform plugin.
-class plugin final : public store_plugin {
-public:
-  /// Initializes the aggregate plugin. This plugin has no general
-  /// configuration, and is configured per instantiation as part of the
-  /// transforms definition. We only check whether there's no unexpected
-  /// configuration here.
-  caf::error initialize(data options) override {
-    if (caf::holds_alternative<caf::none_t>(options))
+caf::error plugin::initialize(data options) {
+  if (caf::holds_alternative<caf::none_t>(options))
+    return caf::none;
+  if (const auto* rec = caf::get_if<record>(&options))
+    if (rec->empty())
       return caf::none;
-    if (const auto* rec = caf::get_if<record>(&options))
-      if (rec->empty())
-        return caf::none;
-    return caf::make_error(ec::invalid_configuration, //
-                           "expected empty configuration under "
-                           "vast.plugins.parquet-store");
-  }
+  return caf::make_error(ec::invalid_configuration, //
+                         "expected empty configuration under "
+                         "vast.plugins.parquet-store");
+}
 
-  [[nodiscard]] const char* name() const override {
-    return "parquet-store";
-  };
-
-  /// Create a store builder actor that accepts incoming table slices.
-  /// @param accountant The actor handle of the accountant.
-  /// @param fs The actor handle of a filesystem.
-  /// @param id The partition id for which we want to create a store. Can
-  ///           be used as a unique key by the implementation.
-  /// @returns A store_builder actor and a chunk called the "header". The
-  ///          contents of the header will be persisted on disk, and should
-  ///          allow the plugin to retrieve the correct store actor when
-  ///          `make_store()` below is called.
-  [[nodiscard]] caf::expected<builder_and_header>
-  make_store_builder(system::accountant_actor accountant,
-                     system::filesystem_actor fs,
-                     const vast::uuid& id) const override {
-    auto actor_handle
-      = fs.home_system().spawn(store_builder, accountant, fs, id);
-    auto header = chunk::copy(id);
-    return builder_and_header{actor_handle, header};
-  }
-
-  /// Create a store actor from the given header. Called when deserializing a
-  /// partition that uses this partition as a store backend.
-  /// @param accountant The actor handle the accountant.
-  /// @param fs The actor handle of a filesystem.
-  /// @param header The store header as found in the partition flatbuffer.
-  /// @returns A new store actor.
-  [[nodiscard]] caf::expected<system::store_actor>
-  make_store(system::accountant_actor accountant, system::filesystem_actor fs,
-             std::span<const std::byte> header) const override {
-    if (header.size() != uuid::num_bytes)
-      return caf::make_error(ec::invalid_argument, "header must have size of "
-                                                   "single uuid");
-    auto id = uuid(std::span<const std::byte, uuid::num_bytes>(header.data(),
-                                                               header.size()));
-    return fs.home_system().spawn(store, accountant, fs, id);
-  }
+[[nodiscard]] const char* plugin::name() const {
+  return "parquet-store";
 };
+
+[[nodiscard]] caf::expected<plugin::builder_and_header>
+plugin::make_store_builder(system::accountant_actor accountant,
+                           system::filesystem_actor fs,
+                           const vast::uuid& id) const {
+  auto actor_handle = fs.home_system().spawn(store_builder, accountant, fs, id);
+  auto header = chunk::copy(id);
+  return builder_and_header{actor_handle, header};
+}
+
+[[nodiscard]] caf::expected<system::store_actor>
+plugin::make_store(system::accountant_actor accountant,
+                   system::filesystem_actor fs,
+                   std::span<const std::byte> header) const {
+  if (header.size() != uuid::num_bytes)
+    return caf::make_error(ec::invalid_argument, "header must have size of "
+                                                 "single uuid");
+  auto id = uuid(
+    std::span<const std::byte, uuid::num_bytes>(header.data(), header.size()));
+  return fs.home_system().spawn(store, accountant, fs, id);
+}
 
 } // namespace vast::plugins::parquet_store
 
--- plugins/parquet-store/parquet_store.hpp
+++ plugins/parquet-store/parquet_store.hpp
@@ -8,7 +8,12 @@
 
 #pragma once
 
+#include "vast/data.hpp"
+
+#include <vast/chunk.hpp>
+#include <vast/error.hpp>
 #include <vast/fwd.hpp>
+#include <vast/plugin.hpp>
 #include <vast/system/actors.hpp>
 #include <vast/uuid.hpp>
 
@@ -56,6 +61,52 @@ struct store_state {
   using request
     = std::tuple<vast::query, caf::typed_response_promise<uint64_t>>;
   std::vector<request> deferred_requests_ = {};
+};
+
+system::store_builder_actor::behavior_type store_builder(
+  system::store_builder_actor::stateful_pointer<store_builder_state> self,
+  system::accountant_actor accountant, system::filesystem_actor fs,
+  const uuid& id);
+
+system::store_actor::behavior_type
+store(system::store_actor::stateful_pointer<store_state> self,
+      const system::accountant_actor& accountant,
+      const system::filesystem_actor& fs, const uuid& id);
+
+/// The plugin entrypoint for the parquet store plugin.
+class plugin final : public store_plugin {
+public:
+  /// Initializes the aggregate plugin. This plugin has no general
+  /// configuration, and is configured per instantiation as part of the
+  /// transforms definition. We only check whether there's no unexpected
+  /// configuration here.
+  caf::error initialize(data options) override;
+
+  [[nodiscard]] const char* name() const override;
+
+  /// Create a store builder actor that accepts incoming table slices.
+  /// @param accountant The actor handle of the accountant.
+  /// @param fs The actor handle of a filesystem.
+  /// @param id The partition id for which we want to create a store. Can
+  ///           be used as a unique key by the implementation.
+  /// @returns A store_builder actor and a chunk called the "header". The
+  ///          contents of the header will be persisted on disk, and should
+  ///          allow the plugin to retrieve the correct store actor when
+  ///          `make_store()` below is called.
+  [[nodiscard]] caf::expected<builder_and_header>
+  make_store_builder(system::accountant_actor accountant,
+                     system::filesystem_actor fs,
+                     const vast::uuid& id) const override;
+
+  /// Create a store actor from the given header. Called when deserializing a
+  /// partition that uses this partition as a store backend.
+  /// @param accountant The actor handle the accountant.
+  /// @param fs The actor handle of a filesystem.
+  /// @param header The store header as found in the partition flatbuffer.
+  /// @returns A new store actor.
+  [[nodiscard]] caf::expected<system::store_actor>
+  make_store(system::accountant_actor accountant, system::filesystem_actor fs,
+             std::span<const std::byte> header) const override;
 };
 
 } // namespace vast::plugins::parquet_store
