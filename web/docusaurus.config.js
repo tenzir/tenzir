72b54b004c746e36111f4293ec8ef620e6adb24b
Matthias Vallentin <matthias@vallentin.net>
2022-05-23 17:36:47 +0200

    Use a nicer dark mode color scheme

--- web/docusaurus.config.js
+++ web/docusaurus.config.js
@@ -2,7 +2,7 @@
 // Note: type annotations allow type checking and IDEs autocompletion
 
 const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const darkCodeTheme = require('prism-react-renderer/themes/vsDark');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
