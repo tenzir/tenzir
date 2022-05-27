c54f8c6b61f635c66d622a9e4932e1dc749868d8
Benno Evers <benno.evers@tenzir.com>
2022-05-27 16:52:48 +0200

    Revert "Revive the old index implementation as legacy index"
    This reverts commit 8beee922e94d50fd41384575ad925bc9a34e2eeb.

--- libvast/test/system/query_processor.cpp
+++ libvast/test/system/query_processor.cpp
@@ -43,16 +43,6 @@ struct mock_index_state {
 system::index_actor::behavior_type
 mock_index(system::index_actor::stateful_pointer<mock_index_state> self) {
   return {
-    [=](atom::worker, system::query_supervisor_actor) {
-      FAIL("no mock implementation available");
-    },
-    [=](atom::worker, atom::wakeup, system::query_supervisor_actor) {
-      FAIL("no mock implementation available");
-    },
-    [=](atom::internal, vast::query&, system::query_supervisor_actor&,
-        caf::actor_addr) -> caf::result<system::query_cursor> {
-      FAIL("no mock implementation available");
-    },
     [=](atom::done, uuid) {
       FAIL("no mock implementation available");
     },
