b8e2972235e58d50b98bc30a97a2f420677f4fac
Remi Dettai <rdettai@gmail.com>
2022-06-13 11:46:29 +0200

    Convert to JSON to avoid CSV import bugs

--- cloud/aws/terraform/flowlogs/inputs.tf
+++ cloud/aws/terraform/flowlogs/inputs.tf
@@ -7,8 +7,10 @@ variable "vast_lambda_role_name" {}
 
 locals {
   s3_subcmd   = "aws s3 --region ${var.source_bucket_region} cp s3://${var.source_bucket_name}/$SRC_KEY -"
-  vast_subcmd = "vast import --type=aws.flowlogs csv"
-  import_cmd  = "${local.s3_subcmd} | gzip -d | tr ' ' , | ${local.vast_subcmd}"
+  # patch: convert csv to json first
+  conv_subcmd = "python3 -c 'import csv, json, sys; [print(json.dumps(dict(r))) for r in csv.DictReader(sys.stdin, delimiter=\" \")]'"
+  vast_subcmd = "vast import --type=aws.flowlogs json"
+  import_cmd  = "${local.s3_subcmd} | gzip -d | ${local.conv_subcmd} | ${local.vast_subcmd}"
 }
 
 module "env" {
