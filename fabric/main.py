c4be3806e03e5064bc9d5164fa10384ff1212dbe
Matthias Vallentin <matthias@vallentin.net>
2022-07-06 21:51:24 +0200

    Call fabric what it is

--- fabric/main.py
+++ fabric/main.py
@@ -1,20 +1,19 @@
 import apps.db as db
 import apps.misp as misp
 import asyncio
+import logging
 from backbone import InMemoryBackbone
 from fabric import Fabric
 
-
 async def main():
     backbone = InMemoryBackbone()
-    vast = Fabric(backbone)
+    fabric = Fabric(backbone)
 
     # run all the apps in parallel
-    db_task = asyncio.create_task(db.start(vast))
-    misp_task = asyncio.create_task(misp.start(vast))
+    db_task = asyncio.create_task(db.start(fabric))
+    misp_task = asyncio.create_task(misp.start(fabric))
     await db_task
     await misp_task
-
 
 if __name__ == "__main__":
     asyncio.run(main())
