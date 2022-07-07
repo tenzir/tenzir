be8ff7ed8d92743fcd09887817c2cca84bbccb3a
Matthias Vallentin <matthias@vallentin.net>
2022-07-07 12:30:03 +0200

    Switch to asyncio version of zmq

--- fabric/apps/misp.py
+++ fabric/apps/misp.py
@@ -2,6 +2,7 @@ import asyncio
 import fabric
 import pymisp
 import zmq
+import zmq.asyncio
 import json
 import logging
 import misp_stix_converter
@@ -19,38 +20,36 @@ class MISP:
         await self.fabric.subscribe(topic, self._callback)
 
     async def run(self):
-        socket = zmq.Context().socket(zmq.SUB)
+        socket = zmq.asyncio.Context().socket(zmq.SUB)
         # TODO: configure according to self.config
         socket.connect("tcp://localhost:50000")
         socket.setsockopt(zmq.SUBSCRIBE, b"misp_json")
-        poller = zmq.Poller()
-        poller.register(socket, zmq.POLLIN)
-        while True:
-            self.logger.debug("polling 0mq socket")
-            socks = dict(poller.poll(timeout=1000))
-            if socket not in socks or socks[socket] != zmq.POLLIN:
-                continue
-            raw = socket.recv()
-            _, message = raw.decode("utf-8").split(" ", 1)
-            try:
-                json_msg = json.loads(message)
-                self.logger.debug(json_msg)
-            except Exception as e:
-                self.logger.error(f"failed to message {message}: {e}")
-                continue
-            event = json_msg.get("Event", None)
-            attribute = json_msg.get("Attribute", None)
-            # Only consider events, not Attributes that ship with Events
-            if not event or attribute:
-                continue
-            parser = misp_stix_converter.MISPtoSTIX21Parser()
-            try:
-                parser.parse_misp_event(event)
-                self.logger.info(parser.bundle.serialize(pretty=True))
-                await self.fabric.publish("stix.bundle", fabric.Message(parser.bundle))
-            except Exception as e:
-                self.logger.error(f"failed to parse MISP event as STIX: {e}")
-                continue
+        try:
+            while True:
+                raw = await socket.recv()
+                _, message = raw.decode("utf-8").split(" ", 1)
+                try:
+                    json_msg = json.loads(message)
+                    self.logger.debug(json_msg)
+                except Exception as e:
+                    self.logger.error(f"failed to message {message}: {e}")
+                    continue
+                event = json_msg.get("Event", None)
+                attribute = json_msg.get("Attribute", None)
+                # Only consider events, not Attributes that ship with Events
+                if not event or attribute:
+                    continue
+                parser = misp_stix_converter.MISPtoSTIX21Parser()
+                try:
+                    parser.parse_misp_event(event)
+                    self.logger.info(parser.bundle.serialize(pretty=True))
+                    await self.fabric.publish("stix.bundle", fabric.Message(parser.bundle))
+                except Exception as e:
+                    self.logger.error(f"failed to parse MISP event as STIX: {e}")
+                    continue
+        finally:
+            socket.setsockopt(zmq.LINGER, 0)
+            socket.close()
 
     async def _callback(self, msg: fabric.Message):
         self.logger.debug(msg)
