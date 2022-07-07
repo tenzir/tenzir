5306cb6065f4a3328f4a4ad7e10ee341ebcf1d7f
Remi Dettai <rdettai@gmail.com>
2022-07-07 11:03:10 +0200

    Fix setup script

--- fabric/setup.sh
+++ fabric/setup.sh
@@ -4,8 +4,10 @@ set -e
 
 ABSOLUTE_DIRNAME=$(dirname $(readlink -f  $0))
 
+cd $ABSOLUTE_DIRNAME/../cloud/aws/
+
 export VAST_VERSION=latest
 
-$ABSOLUTE_DIRNAME/../cloud/aws/vast-cloud pro.login pro.pull-image 
+./vast-cloud pro.login pro.pull-image 
 
 docker run --name vast-pro --rm -d tenzir/vast-pro:latest start
