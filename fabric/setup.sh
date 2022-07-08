66576583b931c9a4eba594ac7301b3a74e4a2a74
Remi Dettai <rdettai@gmail.com>
2022-07-08 09:02:38 +0200

    Remove MISP connection config

--- fabric/setup.sh
+++ fabric/setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Run this scrip to setup a running VAST container with the matcher plugin installed
+
 set -e
 
 ABSOLUTE_DIRNAME=$(dirname $(readlink -f  $0))
@@ -11,16 +13,7 @@ export VAST_AWS_REGION=eu-west-1
 
 export VASTCLOUD_REBUILD=1
 
-./vast-cloud pro.login
-
-unset VASTCLOUD_REBUILD
-
-./vast-cloud pro.pull-image
+./vast-cloud pro.login pro.pull-image
 
 [[ $(docker ps -f "name=vast-pro" --format '{{.Names}}') == vast-pro ]] || \
    docker run --name vast-pro --rm -d tenzir/vast-pro:latest start
-
-echo "Connect to MISP with:"
-echo "ssh -i /dev/stdin -N -L 5000:localhost:5000 -L 50000:localhost:50000 ubuntu@51.91.208.2"
-echo "And the following key:"
-VASTCLOUD_NOTTY=1 ./vast-cloud pro.key
