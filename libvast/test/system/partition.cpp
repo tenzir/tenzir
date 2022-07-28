8976f6dfd5900a8fd06466e4f2686ec526017f78
Benno Evers <benno.evers@tenzir.com>
2022-07-28 15:29:41 +0200

    Add new filesystem handlers to unit tests

--- libvast/test/system/partition.cpp
+++ libvast/test/system/partition.cpp
@@ -12,6 +12,7 @@
 
 #include "vast/detail/collect.hpp"
 #include "vast/detail/spawn_container_source.hpp"
+#include "vast/die.hpp"
 #include "vast/qualified_record_field.hpp"
 #include "vast/string_synopsis.hpp"
 #include "vast/system/active_partition.hpp"
@@ -37,6 +38,16 @@ system::filesystem_actor::behavior_type mock_filesystem(
     },
     [](atom::read, const std::filesystem::path&) -> caf::result<chunk_ptr> {
       return nullptr;
+    },
+    [](vast::atom::move, const std::filesystem::path&,
+       const std::filesystem::path&) -> caf::result<vast::atom::done> {
+      vast::die("not implemented");
+    },
+    [](
+      vast::atom::move,
+      const std::vector<std::pair<std::filesystem::path, std::filesystem::path>>&)
+      -> caf::result<vast::atom::done> {
+      vast::die("not implemented");
     },
     [self](atom::mmap, const std::filesystem::path&) -> caf::result<chunk_ptr> {
       return self->make_response_promise<chunk_ptr>();
