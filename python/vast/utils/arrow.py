5d79a6f9dffcb99cd977cd3f788c04a4885e8122
Matthias Vallentin <matthias@vallentin.net>
2022-08-12 16:34:09 +0200

    Remove unused extension type for now

--- python/vast/utils/arrow.py
+++ python/vast/utils/arrow.py
@@ -3,19 +3,6 @@ import ipaddress
 import pyarrow as pa
 
 
-class IPAddressType(pa.PyExtensionType):
-    def __init__(self):
-        pa.PyExtensionType.__init__(self, pa.binary(16))
-
-    def __reduce__(self):
-        return IPAddressType, ()
-
-    def __arrow_ext_scalar_class__(self):
-        # TODO: we should probably write our own IP address type that supports
-        # the v4-in-v6 embedding natively.
-        return ipaddress.IPv6Address
-
-
 def names(schema: pa.Schema):
     meta = schema.metadata
     return [meta[key].decode() for key in meta if key.startswith(b"VAST:name:")]
