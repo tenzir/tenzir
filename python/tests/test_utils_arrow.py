5c983b5de0fcc5c19209e16ee757c0a7f1652018
Anthony VEREZ <netantho@gmail.com>
2022-08-22 13:08:09 +0200

    More style fixes

--- python/tests/test_utils_arrow.py
+++ python/tests/test_utils_arrow.py
@@ -37,9 +37,9 @@ def test_unpack_ip():
 
 def test_fmt_ip():
     df = pd.DataFrame(
-        [b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xff\xff\n\x01\x15\xa5'],
-        columns=['ip']
+        [b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xff\xff\n\x01\x15\xa5"],
+        columns=["ip"],
     )
-    assert vua.fmt_ip(df, ip_columns=['ip']).ip[0] == ipaddress.IPv4Address(
+    assert vua.fmt_ip(df, ip_columns=["ip"]).ip[0] == ipaddress.IPv4Address(
         "10.1.21.165"
     )
