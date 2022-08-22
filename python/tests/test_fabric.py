5b45367c9068a955e8ecfb750d169b0c1d0a420c
Matthias Vallentin <matthias@vallentin.net>
2022-08-22 11:01:48 +0200

    Implement type conversation on push

--- python/tests/test_fabric.py
+++ python/tests/test_fabric.py
@@ -1,20 +1,39 @@
 import pytest
 
-import vast
-import vast.backbones
-
-def test_fabric_context_registration(fabric):
-    fabric.register(str, "string")
-    assert fabric.registry[str][0].type == str
-    assert fabric.registry[str][0].name == "string"
+@pytest.mark.asyncio
+async def test_fabric_convert(fabric):
+    # Convert into something unknown.
+    result = fabric.convert("foo", 42)
+    assert result is None
+    # Convert into a registered type.
+    fabric.register("int", "string", lambda x: f"answer: {x}")
+    result = fabric.convert("int", 42)
+    assert result == {"string": "answer: 42"}
 
 @pytest.mark.asyncio
-async def test_fabric_push_pull(fabric):
-    fabric.register(int, "int")
+async def test_fabric_push_pull_without_convert(fabric):
     result = 0
     def assign(x: int):
         nonlocal result
         result = x
-    await fabric.pull(assign)
-    await fabric.push(42)
+    await fabric.pull("int", assign)
+    await fabric.push("int", 42)
     assert result == 42
+
+@pytest.mark.asyncio
+async def test_fabric_push_pull_convert(fabric):
+    to_string = lambda x: str(x - 2)
+    fabric.register("int", "string", to_string)
+    result = "answer: "
+    def append_int(x: int):
+        nonlocal result
+        result += str(x)
+    def append_str(x: str):
+        nonlocal result
+        result += x
+    # Register callbacks for the original and converted type. A single push
+    # yields two objects.
+    await fabric.pull("string", append_str)
+    await fabric.pull("int", append_int)
+    await fabric.push("int", 4)
+    assert result == "answer: 42"
--- python/vast/fabric.py
+++ python/vast/fabric.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from inspect import signature
+from typing import Any, Callable, TypeAlias
 
 from dynaconf import Dynaconf
 
@@ -8,18 +8,35 @@ import vast.utils.logging
 
 logger = vast.utils.logging.get(__name__)
 
+Converter: TypeAlias = Callable[[Any], Any]
+
+
+def topic(name: str):
+    """Creates a topic from a type name."""
+    return f"/{name.replace('.', '/')}"
+
+
 class Context:
-    """Object context to enable types to travel over the backbone of a
-    fabric."""
+    """Object context that offers type-specific metadata and functions."""
 
-    def __init__(self, type, name):
-        self.type = type
-        self.name = name
+    def __init__(self, name):
+        self._name = name
+        self.transforms = defaultdict(list)
 
+    def register(self, target: str, converter: Converter) -> None:
+        """Registers a converter function to reach another type."""
+        self.transforms[target].append(converter)
 
-def topic(context: Context):
-    """Creates a topic for an object context."""
-    return f"/{context.name.replace('.', '/')}"
+    def convert(self, object: Any) -> dict[str, Any]:
+        """Transform a type into another type using a registered function."""
+        result = {}
+        for (name, funs) in self.transforms.items():
+            result |= {name: fun(object) for fun in funs}
+        return result
+
+    @property
+    def name(self):
+        return self._name
 
 
 class Fabric:
@@ -29,31 +46,41 @@ class Fabric:
     def __init__(self, config: Dynaconf, backbone: Backbone):
         self.config = config
         self.backbone = backbone
-        self.registry = defaultdict(list)
+        self.registry = {}
 
-    def register(self, type, name: str):
-        """Register an object context."""
-        logger.debug(f"registering object {name}: {type}")
+    def register(self, source: str, sink: str, converter: Converter) -> None:
+        """Register a converter between two types."""
+        ctx = self.registry.get(source, None)
+        if not ctx:
+            logger.debug(f"creating new context for type {source}")
+            ctx = Context(source)
+            self.registry[source] = ctx
+        logger.debug(f"registers converter: {source} -> {sink}")
+        ctx.register(sink, converter)
+        # TODO: update existing backbone subscriptions for newly registered
+        # types.
 
-        self.registry[type].append(Context(type, name))
+    def convert(self, source: str, object: Any) -> None | Any | list[Any]:
+        """Convert an object to another type."""
+        if source not in self.registry:
+            logger.error(f"no context for type {source}")
+            return None
+        match self.registry[source].convert(object):
+            case [x]:
+                return x
+            case xs:
+                return xs
 
-    async def push(self, object):
-        """Push a registered object into the fabric."""
-        contexts = self.registry[type(object)]
-        if not contexts:
-            logger.error(f"no context registered for object {object}")
-            return
-        for ctx in contexts:
-            await self.backbone.publish(topic(ctx), object)
+    async def push(self, name: str, object: Any):
+        """Push a registered object instance into the fabric."""
+        # First push out the original object.
+        await self.backbone.publish(topic(name), object)
+        # Then push out all transformed objects.
+        if name in self.registry:
+            ctx = self.registry[name]
+            for new_name, converted in ctx.convert(object).items():
+                await self.backbone.publish(topic(new_name), converted)
 
-    async def pull(self, callback):
+    async def pull(self, name: str, callback):
         """Provide a callback for a registered object."""
-        sig = signature(callback)
-        annotation = list(sig.parameters.values())[0].annotation
-        contexts = self.registry[annotation]
-        if not contexts:
-            logger.error(f"no context registered for object {annotation}")
-            return
-        for ctx in contexts:
-            logger.debug(f"subscribing to {topic(ctx)} for type {annotation}")
-            await self.backbone.subscribe(topic(ctx), callback)
+        await self.backbone.subscribe(topic(name), callback)
