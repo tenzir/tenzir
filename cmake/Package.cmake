dd0d96d8d5cc9ac445556cdff90ff0eb82919134
Tobias Mayer <tobim@fastmail.fm>
2022-08-23 14:03:31 +0200

    Soft-enforce matching packaging prefixes

--- cmake/Package.cmake
+++ cmake/Package.cmake
@@ -1,5 +1,19 @@
 # -- packaging -----------------------------------------------------------------
 
+# We enforce that package trees are in the same location
+# as install trees. This allows us to reuse artifacts with
+# installation related substitutions in packages.
+# This has the drawback that we can't use package specific
+# prefixes.
+if (NOT DEFINED CPACK_PACKAGING_INSTALL_PREFIX)
+  set(CPACK_PACKAGING_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+else ()
+  if ("${CPACK_PACKAGING_INSTALL_PREFIX}" NOT STREQUAL
+      "${CMAKE_INSTALL_PREFIX}")
+    message(WARNING "Mismatches between ${CPACK_PACKAGING_INSTALL_PREFIX} and \
+        ${CMAKE_INSTALL_PREFIX} are known to produce broken packages")
+  endif ()
+endif ()
 set(CPACK_PACKAGE_VENDOR "Tenzir")
 set(CPACK_PACKAGE_CONTACT "engineering@tenzir.com")
 set(CPACK_VERBATIM_VARIABLES ON)
