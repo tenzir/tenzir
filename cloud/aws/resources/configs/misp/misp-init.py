ecd2e3819b5540bc7541b6cda3b82b85f81e5ae2
Remi Dettai <rdettai@gmail.com>
2022-08-31 16:47:21 +0000

    Fix lint

--- cloud/aws/resources/configs/misp/misp-init.py
+++ cloud/aws/resources/configs/misp/misp-init.py
@@ -18,13 +18,13 @@ import yaml
 
 from pymisp import PyMISP
 
-CAKE = '/var/www/MISP/app/Console/cake'
+CAKE = "/var/www/MISP/app/Console/cake"
 
 # Wait until the web server is up an running.
-print('waiting until MISP web server becomes available')
+print("waiting until MISP web server becomes available")
 while True:
     try:
-        socket.socket().connect(('127.0.0.1', 443))
+        socket.socket().connect(("127.0.0.1", 443))
         break
     except Exception as e:
         time.sleep(5)
@@ -34,15 +34,15 @@ while True:
 # behavior on startup, so we must trigger 'cake UserInit' to perform the
 # intilization. The output includes an ephemeral API key that we can use
 # subsequently to connect via PyMISP.
-result = subprocess.run([CAKE, 'UserInit'], capture_output=True)
-api_key = result.stdout.decode('utf-8').splitlines()[-1]
-if 'MISP instance already initialised' in api_key:
-    print('skipping initialization of already existing admin user')
-    api_key = os.environ['MISP_API_KEY']
+result = subprocess.run([CAKE, "UserInit"], capture_output=True)
+api_key = result.stdout.decode("utf-8").splitlines()[-1]
+if "MISP instance already initialised" in api_key:
+    print("skipping initialization of already existing admin user")
+    api_key = os.environ["MISP_API_KEY"]
 else:
-    print(f'using ephemeral API key to initialize MISP: {api_key}')
+    print(f"using ephemeral API key to initialize MISP: {api_key}")
 # Clean up the mess we left after invoking 'cake' as root.
-os.system('chown -R www-data:www-data /var/www/MISP')
+os.system("chown -R www-data:www-data /var/www/MISP")
 
 # Connect to MISP.
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -50,16 +50,16 @@ misp = PyMISP("https://127.0.0.1:443", api_key, False)
 misp.toggle_global_pythonify()
 
 # Execute YAML settings.
-with open('/misp-config.yaml', 'r') as config:
+with open("/misp-config.yaml", "r") as config:
     try:
         settings = yaml.safe_load(config)
         for category, values in settings.items():
             for key, value in values.items():
-                setting = f'{category}.{key}'
-                print(f'updating setting: {setting} = {value}')
+                setting = f"{category}.{key}"
+                print(f"updating setting: {setting} = {value}")
                 put = lambda x: misp.set_server_setting(setting, x, force=True)
                 if type(value) == bool:
-                    put('1' if value else '0')
+                    put("1" if value else "0")
                 else:
                     put(value)
         # Ugly hack: kill the ZeroMQ subsystem so that MISP brings it up correctly
@@ -68,26 +68,26 @@ with open('/misp-config.yaml', 'r') as config:
         # subsystem through the web UI. So we kill any lingering processes
         # prior to adjusting a ZeroMQ setting. ¯\_(ツ)_/¯
         try:
-            if settings['Plugin']['ZeroMQ_enable']:
-                print('resetting ZeroMQ subsystem')
-                os.system('pkill -f mispzmq.py')
-                misp.set_server_setting('Plugin.ZeroMQ_enable', '1')
+            if settings["Plugin"]["ZeroMQ_enable"]:
+                print("resetting ZeroMQ subsystem")
+                os.system("pkill -f mispzmq.py")
+                misp.set_server_setting("Plugin.ZeroMQ_enable", "1")
         except KeyError as error:
-            pass # do nothing if ZeroMQ shouldn't be enabled
+            pass  # do nothing if ZeroMQ shouldn't be enabled
     except yaml.YAMLError as error:
         print(error)
         sys.exit(1)
 
 # Rename admin user and reset password. We do this after applying the YAML
 # configuration because success execution hinges on the password policy.
-if api_key != os.environ['MISP_API_KEY']:
-    print('adjusting admin email, password, and API key')
+if api_key != os.environ["MISP_API_KEY"]:
+    print("adjusting admin email, password, and API key")
     admin = misp.get_user(1)
-    admin.change_pw = '0'
-    admin.email = os.environ['MISP_ADMIN_USER']
-    admin.password = os.environ['MISP_ADMIN_PASSWORD']
-    admin.authkey = os.environ['MISP_API_KEY']
+    admin.change_pw = "0"
+    admin.email = os.environ["MISP_ADMIN_USER"]
+    admin.password = os.environ["MISP_ADMIN_PASSWORD"]
+    admin.authkey = os.environ["MISP_API_KEY"]
     misp.update_user(admin, user_id=1)
 
-print('completed initilization successfully')
+print("completed initilization successfully")
 sys.exit(0)
