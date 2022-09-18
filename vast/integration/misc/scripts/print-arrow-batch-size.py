ab800d78a17bbf67b1db26e78516c4707f4cb1ea
Dominik Lohmann <mail@dominiklohmann.de>
2022-09-18 18:49:52 +0200

    Check batch sizes before and after rebuilding in tests

--- vast/integration/misc/scripts/print-arrow-batch-size.py
+++ vast/integration/misc/scripts/print-arrow-batch-size.py
@@ -1,15 +1,13 @@
 #! /usr/bin/env python3
 
 # Example usage:
-# vast -N export arrow '#type ~ /suricata.*/' | ./scripts/print-arrow.py
+# vast -N export arrow '#type ~ /suricata.*/' | ./scripts/print-arrow-batch-size.py
 
 import sys
 import pyarrow
 
 # Open stdin in binary mode.
 istream = pyarrow.input_stream(sys.stdin.buffer)
-batch_count = 0
-row_count = 0
 
 # An Arrow reader consumes a stream of batches with the same schema. When
 # reading the result for a query that returns multiple schemas, VAST will use
@@ -17,17 +15,14 @@ row_count = 0
 # exception occurs.
 try:
     while True:
-        print("open next reader")
         reader = pyarrow.ipc.RecordBatchStreamReader(istream)
         try:
             while True:
                 batch = reader.read_next_batch()
-                batch_count += 1
-                row_count += batch.num_rows
-                print(str(batch.schema))
+                print(
+                    f"{batch.schema.metadata[b'VAST:name:0'].decode('utf-8')}: {batch.num_rows}"
+                )
         except StopIteration:
-            print("done with current reader, rows: " + str(row_count))
-            batch_count = 0
-            row_count = 0
+            pass
 except:
-    print("done with all readers")
+    pass
