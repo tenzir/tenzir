6bc960ea068f5cc88ff8d5871a9d8e84f1188a3b
Dominik Lohmann <mail@dominiklohmann.de>
2022-09-20 13:16:47 +0200

    Fix an iterator invalidation bug in feather and parquet

--- plugins/parquet/parquet.cpp
+++ plugins/parquet/parquet.cpp
@@ -410,8 +410,11 @@ public:
   /// Retrieve all of the store's slices.
   /// @returns The store's slices.
   [[nodiscard]] detail::generator<table_slice> slices() const override {
-    for (const auto& slice : slices_)
-      co_yield slice;
+    // We need to make a copy of the slices here because the slices_ vector may
+    // get invalidated while we iterate over it.
+    auto slices = slices_;
+    for (auto& slice : slices)
+      co_yield std::move(slice);
   }
 
   [[nodiscard]] uint64_t num_events() const override {
