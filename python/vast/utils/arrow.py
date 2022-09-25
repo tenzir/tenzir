f00aaa99c59404e598207e5cd1c0aef90788b4f2
Matthias Vallentin <matthias@vallentin.net>
2022-09-25 05:47:21 +0200

    Move code chunks around

--- python/vast/utils/arrow.py
+++ python/vast/utils/arrow.py
@@ -4,16 +4,6 @@ from typing import SupportsBytes
 
 import pyarrow as pa
 
-# Accepts a 128-bit buffer holding an IPv6 address and returns an IPv4 or IPv6
-# address.
-def unpack_ip(buffer: SupportsBytes) -> ip.IPv4Address | ip.IPv6Address:
-    num = int.from_bytes(buffer, byteorder="big")
-    # Convert IPv4 mapped addresses back to regular IPv4.
-    # https://tools.ietf.org/html/rfc4291#section-2.5.5.2
-    if (num >> 32) == 65535:
-        num = num - (65535 << 32)
-    return ip.ip_address(num)
-
 
 class AddressScalar(pa.ExtensionScalar):
     def as_py(self) -> ip.IPv4Address | ip.IPv6Address:
@@ -116,11 +106,6 @@ class EnumType(pa.ExtensionType):
         return EnumScalar
 
 
-# TODO: move to appropriate location
-pa.register_extension_type(AddressType())
-pa.register_extension_type(SubnetType())
-
-
 def names(schema: pa.Schema):
     meta = schema.metadata
     return [meta[key].decode() for key in meta if key.startswith(b"VAST:name:")]
@@ -129,3 +114,19 @@ def names(schema: pa.Schema):
 def name(schema: pa.Schema):
     xs = names(schema)
     return xs[0] if xs[0] else ""
+
+
+# Accepts a 128-bit buffer holding an IPv6 address and returns an IPv4 or IPv6
+# address.
+def unpack_ip(buffer: SupportsBytes) -> ip.IPv4Address | ip.IPv6Address:
+    num = int.from_bytes(buffer, byteorder="big")
+    # Convert IPv4 mapped addresses back to regular IPv4.
+    # https://tools.ietf.org/html/rfc4291#section-2.5.5.2
+    if (num >> 32) == 65535:
+        num = num - (65535 << 32)
+    return ip.ip_address(num)
+
+
+# TODO: move to appropriate location
+pa.register_extension_type(AddressType())
+pa.register_extension_type(SubnetType())
