5a8e6e033cce3f8cf4610130ce9a5f54f3809804
Benno Evers <benno.evers@tenzir.com>
2022-10-06 01:06:36 +0200

    Fix warning in authenticator

--- plugins/web/src/authenticator.cpp
+++ plugins/web/src/authenticator.cpp
@@ -174,7 +174,7 @@ authenticator(authenticator_actor::stateful_pointer<authenticator_state> self,
     [self](atom::validate, const token_t& token) -> bool {
       return self->state.authenticate(token);
     },
-    [self](atom::status, system::status_verbosity) -> vast::record {
+    [](atom::status, system::status_verbosity) -> vast::record {
       return record{};
     },
   };
