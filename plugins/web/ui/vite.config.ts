8b116e1723ce577bfa7e087faf0dc572b1b475e3
Tobias Mayer <tobim@fastmail.fm>
2022-10-17 14:31:36 +0200

    Integrate a frontend page with a status component
    This commit migrates the state of tenzir/vast-frontend
    as of c88b0f71d into the web plugin.

---
+++ plugins/web/ui/vite.config.ts
@@ -0,0 +1,24 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import type { UserConfig } from 'vite';
+
+import Unocss from 'unocss/vite';
+
+import { presetUno } from 'unocss';
+import presetIcons from '@unocss/preset-icons';
+
+const config: UserConfig = {
+	plugins: [
+		sveltekit(),
+
+		Unocss({
+			presets: [
+				presetUno(),
+				presetIcons({
+					/* options */
+				})
+			]
+		})
+	]
+};
+
+export default config;
