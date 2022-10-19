e9317e5ac04f842371b0ec19ea121b9f2de2d9e8
Patryk Sztyglic <patryk.sztyglic@tenzir.com>
2022-10-19 16:28:32 +0200

    Add vast-io and changelog jobs
    Authored-by: Patryk Sztyglic <patryk.sztyglic@gmail.com>
    Authored-by: Dominik Lohmann <mail@dominiklohmann.de>

---
+++ scripts/debian/install-dev-dependencies.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+set -e
+echo 'deb http://deb.debian.org/debian bullseye-backports main' > /etc/apt/sources.list.d/backports.list 
+apt-get update 
+apt-get -y --no-install-recommends install \
+    build-essential \
+    ca-certificates \
+    cmake/bullseye-backports \
+    cmake-data/bullseye-backports \
+    flatbuffers-compiler-dev \
+    g++-10 \
+    gcc-10 \
+    git-core \
+    gnupg2 \
+    jq \
+    libasio-dev \
+    libcaf-dev \
+    libbroker-dev \
+    libflatbuffers-dev \
+    libfmt-dev \
+    libpcap-dev tcpdump \
+    libhttp-parser-dev \
+    libsimdjson-dev \
+    libspdlog-dev \
+    libssl-dev \
+    libunwind-dev \
+    libyaml-cpp-dev \
+    libxxhash-dev \
+    lsb-release \
+    ninja-build \
+    pkg-config \
+    python3-dev \
+    python3-pip \
+    python3-venv \
+    robin-map-dev \
+    wget
+wget "https://apache.jfrog.io/artifactory/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb"
+apt-get -y --no-install-recommends install ./apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb 
+apt-get update
+apt-get -y --no-install-recommends install libarrow-dev=9.0.0-1 libprotobuf-dev libparquet-dev=9.0.0-1
+rm ./apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb
