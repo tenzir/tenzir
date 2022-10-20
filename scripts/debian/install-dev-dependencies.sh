f6f26ad9cd120fc17e805f110d65da65f3508690
Patryk Sztyglic <patryk.sztyglic@tenzir.com>
2022-10-20 15:46:13 +0200

    Refactor CI

--- scripts/debian/install-dev-dependencies.sh
+++ scripts/debian/install-dev-dependencies.sh
@@ -5,13 +5,14 @@ apt-get update
 apt-get -y --no-install-recommends install \
     build-essential \
     ca-certificates \
+    ccache \
     cmake/bullseye-backports \
     cmake-data/bullseye-backports \
     flatbuffers-compiler-dev \
     g++-10 \
     gcc-10 \
     git-core \
-    gnupg2 \
+    gnupg2 gnupg-agent\
     jq \
     libasio-dev \
     libcaf-dev \
@@ -28,14 +29,16 @@ apt-get -y --no-install-recommends install \
     libxxhash-dev \
     lsb-release \
     ninja-build \
+    pandoc \
     pkg-config \
     python3-dev \
     python3-pip \
     python3-venv \
     robin-map-dev \
+    software-properties-common \
     wget
 wget "https://apache.jfrog.io/artifactory/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb"
-apt-get -y --no-install-recommends install ./apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb 
+apt-get -y --no-install-recommends install ./apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb
 apt-get update
-apt-get -y --no-install-recommends install libarrow-dev=9.0.0-1 libprotobuf-dev libparquet-dev=9.0.0-1
+apt-get -y --no-install-recommends install libarrow-dev=10.0.0-1 libprotobuf-dev libparquet-dev=10.0.0-1
 rm ./apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb
