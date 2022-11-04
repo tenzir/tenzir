c08da776a6b3c7097ef935fadf01be7202ead0e3
Daniel Kostuj <daniel.kostuj@tenzir.com>
2022-11-04 12:35:58 +0100

    Use structured bindings for num_partitions_and_events assignment

--- libvast/src/system/catalog.cpp
+++ libvast/src/system/catalog.cpp
@@ -522,8 +522,7 @@ catalog(catalog_actor::stateful_pointer<catalog_state> self,
       r.data.reserve(num_partitions_and_events_per_schema_and_version.size());
       for (const auto& [schema_and_version, num_partitions_and_events] :
            num_partitions_and_events_per_schema_and_version) {
-        auto schema_num_partitions = num_partitions_and_events.first;
-        auto schema_num_events = num_partitions_and_events.second;
+        auto [schema_num_partitions, schema_num_events] = num_partitions_and_events;
         total_num_partitions += schema_num_partitions;
         total_num_events += schema_num_events;
         r.data.push_back(data_point{
