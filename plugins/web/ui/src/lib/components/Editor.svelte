3df4e0622c3a6db05523add3fd1926494e795cab
Mostly Void <7rat13@gmail.com>
2022-11-16 15:23:35 +0530

    Add buttons for adding blocks

--- plugins/web/ui/src/lib/components/Editor.svelte
+++ plugins/web/ui/src/lib/components/Editor.svelte
@@ -1,17 +1,17 @@
 <script>
-import 'bytemd/dist/index.css'
-  import { Editor, Viewer } from 'bytemd'
-  import gfm from '@bytemd/plugin-gfm'
+  import 'bytemd/dist/index.css';
+  import { Editor, Viewer } from 'bytemd';
+  import gfm from '@bytemd/plugin-gfm';
 
-  let value
-  export let placeholder = "Write here...";
+  let value;
+  export let placeholder = 'Write here...';
   const plugins = [
-    gfm(),
+    gfm()
     // Add more plugins here
-  ]
+  ];
 
   function handleChange(e) {
-    value = e.detail.value
+    value = e.detail.value;
   }
 </script>
 
--- plugins/web/ui/src/routes/report/+page.svelte
+++ plugins/web/ui/src/routes/report/+page.svelte
@@ -1,6 +1,7 @@
-<script>
+<script lang="ts">
   import Editor from '$lib/components/Editor.svelte';
   import MultiLine from '$lib/components/LayerCake/MultiLine/index.svelte';
+  let visibleBlocks: ('bytes' | 'text')[] = [];
 </script>
 
 <svelte:head>
@@ -11,13 +12,47 @@
 <div class="p-2 m-2 text-sm text-left text-gray-600">
   <div class="flex justify-between my-4 mr-4">
     <div class="text-xl font-bold">Reports</div>
+
+  <button
+    class="flex items-center mr-2"
+  >
+    Export
+  </button>
   </div>
-  <div class="text-l font-bold">Inbound vs Outbound Bytes</div>
-  <div class="m-10">
-    <MultiLine />
-  </div>
-  <div class="text-l font-bold">Notes</div>
-  <div class="my-10">
-    <Editor/>
+  {#each visibleBlocks as block}
+    {#if block == 'bytes'}
+      <div class="text-l font-bold">Inbound vs Outbound Bytes</div>
+      <div class="m-10">
+        <MultiLine />
+      </div>
+    {:else}
+      <div class="text-l font-bold">Notes</div>
+      <div class="my-10">
+        <Editor />
+      </div>
+    {/if}
+  {/each}
+  <div class="flex">
+
+
+  <button
+    class="flex items-center mr-2"
+    on:click={() => {
+      visibleBlocks = [...visibleBlocks, "tex"]
+    }}
+  >
+    <div class="i-ant-design:plus-square-outlined w-16px" />
+    Add Text Block
+  </button>
+
+  <button
+    class="flex items-center mr-2"
+    on:click={() => {
+      visibleBlocks = [...visibleBlocks, "bytes"]
+    }}
+  >
+    <div class="i-ant-design:plus-square-outlined w-16px" />
+    Add Bytes Block
+  </button>
   </div>
 </div>
