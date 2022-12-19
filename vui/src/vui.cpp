f6454973ef2067591a7744185348ed780b636ba4
Matthias Vallentin <matthias@vallentin.net>
2022-12-19 07:57:30 +0100

    Add CMake scaffold

---
+++ vui/src/vui.cpp
@@ -0,0 +1,11 @@
+//    _   _____   __________
+//   | | / / _ | / __/_  __/     Visibility
+//   | |/ / __ |_\ \  / /          Across
+//   |___/_/ |_/___/ /_/       Space and Time
+//
+// SPDX-FileCopyrightText: (c) 2022 The VAST Contributors
+// SPDX-License-Identifier: BSD-3-Clause
+
+int main(int argc, char** argv) {
+  return 0;
+}
