e2dcb3334411c534b3c05823e61e4078434086a3
Remi Dettai <rdettai@gmail.com>
2022-12-20 11:45:03 +0000

    Fix python extension types

--- python/vast/utils/arrow.py
+++ python/vast/utils/arrow.py
@@ -1,13 +1,18 @@
 import json
 import ipaddress as ip
-from typing import SupportsBytes
+from typing import Iterable, Optional, Sequence, SupportsBytes, SupportsIndex
 
 import pyarrow as pa
 
+# VAST's flatbuffer type representation uses a 32-bit unsigned integer. We
+# use an 8-bit type here only for backwards compatibility to the legacy
+# type. Eventually this will be a 32-bit type as well.
+ENUM_DICTIONARY_INDEX_TYPE = pa.uint8()
+
 
 class PatternScalar(pa.ExtensionScalar):
-    def as_py(self) -> str:
-        return self.value.as_py()
+    def as_py(self) -> str | None:
+        return None if self.value is None else self.value.as_py()
 
 
 class PatternType(pa.ExtensionType):
@@ -36,8 +41,8 @@ class PatternType(pa.ExtensionType):
 
 
 class AddressScalar(pa.ExtensionScalar):
-    def as_py(self) -> ip.IPv4Address | ip.IPv6Address:
-        return unpack_ip(self.value.as_py())
+    def as_py(self) -> ip.IPv4Address | ip.IPv6Address | None:
+        return None if self.value is None else unpack_ip(self.value.as_py())
 
 
 class AddressType(pa.ExtensionType):
@@ -66,9 +71,11 @@ class AddressType(pa.ExtensionType):
 
 
 class SubnetScalar(pa.ExtensionScalar):
-    def as_py(self) -> ip.IPv4Network | ip.IPv6Network:
+    def as_py(self) -> ip.IPv4Network | ip.IPv6Network | None:
         address = self.value[0].as_py()
         length = self.value[1].as_py()
+        if address is None or length is None:
+            return None
         return ip.ip_network((address, length), strict=False)
 
 
@@ -98,36 +105,29 @@ class SubnetType(pa.ExtensionType):
 
 
 class EnumScalar(pa.ExtensionScalar):
-    def as_py(self) -> str:
-        return self.value.as_py()
+    def as_py(self) -> str | None:
+        return None if self.value is None else self.value.as_py()
 
 
 class EnumType(pa.ExtensionType):
+    """An extension class for enum data types.
+
+    `EnumType` stores as name-to-value mapping called `fields` that maps to the
+    underlying integer type of represented the enum."""
+
     ext_name = "vast.enumeration"
-    # VAST's flatbuffer type representation uses a 32-bit unsigned integer. We
-    # use an 8-bit type here only for backwards compatibility to the legacy
-    # type. Eventually this will be a 32-bit type as well.
-    ext_type = pa.dictionary(pa.uint8(), pa.string())
+    ext_type = pa.dictionary(ENUM_DICTIONARY_INDEX_TYPE, pa.string())
 
     def __init__(self, fields: dict[str, int]):
-        # We're optimizing for use cases that involve reading the integer
-        # representation of enums from VAST, so we keep the key-name mappings in
-        # the inverse order in memory.
-        self._fields = {v: k for k, v in fields.items()}
+        self._fields = fields
         pa.ExtensionType.__init__(self, self.ext_type, self.ext_name)
 
     @property
-    def fields(self):
+    def fields(self) -> dict[str, int]:
         return self._fields
 
-    def field(self, key):
-        return self._fields[key]
-
     def __arrow_ext_serialize__(self) -> bytes:
-        inverse = {v: k for k, v in self._fields.items()}
-        return json.dumps(inverse).encode()
-        # We're optimizing for reading from VAST, so we keep the key-name
-        # mappings in the inverse order.
+        return json.dumps(self._fields).encode()
 
     @classmethod
     def __arrow_ext_deserialize__(self, storage_type, serialized: bytes):
@@ -166,13 +166,81 @@ def pack_ip(address: str | ip.IPv4Address | ip.IPv6Address) -> bytes:
 
 # Accepts a 128-bit buffer holding an IPv6 address and returns an IPv4 or IPv6
 # address.
-def unpack_ip(buffer: SupportsBytes) -> ip.IPv4Address | ip.IPv6Address:
+def unpack_ip(
+    buffer: Iterable[SupportsIndex] | SupportsBytes,
+) -> ip.IPv4Address | ip.IPv6Address:
     num = int.from_bytes(buffer, byteorder="big")
     # Convert IPv4 mapped addresses back to regular IPv4.
     # https://tools.ietf.org/html/rfc4291#section-2.5.5.2
     if (num >> 32) == 65535:
         num = num - (65535 << 32)
     return ip.ip_address(num)
+
+
+def pack_subnet(
+    subnet: str | ip.IPv4Network | ip.IPv6Network | None,
+) -> tuple[bytes | None, int | None]:
+    if subnet is None:
+        return (None, None)
+    match subnet:
+        case str():
+            return pack_subnet(ip.ip_network(subnet))
+        case ip.IPv4Network():
+            prefix = b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xff\xff"
+            return (prefix + subnet.network_address.packed, subnet.prefixlen)
+        case ip.IPv6Network():
+            return (subnet.network_address.packed, subnet.prefixlen)
+
+
+VastExtensionType = PatternType | AddressType | EnumType | SubnetType
+
+
+def py_dict_to_arrow_dict(dict: dict[str, int]) -> pa.StringArray:
+    """Build an Arrow dictionary array from a Python dictionary
+
+    - Values should be greater or equal to 0.
+    - Non-consecutive values will interpolated with null."""
+    dictionary_py: list[Optional[str]] = [None] * (max(dict.values()) + 1)
+    for (value, idx) in dict.items():
+        assert idx >= 0, f"Dictionary indices should be >=0, got {idx}"
+        dictionary_py[idx] = value
+    return pa.array(dictionary_py, pa.string())
+
+
+def extension_array(obj: Sequence, type: pa.DataType) -> pa.Array:
+    """Create a `pyarrow.Array` from a Python object, using an
+    `pyarrow.ExtensionArray` if the type is an extension type."""
+    match type:
+        case AddressType():
+            arr = [pack_ip(e) for e in obj]
+            storage = pa.array(arr, pa.binary(16))
+            return pa.ExtensionArray.from_storage(type, storage)
+        case SubnetType():
+            arrs = list(zip(*[pack_subnet(e) for e in obj]))
+            addr_storage = pa.array(arrs[0], pa.binary(16))
+            addr_array = pa.ExtensionArray.from_storage(AddressType(), addr_storage)
+            prefix_array = pa.array(arrs[1], pa.uint8())
+            storage = pa.StructArray.from_arrays(
+                [
+                    addr_array,
+                    prefix_array,
+                ],
+                names=["address", "length"],
+            )
+            return pa.ExtensionArray.from_storage(type, storage)
+        case PatternType():
+            storage = pa.array(obj, pa.string())
+            return pa.ExtensionArray.from_storage(type, storage)
+        case EnumType(fields=fields):
+            # use the mappings in the `fields` metadata as indices for building
+            # the dictionary
+            indices_py = [None if e is None else fields[e] for e in obj]
+            indices = pa.array(indices_py, ENUM_DICTIONARY_INDEX_TYPE)
+            dictionary = py_dict_to_arrow_dict(fields)
+            storage = pa.DictionaryArray.from_arrays(indices, dictionary)
+            return pa.ExtensionArray.from_storage(type, storage)
+        case _:
+            return pa.array(obj, type)
 
 
 # Modules are intialized exactly once, so we can perform the registration here.
