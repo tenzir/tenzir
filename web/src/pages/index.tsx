f01789ce3b0bc75bc1efc3673547aa16a01f9a14
Mostly Void <7rat13@gmail.com>
2022-12-21 10:18:57 +0530

    Add Carousel component to docs site

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -9,36 +9,102 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import VASTLight from '@site/static/img/vast-white.svg';
 import VASTDark from '@site/static/img/vast-black.svg';
+import Carousel from '../components/Carousel';
+
+// There is no straightforward way to generate these links without
+// overriding the blog plugin, so we hardcode them for now.
+const latestNonReleaseBlogPost = {
+  title: 'The New REST API',
+  link: '/blog/the-new-rest-api',
+};
+
+const latestReleaseBlogPost = {
+  title: 'VAST v2.4.1 Realeased',
+  link: '/blog/vast-v2.4.1',
+};
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   const {colorMode} = useColorMode();
   return (
-    <header className={clsx('hero shadow--lw', styles.heroBanner)}>
+    <>
+      <header className="shadow--lw">
+        <Carousel>
+          <div className={clsx('hero', styles.heroBanner)}>
+            <div className="container">
+              {colorMode === 'dark' ? (
+                <VASTLight className={styles.vastLogo} title="VAST Logo" />
+              ) : (
+                <VASTDark className={styles.vastLogo} title="VAST Logo" />
+              )}
+              <p className="hero__subtitle">{siteConfig.tagline}</p>
+              <div className={styles.buttons}>
+                <Link
+                  className="button button--secondary button--lg"
+                  to="/docs/about"
+                >
+                  Get Started
+                </Link>
+              </div>
+            </div>
+          </div>
+
+          <div className={clsx('hero', styles.leftAligned)}>
+            <div className="container">
+              <p className="hero__title">Sign up for our Newsletter</p>
+              <p className="hero__subtitle">
+                Stay in touch with what's new with VAST
+              </p>
+              <div>
+                <Link
+                  className="button button--secondary button--lg"
+                  to="https://webforms.pipedrive.com/f/clRn2zcF1N5NGHAJ4Rzd3mVU6Xr55uL2Dm3z62Np2KUlq6vxaslf6xQ5Te3P1O1A6T"
+                >
+                  Subscribe
+                </Link>
+              </div>
+            </div>
+          </div>
+          <BlogPostSlide
+            title={latestNonReleaseBlogPost.title}
+            link={latestNonReleaseBlogPost.link}
+          />
+          <BlogPostSlide
+            title={latestReleaseBlogPost.title}
+            link={latestReleaseBlogPost.link}
+            isRelease={true}
+          />
+        </Carousel>
+      </header>
+    </>
+  );
+}
+
+const BlogPostSlide = ({title, link, isRelease = false}) => {
+  return (
+    <div className={clsx('hero', styles.leftAligned)}>
       <div className="container">
-        {colorMode === "dark"
-          ? <VASTLight className={styles.vastLogo} title="VAST Logo" />
-          : <VASTDark className={styles.vastLogo} title="VAST Logo" />
-        }
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/about">
-            Get Started
+        <p className="hero__title">
+          {isRelease ? 'Latest Release' : 'Latest from our blog'}
+        </p>
+        <p className="hero__subtitle">{title}</p>
+        <div>
+          <Link className="button button--secondary button--lg" to={link}>
+            {isRelease ? 'Read Announcement' : 'Read Post'}
           </Link>
         </div>
       </div>
-    </header>
+    </div>
   );
-}
+};
 
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
       title={`Visibility Across Space and Time`}
-      description="The network telemetry engine for data-driven security investigations.">
+      description="The network telemetry engine for data-driven security investigations."
+    >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
