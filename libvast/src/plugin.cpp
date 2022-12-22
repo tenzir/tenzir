88d372faaba30572b5e95f5ac4667fad716c68fd
Dominik Lohmann <mail@dominiklohmann.de>
2022-12-22 22:30:58 +0100

    Fix log message for invalid plugin dirs

--- libvast/src/plugin.cpp
+++ libvast/src/plugin.cpp
@@ -53,7 +53,7 @@ get_plugin_dirs(const caf::actor_system_config& cfg) {
         cfg, "vast.plugin-dirs"))
     result.insert(dirs->begin(), dirs->end());
   else
-    VAST_WARN("Unable to extract vast plugin dirs. {}", dirs.error());
+    VAST_WARN("failed to to extract vast plugin dirs: {}", dirs.error());
   if (!bare_mode)
     if (auto home = detail::getenv("HOME"))
       result.insert(std::filesystem::path{*home} / ".local" / "lib" / "vast"
