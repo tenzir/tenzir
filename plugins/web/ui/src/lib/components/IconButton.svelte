c67390eb82254c8688121eb101de0ee5cd223cf8
Mostly Void <7rat13@gmail.com>
2022-12-22 18:49:29 +0530

    Fix IconButton component

--- plugins/web/ui/src/lib/components/IconButton.svelte
+++ plugins/web/ui/src/lib/components/IconButton.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
   export let onClick: () => void;
-  export let iconName: string; // any icon name from iconify
+  export let iconifyString: string; // in the form of i-<collection>-<icon>
+
   export let classNames =
-    'justify-center px-3 py-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border-0 border-gray-300 rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800';
+    'justify-center p-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border-0 border-gray-300 rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800';
 </script>
 
 <div>
   <button on:click={onClick} class={classNames}>
-    <div class={`i-${iconName}`} />
+    <div class={iconifyString} />
   </button>
 </div>
--- plugins/web/ui/src/routes/report/BlockHeader.svelte
+++ plugins/web/ui/src/routes/report/BlockHeader.svelte
@@ -3,22 +3,16 @@
   import InlineInput from '$lib/components/InlineInput.svelte';
 
   export let title: string;
-  export let onClick: () => void;
-  // export let onDelete: () => void;
+  export let onEdit: () => void;
+  export let onDelete: () => void;
 </script>
 
 <div>
   <div class="flex justify-between">
     <InlineInput placeholder="Untitled Section" bind:value={title} />
     <div class="flex">
-      <IconButton {onClick} iconName="material-symbols:edit-outline-rounded" />
-
-      <IconButton
-        onClick={() => {
-          console.log('Delete clicked');
-        }}
-        iconName="ic:baseline-delete-forever"
-      />
+      <IconButton onClick={onEdit} iconifyString="i-material-symbols:edit-outline-rounded" />
+      <IconButton onClick={onDelete} iconifyString="i-ic:baseline-delete-forever" />
     </div>
   </div>
 </div>
