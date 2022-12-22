94c55d91b367b0967ecde001680ba47ac03ad557
Mostly Void <7rat13@gmail.com>
2022-12-22 11:32:06 +0530

    Clean up a little more

--- plugins/web/ui/src/lib/util/api.ts
+++ plugins/web/ui/src/lib/util/api.ts
@@ -2,7 +2,6 @@ export const getQuery = async (expression: string, limit = 100) => {
   try {
     const API_BASE = import.meta.env.VITE_VAST_API_ENDPOINT ?? `${import.meta.env.BASE_URL}api/v0`;
 
-    // const url = `${API_BASE}/export?expression=${encodeURIComponent('#type == "zeek.conn" && id.orig_h in 192.168.0.0/16')}`;
     const url = `${API_BASE}/export?expression=${encodeURIComponent(
       `${expression}`
     )}&limit=${limit}`;
