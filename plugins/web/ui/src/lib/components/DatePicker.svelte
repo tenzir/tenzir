3fd53bdc255b6ea4e7dc2b44fb78b5a19fe20b21
Mostly Void <7rat13@gmail.com>
2022-12-22 11:28:38 +0530

    Remove LayerCake and charts related stuff

--- plugins/web/ui/src/lib/components/DatePicker.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  import { DateInput } from 'date-picker-svelte';
-  export let date: null | Date = null; // the placeholder only works when it is null
-
-  export let placeholder = 'yyyy-MM-dd';
-</script>
-
-<DateInput bind:value={date} format="yyyy-MM-dd" {placeholder} />
