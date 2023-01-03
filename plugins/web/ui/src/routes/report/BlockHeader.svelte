3f420064f157d02b746a809b3a497f0906c704d6
Mostly Void <7rat13@gmail.com>
2023-01-03 18:54:22 +0530

    Make section title optional

--- plugins/web/ui/src/routes/report/BlockHeader.svelte
+++ plugins/web/ui/src/routes/report/BlockHeader.svelte
@@ -5,12 +5,28 @@
   export let title: string;
   export let onEdit: () => void;
   export let onDelete: () => void;
+
+  let hasSectionTitle = false;
+
+  const toglleSectionTitle = () => {
+    hasSectionTitle = !hasSectionTitle;
+    title = ''; // reset title
+  };
 </script>
 
 <div>
   <div class="flex justify-between">
-    <InlineInput placeholder="Untitled Section" bind:value={title} />
+    {#if hasSectionTitle}
+      <InlineInput placeholder="Untitled section" bind:value={title}
+        labelClasses="text-l hover:bg-slate-200 px-2 py-1 rounded"
+
+      />
+    {:else}
+      <!-- Empty element so that justify-between works as expected -->
+      <div />
+    {/if}
     <div class="flex">
+      <IconButton onClick={toglleSectionTitle} iconifyString="i-material-symbols:title" />
       <IconButton onClick={onEdit} iconifyString="i-material-symbols:edit-outline-rounded" />
       <IconButton onClick={onDelete} iconifyString="i-ic:baseline-delete-forever" />
     </div>
