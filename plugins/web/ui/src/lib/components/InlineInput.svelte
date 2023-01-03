a6a8530a9429dbbcb2669d8d1c70b71d2944b7dc
Mostly Void <7rat13@gmail.com>
2023-01-03 18:31:06 +0530

    Cleanup and refactor InlineInput

--- plugins/web/ui/src/lib/components/InlineInput.svelte
+++ plugins/web/ui/src/lib/components/InlineInput.svelte
@@ -2,8 +2,10 @@
   import InlineInput from 'svelte-inline-input';
   export let placeholder: string;
   export let value: string;
+  export let inputClasses = '';
+  export let labelClasses = '';
 </script>
 
 <div>
-  <InlineInput bind:value bind:placeholder />
+  <InlineInput bind:value bind:placeholder {inputClasses} {labelClasses} />
 </div>
