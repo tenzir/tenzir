6b277e1ecaeaef451b53bf1b66e656b46cbf237e
Remi Dettai <rdettai@gmail.com>
2023-01-04 16:49:04 +0000

    Remove a few unrelated changes

--- python/tests/test_vast.py
+++ python/tests/test_vast.py
@@ -1,12 +1,7 @@
 from vast import VAST
 
-import vast.utils.logging
-
-logger = vast.utils.logging.get(f"vast.{__name__}")
-
 
 def test_vast():
     v = VAST()
-    logger.debug("hello")
     # TODO: make sure we have a vast binary nearby
     assert True
