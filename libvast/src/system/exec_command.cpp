e1ac318373c7ff28e3e753dc34ba22098ae8e3ca
Daniel Kostuj <daniel.kostuj@tenzir.com>
2023-01-09 16:55:50 +0100

    Implement a basic exec_command() function

--- libvast/src/system/exec_command.cpp
+++ libvast/src/system/exec_command.cpp
@@ -7,7 +7,9 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
 #include "vast/system/exec_command.hpp"
+
 #include "vast/command.hpp"
+#include "vast/system/make_pipelines.hpp"
 
 #include <caf/actor.hpp>
 #include <caf/event_based_actor.hpp>
@@ -20,13 +22,20 @@ using namespace caf;
 namespace vast::system {
 
 caf::message exec_command(const invocation& inv, caf::actor_system& sys) {
-  auto split_args = detail::split(inv.arguments.front(), "|");
-  auto trimmed_args = std::vector<std::string>{};
-  for (auto arg : split_args) {
-    trimmed_args.emplace_back(detail::trim(arg));
+  auto pipeline = make_pipeline(inv.arguments.front());
+  std::vector<table_slice> slices;
+  for (const auto& pipeline_op : pipeline.cvalue()) {
+    auto transformed = pipeline_op->finish();
+    if (!transformed) {
+      return caf::make_message(transformed.error());
+    }
+    std::vector<table_slice> result_slices;
+    for (const auto& batch : *transformed) {
+      result_slices.emplace_back(batch.batch);
+    }
+    slices = std::move(result_slices);
   }
-
-  return {};
+  return caf::make_message("ok");
 }
 
 } // namespace vast::system
