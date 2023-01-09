a46320c614fe28f5f43a8fa30fb6318639664db1
Remi Dettai <rdettai@gmail.com>
2023-01-09 17:19:27 +0000

    Document asyncio utilities

--- python/vast/utils/asyncio.py
+++ python/vast/utils/asyncio.py
@@ -1,3 +1,22 @@
+"""This module provides asyncio utilities.
+
+These asyncio utilities are based on Lynn Root's blog post series on asyncio at
+https://www.roguelynn.com/words/asyncio-we-did-it-wrong/. In particular, they
+help with gracefully shutting down and proper exception handling to terminate an
+application, e.g., after catching SIGINT and SIGTERM.
+
+Example:
+    The function ``run_forever`` is the primary entry point that sets up the
+    asyncio event loop and registers exception and shutdown handlers. It takes
+    the "main function" task as argument:
+
+        async def snooze():
+            for i in range(42):
+                await asyncio.sleep(1)
+
+        run_forever(snooze())
+"""
+
 import asyncio
 from signal import SIGINT, SIGTERM, SIGHUP
 
