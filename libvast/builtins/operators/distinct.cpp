4cccdb23b6aa55770ad06bcd869988adbc3122b5
Dominik Lohmann <mail@dominiklohmann.de>
2023-04-12 16:24:09 +0200

    Remove special-handling of list values in `distinct`
    The `distinct` function silently performed a different operation on
    lists, returning the distinct non-null elements in the list rather than
    operating on the list itself. This feature will return in the future as
    unnesting on the extractor level via `distinct(field[])`, but for now it
    has to go to make the distinct aggregation function work consistently.

--- libvast/builtins/operators/distinct.cpp
+++ libvast/builtins/operators/distinct.cpp
@@ -21,11 +21,14 @@ template <concrete_type Type>
 struct heterogeneous_data_hash {
   using is_transparent = void;
 
-  [[nodiscard]] size_t operator()(view<type_to_data_t<Type>> value) const {
+  [[nodiscard]] auto operator()(view<type_to_data_t<Type>> value) const
+    -> size_t {
     return hash(value);
   }
 
-  [[nodiscard]] size_t operator()(const type_to_data_t<Type>& value) const
+  [[nodiscard]] auto
+
+  operator()(const type_to_data_t<Type>& value) const -> size_t
     requires(!std::is_same_v<view<type_to_data_t<Type>>, type_to_data_t<Type>>)
   {
     return hash(make_view(value));
@@ -36,20 +39,20 @@ template <concrete_type Type>
 struct heterogeneous_data_equal {
   using is_transparent = void;
 
-  [[nodiscard]] bool operator()(const type_to_data_t<Type>& lhs,
-                                const type_to_data_t<Type>& rhs) const {
+  [[nodiscard]] auto operator()(const type_to_data_t<Type>& lhs,
+                                const type_to_data_t<Type>& rhs) const -> bool {
     return lhs == rhs;
   }
 
-  [[nodiscard]] bool operator()(const type_to_data_t<Type>& lhs,
-                                view<type_to_data_t<Type>> rhs) const
+  [[nodiscard]] auto operator()(const type_to_data_t<Type>& lhs,
+                                view<type_to_data_t<Type>> rhs) const -> bool
     requires(!std::is_same_v<view<type_to_data_t<Type>>, type_to_data_t<Type>>)
   {
     return make_view(lhs) == rhs;
   }
 };
 
-template <concrete_type Type, bool IsList>
+template <concrete_type Type>
 class distinct_function final : public aggregation_function {
 public:
   explicit distinct_function(type input_type) noexcept
@@ -58,35 +61,22 @@ public:
   }
 
 private:
-  [[nodiscard]] type output_type() const override {
-    if constexpr (IsList)
-      return input_type();
-    else
-      return type{list_type{input_type()}};
+  [[nodiscard]] auto output_type() const -> type override {
+    return type{list_type{input_type()}};
   }
 
   void add(const data_view& view) override {
     using view_type = vast::view<type_to_data_t<Type>>;
-    const auto handle_value_view = [&](const data_view& view) {
-      if (caf::holds_alternative<caf::none_t>(view))
-        return;
-      const auto& typed_view = caf::get<view_type>(view);
-      if (!distinct_.contains(typed_view)) {
-        const auto [it, inserted] = distinct_.insert(materialize(typed_view));
-        VAST_ASSERT(inserted);
-      }
-    };
-    if constexpr (IsList) {
-      if (caf::holds_alternative<caf::none_t>(view))
-        return;
-      for (const auto& value_view : caf::get<vast::view<list>>(view))
-        handle_value_view(value_view);
-    } else {
-      handle_value_view(view);
+    if (caf::holds_alternative<caf::none_t>(view))
+      return;
+    const auto& typed_view = caf::get<view_type>(view);
+    if (!distinct_.contains(typed_view)) {
+      const auto [it, inserted] = distinct_.insert(materialize(typed_view));
+      VAST_ASSERT(inserted);
     }
   }
 
-  [[nodiscard]] caf::expected<data> finish() && override {
+  [[nodiscard]] auto finish() && -> caf::expected<data> override {
     auto result = list{};
     result.reserve(distinct_.size());
     for (auto& value : distinct_)
@@ -101,25 +91,23 @@ private:
 };
 
 class plugin : public virtual aggregation_function_plugin {
-  caf::error initialize([[maybe_unused]] const record& plugin_config,
-                        [[maybe_unused]] const record& global_config) override {
+  auto initialize([[maybe_unused]] const record& plugin_config,
+                  [[maybe_unused]] const record& global_config)
+    -> caf::error override {
     return {};
   }
 
-  [[nodiscard]] std::string name() const override {
+  [[nodiscard]] auto name() const -> std::string override {
     return "distinct";
   };
 
-  [[nodiscard]] caf::expected<std::unique_ptr<aggregation_function>>
-  make_aggregation_function(const type& input_type) const override {
-    const auto* list = caf::get_if<list_type>(&input_type);
+  [[nodiscard]] auto make_aggregation_function(const type& input_type) const
+    -> caf::expected<std::unique_ptr<aggregation_function>> override {
     auto f = [&]<concrete_type Type>(
                const Type&) -> std::unique_ptr<aggregation_function> {
-      if (list)
-        return std::make_unique<distinct_function<Type, true>>(input_type);
-      return std::make_unique<distinct_function<Type, false>>(input_type);
+      return std::make_unique<distinct_function<Type>>(input_type);
     };
-    return caf::visit(f, list ? list->value_type() : input_type);
+    return caf::visit(f, input_type);
   }
 };
 
