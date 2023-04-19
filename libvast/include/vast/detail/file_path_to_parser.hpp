1fd45b6d3bd75bdfe408ddb30030e7764a89b23c
Daniel Kostuj <daniel.kostuj@tenzir.com>
2023-04-19 11:37:50 +0200

    Implement file_path_to_parser()

---
+++ libvast/include/vast/detail/file_path_to_parser.hpp
@@ -0,0 +1,22 @@
+//    _   _____   __________
+//   | | / / _ | / __/_  __/     Visibility
+//   | |/ / __ |_\ \  / /          Across
+//   |___/_/ |_/___/ /_/       Space and Time
+//
+// SPDX-FileCopyrightText: (c) 2023 The VAST Contributors
+// SPDX-License-Identifier: BSD-3-Clause
+
+#pragma once
+
+#include <string_view>
+
+namespace vast::detail {
+
+/// Function to return a parser for a file path to be used as loader operator
+/// input in a pipeline. Currently only strips away the extension and returns
+/// that stripped extension as a parser.
+/// @param path The path to determine the parser for.
+/// @returns A string determining the default parser for a file.
+auto file_path_to_parser(std::string_view path) -> std::string;
+
+} // namespace vast::detail
---
+++ libvast/src/detail/file_path_to_parser.cpp
@@ -0,0 +1,26 @@
+//    _   _____   __________
+//   | | / / _ | / __/_  __/     Visibility
+//   | |/ / __ |_\ \  / /          Across
+//   |___/_/ |_/___/ /_/       Space and Time
+//
+// SPDX-FileCopyrightText: (c) 2023 The VAST Contributors
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include "vast/detail/file_path_to_parser.hpp"
+
+#include <filesystem>
+#include <string>
+
+namespace vast::detail {
+
+constexpr auto fallback_parser = "json";
+
+auto file_path_to_parser(std::string_view path) -> std::string {
+  auto extension = std::filesystem::path(path).extension();
+  if (extension.empty()) {
+    return fallback_parser;
+  }
+  return extension.string().substr(1);
+}
+
+} // namespace vast::detail
