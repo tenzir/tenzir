39cfdd439c558a63687135726aa2465fe27f58d3
Dominik Lohmann <mail@dominiklohmann.de>
2023-06-01 15:58:31 +0200

    Fix comments in `serve` integration test

--- plugins/web/integration/scripts/serve.sh
+++ plugins/web/integration/scripts/serve.sh
@@ -10,13 +10,13 @@ sleep 3
 first_result="$(curl -XPOST -H "Content-Type: application/json" -d '{"serve_id": "version", "timeout": "5s", "max_events": 1}' http://127.0.0.1:42025/api/v0/serve)"
 continuation_token="$(jq -rnec "${first_result} | .next_continuation_token")"
 
-# The first set should be 10 results exactly.
+# The first set should be 1 results exactly.
 jq -nec "${first_result} | .events | length == 1"
 
 # Query the second set.
 second_result="$(curl -XPOST -H "Content-Type: application/json" -d "{\"serve_id\": \"version\", \"continuation_token\": \"${continuation_token}\", \"timeout\": \"5s\", \"max_events\": 4}" http://127.0.0.1:42025/api/v0/serve)"
 
-# The next set should be the reamining 90 results.
+# The next set should be the reamining 4 results.
 jq -nec "${second_result} | .next_continuation_token == null"
 jq -nec "${second_result} | .events | length == 4"
 
