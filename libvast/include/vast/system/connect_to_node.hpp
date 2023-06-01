b860d3fe95f8eedaee65bed71bf575b1e26c2d97
Dominik Lohmann <mail@dominiklohmann.de>
2023-06-01 15:50:44 +0200

    Fix reconnect attempts for remote pipelines
    To reproduce the bug, run `vast exec 'remote version'` before running
    `vast start`, and observe a broken promise error instead of a reconnect
    attempt after three seconds.

--- libvast/include/vast/system/connect_to_node.hpp
+++ libvast/include/vast/system/connect_to_node.hpp
@@ -47,14 +47,17 @@ void connect_to_node(caf::typed_event_based_actor<Sigs...>* self,
   if (!node_endpoint)
     return callback(std::move(node_endpoint.error()));
   auto timeout = node_connection_timeout(opts);
-  auto connector_actor = self->spawn(connector, details::get_retry_delay(opts),
-                                     details::get_deadline(timeout));
+  auto connector
+    = self->spawn(system::connector, details::get_retry_delay(opts),
+                  details::get_deadline(timeout));
   self
-    ->request(connector_actor, caf::infinite, atom::connect_v,
+    ->request(connector, caf::infinite, atom::connect_v,
               connect_request{node_endpoint->port->number(),
                               node_endpoint->host})
     .then(
       [=](node_actor& node) {
+        // We must keep the connector alive until after this request is completed.
+        (void)connector;
         self->request(node, timeout, atom::get_v, atom::version_v)
           .then(
             [callback, node = std::move(node)](record& remote_version) mutable {
