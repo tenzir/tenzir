12b737a7cc549fe090a697ef645bd0819fd9d7ad
tobim <tobim@fastmail.fm>
2023-06-09 10:35:48 +0200

    Adjust env variables and executable name

--- demo-node/entrypoint.bash
+++ demo-node/entrypoint.bash
@@ -12,6 +12,6 @@ export replay_zeek
 curl -L https://storage.googleapis.com/vast-datasets/M57/suricata.tar.zst | tar -x --zstd --to-command="vast import suricata" &
 
 curl -L https://storage.googleapis.com/vast-datasets/M57/zeek.tar.zst | tar -x --zstd
-find Zeek -type f -print0 | xargs --null -P 99 -I{} sh -c "export TENZIR_CONSOLE_FORMAT='{}: %^[%T.%e] %v%\$'; /demo-node/timeshift-zeek.bash {} | tenzirctl -e localhost import zeek"
+find Zeek -type f -print0 | xargs --null -P 99 -I{} sh -c "export VAST_CONSOLE_FORMAT='{}: %^[%T.%e] %v%\$'; /demo-node/timeshift-zeek.bash {} | tenzir-ctl -e localhost import zeek"
 
 wait "$NODE_PID"
