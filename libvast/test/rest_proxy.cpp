b1f140e374f781ab4bf2dd096f0e05ffbb505d30
Benno Evers <benno.evers@tenzir.com>
2023-06-19 15:56:53 +0200

    Add additional unit tests

--- libvast/test/rest_proxy.cpp
+++ libvast/test/rest_proxy.cpp
@@ -21,12 +21,37 @@ struct fixture : public fixtures::node {
 
 } // namespace
 
+TEST(parameter parsing) {
+  auto endpoint = vast::rest_endpoint {
+    .method = vast::http_method::post,
+    .path = "/dummy",
+    .params = vast::record_type{
+      {"id", vast::int64_type{}},
+      {"uid", vast::uint64_type{}},
+      {"timeout", vast::duration_type{}},
+      {"value", vast::string_type{}},
+    },
+    .version = vast::api_version::v0,
+    .content_type = vast::http_content_type::json,
+  };
+  auto params = vast::detail::stable_map<std::string, std::string>{
+    {"id", "0"},
+    {"uid", "0"},
+    {"timeout", "1m"},
+    {"value", "1"},
+  };
+  auto result = parse_endpoint_parameters(endpoint, params);
+  REQUIRE_NOERROR(result);
+}
+
 FIXTURE_SCOPE(rest_api_tests, fixture)
 
-TEST(proxy request) {
+TEST(proxy requests) {
+  // /status
   auto desc = vast::http_request_description{
     .canonical_path = "POST /status (v0)",
-    .params = {},
+    .params
+    = {{"verbosity", "detailed"}, {"components", R"_(["catalog", "index"])_"}},
   };
   auto rp = self->request(test_node, caf::infinite, vast::atom::proxy_v, desc);
   run();
@@ -37,11 +62,38 @@ TEST(proxy request) {
       auto parsed = vast::from_json(body);
       CHECK(parsed);
       CHECK(caf::holds_alternative<vast::record>(*parsed));
-      CHECK(caf::get<vast::record>(*parsed).contains("version"));
+      CHECK(caf::get<vast::record>(*parsed).contains("catalog"));
+      CHECK(caf::get<vast::record>(*parsed).contains("index"));
     },
     [](const caf::error& e) {
       FAIL(e);
     });
+
+  // TODO: Enable this test once the node test fixture includes a
+  // "serve-manager".
+  // auto desc2 = vast::http_request_description{
+  //   .canonical_path = "POST /serve (v0)",
+  //   .params = {
+  //     {"serve_id", "foo"},
+  //     {"max_events", "1"},
+  //   },
+  // };
+  // auto rp2 = self->request(test_node, caf::infinite, vast::atom::proxy_v,
+  //                          desc2);
+  // run();
+  // rp2.receive(
+  //   [](vast::rest_response& response) {
+  //     // We expect an "Unknown serve id" error.
+  //     CHECK_EQUAL(response.code(), size_t{400});
+  //     auto body = std::move(response).release();
+  //     auto parsed = vast::from_json(body);
+  //     CHECK(parsed);
+  //     CHECK(caf::holds_alternative<vast::record>(*parsed));
+  //     CHECK(caf::get<vast::record>(*parsed).contains("error"));
+  //   },
+  //   [](const caf::error& e) {
+  //     FAIL(e);
+  //   });
 }
 
 TEST(invalid request) {
@@ -59,22 +111,22 @@ TEST(invalid request) {
     [](const caf::error& e) {
       FAIL(e);
     });
-  // TODO: Enable when the endpoint does strict parameter validation.
-  // MESSAGE("invalid params");
-  // auto desc2 = vast::http_request_description{
-  //  .canonical_path = "POST /status (v0)",
-  //  .params = {{"asdf", "jkl"}},
-  //};
-  // auto rp2
-  //  = self->request(test_node, caf::infinite, vast::atom::proxy_v, desc2);
-  // run();
-  // rp2.receive(
-  //  [](vast::rest_response& response) {
-  //    CHECK(response.is_error());
-  //  },
-  //  [](const caf::error& e) {
-  //    FAIL(e);
-  //  });
+
+  MESSAGE("invalid params");
+  auto desc2 = vast::http_request_description{
+    .canonical_path = "POST /status (v0)",
+    .params = {{"verbosity", "jklo"}},
+  };
+  auto rp2
+    = self->request(test_node, caf::infinite, vast::atom::proxy_v, desc2);
+  run();
+  rp2.receive(
+    [](vast::rest_response& response) {
+      CHECK(response.is_error());
+    },
+    [](const caf::error& e) {
+      FAIL(e);
+    });
 }
 
 FIXTURE_SCOPE_END()
