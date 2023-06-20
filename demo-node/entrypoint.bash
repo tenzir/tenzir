63bdfeeebc7c6952b25655595049f87ad58de156
Benno Evers <benno.evers@tenzir.com>
2023-06-20 12:18:08 +0200

    Update google storage bucket names in demo node entrypoint

--- demo-node/entrypoint.bash
+++ demo-node/entrypoint.bash
@@ -9,9 +9,9 @@ replay_zeek() {
 }
 export replay_zeek
 
-curl -L https://storage.googleapis.com/vast-datasets/M57/suricata.tar.zst | tar -x --zstd --to-command="vast import suricata" &
+curl -L https://storage.googleapis.com/tenzir-datasets/M57/suricata.tar.zst | tar -x --zstd --to-command="vast import suricata" &
 
-curl -L https://storage.googleapis.com/vast-datasets/M57/zeek.tar.zst | tar -x --zstd
+curl -L https://storage.googleapis.com/tenzir-datasets/M57/zeek.tar.zst | tar -x --zstd
 find Zeek -type f -print0 | xargs --null -P 99 -I{} sh -c "export VAST_CONSOLE_FORMAT='{}: %^[%T.%e] %v%\$'; /demo-node/timeshift-zeek.bash {} | tenzir-ctl -e localhost import zeek"
 
 wait "$NODE_PID"
