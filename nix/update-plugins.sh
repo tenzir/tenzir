e26e54c3124a372f7c0522749027b21726985bdc
Dominik Lohmann <mail@dominiklohmann.de>
2023-06-20 10:05:19 +0200

    Rename proprietary plugins submodule

--- nix/update-plugins.sh
+++ nix/update-plugins.sh
@@ -10,14 +10,14 @@ get-submodule-rev() {
   git -C "${toplevel}" submodule status -- "${_submodule}" | cut -c2- | cut -d' ' -f 1
 }
 
-echo "Updating contrib/vast-plugins"
-vast_plugins_rev="$(get-submodule-rev "${toplevel}/contrib/vast-plugins")"
+echo "Updating contrib/tenzir-plugins"
+vast_plugins_rev="$(get-submodule-rev "${toplevel}/contrib/tenzir-plugins")"
 vast_plugins_json="$(jq --arg rev "${vast_plugins_rev#rev}" \
   '."rev" = $rev' "${dir}/vast/plugins/source.json")"
 
-if git -C "${toplevel}/contrib/vast-plugins" merge-base --is-ancestor \
-  "$(git -C "${toplevel}/contrib/vast-plugins" rev-parse HEAD)" \
-  "$(git -C "${toplevel}/contrib/vast-plugins" rev-parse origin/main)"; then
+if git -C "${toplevel}/contrib/tenzir-plugins" merge-base --is-ancestor \
+  "$(git -C "${toplevel}/contrib/tenzir-plugins" rev-parse HEAD)" \
+  "$(git -C "${toplevel}/contrib/tenzir-plugins" rev-parse origin/main)"; then
   # Remove 'allRefs = true' in case it was there before.
   vast_plugins_json="$(jq 'del(.allRefs)' <<< "$vast_plugins_json")"
 else
@@ -31,7 +31,7 @@ echo -E "${vast_plugins_json}" > "${dir}/vast/plugins/source.json"
 echo "Extracting plugin versions..."
 {
   printf "[\n"
-  for cm in "${toplevel}"/contrib/vast-plugins/*/CMakeLists.txt; do
+  for cm in "${toplevel}"/contrib/tenzir-plugins/*/CMakeLists.txt; do
     plugin="$(basename "$(dirname "$cm")")"
     echo "  \"$plugin\""
   done
--- nix/vast/plugins/default.nix
+++ nix/vast/plugins/default.nix
@@ -5,12 +5,12 @@
   ...
 }: let
   source = builtins.fromJSON (builtins.readFile ./source.json);
-  vast-plugins = builtins.fetchGit source;
+  tenzir-plugins = builtins.fetchGit source;
   versions = import ./names.nix;
   f = name:
     callPackage ./generic.nix {
       inherit name;
-      src = "${vast-plugins}/${name}";
+      src = "${tenzir-plugins}/${name}";
       inherit vast;
     };
 in
