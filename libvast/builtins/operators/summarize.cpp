12cc75a7d917a5c28397202a6e771a93a243df74
Dominik Lohmann <mail@dominiklohmann.de>
2023-06-22 17:20:21 +0200

    Fix duration rounding for values above 2^31 ms

--- libvast/builtins/operators/summarize.cpp
+++ libvast/builtins/operators/summarize.cpp
@@ -40,16 +40,58 @@ namespace {
 /// @param time_resolution The multiple to round to.
 auto make_round_temporal_options(duration time_resolution) noexcept
   -> arrow::compute::RoundTemporalOptions {
-  // Note that using any calendar unit lower than milliseconds causes Arrow
-  // Compute to just not round at all, which is really weird and not documented.
-  // You'd expect at least a NotImplemented status, but you actually just get
-  // the input back unchanged. Last tested with Arrow 8.0.0. -- DL
-  return arrow::compute::RoundTemporalOptions{
-    std::chrono::duration_cast<std::chrono::duration<int, std::milli>>(
-      time_resolution)
-      .count(),
-    arrow::compute::CalendarUnit::MILLISECOND,
-  };
+#define TRY_CAST_EXACTLY(chrono_unit, arrow_unit)                              \
+  do {                                                                         \
+    if (auto cast_resolution = std::chrono::duration_cast<                     \
+          std::chrono::duration<int, std::chrono::chrono_unit::period>>(       \
+          time_resolution);                                                    \
+        time_resolution                                                        \
+        == std::chrono::duration_cast<duration>(cast_resolution)) {            \
+      return arrow::compute::RoundTemporalOptions{                             \
+        cast_resolution.count(),                                               \
+        arrow::compute::CalendarUnit::arrow_unit,                              \
+      };                                                                       \
+    }                                                                          \
+  } while (false)
+  TRY_CAST_EXACTLY(years, YEAR);
+  TRY_CAST_EXACTLY(months, MONTH);
+  TRY_CAST_EXACTLY(weeks, WEEK);
+  TRY_CAST_EXACTLY(days, DAY);
+  TRY_CAST_EXACTLY(hours, HOUR);
+  TRY_CAST_EXACTLY(minutes, MINUTE);
+  TRY_CAST_EXACTLY(seconds, SECOND);
+  TRY_CAST_EXACTLY(milliseconds, MILLISECOND);
+  TRY_CAST_EXACTLY(microseconds, MICROSECOND);
+  TRY_CAST_EXACTLY(nanoseconds, NANOSECOND);
+#undef TRY_CAST_EXACTLY
+  // If neither of these casts are working, then we need nanosecond resolution
+  // but have a value so large that it cannot be represented by a signed 32-bit
+  // integer. In this case we accept the rounding error and take the closest
+  // unit we can without overflow.
+#define TRY_CAST_APPROXIMATELY(chrono_unit, arrow_unit)                        \
+  do {                                                                         \
+    if (auto cast_resolution = std::chrono::duration_cast<                     \
+          std::chrono::duration<uint64_t, std::chrono::chrono_unit::period>>(  \
+          time_resolution);                                                    \
+        cast_resolution.count() <= std::numeric_limits<int>::max()) {          \
+      return arrow::compute::RoundTemporalOptions{                             \
+        static_cast<int>(cast_resolution.count()),                             \
+        arrow::compute::CalendarUnit::arrow_unit,                              \
+      };                                                                       \
+    }                                                                          \
+  } while (false)
+  TRY_CAST_APPROXIMATELY(nanoseconds, NANOSECOND);
+  TRY_CAST_APPROXIMATELY(microseconds, MICROSECOND);
+  TRY_CAST_APPROXIMATELY(milliseconds, MILLISECOND);
+  TRY_CAST_APPROXIMATELY(seconds, SECOND);
+  TRY_CAST_APPROXIMATELY(minutes, MINUTE);
+  TRY_CAST_APPROXIMATELY(hours, HOUR);
+  TRY_CAST_APPROXIMATELY(days, DAY);
+  TRY_CAST_APPROXIMATELY(weeks, WEEK);
+  TRY_CAST_APPROXIMATELY(months, MONTH);
+  TRY_CAST_APPROXIMATELY(years, YEAR);
+#undef TRY_CAST_APPROXIMATELY
+  die("unreachable");
 }
 
 /// The configuration of a summarize pipeline operator.
