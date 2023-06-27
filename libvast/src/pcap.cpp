97fff5894eb192135a058daa2441b3dd1e3ed68e
Matthias Vallentin <matthias@vallentin.net>
2023-06-27 10:11:02 +0200

    Remove superfluous #pragma once

--- libvast/src/pcap.cpp
+++ libvast/src/pcap.cpp
@@ -6,8 +6,6 @@
 // SPDX-FileCopyrightText: (c) 2023 The VAST Contributors
 // SPDX-License-Identifier: BSD-3-Clause
 
-#pragma once
-
 #include "vast/pcap.hpp"
 
 #include "vast/detail/byteswap.hpp"
