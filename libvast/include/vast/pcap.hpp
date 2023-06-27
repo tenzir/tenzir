7318b5a0e363066389786c219c092390a5b16248
Matthias Vallentin <matthias@vallentin.net>
2023-06-27 05:48:28 +0200

    Add PCAP utilities

---
+++ libvast/include/vast/pcap.hpp
@@ -0,0 +1,76 @@
+//    _   _____   __________
+//   | | / / _ | / __/_  __/     Visibility
+//   | |/ / __ |_\ \  / /          Across
+//   |___/_/ |_/___/ /_/       Space and Time
+//
+// SPDX-FileCopyrightText: (c) 2023 The VAST Contributors
+// SPDX-License-Identifier: BSD-3-Clause
+
+#pragma once
+
+#include "vast/type.hpp"
+
+#include <cstdint>
+#include <span>
+
+/// PCAP utilities and data structures as defined in the IETF draft at
+/// https://www.ietf.org/archive/id/draft-gharris-opsawg-pcap-01.html and
+/// https://www.ietf.org/archive/id/draft-tuexen-opsawg-pcapng-05.html.
+namespace vast::pcap {
+
+/// The maximum snaplen as defined by MAXIMUM_SNAPLEN in libpcap (pcap-int.h).
+constexpr uint32_t maximum_snaplen = 262'144;
+
+/// File header magic number for microsecond timestamp precision.
+constexpr uint32_t magic_number_1 = 0xa1b2c3d4;
+
+/// File header magic number for nanosecond timestamp precision.
+constexpr uint32_t magic_number_2 = 0xa1b23c4d;
+
+/// The PCAP file header.
+struct file_header {
+  uint32_t magic_number;
+  uint16_t major_version;
+  uint16_t minor_version;
+  uint32_t reserved1;
+  uint32_t reserved2;
+  uint32_t snaplen;
+  uint32_t linktype;
+} __attribute__((packed));
+
+// The file header length is 24 octets.
+static_assert(sizeof(file_header) == 24);
+
+/// The packet header.
+struct packet_header {
+  uint32_t timestamp;
+  uint32_t timestamp_fraction;
+  uint32_t captured_packet_length;
+  uint32_t original_packet_length;
+} __attribute__((packed));
+
+// The packet header length is 16 octets.
+static_assert(sizeof(packet_header) == 16);
+
+// PCAP files are written out with the system endianness, so we may have to
+// swap bytes whenever the local endianness differs from the trace file. The
+// magic number in the file helps identifying the endianness.
+
+/// Swaps bytes in the file header.
+auto byteswap(file_header hdr) -> file_header;
+
+/// Swaps bytes in the packet header.
+auto byteswap(packet_header hdr) -> packet_header;
+
+/// A container for storing a single coming from the network. Header and data
+/// lay next to each other on the wire.
+struct packet_record {
+  packet_header header;
+  std::span<const std::byte> data;
+};
+
+/// Creates a Tenzir type for the above packet record.
+/// @relates packet_record
+auto packet_record_type() -> type;
+
+} // namespace vast::pcap
---
+++ libvast/src/pcap.cpp
@@ -0,0 +1,52 @@
+//    _   _____   __________
+//   | | / / _ | / __/_  __/     Visibility
+//   | |/ / __ |_\ \  / /          Across
+//   |___/_/ |_/___/ /_/       Space and Time
+//
+// SPDX-FileCopyrightText: (c) 2023 The VAST Contributors
+// SPDX-License-Identifier: BSD-3-Clause
+
+#pragma once
+
+#include "vast/pcap.hpp"
+
+#include "vast/detail/byteswap.hpp"
+
+namespace vast::pcap {
+
+auto byteswap(file_header hdr) -> file_header {
+  auto result = file_header{};
+  result.magic_number = detail::byteswap(hdr.magic_number);
+  result.major_version = detail::byteswap(hdr.major_version);
+  result.minor_version = detail::byteswap(hdr.minor_version);
+  result.reserved1 = detail::byteswap(hdr.reserved1);
+  result.reserved2 = detail::byteswap(hdr.reserved2);
+  result.snaplen = detail::byteswap(hdr.snaplen);
+  result.linktype = detail::byteswap(hdr.linktype);
+  return result;
+}
+
+auto byteswap(packet_header hdr) -> packet_header {
+  auto result = packet_header{};
+  result.timestamp = detail::byteswap(hdr.timestamp);
+  result.timestamp_fraction = detail::byteswap(hdr.timestamp_fraction);
+  result.captured_packet_length = detail::byteswap(hdr.captured_packet_length);
+  result.original_packet_length = detail::byteswap(hdr.original_packet_length);
+  return result;
+}
+
+auto packet_record_type() -> type {
+  const auto timestamp_type = type{"timestamp", time_type{}};
+  return type{
+    "pcap.packet",
+    record_type{
+      {"linktype", uint64_type{}}, // uint16 would suffice
+      {"timestamp", timestamp_type},
+      {"captured_packet_length", uint64_type{}},
+      {"original_packet_length", uint64_type{}},
+      {"data", type{string_type{}, {{"skip"}}}},
+    },
+  };
+}
+
+} // namespace vast::pcap
