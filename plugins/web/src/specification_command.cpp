3782ae6959ffc95d5fe1b2dbcc0e9764392484ec
Jannis Christopher Köhl <mail@koehl.dev>
2023-07-03 12:02:21 +0200

    Update pipeline manager OpenAPI docs

--- plugins/web/src/specification_command.cpp
+++ plugins/web/src/specification_command.cpp
@@ -21,6 +21,106 @@ auto openapi_record() -> record {
   std::sort(paths.begin(), paths.end(), [](const auto& lhs, const auto& rhs) {
     return lhs.first < rhs.first;
   });
+  auto schemas = from_yaml(R"_(
+Diagnostics:
+  type: array
+  items:
+    $ref: '#/components/schemas/Diagnostic'
+Diagnostic:
+  type: object
+  properties:
+    severity:
+      type: string
+      enum: ["error", "warning", "note"]
+    message:
+      type: string
+      example: "unknown option `--frobnify`"
+    annotation:
+      type: array
+      items:
+        $ref: '#/components/schemas/Annotation'
+    notes:
+      type: array
+      items:
+        $ref: '#/components/schemas/Note'
+Annotation:
+  type: object
+  properties:
+    primary:
+      type: boolean
+    text:
+      type: string
+      example: "this option does not exist"
+      description: A potentially empty label.
+    source:
+      $ref: '#/components/schemas/Location'
+Note:
+  type: object
+  properties:
+    kind:
+      type: string
+      enum: ["note", "usage", "hint", "docs"]
+      example: "usage"
+    message:
+      type: string
+      example: "file <path> [-f|--follow] [-m|--mmap] [-t|--timeout <duration>]"
+Location:
+  type: object
+  description: A region in the source code, defined by byte offsets.
+  properties:
+    begin:
+      type: number
+      example: 42
+    end:
+      type: number
+      example: 48
+PipelineInfo:
+  type: object
+  properties:
+    id:
+      type: string
+      description: The pipeline ID.
+    name:
+      type: string
+      description: The human-readable name of the pipeline.
+    definition:
+      type: string
+      description: The pipeline definition.
+    state:
+      type: string
+      enum:
+        - starting
+        - running
+        - stopping
+        - stopped
+    error:
+      type: string
+      description: The error that the pipeline may have encountered during running.
+    delete_when_stopped:
+      type: boolean
+      description: A flag specifying whether this pipeline is implicitly removed once it is terminated.
+    restart_with_node:
+      type: boolean
+      description: A flag specifying whether this pipeline should start upon launching the parent node.
+      default: false
+    operators:
+      type: array
+      items:
+        type: object
+        properties:
+          id:
+            type: string
+            description: The pipeline operator ID.
+          definition:
+            type: string
+            description: The pipeline operator definition.
+          instrumented:
+            type: boolean
+            description: Flag that enables subscribing to this operator's metrics and warnings under /pipeline/(pipeline-id)/(operator-id).
+    diagnostics:
+      $ref: '#/components/schemas/Diagnostics'
+  )_");
+  VAST_ASSERT_CHEAP(schemas);
   // clang-format off
   auto openapi = record{
     {"openapi", "3.0.0"},
@@ -42,6 +142,7 @@ on the command-line using the `vast rest generate-token` command.)_"},
       record {{"VastToken", list{}}},
     }}},
     {"components", record{
+      {"schemas", std::move(*schemas)},
       {"securitySchemes",
         record{{"VastToken", record {
             {"type", "apiKey"},
