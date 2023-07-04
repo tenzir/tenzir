662725466a59c2643473f3856de77a2807b81bb1
Dominik Lohmann <mail@dominiklohmann.de>
2023-07-04 17:38:48 +0200

    Reduce calls to get the current time

--- libvast/builtins/formats/zeek_tsv.cpp
+++ libvast/builtins/formats/zeek_tsv.cpp
@@ -666,10 +666,10 @@ auto parser_impl(generator<std::optional<std::string_view>> lines,
         co_return;
       }
     }
-    if (b->rows() > defaults::import::table_slice_size
-        or last_finish + std::chrono::seconds{1}
-             < std::chrono::steady_clock ::now()) {
-      last_finish = std::chrono::steady_clock::now();
+    const auto now = std::chrono::steady_clock::now();
+    if (b->rows() >= defaults::import::table_slice_size
+        or last_finish + std::chrono::seconds{1} < now) {
+      last_finish = now;
       co_yield b->finish();
     }
   }
