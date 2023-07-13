806394a5645deb3cfe849355ca882e16b41f1155
Matthias Vallentin <matthias@vallentin.net>
2023-07-13 12:33:24 +0200

    Add giscus commenting to blog posts

---
+++ web/src/theme/BlogPostItem/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useBlogPost } from '@docusaurus/theme-common/internal'
+import BlogPostItem from '@theme-original/BlogPostItem';
+import GiscusComponent from '@site/src/components/GiscusComponent';
+import useIsBrowser from '@docusaurus/useIsBrowser';
+
+export default function BlogPostItemWrapper(props) {
+  const { metadata, isBlogPostPage } = useBlogPost()
+  const isBrowser = useIsBrowser();
+
+  const { frontMatter, slug, title } = metadata
+  const { comments } = frontMatter
+
+  return (
+    <>
+      <BlogPostItem {...props} />
+      {(comments && isBlogPostPage) && (
+        <GiscusComponent />
+      )}
+    </>
+  );
+}
