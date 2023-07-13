ad88ce6584ccf44512b149b194d7861c89a62bb2
Matthias Vallentin <matthias@vallentin.net>
2023-07-13 16:41:02 +0200

    Remove uneeded code

--- web/src/theme/BlogPostItem/index.js
+++ web/src/theme/BlogPostItem/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { useBlogPost } from '@docusaurus/theme-common/internal'
 import BlogPostItem from '@theme-original/BlogPostItem';
 import GiscusComponent from '@site/src/components/GiscusComponent';
-import useIsBrowser from '@docusaurus/useIsBrowser';
 
 export default function BlogPostItemWrapper(props) {
   const { metadata, isBlogPostPage } = useBlogPost()
-  const isBrowser = useIsBrowser();
 
-  const { frontMatter, slug, title } = metadata
+  const { frontMatter } = metadata
   const { comments } = frontMatter
 
   return (
