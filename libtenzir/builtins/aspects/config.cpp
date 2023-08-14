1e45697dbe50a4a806a529cb7754106e6ed1abb5
Dominik Lohmann <mail@dominiklohmann.de>
2023-08-14 10:52:30 -0700

    Factor `adaptive_table_slice_builder::add_row`
    The new function allows for adding an entire record view resembling one row all
    in one go.

--- libtenzir/builtins/aspects/config.cpp
+++ libtenzir/builtins/aspects/config.cpp
@@ -14,63 +14,15 @@ namespace tenzir::plugins::config {
 
 namespace {
 
-template <class Guard>
-struct recursive_add {
-  explicit recursive_add(Guard& guard) noexcept : guard{guard} {
-  }
-
-  auto operator()(const record& x) {
-    auto r = guard.push_record();
-    for (const auto& [key, value] : x) {
-      auto field = r.push_field(key);
-      auto add = recursive_add<detail::field_guard>{field};
-      caf::visit(
-        [&](const auto& value) {
-          add(value);
-        },
-        value);
-    }
-  }
-
-  auto operator()(const list& x) {
-    auto l = guard.push_list();
-    auto add = recursive_add<detail::list_guard>{l};
-    for (const auto& value : x) {
-      caf::visit(
-        [&](const auto& value) {
-          add(value);
-        },
-        value);
-    }
-  }
-
-  auto operator()(caf::none_t) {
-  }
-
-  auto operator()(const pattern&) {
-    die("unreachable");
-  }
-
-  auto operator()(const map&) {
-    die("unreachable");
-  }
-
-  template <class T>
-    requires(not std::is_same_v<T, data>)
-  auto operator()(const T& x) {
-    // FIXME
-    (void)guard.add(x);
-  }
-
-  Guard& guard;
-};
-
 class plugin final : public virtual aspect_plugin {
 public:
   auto initialize(const record& plugin_config, const record& global_config)
     -> caf::error override {
     (void)plugin_config;
     config_ = global_config;
+    // We remove the CAF config section as it's prefilled with some config
+    // settings that are irrelevant to users.
+    config_.erase("caf");
     return {};
   }
 
@@ -82,24 +34,13 @@ public:
     return operator_location::local;
   }
 
-  auto show(operator_control_plane&) const -> generator<table_slice> override {
+  auto show(operator_control_plane& ctrl) const
+    -> generator<table_slice> override {
     auto builder = adaptive_table_slice_builder{};
-    {
-      auto row = builder.push_row();
-      for (const auto& [key, value] : config_) {
-        if (key == "caf") {
-          // We skip CAF's configuration as it adds a lot of noise for very
-          // little value.
-          continue;
-        }
-        auto field = row.push_field(key);
-        auto add = recursive_add{field};
-        caf::visit(
-          [&](const auto& value) {
-            add(value);
-          },
-          value);
-      }
+    if (auto err = builder.add_row(make_view(config_))) {
+      diagnostic::error("failed to add config: {}", err)
+        .emit(ctrl.diagnostics());
+      co_return;
     }
     co_yield builder.finish("tenzir.config");
   }
