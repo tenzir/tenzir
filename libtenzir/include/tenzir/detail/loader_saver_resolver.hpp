eaf94c5bc5d24b372c313cf8854a7ea4089f09f8
Elias Kosunen <elias.kosunen@gmail.com>
2023-11-10 19:03:48 +0200

    Add file extension detection to `from/to <uri>`
    For a path `foo.gz`, automatic `| (de)compress gzip` is added
    to the pipeline. Same applies to other compression algorithms.
    
    For a path `foo.json`, automatic `| read/write json` is added
    to the pipeline. Same applies to other file formats and file extensions.

--- libtenzir/include/tenzir/detail/loader_saver_resolver.hpp
+++ libtenzir/include/tenzir/detail/loader_saver_resolver.hpp
@@ -17,13 +17,26 @@ template <typename Plugin>
 struct resolve_loader_saver_result {
   std::unique_ptr<Plugin> plugin;
   located<std::string_view> name;
+  located<std::string> path;
   bool matched_uri;
 };
 
-auto resolve_loader(parser_interface& parser, const located<std::string>& name)
+auto resolve_loader(parser_interface& parser, located<std::string_view> name)
   -> resolve_loader_saver_result<plugin_loader>;
 
-auto resolve_saver(parser_interface& parser, const located<std::string>& name)
+auto resolve_saver(parser_interface& parser, located<std::string_view> name)
   -> resolve_loader_saver_result<plugin_saver>;
+
+auto resolve_decompressor(located<std::string_view> path) -> operator_ptr;
+
+auto resolve_compressor(located<std::string_view> path) -> operator_ptr;
+
+auto resolve_parser(located<std::string_view> path,
+                    std::string_view default_parser)
+  -> std::pair<operator_ptr, std::unique_ptr<plugin_parser>>;
+
+auto resolve_printer(located<std::string_view> path,
+                     std::string_view default_printer)
+  -> std::pair<operator_ptr, std::unique_ptr<plugin_printer>>;
 
 } // namespace tenzir::detail
