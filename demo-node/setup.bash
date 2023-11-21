59a78688fa10cf4768e41301e354afa6e2a58219
Mostly Void <7rat13@gmail.com>
2023-11-21 16:34:23 +0100

    Fix demo-node label color strings
    The app uses all-lowercase RGB strings. Although I have just opened
    a PR for coercing to lowercase before matching the fg and bg colors
    for the labels, it does not hurt to change them here too.
    
    Also, the suricata label color was slightly wrong from the app value.

--- demo-node/setup.bash
+++ demo-node/setup.bash
@@ -13,14 +13,14 @@ echo "Spawnng M57 Suricata pipeline"
 m57_suricata_definition='load https https://storage.googleapis.com/tenzir-datasets/M57/suricata.json.zst\n| decompress zstd\n| read suricata\n| where #schema != \"suricata.stats\"\n| import'
 m57_suricata_id=$(tenzir -q "api /pipeline/create '{\"definition\": \"${m57_suricata_definition}\", \"name\": \"M57 Suricata\", \"autostart\": {\"created\": true}}'" | jq -re ".id")
 echo "Setting labels for M57 Suricata pipeline"
-m57_suricata_labels='[{"text": "suricata", "color": "#0086E4"}, {"text": "import", "color": "#2F00CC"}]'
+m57_suricata_labels='[{"text": "suricata", "color": "#0086e5"}, {"text": "import", "color": "#2f00cc"}]'
 tenzir -q "api /pipeline/update '{\"id\": \"${m57_suricata_id}\", \"labels\": ${m57_suricata_labels}}'" | jq -er '"id = \(.pipeline.id)"'
 
 echo "Spawnng M57 Zeek pipeline"
 m57_zeek_definition='load https https://storage.googleapis.com/tenzir-datasets/M57/zeek-all.log.zst\n| decompress zstd\n| read zeek-tsv\n| import'
 m57_zeek_id=$(tenzir -q "api /pipeline/create '{\"definition\": \"${m57_zeek_definition}\", \"name\": \"M57 Zeek\", \"autostart\": {\"created\": true}}'" | jq -re ".id")
 echo "Setting labels for M57 Zeek pipeline"
-m57_zeek_labels='[{"text": "zeek", "color": "#E89400"}, {"text": "import", "color": "#2F00CC"}]'
+m57_zeek_labels='[{"text": "zeek", "color": "#e89400"}, {"text": "import", "color": "#2f00cc"}]'
 tenzir -q "api /pipeline/update '{\"id\": \"${m57_zeek_id}\", \"labels\": ${m57_zeek_labels}}'" | jq -er '"id = \(.pipeline.id)"'
 
 done="false"
