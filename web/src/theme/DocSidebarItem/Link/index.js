742eb1eb0b4ca3b62272a44f479f6f9741542204
Danyl Fernandes <realllydan@gmail.com>
2023-12-14 13:59:18 +0530

    Add type icons alongside labels to operator links

--- web/src/theme/DocSidebarItem/Link/index.js
+++ web/src/theme/DocSidebarItem/Link/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import clsx from 'clsx';
-import {ThemeClassNames} from '@docusaurus/theme-common';
-import {isActiveSidebarItem} from '@docusaurus/theme-common/internal';
-import Link from '@docusaurus/Link';
-import isInternalUrl from '@docusaurus/isInternalUrl';
-import IconExternalLink from '@theme/Icon/ExternalLink';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import { ThemeClassNames } from "@docusaurus/theme-common";
+import { isActiveSidebarItem } from "@docusaurus/theme-common/internal";
+import Link from "@docusaurus/Link";
+import isInternalUrl from "@docusaurus/isInternalUrl";
+import IconExternalLink from "@theme/Icon/ExternalLink";
+import styles from "./styles.module.css";
+import SVGSource from "./IconSource.svg";
+import SVGTransformation from "./IconTransformation.svg";
+import SVGSink from "./IconSink.svg";
+
 export default function DocSidebarItemLink({
   item,
   onItemClick,
@@ -14,36 +18,79 @@ export default function DocSidebarItemLink({
   index,
   ...props
 }) {
-  const {href, label, className, autoAddBaseUrl} = item;
+  const { href, label, className, autoAddBaseUrl, customProps } = item;
   const isActive = isActiveSidebarItem(item, activePath);
   const isInternalLink = isInternalUrl(href);
+
   return (
     <li
       className={clsx(
         ThemeClassNames.docs.docSidebarItemLink,
         ThemeClassNames.docs.docSidebarItemLinkLevel(level),
-        'menu__list-item',
-        className,
+        "menu__list-item",
+        className
       )}
-      key={label}>
+      key={label}
+    >
       <Link
         className={clsx(
-          'menu__link',
+          "menu__link",
           !isInternalLink && styles.menuExternalLink,
           {
-            'menu__link--active': isActive,
-          },
+            "menu__link--active": isActive,
+          }
         )}
         autoAddBaseUrl={autoAddBaseUrl}
-        aria-current={isActive ? 'page' : undefined}
+        aria-current={isActive ? "page" : undefined}
         to={href}
         {...(isInternalLink && {
           onClick: onItemClick ? () => onItemClick(item) : undefined,
         })}
-        {...props}>
-        {label}
+        {...props}
+      >
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          {label}
+          <div
+            style={{
+              display: "flex",
+            }}
+          >
+            {customProps?.operator?.source && <IconSource />}
+            {customProps?.operator?.transformation && <IconTransformation />}
+            {customProps?.operator?.sink && <IconSink />}
+          </div>
+        </div>
         {!isInternalLink && <IconExternalLink />}
       </Link>
     </li>
   );
 }
+
+const IconContainer = ({ children }) => (
+  <div
+    style={{
+      height: 20,
+      marginRight: -10,
+    }}
+  >
+    {children}
+  </div>
+);
+
+const withIconContainer = (Icon) => () =>
+  (
+    <IconContainer>
+      <Icon />
+    </IconContainer>
+  );
+
+const IconSource = withIconContainer(SVGSource);
+const IconTransformation = withIconContainer(SVGTransformation);
+const IconSink = withIconContainer(SVGSink);
