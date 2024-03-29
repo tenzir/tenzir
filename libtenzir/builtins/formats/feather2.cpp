86d07ce8404e450f9aed4f96f7ff56dc6cecd710
balavinaithirthan <balabv1@gmail.com>
2024-03-29 13:47:25 +0100

    Added error messages to feather printer

--- libtenzir/builtins/formats/feather2.cpp
+++ libtenzir/builtins/formats/feather2.cpp
@@ -8,6 +8,7 @@
 
 #include <tenzir/argument_parser.hpp>
 #include <tenzir/plugin.hpp>
+#include <tenzir/pcap.hpp>
 
 #include <arrow/array.h>
 #include <arrow/compute/cast.h>
@@ -25,29 +26,36 @@ namespace {
 
 auto parse_feather(generator<chunk_ptr> input, operator_control_plane& ctrl)
   -> generator<table_slice> {
+  //auto bytes = tenzir::plugins::pcap::make_byte_reader(input);
+  auto listener = std::make_shared<arrow::ipc::Listener>(arrow::ipc::Listener()); // expiremental API
+  auto stream_decoder = arrow::ipc::StreamDecoder(listener);
+
+
   co_return;
 }
 
-auto print_feather(table_slice input, operator_control_plane& ctrl)
+auto print_feather(table_slice input, operator_control_plane& ctrl, const std::shared_ptr<arrow::ipc::RecordBatchWriter>& stream_writer, const std::shared_ptr<arrow::io::BufferOutputStream>& sink)
   -> generator<chunk_ptr> {
   auto batch = to_record_batch(input); // work with this instead
-  auto event_array = batch->ToStructArray().ValueOrDie();
-  auto batch_schema = batch->schema()->metadata();
-  auto sink = arrow::io::BufferOutputStream::Create().ValueOrDie();
-  const arrow::ipc::IpcWriteOptions &options = arrow::ipc::IpcWriteOptions::Defaults();
-  //is schema info correct?
-  auto schema = arrow::schema(
-    {arrow::field("import_time", time_type::to_arrow_type()),
-     arrow::field("event", event_array->type(), batch_schema)});
-  auto stream_writer_result = arrow::ipc::MakeStreamWriter(sink, schema, options);
-  auto stream_writer = stream_writer_result.ValueOrDie();
-  auto status = stream_writer->WriteRecordBatch(*batch); // do I need custom metadata?
+  auto stream_writer_status = stream_writer->WriteRecordBatch(*batch);
+  if (!stream_writer_status.ok()){
+    diagnostic::error("failed to write a record batch to the stream").note("{}", stream_writer_status.ToString()).emit(ctrl.diagnostics());
+  }
   auto finished_buffer_result = sink->Finish();
-  auto finished_buffer = finished_buffer_result.MoveValueUnsafe();
-  auto chunk = chunk::make(finished_buffer); // must be cpu buffer, how to get it over without copying
+  if(!finished_buffer_result.ok()) {
+    diagnostic::error("failed to close the stream and return the buffer").note("{}", finished_buffer_result.status().ToString()).emit(ctrl.diagnostics());
+    co_return;
+  }
+  auto finished_buffer = finished_buffer_result.ValueOrDie();;
+  auto chunk = chunk::make(finished_buffer); //does chunk make already have error handling
   co_yield chunk;
-  co_return;
+  auto reset_buffer_result = sink->Reset();
+  if(!reset_buffer_result.ok()) {
+    diagnostic::error("failed to reset buffer").note("{}", reset_buffer_result.ToString()).emit(ctrl.diagnostics());
+  }
 }
+
+// git work tree
 
 class feather2_parser final : public plugin_parser {
 public:
@@ -79,9 +87,16 @@ public:
   auto instantiate([[maybe_unused]] type input_schema,
                    operator_control_plane& ctrl) const
     -> caf::expected<std::unique_ptr<printer_instance>> override {
+
+    auto sink = arrow::io::BufferOutputStream::Create().ValueOrDie();
+    const arrow::ipc::IpcWriteOptions &options = arrow::ipc::IpcWriteOptions::Defaults();
+    auto schema = input_schema.to_arrow_schema();
+    auto stream_writer_result = arrow::ipc::MakeStreamWriter(sink, schema, options);
+    auto stream_writer = stream_writer_result.ValueOrDie();
+
     return printer_instance::make(
-      [&ctrl](table_slice slice) -> generator<chunk_ptr> {
-        return print_feather(std::move(slice), ctrl);
+      [&ctrl, sink = std::move(sink), stream_writer = std::move(stream_writer)](table_slice slice) -> generator<chunk_ptr> {
+        return print_feather(std::move(slice), ctrl, stream_writer, sink);
       });
   }
 
