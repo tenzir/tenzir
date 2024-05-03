6916ba4aae12a2e3f59438dfe6f6a32aa6ec58fc
IyeOnline <johannes.misch@tezir.com>
2024-05-03 11:48:38 +0200

    Add unordered batch optimization

--- libtenzir/builtins/operators/batch.cpp
+++ libtenzir/builtins/operators/batch.cpp
@@ -6,6 +6,8 @@
 // SPDX-FileCopyrightText: (c) 2023 The Tenzir Contributors
 // SPDX-License-Identifier: BSD-3-Clause
 
+#include "tenzir/detail/assert.hpp"
+
 #include <tenzir/argument_parser.hpp>
 #include <tenzir/concept/parseable/numeric/integral.hpp>
 #include <tenzir/concept/parseable/tenzir/pipeline.hpp>
@@ -21,63 +23,88 @@ namespace tenzir::plugins::batch {
 
 namespace {
 
+struct buffer_entry {
+  std::vector<table_slice> events;
+  uint64_t num_buffered = {};
+  std::chrono::steady_clock::time_point last_yield
+    = std::chrono::steady_clock::now();
+};
+
 class batch_operator final : public crtp_operator<batch_operator> {
 public:
   batch_operator() = default;
 
-  batch_operator(uint64_t limit, duration timeout)
-    : limit_{limit}, timeout_{timeout} {
+  batch_operator(uint64_t limit, duration timeout, event_order order)
+    : limit_{limit}, timeout_{timeout}, order_{order} {
     // nop
   }
 
   auto operator()(generator<table_slice> input) const
     -> generator<table_slice> {
-    auto buffer = std::vector<table_slice>{};
-    auto num_buffered = uint64_t{0};
-    auto last_yield = std::chrono::steady_clock::now();
+    std::unordered_map<type, buffer_entry> buffers;
     for (auto&& slice : input) {
       const auto now = std::chrono::steady_clock::now();
-      if (now - last_yield > timeout_ and num_buffered > 0) {
-        TENZIR_ASSERT(num_buffered < limit_);
-        last_yield = now;
-        co_yield concatenate(std::exchange(buffer, {}));
-        num_buffered = 0;
+      // Check all current buffers to see if we have hit a timeout.
+      for (auto it = buffers.begin(); it != buffers.end(); ++it) {
+        auto& entry = it->second;
+        if (now - entry.last_yield > timeout_ and entry.num_buffered > 0) {
+          TENZIR_ASSERT(entry.num_buffered < limit_);
+          entry.last_yield = now;
+          co_yield concatenate(std::exchange(entry.events, {}));
+          it = buffers.erase(it);
+          entry.num_buffered = 0;
+        }
       }
       if (slice.rows() == 0) {
         co_yield {};
         continue;
       }
-      if (not buffer.empty() and buffer.back().schema() != slice.schema()) {
-        while (not buffer.empty()) {
-          auto [lhs, rhs] = split(buffer, limit_);
+      // In case its an ordered batch and the schema changed, we need to yield
+      // the current buffer.
+      if (order_ == event_order::ordered and not buffers.empty()
+          and buffers.begin()->first != slice.schema()) {
+        TENZIR_ASSERT(buffers.size() == 1);
+        auto& entry = buffers.begin()->second;
+        while (not entry.events.empty()) {
+          auto [lhs, rhs] = split(entry.events, limit_);
           auto result = concatenate(std::move(lhs));
-          num_buffered -= result.rows();
-          last_yield = now;
+          entry.num_buffered -= result.rows();
+          entry.last_yield = now;
           co_yield std::move(result);
-          buffer = std::move(rhs);
+          entry.events = std::move(rhs);
         }
+        TENZIR_ASSERT(entry.events.empty());
+        buffers.clear();
       }
-      num_buffered += slice.rows();
-      buffer.push_back(std::move(slice));
-      while (num_buffered >= limit_) {
-        auto [lhs, rhs] = split(buffer, limit_);
+      // Get the buffer for the current slice schema and append to it.
+      auto [it, _] = buffers.try_emplace(slice.schema());
+      auto& entry = it->second;
+      entry.num_buffered += slice.rows();
+      entry.events.push_back(std::move(slice));
+      // If the buffer hit the limit, yield until its below again.
+      while (entry.num_buffered >= limit_) {
+        auto [lhs, rhs] = split(entry.events, limit_);
         auto result = concatenate(std::move(lhs));
-        num_buffered -= result.rows();
-        last_yield = now;
+        entry.num_buffered -= result.rows();
+        entry.last_yield = now;
         co_yield std::move(result);
-        buffer = std::move(rhs);
+        entry.events = std::move(rhs);
+      }
+      if (entry.num_buffered == 0) {
+        buffers.erase(it);
       }
     }
-    if (not buffer.empty()) {
-      co_yield concatenate(std::move(buffer));
+    // When our input is done, yield the rest of all buffers.
+    for (auto& [_, entry] : buffers) {
+      TENZIR_ASSERT(not entry.events.empty());
+      co_yield concatenate(std::move(entry.events));
     }
   }
 
   auto optimize(expression const& filter, event_order order) const
     -> optimize_result override {
-    // TODO: This operator can massively benefit from an unordered
-    // implementation, where it can keep multiple buffers per schema.
-    return optimize_result{filter, order, copy()};
+    return optimize_result{
+      filter, order, std::make_unique<batch_operator>(limit_, timeout_, order)};
   }
 
   auto name() const -> std::string override {
@@ -87,12 +114,14 @@ public:
   friend auto inspect(auto& f, batch_operator& x) -> bool {
     return f.object(x)
       .pretty_name("batch_operator")
-      .fields(f.field("limit", x.limit_), f.field("timeout", x.timeout_));
+      .fields(f.field("limit", x.limit_), f.field("timeout", x.timeout_),
+              f.field("order", x.order_));
   }
 
 private:
   uint64_t limit_ = defaults::import::table_slice_size;
   duration timeout_ = {};
+  event_order order_ = event_order::ordered;
 };
 
 class plugin final : public virtual operator_plugin<batch_operator> {
@@ -122,7 +151,7 @@ public:
 
     return std::make_unique<batch_operator>(
       limit ? limit->inner : defaults::import::table_slice_size,
-      timeout ? timeout->inner : duration::max());
+      timeout ? timeout->inner : duration::max(), event_order::ordered);
   }
 };
 
