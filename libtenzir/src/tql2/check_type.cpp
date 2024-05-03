d61d878f99cead3693bd07bec6b51af22a873621
Jannis Christopher Köhl <mail@koehl.dev>
2024-05-03 19:35:36 +0200

    wip

--- libtenzir/src/tql2/check_type.cpp
+++ libtenzir/src/tql2/check_type.cpp
@@ -6,8 +6,6 @@
 // SPDX-FileCopyrightText: (c) 2024 The Tenzir Contributors
 // SPDX-License-Identifier: BSD-3-Clause
 
-#pragma once
-
 #include "tenzir/tql2/check_type.hpp"
 
 #include "tenzir/diagnostics.hpp"
