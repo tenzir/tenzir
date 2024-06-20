86360b954093c061ca184a34a5ae7e5e9caf8cbb
Dominik Lohmann <mail@dominiklohmann.de>
2024-06-20 10:27:50 +0200

    Use a simpler approach for reconstructing struct array

--- libtenzir/src/offset.cpp
+++ libtenzir/src/offset.cpp
@@ -58,11 +58,7 @@ auto offset::get(const arrow::RecordBatch& batch) const noexcept
 auto offset::get(const arrow::StructArray& struct_array) const noexcept
   -> std::shared_ptr<arrow::Array> {
   if (empty()) {
-    // If the offset is empty, we are already at the desired level, and can just
-    // wrap the struct array back into a shared pointer.
-    auto result = struct_array.View(struct_array.type());
-    TENZIR_ASSERT(result.ok(), result.status().ToString().c_str());
-    return result.MoveValueUnsafe();
+    return std::make_shared<arrow::StructArray>(struct_array.data());
   }
   auto impl
     = [](auto&& impl, std::span<const offset::value_type> index,
