fc994b23b70d6b202ddb2a40491821628ace598d
Dominik Lohmann <mail@dominiklohmann.de>
2024-07-04 16:50:38 +0200

    Remove `plugin::deinitialize()`
    Turns out all we needed to do was deinitialize the logger first, and now
    we can just call the destructor.

--- libtenzir/include/tenzir/plugin.hpp
+++ libtenzir/include/tenzir/plugin.hpp
@@ -129,10 +129,6 @@ public:
     return {};
   }
 
-  // Deinitializes a plugin.
-  virtual auto deinitialize() -> void {
-  }
-
   /// Returns the unique name of the plugin.
   [[nodiscard]] virtual std::string name() const = 0;
 };
--- libtenzir_test/src/main.cpp
+++ libtenzir_test/src/main.cpp
@@ -86,14 +86,7 @@ int main(int argc, char** argv) {
 
   // Make sure to deinitialize all plugins at the end.
   auto plugin_guard = caf::detail::make_scope_guard([]() noexcept {
-    // Ideally, we would not have this deinitialize function at all and could
-    // just call `plugins::get_mutable().clear()`, but that has a race condition
-    // in that some detached actors may still be alive that are owned by
-    // plugins, which then often dereference a nullptr through the global actor
-    // system config.
-    for (auto& plugin : tenzir::plugins::get_mutable()) {
-      plugin->deinitialize();
-    }
+    tenzir::plugins::get_mutable().clear();
   });
   caf::settings log_settings;
   put(log_settings, "tenzir.console-verbosity", tenzir_loglevel);
