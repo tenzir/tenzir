ec3d6e98d3847de69c570f63914fabea388460c4
Tobias Mayer <tobim@fastmail.fm>
2024-07-09 16:54:28 +0200

    Add missing time format specifiers for the static binary

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -9,6 +9,15 @@
     then package
     else package.overrideAttrs f;
 in {
+  musl = overrideAttrsIf isStatic prev.musl (orig: {
+    patches = (orig.patches or []) ++ [
+      (prev.buildPackages.fetchpatch {
+        name = "musl-strptime-new-format-specifiers";
+        url = "https://git.musl-libc.org/cgit/musl/patch?id=fced99e93daeefb0192fd16304f978d4401d1d77";
+        hash = "sha256-WhT9C7Mn94qf12IlasVNGXwpR0XnnkFNLDJ6lYx3Xag=";
+      })
+    ];
+  });
   google-cloud-cpp =
     if !isStatic
     then prev.google-cloud-cpp
