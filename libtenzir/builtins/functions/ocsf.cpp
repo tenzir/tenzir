e63a1c583caf620af2b013ba65106d04e09f1b2a
Johannes Misch <johannes.misch@tenzir.com>
2024-08-05 17:57:33 +0200

    Make simplifications and add warnings

--- libtenzir/builtins/functions/ocsf.cpp
+++ libtenzir/builtins/functions/ocsf.cpp
@@ -104,60 +104,62 @@ constexpr ocsf_pair class_map[]{
   {"Network Remediation Activity", 7004},
 };
 
-template <typename T, typename... Ts>
-concept any_of = (std::same_as<T, Ts> or ...);
-
 auto name_to_id(std::span<const ocsf_pair> lookup,
-                std::string_view key) -> int64_t {
+                std::string_view key) -> std::optional<int64_t> {
   for (const auto& [category, id] : lookup) {
     if (key == category) {
       return id;
     }
   }
-  return 0;
+  return std::nullopt;
 };
 auto id_to_name(std::span<const ocsf_pair> lookup,
-                int64_t key) -> std::string_view {
+                int64_t key) -> std::optional<std::string_view> {
   for (const auto& [category, id] : lookup) {
     if (key == id) {
       return category;
     }
   }
-  return {};
+  return std::nullopt;
 };
 
-/// @brief generic mapping function that supports OCSF `Event Type String` <-> `OCSF UID`
-/// @tparam Name the name of the mapping function
-/// @tparam Input_Meta the `<meta>` for the input type
-/// @tparam &Map The "map" aka array to use for lookup
+/// @brief generic mapping function that supports OCSF `Event Type String` <->
+/// `OCSF UID`
 /// @tparam Operation the mapping function to use
-/// @tparam Output_Builder_Type the builder for the resulting type of the mapping
 /// @tparam Output_Type the tenzir type the operation produces
 /// @tparam Input_Types... The input type arrays that the operation accepts
-template <detail::string_literal Name, detail::string_literal Input_Meta,
-          auto& Map, auto Operation, typename Output_Builder_Type,
-          typename Output_Type, typename... Input_Types>
-class generic_mapping_function final : public function_plugin {
+template <auto Operation, typename Output_Type, typename... Input_Types>
+class generic_mapping_plugin final : public function_plugin {
 public:
   auto name() const -> std::string override {
-    return std::string{Name.str()};
+    return name_;
+  }
+
+  generic_mapping_plugin(std::string name, std::string input_meta,
+                         const std::span<const ocsf_pair> map,
+                         std::string warning_text)
+    : name_{std::move(name)},
+      input_meta_{std::move(input_meta)},
+      map_{map},
+      warning_text_{std::move(warning_text)} {
   }
 
   auto make_function(invocation inv,
                      session ctx) const -> failure_or<function_ptr> override {
     auto expr = ast::expression{};
-    TRY(argument_parser2::function(std::string{Name.str()})
-          .add(expr, std::string{Input_Meta.str()})
-          .parse(inv, ctx));
+    TRY(
+      argument_parser2::function(name_).add(expr, input_meta_).parse(inv, ctx));
     return function_use::make(
-      [expr = std::move(expr)](evaluator eval, session ctx) -> series {
+      [&, expr = std::move(expr)](evaluator eval, session ctx) -> series {
         auto arg = eval(expr);
         auto f = detail::overload{
           [&](const arrow::NullArray& arg) {
             return series::null(Output_Type{}, arg.length());
           },
-          []<any_of<Input_Types...> T>(const T& arg) {
-            auto b = Output_Builder_Type{};
+          [&](const type_to_arrow_array_t<Input_Types>& arg) {
+            using input_data_type = type_to_data_t<Input_Types>;
+            std::optional<input_data_type> warn_value;
+            auto b = type_to_arrow_builder_t<Output_Type>{};
             check(b.Reserve(arg.length()));
             for (auto i = int64_t{0}; i < arg.length(); ++i) {
               if (arg.IsNull(i)) {
@@ -165,14 +167,36 @@ public:
                 continue;
               }
               auto in = arg.GetView(i);
-              auto out = Operation(std::span{Map}, in);
-              check(b.Append(out));
+              // handle numeric bounds for uint inputs
+              if constexpr (std::same_as<input_data_type, uint64_type>) {
+                if (in > static_cast<uint64_t>(
+                      std::numeric_limits<int64_t>::max())) {
+                  warn_value = in;
+                  check(b.AppendNull());
+                  continue;
+                }
+              }
+              auto out = Operation(map_, in);
+              if (out) {
+                check(b.Append(*out));
+              } else {
+                if (not warn_value) {
+                  warn_value = in;
+                }
+                check(b.AppendNull());
+              }
+            }
+            if (warn_value) {
+              diagnostic::warning("invalid {}", warning_text_)
+                .note("got `{}`", *warn_value)
+                .primary(expr)
+                .emit(ctx);
             }
             return series{Output_Type{}, finish(b)};
-          },
+          }...,
           [&](const auto&) {
-            diagnostic::warning("`{}` expected `string`, but got `{}`",
-                                Name.str(), arg.type.kind())
+            diagnostic::warning("`{}` expected `string`, but got `{}`", name_,
+                                arg.type.kind())
               .primary(expr)
               .emit(ctx);
             return series::null(Output_Type{}, arg.length());
@@ -181,32 +205,34 @@ public:
         return caf::visit(f, *arg.array);
       });
   }
+
+private:
+  std::string name_;
+  std::string input_meta_;
+  std::span<const ocsf_pair> map_;
+  std::string warning_text_;
 };
 
-using category_uid
-  = generic_mapping_function<"tql2.ocsf_category_uid", "<string>", category_map,
-                   name_to_id, arrow::Int64Builder, tenzir::int64_type,
-                   arrow::StringArray>;
+using name_to_id_plugin
+  = generic_mapping_plugin<name_to_id, tenzir::int64_type, tenzir::string_type>;
 
-using uid_category
-  = generic_mapping_function<"tql2.ocsf_uid_category", "<int>", category_map, id_to_name,
-                   arrow::StringBuilder, tenzir::string_type, arrow::Int64Array,
-                   arrow::UInt64Array>;
-
-using class_uid
-  = generic_mapping_function<"tql2.ocsf_class_uid", "<string>", class_map, name_to_id,
-                   arrow::Int64Builder, tenzir::int64_type, arrow::StringArray>;
-
-using uid_class
-  = generic_mapping_function<"tql2.ocsf_uid_class", "<int>", class_map, id_to_name,
-                   arrow::StringBuilder, tenzir::string_type, arrow::Int64Array,
-                   arrow::UInt64Array>;
+using id_to_name_plugin
+  = generic_mapping_plugin<id_to_name, tenzir::string_type, tenzir::int64_type,
+                           tenzir::uint64_type>;
 
 } // namespace
 
 } // namespace tenzir::plugins::ocsf
 
-TENZIR_REGISTER_PLUGIN(tenzir::plugins::ocsf::category_uid)
-TENZIR_REGISTER_PLUGIN(tenzir::plugins::ocsf::uid_category)
-TENZIR_REGISTER_PLUGIN(tenzir::plugins::ocsf::class_uid)
-TENZIR_REGISTER_PLUGIN(tenzir::plugins::ocsf::uid_class)
+using tenzir::plugins::ocsf::category_map;
+
+TENZIR_REGISTER_PLUGIN(tenzir::plugins::ocsf::name_to_id_plugin{
+  "ocsf_category_uid", "<string>", category_map, "OCSF category name"})
+TENZIR_REGISTER_PLUGIN(tenzir::plugins::ocsf::id_to_name_plugin{
+  "ocsf_category_name", "<int>", category_map, "OCSF category ID"})
+TENZIR_REGISTER_PLUGIN(tenzir::plugins::ocsf::name_to_id_plugin{
+  "ocsf_class_uid", "<string>", tenzir::plugins::ocsf::class_map,
+  "OCSF class name"})
+TENZIR_REGISTER_PLUGIN(tenzir::plugins::ocsf::id_to_name_plugin{
+  "ocsf_class_name", "<int>", tenzir::plugins::ocsf::class_map,
+  "OCSF class ID"})
