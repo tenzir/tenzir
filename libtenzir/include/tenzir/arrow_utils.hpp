765fedcdd195fb507e3ae867a65b7b9238feac03
Dominik Lohmann <mail@dominiklohmann.de>
2024-08-27 21:45:37 +0200

    Port the `api` operator to TQL2
    For example:
    
    ```
    // tql2
    api "/pipeline/update", {
      id: "1e6b6d7c-4b7f-4cbf-bf65-115a7fc27145",
      action: "stop",
    }
    ```

--- libtenzir/include/tenzir/arrow_utils.hpp
+++ libtenzir/include/tenzir/arrow_utils.hpp
@@ -15,21 +15,30 @@
 #include <arrow/result.h>
 #include <arrow/type_traits.h>
 
+#include <source_location>
+
 namespace tenzir {
 
-inline void check(const arrow::Status& status) {
-  TENZIR_ASSERT(status.ok(), status.ToString());
+inline void
+check(const arrow::Status& status, std::source_location location
+                                   = std::source_location::current()) {
+  if (not status.ok()) [[unlikely]] {
+    detail::panic_impl(status.ToString(), location);
+  }
 }
 
 template <class T>
-[[nodiscard]] auto check(arrow::Result<T> result) -> T {
-  check(result.status());
+[[nodiscard]] auto
+check(arrow::Result<T> result, std::source_location location
+                               = std::source_location::current()) -> T {
+  check(result.status(), location);
   return result.MoveValueUnsafe();
 }
 
 template <std::derived_from<arrow::ArrayBuilder> T>
-[[nodiscard]] auto finish(T& x) {
-  auto array = check(x.Finish());
+[[nodiscard]] auto
+finish(T& x, std::source_location location = std::source_location::current()) {
+  auto array = check(x.Finish(), location);
   auto cast
     = std::dynamic_pointer_cast<type_to_arrow_array_t<type_from_arrow_t<T>>>(
       std::move(array));
