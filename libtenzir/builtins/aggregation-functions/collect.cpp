00c21543d49121bd5b29333bd47b6434d81515cd
Raghav Narang <dev@raxyte.com>
2024-09-10 19:06:13 +0530

    Port `collect()` aggregation to TQL2

--- libtenzir/builtins/aggregation-functions/collect.cpp
+++ libtenzir/builtins/aggregation-functions/collect.cpp
@@ -8,6 +8,8 @@
 
 #include <tenzir/aggregation_function.hpp>
 #include <tenzir/plugin.hpp>
+#include <tenzir/tql2/eval.hpp>
+#include <tenzir/tql2/plugin.hpp>
 
 namespace tenzir::plugins::collect {
 
@@ -47,13 +49,44 @@ private:
   }
 
   auto finish() && -> caf::expected<data> override {
+    // XXX: Why exchange? Is this same instance reused?
     return data{std::exchange(result_, {})};
   }
 
   list result_ = {};
 };
 
-class plugin : public virtual aggregation_function_plugin {
+class collect_instance final : public aggregation_instance {
+public:
+  explicit collect_instance(ast::expression expr) : expr_{std::move(expr)} {
+  }
+
+  auto update(const table_slice& input, session ctx) -> void override {
+    auto arg = eval(expr_, input, ctx);
+    if (caf::holds_alternative<null_type>(arg.type)) {
+      return;
+    }
+    // XXX: What should happen if it receives different types?
+    // Currently, everything ends up coerced to strings.
+    for (auto i = int64_t{}; i < arg.array->length(); ++i) {
+      if (arg.array->IsNull(i)) {
+        continue;
+      }
+      result_.push_back(materialize(value_at(arg.type, *arg.array, i)));
+    }
+  }
+
+  auto finish() -> data override {
+    return result_;
+  }
+
+private:
+  ast::expression expr_;
+  list result_;
+};
+
+class plugin : public virtual aggregation_function_plugin,
+               public virtual aggregation_plugin {
   auto name() const -> std::string override {
     return "collect";
   };
@@ -65,6 +98,15 @@ class plugin : public virtual aggregation_function_plugin {
       return std::make_unique<collect_function<Type>>(input_type);
     };
     return caf::visit(f, input_type);
+  }
+
+  auto make_aggregation(invocation inv, session ctx) const
+    -> failure_or<std::unique_ptr<aggregation_instance>> override {
+    auto expr = ast::expression{};
+    TRY(argument_parser2::function("tql2.collect")
+          .add(expr, "<expr>")
+          .parse(inv, ctx));
+    return std::make_unique<collect_instance>(std::move(expr));
   }
 
   auto aggregation_default() const -> data override {
