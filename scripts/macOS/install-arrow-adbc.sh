69bbec3eeb9be2919179bc6a40d978c8595be1e0
Johannes Misch <johannes.misch@tenzir.com>
2024-09-19 15:06:41 +0200

    Add explicit build step

--- scripts/macOS/install-arrow-adbc.sh
+++ scripts/macOS/install-arrow-adbc.sh
@@ -8,4 +8,5 @@ brew install --overwrite go
 git clone https://github.com/apache/arrow-adbc.git
 cd arrow-adbc
 cmake -B build c/ -DCMAKE_BUILD_TYPE=Release -DADBC_DRIVER_SNOWFLAKE=ON -DADBC_DRIVER_MANAGER=ON
+cmake --build build
 cmake --install build
