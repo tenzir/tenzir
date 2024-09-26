5a989b15dc7026a0b4bbaaf0db20678e7936ac89
Jannis Christopher Köhl <mail@koehl.dev>
2024-09-26 11:53:57 +0200

    Fix a few things

--- libtenzir/builtins/functions/time.cpp
+++ libtenzir/builtins/functions/time.cpp
@@ -121,22 +121,23 @@ public:
   }
 };
 
-class from_epoch_ns final : public method_plugin {
+class from_epoch_ms final : public function_plugin {
 public:
   auto name() const -> std::string override {
-    return "from_epoch_ns";
+    return "from_epoch_ms";
   }
 
   auto make_function(invocation inv,
                      session ctx) const -> failure_or<function_ptr> override {
     auto expr = ast::expression{};
-    TRY(argument_parser2::function(name()).add(expr, "<int>").parse(inv, ctx));
+    TRY(
+      argument_parser2::function(name()).add(expr, "<number>").parse(inv, ctx));
     return function_use::make([expr = std::move(expr),
                                this](evaluator eval, session ctx) -> series {
       auto arg = eval(expr);
       auto f = detail::overload{
         [](const arrow::NullArray& arg) {
-          return series::null(duration_type{}, arg.length());
+          return series::null(time_type{}, arg.length());
         },
         [&](const arrow::Int64Array& arg) {
           auto b = time_type::make_arrow_builder(arrow::default_memory_pool());
@@ -147,9 +148,10 @@ public:
               continue;
             }
             check(append_builder(
-              time_type{}, *b, time{duration{value_at(int64_type{}, arg, i)}}));
+              time_type{}, *b,
+              time{duration{value_at(int64_type{}, arg, i) * 1'000'000}}));
           }
-          return series{duration_type{}, finish(*b)};
+          return series{time_type{}, finish(*b)};
         },
         [&](const arrow::UInt64Array& arg) {
           auto b = time_type::make_arrow_builder(arrow::default_memory_pool());
@@ -159,11 +161,11 @@ public:
               check(b->AppendNull());
               continue;
             }
-            check(
-              append_builder(time_type{}, *b,
-                             time{duration{value_at(uint64_type{}, arg, i)}}));
+            check(append_builder(
+              time_type{}, *b,
+              time{duration{value_at(uint64_type{}, arg, i) * 1'000'000}}));
           }
-          return series{duration_type{}, finish(*b)};
+          return series{time_type{}, finish(*b)};
         },
         [&](const arrow::DoubleArray& arg) {
           auto b = time_type::make_arrow_builder(arrow::default_memory_pool());
@@ -173,14 +175,15 @@ public:
               check(b->AppendNull());
               continue;
             }
-            check(append_builder(
-              time_type{}, *b,
-              time{duration{int64_t(value_at(double_type{}, arg, i))}}));
+            check(
+              append_builder(time_type{}, *b,
+                             time{duration{int64_t(
+                               value_at(double_type{}, arg, i) * 1'000'000)}}));
           }
-          return series{duration_type{}, finish(*b)};
+          return series{time_type{}, finish(*b)};
         },
         [&](const auto&) {
-          diagnostic::warning("`{}` expected `time`, but got `{}`", name(),
+          diagnostic::warning("`{}` expected number, but got `{}`", name(),
                               arg.type.kind())
             .primary(expr)
             .emit(ctx);
@@ -336,7 +339,7 @@ using namespace tenzir::plugins::time_;
 
 TENZIR_REGISTER_PLUGIN(time_)
 TENZIR_REGISTER_PLUGIN(since_epoch)
-TENZIR_REGISTER_PLUGIN(from_epoch_ns)
+TENZIR_REGISTER_PLUGIN(from_epoch_ms)
 TENZIR_REGISTER_PLUGIN(as_secs)
 TENZIR_REGISTER_PLUGIN(year_month_day{ymd_subtype::year});
 TENZIR_REGISTER_PLUGIN(year_month_day{ymd_subtype::month});
