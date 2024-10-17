c0f7b01c5b2b0be460f86488d855ad1b49ad47ac
Tobias Mayer <tobim@fastmail.fm>
2024-10-17 09:19:13 +0200

    Fix google-cloud-cpp checks on darwin

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -30,6 +30,25 @@ in {
   google-cloud-cpp =
     let
       google-cloud-cpp' = prev.google-cloud-cpp.overrideAttrs (orig: {
+        installCheckPhase = let
+          disabledTests = lib.optionalString stdenv.hostPlatform.isDarwin ''
+            common_internal_async_connection_ready_test
+            bigtable_async_read_stream_test
+            bigtable_metadata_update_policy_test
+            bigtable_bigtable_benchmark_test
+            bigtable_embedded_server_test
+          '';
+        in ''
+          runHook preInstallCheck
+
+          # Disable any integration tests, which need to contact the internet.
+          ctest \
+            --label-exclude integration-test \
+            --exclude-from-file <(echo '${disabledTests}')
+
+          runHook postInstallCheck
+        '';
+
         meta = orig.meta // {
           platforms = lib.platforms.linux ++ lib.platforms.darwin;
         };
@@ -39,7 +58,6 @@ in {
     then google-cloud-cpp'
     else
       google-cloud-cpp'.overrideAttrs (orig: {
-        buildInputs = orig.buildInputs ++ [final.gbenchmark];
         propagatedNativeBuildInputs = (orig.propagatedNativeBuildInputs or [])
         ++ [prev.pkgsBuildBuild.pkg-config];
         patches =
