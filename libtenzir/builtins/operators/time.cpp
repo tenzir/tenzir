4c33b36568fd26a8d991c105c8a268e21ab822ff
Jannis Christopher Köhl <mail@koehl.dev>
2024-11-20 22:57:57 +0100

    Remove `errno` checking from `parse time` as it is not set

--- libtenzir/builtins/operators/time.cpp
+++ libtenzir/builtins/operators/time.cpp
@@ -444,7 +444,6 @@ auto strptime_partial(diagnostic_handler& diag, const char* input,
       return val == std::numeric_limits<T>::max();
     }
   };
-  errno = 0;
   // Using strptime, which is a POSIX function, available both on Linux and
   // macOS.
   //
@@ -460,7 +459,6 @@ auto strptime_partial(diagnostic_handler& diag, const char* input,
   const auto* result = ::strptime(input, format, &time);
   if (not result) {
     diagnostic::error("failed to parse time")
-      .note("strptime error: `{}`", detail::describe_errno(errno))
       .hint("input: `{}`, format: `{}`", input, format)
       .emit(diag);
     return std::nullopt;
