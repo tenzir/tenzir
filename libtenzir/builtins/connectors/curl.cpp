b6013449ec108eb7f943c7399af092f3942152c9
Johannes Misch <johannes.misch@tenzir.com>
2025-05-13 15:26:28 +0200

    Remove loader/saver plugins from TQL1 http&ftp
    This also updates the integration tests.

--- libtenzir/builtins/connectors/curl.cpp
+++ libtenzir/builtins/connectors/curl.cpp
@@ -18,7 +18,6 @@
 
 #include <caf/actor_system_config.hpp>
 
-#include <regex>
 #include <string_view>
 
 using namespace std::chrono_literals;
@@ -85,306 +84,6 @@ auto make_request(const connector_args& args) -> caf::expected<http::request> {
   return result;
 }
 
-template <detail::string_literal Protocol>
-class curl_loader final : public plugin_loader {
-public:
-  curl_loader() = default;
-
-  explicit curl_loader(connector_args args) : args_{std::move(args)} {
-  }
-
-  auto instantiate(operator_control_plane& ctrl) const
-    -> std::optional<generator<chunk_ptr>> override {
-    auto make = [](operator_control_plane& ctrl,
-                   connector_args args) mutable -> generator<chunk_ptr> {
-      auto tx = transfer{args.transfer_opts};
-      auto req = make_request(args);
-      if (not req) {
-        diagnostic::error("failed to construct HTTP request")
-          .note("{}", req.error())
-          .emit(ctrl.diagnostics());
-        co_return;
-      }
-      if (auto err = tx.prepare(*req)) {
-        diagnostic::error("failed to prepare HTTP request")
-          .note("{}", err)
-          .emit(ctrl.diagnostics());
-        co_return;
-      }
-      if (args.http_opts.multipart) {
-        if (req->body.empty()) {
-          diagnostic::warning("ignoring request to send multipart message")
-            .note("HTTP request body is empty")
-            .emit(ctrl.diagnostics());
-        } else {
-          // Move body over to MIME part.
-          auto& easy = tx.handle();
-          auto mime = curl::mime{easy};
-          auto part = mime.add();
-          part.data(as_bytes(req->body));
-          if (auto* header = req->header("Content-Type")) {
-            part.type(header->value);
-            easy.set_http_header("Content-Type", "multipart/form-data");
-          }
-          req->body.clear();
-          auto code = easy.set(std::move(mime));
-          if (code != curl::easy::code::ok) {
-            diagnostic::error("failed to construct HTTP request")
-              .note("{}", req.error())
-              .emit(ctrl.diagnostics());
-            co_return;
-          }
-        }
-      }
-      co_yield {};
-      for (auto&& chunk : tx.download_chunks()) {
-        if (not chunk) {
-          diagnostic::error("failed to download {}", args.url)
-            .hint(fmt::format("{}", chunk.error()))
-            .emit(ctrl.diagnostics());
-          co_return;
-        }
-        co_yield *chunk;
-      }
-    };
-    return make(ctrl, args_);
-  }
-
-  auto name() const -> std::string override {
-    return std::string{Protocol.str()};
-  }
-
-  auto default_parser() const -> std::string override {
-    return "json";
-  }
-
-  friend auto inspect(auto& f, curl_loader& x) -> bool {
-    return f.object(x)
-      .pretty_name("curl_loader")
-      .fields(f.field("args", x.args_));
-  }
-
-private:
-  connector_args args_;
-};
-
-template <detail::string_literal Protocol>
-class curl_saver final : public plugin_saver {
-public:
-  curl_saver() = default;
-
-  explicit curl_saver(connector_args args) : args_{std::move(args)} {
-  }
-
-  auto instantiate(operator_control_plane& ctrl, std::optional<printer_info>)
-    -> caf::expected<std::function<void(chunk_ptr)>> override {
-    auto req = make_request(args_);
-    if (not req) {
-      diagnostic::error("failed to construct HTTP request")
-        .note("{}", req.error())
-        .emit(ctrl.diagnostics());
-      return req.error();
-    }
-    // We're trying to accommodate the most common scenario of getting JSON to
-    // be submitted via a POST request.
-    if (req->method.empty()) {
-      req->method = "POST";
-    }
-    if (not req->body.empty()) {
-      diagnostic::error("found {}-byte HTTP request body", req->body.size())
-        .note("cannot use request body in HTTP saver")
-        .note("pipeline input is the only request body")
-        .hint("remove arguments that create a request body")
-        .emit(ctrl.diagnostics());
-      return caf::make_error(ec::invalid_argument, "bogus operator arguments");
-    }
-    auto tx = std::make_shared<transfer>(args_.transfer_opts);
-    if (auto err = tx->prepare(std::move(*req))) {
-      diagnostic::error("failed to prepare HTTP request")
-        .note("{}", err)
-        .emit(ctrl.diagnostics());
-      return err;
-    }
-    return [&ctrl, args = args_, tx](chunk_ptr chunk) mutable {
-      if (!chunk || chunk->size() == 0) {
-        return;
-      }
-      if (auto err = tx->prepare(chunk)) {
-        diagnostic::error("failed to prepare transfer")
-          .note("chunk size: {}", chunk->size())
-          .note("{}", err)
-          .emit(ctrl.diagnostics());
-        return;
-      }
-      if (auto err = tx->perform()) {
-        diagnostic::error("failed to upload chunk to {}", args.url)
-          .note("{}", err)
-          .emit(ctrl.diagnostics());
-        return;
-      }
-    };
-  }
-
-  auto name() const -> std::string override {
-    return std::string{Protocol.str()};
-  }
-
-  auto default_printer() const -> std::string override {
-    return "json";
-  }
-
-  auto is_joining() const -> bool override {
-    return true;
-  }
-
-  friend auto inspect(auto& f, curl_saver& x) -> bool {
-    return f.object(x)
-      .pretty_name("curl_saver")
-      .fields(f.field("args", x.args_));
-  }
-
-private:
-  connector_args args_;
-};
-
-template <detail::string_literal Protocol>
-class plugin final : public virtual loader_plugin<curl_loader<Protocol>>,
-                     public virtual saver_plugin<curl_saver<Protocol>> {
-public:
-  static auto protocol() -> std::string {
-    return std::string{Protocol.str()};
-  }
-
-  auto parse_loader(parser_interface& p) const
-    -> std::unique_ptr<plugin_loader> override {
-    return std::make_unique<curl_loader<Protocol>>(parse_args(p));
-  }
-
-  auto parse_saver(parser_interface& p) const
-    -> std::unique_ptr<plugin_saver> override {
-    return std::make_unique<curl_saver<Protocol>>(parse_args(p));
-  }
-
-  auto name() const -> std::string override {
-    return protocol();
-  }
-
-private:
-  /// Auto-completes a scheme-less URL with the scheme from this plugin.
-  static auto auto_complete(std::string_view url) -> std::string {
-    if (url.find("://") != std::string_view::npos) {
-      return std::string{url};
-    }
-    return fmt::format("{}://{}", Protocol.str(), url);
-  }
-
-  static auto parse_args(parser_interface& p) -> connector_args {
-    auto result = connector_args{};
-    result.transfer_opts.default_protocol = protocol();
-    // For HTTP and HTTPS the desired CLI UX is HTTPie:
-    //
-    //     [<method>] <url> [<item>..]
-    //
-    // Please see `man http` for an explanation of the desired outcome.
-    if (protocol() == "http" || protocol() == "https") {
-      // Collect all arguments first until `argument_parser` becomes mightier.
-      auto args = std::vector<located<std::string>>{};
-      // Process options here manually until argument_parser becomes more
-      // powerful.
-      while (auto arg = p.accept_shell_arg()) {
-        if (arg->inner == "-v" || arg->inner == "--verbose") {
-          result.transfer_opts.verbose = true;
-        } else if (arg->inner == "-j" || arg->inner == "--json") {
-          result.http_opts.json = true;
-        } else if (arg->inner == "-f" || arg->inner == "--form") {
-          result.http_opts.form = true;
-        } else if (arg->inner == "--chunked") {
-          result.http_opts.chunked = true;
-        } else if (arg->inner == "--multipart") {
-          result.http_opts.multipart = true;
-          // TODO: factor these TLS options in the future, as they apply to many
-          // connectors, such as email.
-        } else if (arg->inner == "-P"
-                   || arg->inner == "--skip-peer-verification") {
-          result.transfer_opts.ssl.skip_peer_verification = location::unknown;
-        } else if (arg->inner == "-H"
-                   || arg->inner == "--skip-hostname-verification") {
-          result.transfer_opts.ssl.skip_peer_verification = location::unknown;
-        } else {
-          args.push_back(std::move(*arg));
-        }
-      }
-      TENZIR_DEBUG("parsed shell arguments:");
-      for (auto i = 0u; i < args.size(); ++i) {
-        TENZIR_DEBUG("- args[{}] = {}", i, args[i].inner);
-      }
-      if (args.empty()) {
-        diagnostic::error("no URL provided").throw_();
-      }
-      // No ambiguity, just go with <url>.
-      if (args.size() == 1) {
-        result.url = auto_complete(args[0].inner);
-        return result;
-      }
-      TENZIR_ASSERT(args.size() >= 2);
-      // Try <method> <url> [<item>..]
-      auto method_regex = std::regex{"[a-zA-Z]+"};
-      if (std::regex_match(args[0].inner, method_regex)) {
-        // FIXME: find a strategy to deal with some false positives here, e.g.,
-        // "localhost" should be interepreted as URL and not HTTP method.
-        TENZIR_DEBUG("detected syntax: <method> <url> [<item>..]");
-        result.http_opts.method = std::move(args[0].inner);
-        result.url = auto_complete(args[1].inner);
-        args.erase(args.begin());
-        args.erase(args.begin());
-        for (auto& arg : args) {
-          if (auto item = http::request_item::parse(arg.inner)) {
-            result.http_opts.items.push_back(std::move(*item));
-          } else {
-            diagnostic::error("invalid HTTP request item")
-              .primary(arg.source)
-              .note("{}", arg.inner)
-              .throw_();
-          }
-        }
-        return result;
-      }
-      TENZIR_DEBUG("trying last possible syntax: <url> <item> [<item>..]");
-      result.url = auto_complete(args[0].inner);
-      args.erase(args.begin());
-      for (auto& arg : args) {
-        if (auto item = http::request_item::parse(arg.inner)) {
-          result.http_opts.items.push_back(std::move(*item));
-        } else {
-          diagnostic::error("invalid HTTP request item")
-            .primary(arg.source)
-            .note("{}", arg.inner)
-            .throw_();
-        }
-      }
-      return result;
-    } else {
-      auto parser = argument_parser{
-        protocol(),
-        fmt::format("https://docs.tenzir.com/connectors/{}", protocol())};
-      parser.add("-v,--verbose", result.transfer_opts.verbose);
-      parser.add(result.url, "<url>");
-      parser.parse(p);
-    }
-    return result;
-  }
-};
-
-// Available protocol names according to the documentation at
-// https://curl.se/libcurl/c/CURLOPT_DEFAULT_PROTOCOL.html are: dict, file, ftp,
-// ftps, gopher, http, https, imap, imaps, ldap, ldaps, pop3, pop3s, rtsp, scp,
-// sftp, smb, smbs, smtp, smtps, telnet, tftp
-
-using ftp = plugin<"ftp">;
-using ftps = plugin<"ftps">;
-using http = plugin<"http">;
-using https = plugin<"https">;
-
 class load_http_operator final : public crtp_operator<load_http_operator> {
 public:
   load_http_operator() = default;
@@ -400,11 +99,54 @@ public:
     // TODO: Clean this up.
     auto args = args_;
     args.transfer_opts.ssl.update_cacert(ctrl);
-    auto loader = curl_loader<"TODO: not using this">{args};
-    auto gen = loader.instantiate(ctrl);
-    TENZIR_ASSERT(gen);
-    for (auto chunk : *gen) {
-      co_yield std::move(chunk);
+    auto tx = transfer{args.transfer_opts};
+    auto req = make_request(args);
+    if (not req) {
+      diagnostic::error("failed to construct HTTP request")
+        .note("{}", req.error())
+        .emit(ctrl.diagnostics());
+      co_return;
+    }
+    if (auto err = tx.prepare(*req)) {
+      diagnostic::error("failed to prepare HTTP request")
+        .note("{}", err)
+        .emit(ctrl.diagnostics());
+      co_return;
+    }
+    if (args.http_opts.multipart) {
+      if (req->body.empty()) {
+        diagnostic::warning("ignoring request to send multipart message")
+          .note("HTTP request body is empty")
+          .emit(ctrl.diagnostics());
+      } else {
+        // Move body over to MIME part.
+        auto& easy = tx.handle();
+        auto mime = curl::mime{easy};
+        auto part = mime.add();
+        part.data(as_bytes(req->body));
+        if (auto* header = req->header("Content-Type")) {
+          part.type(header->value);
+          easy.set_http_header("Content-Type", "multipart/form-data");
+        }
+        req->body.clear();
+        auto code = easy.set(std::move(mime));
+        if (code != curl::easy::code::ok) {
+          diagnostic::error("failed to construct HTTP request")
+            .note("{}", req.error())
+            .emit(ctrl.diagnostics());
+          co_return;
+        }
+      }
+    }
+    co_yield {};
+    for (auto&& chunk : tx.download_chunks()) {
+      if (not chunk) {
+        diagnostic::error("failed to download {}", args.url)
+          .hint(fmt::format("{}", chunk.error()))
+          .emit(ctrl.diagnostics());
+        co_return;
+      }
+      co_yield *chunk;
     }
   }
 
@@ -416,8 +158,8 @@ public:
     return true;
   }
 
-  auto optimize(expression const& filter,
-                event_order order) const -> optimize_result override {
+  auto optimize(expression const& filter, event_order order) const
+    -> optimize_result override {
     TENZIR_UNUSED(filter, order);
     return do_not_optimize(*this);
   }
@@ -442,21 +184,50 @@ public:
   }
 
   auto
-  operator()(generator<chunk_ptr> input,
-             operator_control_plane& ctrl) const -> generator<std::monostate> {
+  operator()(generator<chunk_ptr> input, operator_control_plane& ctrl) const
+    -> generator<std::monostate> {
     // TODO: Clean this up.
     auto args = args_;
     args.transfer_opts.ssl.update_cacert(ctrl);
-    auto saver = curl_saver<"TODO: not using this">{args};
-    auto func = saver.instantiate(ctrl, std::nullopt);
-    if (not func) {
-      diagnostic::error(func.error()).emit(ctrl.diagnostics());
-      co_return;
+    auto req = make_request(args_);
+    if (not req) {
+      diagnostic::error("failed to construct HTTP request")
+        .note("{}", req.error())
+        .emit(ctrl.diagnostics());
     }
-    co_yield {};
+    // We're trying to accommodate the most common scenario of getting JSON to
+    // be submitted via a POST request.
+    if (req->method.empty()) {
+      req->method = "POST";
+    }
+    if (not req->body.empty()) {
+      diagnostic::error("found {}-byte HTTP request body", req->body.size())
+        .note("cannot use request body in HTTP saver")
+        .note("pipeline input is the only request body")
+        .hint("remove arguments that create a request body")
+        .emit(ctrl.diagnostics());
+    }
+    auto tx = std::make_shared<transfer>(args_.transfer_opts);
+    if (auto err = tx->prepare(std::move(*req))) {
+      diagnostic::error("failed to prepare HTTP request")
+        .note("{}", err)
+        .emit(ctrl.diagnostics());
+    }
     for (auto chunk : input) {
-      (*func)(std::move(chunk));
-      co_yield {};
+      if (! chunk || chunk->size() == 0) {
+        co_yield {};
+      }
+      if (auto err = tx->prepare(chunk)) {
+        diagnostic::error("failed to prepare transfer")
+          .note("chunk size: {}", chunk->size())
+          .note("{}", err)
+          .emit(ctrl.diagnostics());
+      }
+      if (auto err = tx->perform()) {
+        diagnostic::error("failed to upload chunk to {}", args.url)
+          .note("{}", err)
+          .emit(ctrl.diagnostics());
+      }
     }
   }
 
@@ -593,6 +364,7 @@ auto parse_ftp_args(std::string name,
                     session ctx) -> failure_or<connector_args> {
   auto args = connector_args{};
   auto parser = argument_parser2::operator_(name);
+  parser.positional("url", args.url);
   args.transfer_opts.ssl.add_tls_options(parser);
   TRY(parser.parse(inv, ctx));
   if (not(args.url.starts_with("ftp://") or args.url.starts_with("ftps://"))) {
@@ -647,10 +419,6 @@ public:
 
 } // namespace tenzir::plugins
 
-TENZIR_REGISTER_PLUGIN(tenzir::plugins::ftp)
-TENZIR_REGISTER_PLUGIN(tenzir::plugins::ftps)
-TENZIR_REGISTER_PLUGIN(tenzir::plugins::http)
-TENZIR_REGISTER_PLUGIN(tenzir::plugins::https)
 TENZIR_REGISTER_PLUGIN(tenzir::plugins::load_http_plugin)
 TENZIR_REGISTER_PLUGIN(tenzir::plugins::save_http_plugin)
 TENZIR_REGISTER_PLUGIN(tenzir::plugins::load_ftp_plugin)
