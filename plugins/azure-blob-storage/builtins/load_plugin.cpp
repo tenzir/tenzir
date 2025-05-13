65e82571764c0fc6b863e35e9e92675c008d8dc9
Johannes Misch <johannes.misch@tenzir.com>
2025-05-13 12:39:41 +0200

    Remove loader/saver adaptors from Azure Blob Storage

--- plugins/azure-blob-storage/builtins/load_plugin.cpp
+++ plugins/azure-blob-storage/builtins/load_plugin.cpp
@@ -8,9 +8,19 @@
 
 #include <tenzir/tql2/plugin.hpp>
 
-#include "loader.hpp"
+#include <arrow/filesystem/azurefs.h>
+#include <arrow/filesystem/filesystem.h>
+#include <arrow/filesystem/type_fwd.h>
+#include <arrow/io/api.h>
+#include <arrow/util/uri.h>
+#include <fmt/core.h>
 
 namespace tenzir::plugins::abs {
+
+// We use 2^20 for the upper bound of a chunk size, which exactly matches the
+// upper limit defined by execution nodes for transporting events.
+// TODO: Get the backpressure-adjusted value at runtime from the execution node.
+constexpr size_t max_chunk_size = 1 << 20;
 
 class load_abs_operator final : public crtp_operator<load_abs_operator> {
 public:
@@ -20,13 +30,52 @@ public:
   }
 
   auto operator()(operator_control_plane& ctrl) const -> generator<chunk_ptr> {
-    auto loader = abs_loader{uri_};
-    auto instance = loader.instantiate(ctrl);
-    if (not instance) {
+    auto path = std::string{};
+    auto opts = arrow::fs::AzureOptions::FromUri(uri_.inner, &path);
+    if (not opts.ok()) {
+      diagnostic::error("failed to create Arrow Azure Blob Storage "
+                        "filesystem: {}",
+                        opts.status().ToString())
+        .emit(ctrl.diagnostics());
       co_return;
     }
-    for (auto&& chunk : *instance) {
-      co_yield std::move(chunk);
+    auto fs = arrow::fs::AzureFileSystem::Make(*opts);
+    if (not fs.ok()) {
+      diagnostic::error("failed to create Arrow Azure Blob Storage "
+                        "filesystem: {}",
+                        fs.status().ToString())
+        .emit(ctrl.diagnostics());
+      co_return;
+    }
+    auto file_info = fs.ValueUnsafe()->GetFileInfo(path);
+    if (not file_info.ok()) {
+      diagnostic::error("failed to get file info from path {}",
+                        file_info.status().ToString())
+        .primary(uri_)
+        .emit(ctrl.diagnostics());
+      co_return;
+    }
+    auto input_stream = fs.ValueUnsafe()->OpenInputStream(*file_info);
+    if (not input_stream.ok()) {
+      diagnostic::error("failed to open input stream: {}",
+                        input_stream.status().ToString())
+        .primary(uri_)
+        .emit(ctrl.diagnostics());
+      co_return;
+    }
+    while (not input_stream.ValueUnsafe()->closed()) {
+      auto buffer = input_stream.ValueUnsafe()->Read(max_chunk_size);
+      if (not input_stream.ok()) {
+        diagnostic::error("failed to read from input stream: {}",
+                          buffer.status().ToString())
+          .primary(uri_)
+          .emit(ctrl.diagnostics());
+        co_return;
+      }
+      if (buffer.ValueUnsafe()->size() == 0) {
+        break;
+      }
+      co_yield chunk::make(buffer.MoveValueUnsafe());
     }
   }
 
