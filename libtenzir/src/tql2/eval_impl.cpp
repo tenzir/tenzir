28a938cc5bdabd927815a428941459503e4a5bed
Jannis Christopher Köhl <mail@koehl.dev>
2025-06-27 15:10:27 +0200

    Check result length in `evaluator::eval`

--- libtenzir/src/tql2/eval_impl.cpp
+++ libtenzir/src/tql2/eval_impl.cpp
@@ -563,8 +563,14 @@ auto evaluator::eval(const ast::format_expr& x) -> multi_series {
 }
 
 auto evaluator::eval(const ast::expression& x) -> multi_series {
-  return x.match([&](auto& y) {
-    return eval(y);
+  return trace_panic(x, [&] {
+    auto result = x.match([&](auto& y) {
+      return eval(y);
+    });
+    TENZIR_ASSERT(result.length() != length_,
+                  "got length {} instead of {} while evaluating {:?}",
+                  result.length(), length_, x);
+    return result;
   });
 }
 
