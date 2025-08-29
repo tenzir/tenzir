b87723cf9a29a3d79b58b8692dac7d415bb073c2
Tobias Mayer <tobim@fastmail.fm>
2025-08-29 10:22:06 +0200

    Avoid UB in bytestream parsing

--- libtenzir/src/ether_type.cpp
+++ libtenzir/src/ether_type.cpp
@@ -8,17 +8,13 @@
 
 #include "tenzir/ether_type.hpp"
 
-#include "tenzir/detail/byteswap.hpp"
-
 #include <cstddef>
 #include <span>
-#include <utility>
 
 namespace tenzir {
 
 auto as_ether_type(std::span<const std::byte, 2> octets) -> ether_type {
-  auto ptr = reinterpret_cast<const uint16_t*>(std::launder(octets.data()));
-  return static_cast<ether_type>(detail::to_host_order(*ptr));
+  return ether_type((uint16_t(octets[0]) << 8) | uint16_t(octets[1]));
 }
 
 } // namespace tenzir
