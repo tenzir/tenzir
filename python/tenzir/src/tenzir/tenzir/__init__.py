8471d9bb79518231a1adb443d998f2c2813cd4e2
Tobias Mayer <tobim@fastmail.fm>
2025-10-12 07:00:08 +0200

    Revamp python bindings

--- python/tenzir/src/tenzir/tenzir/__init__.py
+++ python/tenzir/src/tenzir/tenzir/__init__.py
@@ -1,2 +1,15 @@
-from .tenzir import Tenzir, ExportMode, TableSlice
-from .convert import *
+from .tenzir import (  # noqa: F401
+    AsyncRecordBatchStreamReader,
+    CompletedPipeline,
+    PipelineError,
+    PipelineIO,
+    PipelineOptions,
+    PipelineRun,
+    PipelineSpec,
+    PyArrowTableSlice,
+    TableSlice,
+    run_pipeline,
+    run_pipeline_sync,
+    stream_pipeline,
+)
+from .convert import *  # noqa: F401,F403
