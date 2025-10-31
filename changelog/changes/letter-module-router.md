9323bcdbbf8114b2a319773890b804955976c04d
Johannes Misch <johannes.misch@tenzir.com>
2025-10-31 21:50:40 +0100

    Expand and document `metrics "memory"`

---
+++ changelog/changes/letter-module-router.md
@@ -0,0 +1,24 @@
+---
+title: "Improved memory allocation & metrics"
+type: feature
+authors: IyeOnline
+pr: [5512,5544]
+---
+
+We have expanded the `metrics "memory"` the application provides. When enabled,
+you can now see metrics for memory allocated via different means. Since this
+metrics collection affects every single allocation, it is currently disabled
+by default. To enable collection of these statistics, you can set an
+environment variable `TENZIR_ALLOC_STATS=true`.
+
+:::info[Changed metric layout]
+This change also changes the structure of `tenzir.metrics.memory`. The
+system-wide stats `total_bytes`, `free_bytes` and `used_bytes` are now grouped
+under a `system` key to differentiate them from the `process` memory usage and
+per-component memory usage added in this release. These process metrics were
+previously only found in `metrics.process`.
+:::
+
+We also switched the default memory allocator used on all
+platforms to [mimalloc](https://github.com/microsoft/mimalloc), which may
+reduce the effective memory usage of Tenzir.
--- libtenzir/builtins/metrics/memory.cpp
+++ libtenzir/builtins/metrics/memory.cpp
@@ -6,9 +6,9 @@
 // SPDX-FileCopyrightText: (c) 2023 The Tenzir Contributors
 // SPDX-License-Identifier: BSD-3-Clause
 
-#include "tenzir/allocator.hpp"
-
+#include <tenzir/allocator.hpp>
 #include <tenzir/arrow_memory_pool.hpp>
+#include <tenzir/os.hpp>
 #include <tenzir/plugin.hpp>
 #include <tenzir/type.hpp>
 
@@ -46,60 +46,94 @@ auto make_from(const memory::stats& stats) -> record {
   return result;
 };
 
+auto make_process_statistics() -> record {
+  static auto os = os::make();
+  auto result = record{};
+  if (not os) {
+    return result;
+  }
+  const auto process = os->current_process();
+  result.reserve(3);
+  auto assign_optional = [&result](std::string_view key, auto value) {
+    if (value) {
+      result.try_emplace(key, *value);
+    } else {
+      result.try_emplace(key, caf::none);
+    }
+  };
+  assign_optional("peak_bytes", process.peak_mem);
+  assign_optional("process_bytes", process.rsize);
+  assign_optional("swap_bytes", process.swap);
+  return result;
+}
+
 #ifdef _SC_AVPHYS_PAGES
 
-auto get_raminfo() -> caf::expected<record> {
+auto make_system_info() -> record {
   static const auto pagesize = ::sysconf(_SC_PAGESIZE);
   const auto phys_pages = ::sysconf(_SC_PHYS_PAGES);
   const auto available_pages = ::sysconf(_SC_AVPHYS_PAGES);
   const auto total_bytes = phys_pages * pagesize;
   const auto free_bytes = available_pages * pagesize;
   auto result = record{};
-  result.reserve(3 + 3);
+  result.reserve(3);
   result.try_emplace("total_bytes", total_bytes);
   result.try_emplace("free_bytes", free_bytes);
   result.try_emplace("used_bytes", total_bytes - free_bytes);
-  result.try_emplace("arrow", make_from(memory::arrow_allocator().stats()));
-  result.try_emplace("cpp", make_from(memory::cpp_allocator().stats()));
-  result.try_emplace("c", make_from(memory::c_allocator().stats()));
   return result;
 }
 
 #elif __has_include(<mach/mach.h>)
 
-auto get_raminfo() -> caf::expected<record> {
+auto make_system_info() -> record {
   static const auto page_size = getpagesize();
   auto host_count = mach_msg_type_number_t{HOST_BASIC_INFO_COUNT};
   auto host = host_basic_info_data_t{};
   if (KERN_SUCCESS
       != host_info(mach_host_self(), HOST_BASIC_INFO, (host_info_t)&host,
                    &host_count)) {
-    return caf::make_error(ec::system_error,
-                           "failed to get mach host basic info");
+    auto result = record{};
+    result.reserve(3);
+    result.try_emplace("total_bytes", caf::none);
+    result.try_emplace("free_bytes", caf::none);
+    result.try_emplace("used_bytes", caf::none);
+    return result;
   }
   auto vm_count = mach_msg_type_number_t{HOST_VM_INFO64_COUNT};
   auto vm = vm_statistics64_data_t{};
   if (KERN_SUCCESS
       != host_statistics64(mach_host_self(), HOST_VM_INFO64, (host_info_t)&vm,
                            &vm_count)) {
-    return caf::make_error(ec::system_error,
-                           "failed to get mach vm statistics");
+    auto result = record{};
+    result.reserve(3);
+    result.try_emplace("total_bytes", caf::none);
+    result.try_emplace("free_bytes", caf::none);
+    result.try_emplace("used_bytes", caf::none);
+    return result;
   }
   const auto total_bytes = static_cast<uint64_t>(host.max_mem);
   const auto free_bytes
     = static_cast<uint64_t>(vm.free_count + vm.inactive_count) * page_size;
   auto result = record{};
-  result.reserve(3 + 3);
+  result.reserve(3);
   result.try_emplace("total_bytes", total_bytes);
   result.try_emplace("free_bytes", free_bytes);
   result.try_emplace("used_bytes", total_bytes - free_bytes);
+  return result;
+}
+
+#endif
+
+auto get_raminfo() -> caf::expected<record> {
+  auto result = record{};
+  result.reserve(5);
+  result.try_emplace("system", make_system_info());
+  result.try_emplace("process", make_process_statistics());
   result.try_emplace("arrow", make_from(memory::arrow_allocator().stats()));
   result.try_emplace("cpp", make_from(memory::cpp_allocator().stats()));
   result.try_emplace("c", make_from(memory::c_allocator().stats()));
   return result;
 }
-
-#endif
 
 class plugin final : public virtual metrics_plugin {
 public:
@@ -134,11 +168,21 @@ public:
       {"allocations", stats},
     };
     return record_type{{
-      {"total_bytes", uint64_type{}},
-      {"free_bytes", uint64_type{}},
-      {"used_bytes", uint64_type{}},
-      {"cpp", bytes_and_allocations},
+      {"system",
+       record_type{
+         {"total_bytes", uint64_type{}},
+         {"free_bytes", uint64_type{}},
+         {"used_bytes", uint64_type{}},
+       }},
+      {"process",
+       record_type{
+         {"peak_bytes", uint64_type{}},
+         {"current_bytes", uint64_type{}},
+         {"swap_bytes", uint64_type{}},
+       }},
       {"arrow", bytes_and_allocations},
+      {"cpp", bytes_and_allocations},
+      {"c", bytes_and_allocations},
     }};
   }
 };
