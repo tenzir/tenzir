f972e792f3315f01eb7312c4132691641cdd200d
Tobias Mayer <tobim@fastmail.fm>
2025-11-12 09:03:17 +0100

    Use jemalloc by default in the nix build

--- nix/tenzir/default.nix
+++ nix/tenzir/default.nix
@@ -283,6 +283,7 @@ let
             [
               "-DCMAKE_FIND_PACKAGE_PREFER_CONFIG=ON"
               "-DCAF_ROOT_DIR=${caf}"
+              "-DTENZIR_ALLOCATOR=jemalloc"
               "-DTENZIR_ENABLE_RELOCATABLE_INSTALLATIONS=ON"
               "-DTENZIR_ENABLE_MANPAGES=OFF"
               "-DTENZIR_ENABLE_BUNDLED_AND_PATCHED_RESTINIO=OFF"
@@ -314,7 +315,6 @@ let
               "-DCMAKE_INTERPROCEDURAL_OPTIMIZATION:BOOL=ON"
               "-DCPACK_GENERATOR=${if stdenv.hostPlatform.isDarwin then "TGZ;productbuild" else "TGZ;DEB;RPM"}"
               "-DTENZIR_UV_PATH:STRING=${lib.getExe uv-bin}"
-              "-DTENZIR_ALLOCATOR=mimalloc"
               "-DTENZIR_ENABLE_STATIC_EXECUTABLE:BOOL=ON"
               "-DTENZIR_PACKAGE_FILE_NAME_SUFFIX=static"
             ]
