885184485dc5d28ad14da598d9d7ff68a4b1e1eb
Jannis Christopher Köhl <mail@koehl.dev>
2025-11-18 10:55:12 +0100

    Update copyright notice year

--- libtenzir/builtins/operators/block.cpp
+++ libtenzir/builtins/operators/block.cpp
@@ -3,7 +3,7 @@
 //   | |/ / __ |_\ \  / /          Across
 //   |___/_/ |_/___/ /_/       Space and Time
 //
-// SPDX-FileCopyrightText: (c) 2021 The Tenzir Contributors
+// SPDX-FileCopyrightText: (c) 2025 The Tenzir Contributors
 // SPDX-License-Identifier: BSD-3-Clause
 
 #include <tenzir/argument_parser.hpp>
