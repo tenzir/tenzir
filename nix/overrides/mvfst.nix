1847061a3be14dd9ceb9f17ff42e06681c35001c
Matthias Vallentin <matthias@vallentin.net>
2026-02-13 23:29:14 +0100

    fix mvfst static lzma and x86 SIMD flags in nix

--- nix/overrides/mvfst.nix
+++ nix/overrides/mvfst.nix
@@ -1,6 +1,9 @@
 {
   fetchFromGitHub,
+  lib,
+  stdenv,
   mvfst,
+  xz,
 }:
 let
   facebookNetworkStack = import ../facebook-network-stack.nix;
@@ -24,4 +27,17 @@ mvfst.overrideAttrs (orig: {
       printf 'install(TARGETS mvfst_dsr_backend)\n' >> quic/dsr/CMakeLists.txt
     fi
   '';
+  env =
+    let
+      origEnv = orig.env or { };
+    in
+    origEnv
+    // {
+      NIX_LDFLAGS = (origEnv.NIX_LDFLAGS or "") + lib.optionalString stdenv.hostPlatform.isStatic " -L${xz.out}/lib -llzma";
+    };
+  preConfigure =
+    (orig.preConfigure or "")
+    + lib.optionalString stdenv.hostPlatform.isx86_64 ''
+      cmakeFlagsArray+=("-DCMAKE_CXX_FLAGS=-msse -msse2 -msse3 -mssse3 -msse4.1 -msse4.2 -mavx -mavx2")
+    '';
 })
