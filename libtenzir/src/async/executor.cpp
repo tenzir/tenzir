3da2fe7de817ded17842f498cfe74d03561cfe21
Matthias Vallentin <matthias@vallentin.net>
2026-02-19 11:43:35 +0100

    Fix run_chain explicit instantiation signatures

--- libtenzir/src/async/executor.cpp
+++ libtenzir/src/async/executor.cpp
@@ -1427,27 +1427,24 @@ template auto run_chain(OperatorChain<void, table_slice> chain,
                         Box<Push<OperatorMsg<table_slice>>> push_downstream,
                         Receiver<FromControl> from_control,
                         Sender<ToControl> to_control, PipeId id,
-                        ChannelFactory& channel_factory,
-                        caf::actor_system& sys, DiagHandler& dh)
-  -> Task<void>;
+                        ExecCtx& exec_ctx, caf::actor_system& sys,
+                        DiagHandler& dh) -> Task<void>;
 
 template auto run_chain(OperatorChain<chunk_ptr, table_slice> chain,
                         Box<Pull<OperatorMsg<chunk_ptr>>> pull_upstream,
                         Box<Push<OperatorMsg<table_slice>>> push_downstream,
                         Receiver<FromControl> from_control,
                         Sender<ToControl> to_control, PipeId id,
-                        ChannelFactory& channel_factory,
-                        caf::actor_system& sys, DiagHandler& dh)
-  -> Task<void>;
+                        ExecCtx& exec_ctx, caf::actor_system& sys,
+                        DiagHandler& dh) -> Task<void>;
 
 template auto run_chain(OperatorChain<table_slice, table_slice> chain,
                         Box<Pull<OperatorMsg<table_slice>>> pull_upstream,
                         Box<Push<OperatorMsg<table_slice>>> push_downstream,
                         Receiver<FromControl> from_control,
                         Sender<ToControl> to_control, PipeId id,
-                        ChannelFactory& channel_factory,
-                        caf::actor_system& sys, DiagHandler& dh)
-  -> Task<void>;
+                        ExecCtx& exec_ctx, caf::actor_system& sys,
+                        DiagHandler& dh) -> Task<void>;
 
 /// Run a potentially-open pipeline without external control.
 template <class Output>
