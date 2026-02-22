40f614e916c2a228ed68c24b0bee476dc138bb6c
Tobias Mayer <tobim@fastmail.fm>
2026-02-22 17:09:25 +0100

    Fix mold link in presence of unused foreign lto objects

--- nix/overrides/mold.nix
+++ nix/overrides/mold.nix
@@ -5,5 +5,6 @@ mold-unwrapped.overrideAttrs (orig: {
   patches = (orig.patches or [ ]) ++ [
     ./mold-comdat-lto.patch
     ./mold-gcc-fat-lto-fallback.patch
+    ./mold-gcc-slim-archive-lto.patch
   ];
 })
