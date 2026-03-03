04ce3e7f34edbcc814f0703a9e153813cb6f93fd
Tobias Mayer <tobim@fastmail.fm>
2026-03-03 18:52:50 +0100

    Fix export remainder filtering with shared filter helper

--- libtenzir/builtins/operators/where_map.cpp
+++ libtenzir/builtins/operators/where_map.cpp
@@ -31,6 +31,7 @@
 #include <tenzir/tql/basic.hpp>
 #include <tenzir/tql2/ast.hpp>
 #include <tenzir/tql2/eval.hpp>
+#include <tenzir/tql2/filter.hpp>
 #include <tenzir/tql2/plugin.hpp>
 #include <tenzir/tql2/set.hpp>
 #include <tenzir/try.hpp>
@@ -144,47 +145,6 @@ public:
     return std::make_unique<where_operator>(std::move(expr));
   }
 };
-
-auto filter2(const table_slice& slice, const ast::expression& expr,
-             diagnostic_handler& dh, bool warn) -> std::vector<table_slice> {
-  auto results = std::vector<table_slice>{};
-  auto offset = int64_t{0};
-  for (auto& filter : eval(expr, slice, dh)) {
-    auto array = try_as<arrow::BooleanArray>(&*filter.array);
-    if (not array) {
-      diagnostic::warning("expected `bool`, got `{}`", filter.type.kind())
-        .primary(expr)
-        .emit(dh);
-      offset += filter.array->length();
-      continue;
-    }
-    if (array->true_count() == array->length()) {
-      results.push_back(subslice(slice, offset, offset + array->length()));
-      offset += array->length();
-      continue;
-    }
-    if (warn) {
-      diagnostic::warning("assertion failure").primary(expr).emit(dh);
-    }
-    auto length = array->length();
-    auto current_value = array->Value(0);
-    auto current_begin = int64_t{0};
-    // We add an artificial `false` at index `length` to flush.
-    for (auto i = int64_t{1}; i < length + 1; ++i) {
-      const auto next = i != length && array->IsValid(i) && array->Value(i);
-      if (current_value == next) {
-        continue;
-      }
-      if (current_value) {
-        results.push_back(subslice(slice, offset + current_begin, offset + i));
-      }
-      current_value = next;
-      current_begin = i;
-    }
-    offset += length;
-  }
-  return results;
-}
 
 class where_assert_operator final
   : public crtp_operator<where_assert_operator> {
