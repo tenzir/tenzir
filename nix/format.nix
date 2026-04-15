88d07d5cae27b1cd1cc94b0c16ff67ef35a03b66
Matthias Vallentin <matthias@vallentin.net>
2026-04-15 09:42:29 +0200

    Fix single-file nix fmt with plugin submodule

--- nix/format.nix
+++ nix/format.nix
@@ -8,17 +8,40 @@ pkgs.writeShellScriptBin "format" ''
 
   treefmt=${treefmtEval.config.package}/bin/treefmt
   config_file=${treefmtEval.config.build.configFile}
+  submodule_prefix=contrib/tenzir-plugins
+  submodule_root="$PWD/$submodule_prefix"
 
   "$treefmt" \
     --config-file="$config_file" \
     --tree-root-file=${treefmtEval.config.projectRootFile} \
     "$@"
 
-  if [ -f contrib/tenzir-plugins/README.md ]; then
-    "$treefmt" \
-      --config-file="$config_file" \
-      --working-dir=contrib/tenzir-plugins \
-      --tree-root-file=README.md \
-      "$@"
+  if [ -f "$submodule_prefix/README.md" ]; then
+    submodule_args=()
+
+    for arg in "$@"; do
+      case "$arg" in
+        "$submodule_root")
+          submodule_args+=(.)
+          ;;
+        "$submodule_root"/*)
+          submodule_args+=("''${arg#"$submodule_root"/}")
+          ;;
+        "$submodule_prefix")
+          submodule_args+=(.)
+          ;;
+        "$submodule_prefix"/*)
+          submodule_args+=("''${arg#"$submodule_prefix"/}")
+          ;;
+      esac
+    done
+
+    if [ "$#" -eq 0 ] || [ "''${#submodule_args[@]}" -gt 0 ]; then
+      "$treefmt" \
+        --config-file="$config_file" \
+        --working-dir="$submodule_prefix" \
+        --tree-root-file=README.md \
+        "''${submodule_args[@]}"
+    fi
   fi
 ''
