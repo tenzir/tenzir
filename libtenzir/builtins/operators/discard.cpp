ae35f9b4d18ff0846cc441904501f57f65044f46
Jannis Christopher Köhl <mail@koehl.dev>
2026-04-21 16:06:39 +0200

    Update `discard` to use `Describer`

--- libtenzir/builtins/operators/discard.cpp
+++ libtenzir/builtins/operators/discard.cpp
@@ -9,6 +9,7 @@
 #include "tenzir/async.hpp"
 #include "tenzir/compile_ctx.hpp"
 #include "tenzir/ir.hpp"
+#include "tenzir/operator_plugin.hpp"
 #include "tenzir/substitute_ctx.hpp"
 #include "tenzir/view3.hpp"
 
@@ -52,9 +53,15 @@ public:
   }
 };
 
+struct DiscardArgs {};
+
 template <class Input>
 class Discard final : public Operator<Input, void> {
 public:
+  explicit Discard(DiscardArgs args) {
+    TENZIR_UNUSED(args);
+  }
+
   auto start(OpCtx& ctx) -> Task<void> override {
     write_bytes_counter_ = ctx.make_counter(
       MetricsLabel{
@@ -82,61 +89,9 @@ private:
   MetricsCounter write_bytes_counter_;
 };
 
-class DiscardIr final : public ir::Operator {
-public:
-  DiscardIr() = default;
-
-  auto name() const -> std::string override {
-    return "DiscardIr";
-  }
-
-  auto substitute(substitute_ctx ctx, bool instantiate)
-    -> failure_or<void> override {
-    TENZIR_UNUSED(ctx, instantiate);
-    return {};
-  }
-
-  auto spawn(element_type_tag input) and -> AnyOperator override {
-    return input.match([]<class T>(tag<T>) -> AnyOperator {
-      if constexpr (std::is_void_v<T>) {
-        TENZIR_UNREACHABLE();
-      } else {
-        return Discard<T>{};
-      }
-    });
-  }
-
-  auto optimize(ir::optimize_filter filter,
-                event_order order) && -> ir::optimize_result override {
-    TENZIR_UNUSED(order);
-    auto replacement = std::vector<Box<Operator>>{};
-    replacement.push_back(std::move(*this).move());
-    return {
-      std::move(filter),
-      event_order::unordered,
-      ir::pipeline{{}, std::move(replacement)},
-    };
-  }
-
-  auto infer_type(element_type_tag input, diagnostic_handler& dh) const
-    -> failure_or<std::optional<element_type_tag>> override {
-    if (input.is<void>()) {
-      diagnostic::error("`discard` cannot be used as a source")
-        .primary(main_location())
-        .emit(dh);
-      return failure::promise();
-    }
-    return tag_v<void>;
-  }
-
-  friend auto inspect(auto& f, DiscardIr& x) -> bool {
-    return f.object(x).fields();
-  }
-};
-
 class plugin final : public virtual operator_plugin<discard_operator>,
                      public virtual operator_factory_plugin,
-                     public virtual operator_compiler_plugin {
+                     public virtual OperatorPlugin {
 public:
   auto signature() const -> operator_signature override {
     return {.sink = true};
@@ -155,12 +110,9 @@ public:
     return std::make_unique<discard_operator>();
   }
 
-  auto compile(ast::invocation inv, compile_ctx ctx) const
-    -> failure_or<ir::CompileResult> override {
-    // TODO
-    TENZIR_UNUSED(ctx);
-    TENZIR_ASSERT(inv.args.empty());
-    return DiscardIr{};
+  auto describe() const -> Description override {
+    auto d = Describer<DiscardArgs, Discard<chunk_ptr>, Discard<table_slice>>{};
+    return d.unordered();
   }
 };
 
@@ -169,6 +121,3 @@ public:
 } // namespace tenzir::plugins::discard
 
 TENZIR_REGISTER_PLUGIN(tenzir::plugins::discard::plugin)
-TENZIR_REGISTER_PLUGIN(
-  tenzir::inspection_plugin<tenzir::ir::Operator,
-                            tenzir::plugins::discard::DiscardIr>);
