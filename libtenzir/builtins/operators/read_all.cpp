93314012d1abbad81248ba67f24c76b22b89e745
Raghav Narang <raxyte@raxyte.dev>
2026-09-21 12:55:21 +0000

    Port read_all to Nova
    Mirror-Hash: 219b8b5e77dc0cb09bdce2c8dc526a705b5fb258

--- libtenzir/builtins/operators/read_all.cpp
+++ libtenzir/builtins/operators/read_all.cpp
@@ -9,6 +9,8 @@
 #include <tenzir/as_bytes.hpp>
 #include <tenzir/defaults.hpp>
 #include <tenzir/detail/narrow.hpp>
+#include <tenzir/nova/array_builder.hpp>
+#include <tenzir/nova/events.hpp>
 #include <tenzir/operator_plugin.hpp>
 #include <tenzir/plugin/register.hpp>
 #include <tenzir/series_builder.hpp>
@@ -26,26 +28,28 @@ struct ReadAllArgs {
   location operator_location;
 };
 
-class ReadAll final : public Operator<chunk_ptr, table_slice> {
+template <class Output>
+class ReadAll final : public Operator<chunk_ptr, Output> {
 public:
   explicit ReadAll(ReadAllArgs args) : args_{args} {
   }
 
-  auto process(chunk_ptr input, Push<table_slice>&, OpCtx&)
-    -> Task<void> override {
+  auto process(chunk_ptr input, Push<Output>&, OpCtx&) -> Task<void> override {
     buffer_.push_back(std::move(input));
     co_return;
   }
 
-  auto finalize(Push<table_slice>& push, OpCtx& ctx)
+  auto finalize(Push<Output>& push, OpCtx& ctx)
     -> Task<FinalizeBehavior> override {
     auto joined = join_chunks(buffer_);
-    auto builder = series_builder{};
+    auto builder
+      = std::conditional_t<std::same_as<Output, nova::Events>,
+                           nova::ArrayBuilder<nova::Record>, series_builder>{};
     if (joined->size() == 0) {
       co_return FinalizeBehavior::done;
     }
     if (args_.binary) {
-      builder.record().field("data", as_bytes(joined));
+      builder.record().field("data").data(blob_view{as_bytes(joined)});
     } else {
       if (not arrow::util::ValidateUTF8(
             reinterpret_cast<const uint8_t*>(joined->data()),
@@ -56,11 +60,16 @@ public:
           .emit(ctx);
         co_return FinalizeBehavior::done;
       }
-      builder.record().field(
-        "data", std::string_view{reinterpret_cast<const char*>(joined->data()),
-                                 joined->size()});
+      builder.record().field("data").data(std::string_view{
+        reinterpret_cast<const char*>(joined->data()), joined->size()});
     }
-    co_await push(builder.finish_assert_one_slice());
+    if constexpr (std::same_as<Output, nova::Events>) {
+      co_await push(
+        nova::Events{builder.finish(), nova::storage::BitMap{1, true},
+                     nova::Events::Meta::make_empty(1, "tenzir.unknown")});
+    } else {
+      co_await push(builder.finish_assert_one_slice());
+    }
     co_return FinalizeBehavior::done;
   }
 
@@ -80,7 +89,8 @@ public:
   }
 
   auto describe() const -> Description override {
-    auto d = Describer<ReadAllArgs, ReadAll>{};
+    auto d
+      = Describer<ReadAllArgs, ReadAll<table_slice>, ReadAll<nova::Events>>{};
     d.named_optional("binary", &ReadAllArgs::binary);
     d.operator_location(&ReadAllArgs::operator_location);
     return d.without_optimize();
